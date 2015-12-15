f34e7ef381176088d1ed4ab1aa0f5184929a3e3e
Ehsan Iran-Nejad <eirannejad@gmail.com>
2015-12-15 14:15:09 -0800

    1.7.0

---
+++ pyRevit/Views_listUnreferencedViews.py
@@ -0,0 +1,56 @@
+__window__.Width = 1200
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, View
+
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+selection = [ doc.GetElement( elId ) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds() ]
+
+views = []
+
+if len(selection) == 0:
+	cl_views = FilteredElementCollector(doc)
+	views = cl_views.OfCategory( BuiltInCategory.OST_Views ).WhereElementIsNotElementType().ToElements()
+else:
+	for sel in selection:
+		if isinstance(sel, View):
+			views.append(sel)
+
+views = list( views )
+dviews = []
+
+for v in views:
+	if 'drafting' in str( v.ViewType ).lower():
+		dviews.append( v )
+		views.remove( v )
+
+print('UNREFERENCED DRAFTING VIEWS-----------------------------------------------------------------------------------')
+for v in dviews:
+	phasep = v.LookupParameter('Phase')
+	refsheet = v.LookupParameter('Referencing Sheet')
+	refviewport = v.LookupParameter('Referencing Detail')
+	if refsheet and refviewport and refsheet.HasValue and refviewport.HasValue:
+		underlayp = v.LookupParameter('Underlay')
+		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}'.format(
+				v.ViewName,
+				str( v.ViewType ).ljust(20),
+				str(v.Id).ljust(10),
+				str(v.IsTemplate).ljust(10),
+				phasep.AsValueString().ljust(25) if phasep else '---'.ljust(25),
+				underlayp.AsValueString().ljust(25) if underlayp else '---'.ljust(25)
+			))
+
+print('\n\n\nUNREFERENCED MODEL VIEWS-----------------------------------------------------------------------------------')
+for v in views:
+	phasep = v.LookupParameter('Phase')
+	refsheet = v.LookupParameter('Referencing Sheet')
+	refviewport = v.LookupParameter('Referencing Detail')
+	if refsheet and refviewport and refsheet.HasValue and refviewport.HasValue:
+		underlayp = v.LookupParameter('Underlay')
+		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}'.format(
+				v.ViewName,
+				str( v.ViewType ).ljust(20),
+				str(v.Id).ljust(10),
+				str(v.IsTemplate).ljust(10),
+				phasep.AsValueString().ljust(25) if phasep else '---'.ljust(25),
+				underlayp.AsValueString().ljust(25) if underlayp else '---'.ljust(25)
+			))
