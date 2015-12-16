52a5f2085680e04c1aa434e83fb13b74b4cf7f83
Ehsan Iran-Nejad <eirannejad@gmail.com>
2015-12-15 16:50:43 -0800

    1.7.02

--- pyRevit/Views_listUnreferencedViews.py
+++ pyRevit/Views_listUnreferencedViews.py
@@ -3,58 +3,58 @@ from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, View
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
-selection = [ doc.GetElement( elId ) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds() ]
+# selection = [ doc.GetElement( elId ) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds() ]
 
 views = []
 
-if len(selection) == 0:
-	cl_views = FilteredElementCollector(doc)
-	views = cl_views.OfCategory( BuiltInCategory.OST_Views ).WhereElementIsNotElementType().ToElements()
-else:
-	for sel in selection:
-		if isinstance(sel, View):
-			views.append(sel)
+cl_views = FilteredElementCollector(doc)
+views = cl_views.OfCategory( BuiltInCategory.OST_Views ).WhereElementIsNotElementType().ToElements()
 
-views = list( views )
+mviews = []
 dviews = []
 
 for v in views:
 	if 'drafting' in str( v.ViewType ).lower():
 		dviews.append( v )
-		views.remove( v )
+	else:
+		mviews.append( v )
 
 print('UNREFERENCED DRAFTING VIEWS-----------------------------------------------------------------------------------')
 for v in dviews:
 	phasep = v.LookupParameter('Phase')
+	sheetnum = v.LookupParameter('Sheet Number')
+	detnum = v.LookupParameter('Detail Number')
 	refsheet = v.LookupParameter('Referencing Sheet')
 	refviewport = v.LookupParameter('Referencing Detail')
-	if refsheet and refviewport and refsheet.HasValue and refviewport.HasValue:
-		underlayp = v.LookupParameter('Underlay')
-		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}\nPLACED ON DETAIL/SHEET: {7} / {6}\n'.format(
+	if refsheet and refviewport and refsheet.AsString() != '' and refviewport.AsString() != '':
+		continue
+	else:
+		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}\nPLACED ON DETAIL/SHEET: {6} / {5}\n'.format(
 				v.ViewName,
 				str( v.ViewType ).ljust(20),
 				str(v.Id).ljust(10),
 				str(v.IsTemplate).ljust(10),
 				phasep.AsValueString().ljust(25) if phasep else '---'.ljust(25),
-				underlayp.AsValueString().ljust(25) if underlayp else '---'.ljust(25),
-				v.LookupParameter('Sheet Number').AsString(),
-				v.LookupParameter('Detail Number').AsString()
+				sheetnum.AsString() if sheetnum else '-',
+				detnum.AsString() if detnum else '-',
 			))
 
 print('\n\n\nUNREFERENCED MODEL VIEWS-----------------------------------------------------------------------------------')
-for v in views:
+for v in mviews:
 	phasep = v.LookupParameter('Phase')
+	sheetnum = v.LookupParameter('Sheet Number')
+	detnum = v.LookupParameter('Detail Number')
 	refsheet = v.LookupParameter('Referencing Sheet')
 	refviewport = v.LookupParameter('Referencing Detail')
-	if refsheet and refviewport and refsheet.HasValue and refviewport.HasValue:
-		underlayp = v.LookupParameter('Underlay')
-		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}\nPLACED ON DETAIL/SHEET: {7} / {6}\n'.format(
+	if refsheet and refviewport and refsheet.AsString() != '' and refviewport.AsString() != '':
+		continue
+	else:
+		print('TYPE: {1}ID: {2}TEMPLATE: {3}PHASE:{4}  {0}\nPLACED ON DETAIL/SHEET: {6} / {5}\n'.format(
 				v.ViewName,
 				str( v.ViewType ).ljust(20),
 				str(v.Id).ljust(10),
 				str(v.IsTemplate).ljust(10),
 				phasep.AsValueString().ljust(25) if phasep else '---'.ljust(25),
-				underlayp.AsValueString().ljust(25) if underlayp else '---'.ljust(25),
-				v.LookupParameter('Sheet Number').AsString(),
-				v.LookupParameter('Detail Number').AsString()
+				sheetnum.AsString() if sheetnum else '-',
+				detnum.AsString() if detnum else '-',
 			))
