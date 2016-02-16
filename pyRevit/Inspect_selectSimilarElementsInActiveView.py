7b841ccc71ade6d1382b40ebe6eac0645e6964cb
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-02-16 07:53:03 -0800

    1.16.0

---
+++ pyRevit/Inspect_selectSimilarElementsInActiveView.py
@@ -0,0 +1,34 @@
+__window__.Close()
+from Autodesk.Revit.DB import FilteredElementCollector, Group, ElementId
+from System.Collections.Generic import List
+
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+curview = uidoc.ActiveGraphicalView
+cl = FilteredElementCollector( doc, curview.Id ).WhereElementIsNotElementType().ToElementIds()
+
+matchlist = []
+selCatList = set()
+
+for elId in uidoc.Selection.GetElementIds():
+	el = doc.GetElement( elId )
+	try:
+		selCatList.add( el.Category.Name )
+	except:
+		continue
+
+for elid in cl:
+	el = doc.GetElement( elid )
+	try:
+		if el.ViewSpecific and ( el.Category.Name in selCatList):
+			matchlist.append( elid )
+	except:
+		continue
+
+set = []
+for elid in matchlist:
+	set.append( elid )
+
+uidoc.Selection.SetElementIds( List[ElementId]( set ) )
+uidoc.RefreshActiveView()
