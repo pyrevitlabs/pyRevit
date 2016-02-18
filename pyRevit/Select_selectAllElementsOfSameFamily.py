0747dedf00f8b98052f1b890f6b4e5238a5ca7e8
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-02-18 08:14:41 -0800

    1.17.0

---
+++ pyRevit/Select_selectAllElementsOfSameFamily.py
@@ -0,0 +1,30 @@
+__window__.Close()
+from Autodesk.Revit.DB import FilteredElementCollector, FamilyInstanceFilter, ElementId
+from System.Collections.Generic import List
+
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+curview = uidoc.ActiveGraphicalView
+
+matchlist = []
+famSymbolList = set()
+
+for elId in uidoc.Selection.GetElementIds():
+	el = doc.GetElement( elId )
+	famSymbolList.add( doc.GetElement( el.GetTypeId()))
+
+for fsym in famSymbolList:
+	family = fsym.Family
+	symbolSet = family.Symbols
+	for sym in symbolSet:
+		cl = FilteredElementCollector(doc).WherePasses( FamilyInstanceFilter( doc, sym.Id )).ToElements()
+		for el in cl:
+			matchlist.append( el.Id )
+
+set = []
+for elid in matchlist:
+	set.append( elid )
+
+uidoc.Selection.SetElementIds( List[ElementId]( set ) )
+uidoc.RefreshActiveView()
