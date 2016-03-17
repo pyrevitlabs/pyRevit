297ce18dc59239cb2cbf46cd23ce97e37e46c62a
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-03-16 19:40:43 -0700

    1.40.1
    Added GPL license headers to scripts

--- pyRevit/Memory_pasteSectionBoxLocation.py
+++ pyRevit/Memory_pasteSectionBoxLocation.py
@@ -1,3 +1,22 @@
+'''
+Copyright (c) 2014-2016 Ehsan Iran-Nejad
+Python scripts for Autodesk Revit
+
+This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+
+pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See this link for a copy of the GNU General Public License protecting this package.
+https://github.com/eirannejad/pyRevit/blob/master/LICENSE
+'''
+
 __doc__ = '''Apply the copied Section Box settings to the active 3D view.'''
 
 __window__.Hide()
