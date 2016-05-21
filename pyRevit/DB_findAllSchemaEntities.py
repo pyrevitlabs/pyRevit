d01d8594ffbb3cae31a02a584eeb5dde12674f5c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-05-21 16:03:05 -0700

    2.30.33
    Added scripts to find schema entities loaded in memory and attached to
    model elements

---
+++ pyRevit/DB_findAllSchemaEntities.py
@@ -0,0 +1,37 @@
+'''
+Copyright (c) 2014-2016 Ehsan Iran-Nejad
+Python scripts for Autodesk Revit
+
+This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+
+pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See this link for a copy of the GNU General Public License protecting this package.
+https://github.com/eirannejad/pyRevit/blob/master/LICENSE
+'''
+
+__doc__ = 'Lists all Elements containing a Schema entity.'
+
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, ElementIsElementTypeFilter, LogicalOrFilter
+from Autodesk.Revit.DB.ExtensibleStorage import Schema
+
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+allElements = list(FilteredElementCollector(doc).WherePasses(LogicalOrFilter(ElementIsElementTypeFilter(False), ElementIsElementTypeFilter(True))))
+guids = {sc.GUID.ToString():sc.SchemaName for sc in Schema.ListSchemas()}
+
+for el in allElements:
+	schemaGUIDs = el.GetEntitySchemaGuids()
+	for guid in schemaGUIDs:
+		if guid.ToString() in guids.keys():
+			print('ELEMENT ID: {0}\t\tSCHEMA NAME: {1}'.format(el.Id.IntegerValue, guids[guid.ToString()]))
+
+print('Iteration completed over {0} elements.'.format(len(allElements)))
