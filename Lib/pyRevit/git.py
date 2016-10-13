a5e58107d17623d5621e4976fb69c1a6afa98b6d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-10-13 12:11:40 -0700

    git fix

--- Lib/pyRevit/git.py
+++ Lib/pyRevit/git.py
@@ -2,7 +2,6 @@ import sys
 import os
 import os.path as op
 import re
-import clr
 import importlib
 
 import pyRevit.utils as utils
@@ -10,7 +9,8 @@ import pyRevit.config as cfg
 from pyRevit.exceptions import GitError
 from pyRevit.logger import logger
 
-sys.path.append(op.join(cfg.LOADER_DIR, cfg.GIT_LIB)
+sys.path.append(op.join(cfg.LOADER_DIR, cfg.GIT_LIB))
 
+import clr
 clr.AddReferenceByName(cfg.GIT_LIB)
 git = importlib.import_module('LibGit2Sharp')
