3535c0f4ef1b86148da8fccaf2a9bd1524fe6279
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-10-13 17:51:33 -0700

    more git fixes

--- Lib/pyRevit/git.py
+++ Lib/pyRevit/git.py
@@ -12,5 +12,23 @@ from pyRevit.logger import logger
 sys.path.append(op.join(cfg.LOADER_DIR, cfg.GIT_LIB))
 
 import clr
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+clr.AddReference("System.Core")
+
+import System
+clr.ImportExtensions(System.Linq)
+
 clr.AddReferenceByName(cfg.GIT_LIB)
-git = importlib.import_module('LibGit2Sharp')
+git = importlib.import_module(cfg.GIT_LIB)
+
+# from pyRevit.git import git
+# repo = git.Repository(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev')
+# for c in repo.Diff.Compare[git.TreeChanges]():
+#     print(c)
+#
+#
+# from pyRevit.git import git
+# repo = git.Repository(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev')
+# for c in repo.Diff.Compare(repo.Head.Tip.Parents.Single().Tree, repo.Head.Tip.Tree):
+#     print(c)
