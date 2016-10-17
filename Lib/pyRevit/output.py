45de35296fb5a68c4fb1df7c0636ba5a608fc218
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-10-16 19:56:42 -0700

    Classifying output window

--- Lib/pyRevit/output.py
+++ Lib/pyRevit/output.py
@@ -1,14 +1,27 @@
-from .config import OUTPUT_WINDOW
+import inspect
 
 
-# todo turn this into a class
-def set_width(width):
-    OUTPUT_WINDOW.Width = width
+def _calling_scope_variable(name):
+    frame = inspect.stack()[1][0]
+    while name not in frame.f_locals:
+        frame = frame.f_back
+        if frame is None:
+            return None
+    return frame.f_locals[name]
 
 
-def close():
-    OUTPUT_WINDOW.Close()
+class PyRevitConsoleWindow(object):
+    def __init__(self, window_handle):
+        self.win_handle = window_handle
+
+    def set_width(self, width):
+        self.win_handle.Width = width
+
+    def close(self):
+        self.win_handle.Close()
+
+    def hide(self):
+        self.win_handle.Hide()
 
 
-def hide():
-    OUTPUT_WINDOW.Hide()
+output_window = PyRevitConsoleWindow(_calling_scope_variable('__window__'))
