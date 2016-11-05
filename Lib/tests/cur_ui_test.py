8826a5b92a54e8c84a4f1c39283c820215186d00
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-05 11:41:43 -0700

    pyRevit testing library

---
+++ Lib/tests/cur_ui_test.py
@@ -0,0 +1,16 @@
+import pyRevit.ui as ui
+
+cui = ui.get_current_ui()
+
+for tabs in cui:
+    print tabs, tabs.name
+
+for panel in cui.ribbon_tab('Systems'):
+    print panel, panel.name
+
+for item in cui.ribbon_tab('Systems').ribbon_panel('Electrical'):
+    print item, item.AutomationName
+
+panel = cui.ribbon_tab('Systems').ribbon_panel('Electrical')
+
+p = panel._get_rvtapi_object()
