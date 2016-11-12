431f4eea4096bd459d6ab738fcdbdf48a7b48fa9
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-11 16:57:50 -0800

    added Testing panel

---
+++ pyRevit.package/pyRevit.tab/Testing.panel/Test1.pushbutton/config.py
@@ -0,0 +1,4 @@
+from pyRevit.logger import logger
+
+print('config script')
+logger.debug('Forced debug mode is active')
---
+++ pyRevit.package/pyRevit.tab/Testing.panel/Test1.pushbutton/script.py
@@ -0,0 +1,8 @@
+from pyRevit.logger import logger
+
+__cmdoptions__ = ['op1', 'op2', 'op3']
+
+print(__name__)
+
+print('main script')
+logger.debug('Forced debug mode is active')
