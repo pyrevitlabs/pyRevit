3a4c1a0a71cf7fa0c8e1b8cab75785804331df5c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-11 18:43:42 -0800

    WIP update sub module

---
+++ loader/Lib/pyRevit/loader/update.py
@@ -0,0 +1,13 @@
+
+# fixme rewrite update mechanism to use this to update all(?) extensions
+
+# from pyRevit.git import git
+# repo = git.Repository(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev')
+# for c in repo.Diff.Compare[git.TreeChanges]():
+#     print(c)
+#
+#
+# from pyRevit.git import git
+# repo = git.Repository(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev')
+# for c in repo.Diff.Compare(repo.Head.Tip.Parents.Single().Tree, repo.Head.Tip.Tree):
+#     print(c)
