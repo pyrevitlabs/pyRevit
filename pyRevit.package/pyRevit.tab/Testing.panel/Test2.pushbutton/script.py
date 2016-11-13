63ebdea2dafdd2604660f85b256c3357557df35b
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-13 14:52:19 -0800

    Tool update

--- pyRevit.package/pyRevit.tab/Testing.panel/Test2.pushbutton/script.py
+++
@@ -1,8 +0,0 @@
-from pyrevit.loader import updater
-
-from pyrevit.loader import updater
-for repo in updater._find_all_pkg_repos():
-    if updater.has_pending_updates(repo):
-        print('Yes on:\n{}'.format(repo))
-    else:
-        print('No updates on:\n{}'.format(repo.Info.WorkingDirectory))
