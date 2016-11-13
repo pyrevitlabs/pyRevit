97a032cbba7f3b255cd123293afda7485162339b
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-12 17:40:43 -0800

    Moved startup folder name info dll.config file

--- loader/pyRevitLoaderDev/pyRevitLoader/PyRevitLoaderApplication.cs
+++ loader/pyRevitLoaderDev/pyRevitLoader/PyRevitLoaderApplication.cs
@@ -12,6 +12,8 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace PyRevitLoader
 {
@@ -67,14 +69,22 @@ namespace PyRevitLoader
             }
         }
 
-        public static string GetStartupScriptPath()
+        private static string GetStartupScriptPath()
         {
-            var startupScriptName = "PyRevitLoader.py";
+            var startupScriptName = GetDLLAppSettings("startupscript");
             var dllfolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var loaderScriptFolder = Path.GetDirectoryName(dllfolder);
             return Path.Combine(loaderScriptFolder, startupScriptName);
-        }
+         }
 
+        private static string GetDLLAppSettings(string parameter)
+        {
+            ExeConfigurationFileMap map = new ExeConfigurationFileMap();
+            map.ExeConfigFilename = Assembly.GetExecutingAssembly().Location + ".config";
+            Configuration libConfig = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+            AppSettingsSection section = (libConfig.GetSection("appSettings") as AppSettingsSection);
+            return section.Settings[parameter].Value;
+        }
         public static string GetStartupScript()
         {
             var startupScriptFullPath = GetStartupScriptPath();
