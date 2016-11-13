7b53521aaf161181fb6caf9da53e07edbf4c1a7f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-13 11:29:47 -0800

    Updater Testing 02

--- pyRevit.package/pyRevit.tab/Testing.panel/Test2.pushbutton/script.py
+++ pyRevit.package/pyRevit.tab/Testing.panel/Test2.pushbutton/script.py
@@ -15,12 +15,11 @@ print r.Head.Tip.Message
 options = git.PullOptions()
 options.FetchOptions = git.FetchOptions()
 
-up = git.UsernamePasswordCredentials()
-up.Username = 'eirannejad'
-up.Password = 'ehsan2010'
-
 
 def hndlr(url, uname, types):
+    up = git.UsernamePasswordCredentials()
+    up.Username = 'eirannejad@gmail.com'
+    up.Password = 'ehsan2010'
     return up
 
 
@@ -28,3 +27,7 @@ options.FetchOptions.CredentialsProvider = git.Handlers.CredentialsHandler(hndlr
 sig = git.Signature('eirannejad', 'eirannejad@gmail.com', DateTimeOffset(DateTime.Now))
 
 r.Network.Pull(sig, options)
+
+print r.Head.Tip.Id.Sha
+print r.Head.Tip.Message
+
