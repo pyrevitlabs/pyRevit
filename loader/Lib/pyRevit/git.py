631509274832b97450c483329a9d8cc383a15077
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-12 20:00:30 -0800

    git in good shape

--- loader/Lib/pyRevit/git.py
+++ loader/Lib/pyRevit/git.py
@@ -1,7 +1,30 @@
-# doc: https://github.com/libgit2/libgit2sharp/wiki
+""" Module name: git.py
+Copyright (c) 2014-2016 Ehsan Iran-Nejad
+Python scripts for Autodesk Revit
 
-import sys
-import os.path as op
+This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+
+pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See this link for a copy of the GNU General Public License protecting this package.
+https://github.com/eirannejad/pyRevit/blob/master/LICENSE
+
+
+~~~
+Description:
+LibGit2Sharp wrapper module for pyRevit.
+
+Documentation:
+https://github.com/libgit2/libgit2sharp/wiki
+"""
+
 import importlib
 
 from .config import GIT_LIB
@@ -9,9 +32,10 @@ from .config import GIT_LIB
 import clr
 import System
 
+# todo: figure out how to import extensions on the caller's scope. Can't expect the user to import this everytime
 clr.AddReference("System.Core")
-# todo figure out how to import extensions on the caller's scope. Can't expect the user to import this everytime.
 clr.ImportExtensions(System.Linq)
 clr.AddReferenceByName(GIT_LIB)
 
+# public git module
 git = importlib.import_module(GIT_LIB)
