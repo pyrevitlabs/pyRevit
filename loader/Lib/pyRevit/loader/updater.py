25ef0cd9755da4892b1f4e649938abda8821ea26
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-13 11:46:19 -0800

    WIP updater

--- loader/Lib/pyRevit/loader/updater.py
+++ loader/Lib/pyRevit/loader/updater.py
@@ -5,34 +5,47 @@
 import sys
 import clr
 
-from ..config import HOME_DIR
+from ..logger import get_logger
+logger = get_logger(__name__)
 
+from ..config import HOME_DIR
 from ..git import git
 
 from System import DateTime, DateTimeOffset
 
 
-
-r = git.Repository(HOME_DIR)
-
-print r.Head.Tip.Id.Sha
-print r.Head.Tip.Message
-
-# options = git.PullOptions()
-# options.FetchOptions = git.FetchOptions()
-
-options = git.FetchOptions()
-
-up = git.UsernamePasswordCredentials()
-up.Username = 'eirannejad'
-up.Password = 'ehsan2010'
-
-
 def hndlr(url, uname, types):
+    up = git.UsernamePasswordCredentials()
+    up.Username = 'eirannejad@gmail.com'
+    up.Password = 'ehsan2010'
     return up
 
 
-options.FetchOptions.CredentialsProvider = git.Handlers.CredentialsHandler(hndlr)
-sig = git.Signature('eirannejad', 'eirannejad@gmail.com', DateTimeOffset(DateTime.Now))
+def _make_pull_options():
+    options = git.PullOptions()
+    options.FetchOptions = git.FetchOptions()
+    options.FetchOptions.CredentialsProvider = git.Handlers.CredentialsHandler(hndlr)
+    return options
 
-r.Network.Pull(sig, options)
+def _make_pull_signature():
+    return git.Signature('eirannejad', 'eirannejad@gmail.com', DateTimeOffset(DateTime.Now))
+
+
+def _find_all_pkg_repos():
+    logger.debug('Finding installed repos.')
+    repos = [git.Repository(HOME_DIR)]
+    logger.debug('Installed repos: {}'.format(repos))
+    return repos
+
+
+def update_pyrevit():
+    for repo in _find_all_pkg_repos():
+        repo_dir = repo.Info.WorkingDirectory
+        logger.debug('Updating repo: {}'.format(repo_dir))
+        head_msg = str(repo.Head.Tip.Message).replace('\n','')
+        logger.debug('Current head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
+        try:
+            repo.Network.Pull(_make_pull_signature(), _make_pull_options())
+            logger.info('Successfully updated repo: {}'.format(repo_dir))
+        except Exception as pull_err:
+            logger.error('Failed updating: {} | {}'.format(repo_dir, pull_err))
--- pyRevit.package/pyRevit.tab/pyRevit.panel/Update.pushbutton/script.py
+++ pyRevit.package/pyRevit.tab/pyRevit.panel/Update.pushbutton/script.py
@@ -21,107 +21,112 @@ __doc__ = 'Downloads updates from the github repository. This function works onl
           'installed using the setup package provided on the github repo. ' \
           '(It needs the portable git tool to download the updates)'
 
-import os.path as op
-import subprocess as sp
-import re
 
-from Autodesk.Revit.UI import TaskDialog
+from pyrevit.loader import updater
 
+updater.update_pyrevit()
 
-class ErrorFindingBranch(Exception):
-    pass
+# import os.path as op
+# import subprocess as sp
+# import re
 
-
-def get_parent_directory(path):
-    return op.dirname(path)
+# from Autodesk.Revit.UI import TaskDialog
 
 
-def get_install_dir():
-    return get_parent_directory(get_parent_directory(get_parent_directory(__file__)))
+# class ErrorFindingBranch(Exception):
+    # pass
 
 
-def get_pyrevit_clone_dir():
-    return op.join(get_install_dir(), 'pyrevit')
+# def get_parent_directory(path):
+    # return op.dirname(path)
 
 
-def get_loader_clone_dir():
-    return op.join(get_install_dir(), '__init__')
+# def get_install_dir():
+    # return get_parent_directory(get_parent_directory(get_parent_directory(__file__)))
 
 
-def get_git_dir():
-    return op.join(get_install_dir(), '__git__', 'cmd')
+# def get_pyrevit_clone_dir():
+    # return op.join(get_install_dir(), 'pyrevit')
 
 
-def git_get_current_branch(cloneDir):
-    global git_executive
-    bfinder = re.compile('\*\s(.+)')
-    output = sp.Popen(r'{0} branch'.format(git_executive), \
-                      stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
-    res = bfinder.findall(output.communicate()[0])
-    if len(res) > 0:
-        print('Current branch is {}'.format(res[0]))
-        return res[0]
-    else:
-        raise ErrorFindingBranch()
+# def get_loader_clone_dir():
+    # return op.join(get_install_dir(), '__init__')
 
 
-def git_pull_overwrite(cloneDir):
-    global git_executive
-    report = ''
-    r1 = r2 = False
-
-    # Fetch changes
-    output = sp.Popen(r'{0} fetch --all'.format(git_executive), \
-                      stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
-    print(output.communicate()[0])
-    # print(output.returncode)
-    r1 = output.returncode
-
-    # Hard reset current branch to origin/branch
-    try:
-        output = sp.Popen(r'{0} reset --hard origin/{1}'.format(git_executive, git_get_current_branch(cloneDir)), \
-                          stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
-        print(output.communicate()[0])
-        # print(output.returncode)
-        r2 = output.returncode
-    except ErrorFindingBranch as err:
-        print('Error finding current git branch...Skipping update...')
-        raise err
-
-    if (r1 == r2 == 0):
-        print('Successfully updated repository...')
-    else:
-        print('Error in updating repository...')
+# def get_git_dir():
+    # return op.join(get_install_dir(), '__git__', 'cmd')
 
 
-installDir = get_install_dir()
-pyrevitCloneDir = get_pyrevit_clone_dir()
-loaderCloneDir = get_loader_clone_dir()
-gitDir = get_git_dir()
-git_executive = '\"' + op.join(gitDir, 'git.exe') + '\"'
+# def git_get_current_branch(cloneDir):
+    # global git_executive
+    # bfinder = re.compile('\*\s(.+)')
+    # output = sp.Popen(r'{0} branch'.format(git_executive), \
+                      # stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
+    # res = bfinder.findall(output.communicate()[0])
+    # if len(res) > 0:
+        # print('Current branch is {}'.format(res[0]))
+        # return res[0]
+    # else:
+        # raise ErrorFindingBranch()
 
-print('Installation directory is: {0}'.format(installDir))
-print('Portable git package is located at: {0}'.format(gitDir))
 
-if op.exists('{0}\git.exe'.format(gitDir)):
-    try:
-        print('\nUPDATING PYTHON LOADER '.ljust(100, '-'))
-        print('pyrevit loader has been cloned to: {0}\n'.format(loaderCloneDir))
-        git_pull_overwrite(loaderCloneDir)
-    except:
-        print('Error Updating loader repository...The cloned repo might be corrupt or no internet access.\n'      \
-              'Trying to update the main repository now')
-    try:
-        print('\n\nUPDATING PYTHON SCRIPT LIBRARY '.ljust(100, '-'))
-        print('pyrevit has been cloned to: {0}\n'.format(pyrevitCloneDir))
-        git_pull_overwrite(pyrevitCloneDir)
+# def git_pull_overwrite(cloneDir):
+    # global git_executive
+    # report = ''
+    # r1 = r2 = False
 
-        TaskDialog.Show('pyrevit', 'Update completed. reload pyrevit now to see changes...')
-        __window__.Focus()
-    except:
-        TaskDialog.Show('pyrevit', 'Error Updating repository...Please check your internet connection. ' \
-                                   'If the updater still did not work please copy the printed report and ' \
-                                   'contact the developers...')
-        __window__.Focus()
-else:
-    print('Can not find portable git package.')
+    # # Fetch changes
+    # output = sp.Popen(r'{0} fetch --all'.format(git_executive), \
+                      # stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
+    # print(output.communicate()[0])
+    # # print(output.returncode)
+    # r1 = output.returncode
+
+    # # Hard reset current branch to origin/branch
+    # try:
+        # output = sp.Popen(r'{0} reset --hard origin/{1}'.format(git_executive, git_get_current_branch(cloneDir)), \
+                          # stdout=sp.PIPE, stderr=sp.PIPE, cwd=cloneDir, shell=True)
+        # print(output.communicate()[0])
+        # # print(output.returncode)
+        # r2 = output.returncode
+    # except ErrorFindingBranch as err:
+        # print('Error finding current git branch...Skipping update...')
+        # raise err
+
+    # if (r1 == r2 == 0):
+        # print('Successfully updated repository...')
+    # else:
+        # print('Error in updating repository...')
+
+
+# installDir = get_install_dir()
+# pyrevitCloneDir = get_pyrevit_clone_dir()
+# loaderCloneDir = get_loader_clone_dir()
+# gitDir = get_git_dir()
+# git_executive = '\"' + op.join(gitDir, 'git.exe') + '\"'
+
+# print('Installation directory is: {0}'.format(installDir))
+# print('Portable git package is located at: {0}'.format(gitDir))
+
+# if op.exists('{0}\git.exe'.format(gitDir)):
+    # try:
+        # print('\nUPDATING PYTHON LOADER '.ljust(100, '-'))
+        # print('pyrevit loader has been cloned to: {0}\n'.format(loaderCloneDir))
+        # git_pull_overwrite(loaderCloneDir)
+    # except:
+        # print('Error Updating loader repository...The cloned repo might be corrupt or no internet access.\n'      \
+              # 'Trying to update the main repository now')
+    # try:
+        # print('\n\nUPDATING PYTHON SCRIPT LIBRARY '.ljust(100, '-'))
+        # print('pyrevit has been cloned to: {0}\n'.format(pyrevitCloneDir))
+        # git_pull_overwrite(pyrevitCloneDir)
+
+        # TaskDialog.Show('pyrevit', 'Update completed. reload pyrevit now to see changes...')
+        # __window__.Focus()
+    # except:
+        # TaskDialog.Show('pyrevit', 'Error Updating repository...Please check your internet connection. ' \
+                                   # 'If the updater still did not work please copy the printed report and ' \
+                                   # 'contact the developers...')
+        # __window__.Focus()
+# else:
+    # print('Can not find portable git package.')
