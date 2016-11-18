25f15eb97d9840d1b8c6eb3673f04a0f70c8c81b
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-18 07:56:39 -0800

    Command switch handler moved to scriptutils

--- loader/Lib/pyRevit/scriptutils.py
+++ loader/Lib/pyRevit/scriptutils.py
@@ -9,6 +9,74 @@ from .loader.components import GenericCommand
 logger = get_logger(__name__)
 
 
+import clr
+clr.AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName("PresentationFramework")
+clr.AddReferenceByPartialName('System.Windows.Forms')
+clr.AddReferenceByPartialName('WindowsBase')
+import System.Windows
+
+
+class commandSwitches:
+    def __init__(self, switches, message = 'Pick a command option:'):
+        self.selected_switch = ''
+        # Create window
+        self.my_window = System.Windows.Window()
+        self.my_window.WindowStyle = System.Windows.WindowStyle.None
+        self.my_window.AllowsTransparency = True
+        self.my_window.Background = None
+        self.my_window.Title = 'Command Options'
+        self.my_window.Width = 600
+        self.my_window.SizeToContent = System.Windows.SizeToContent.Height
+        self.my_window.ResizeMode = System.Windows.ResizeMode.CanMinimize
+        self.my_window.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
+        self.my_window.PreviewKeyDown += self.handleEsc
+        border = System.Windows.Controls.Border()
+        border.CornerRadius  = System.Windows.CornerRadius(15)
+        border.Background = System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(220,55,50,50))
+        self.my_window.Content = border
+
+        # Create StackPanel to Layout UI elements
+        stack_panel = System.Windows.Controls.StackPanel()
+        stack_panel.Margin = System.Windows.Thickness(5)
+        border.Child = stack_panel
+
+        label = System.Windows.Controls.Label()
+        label.Foreground = System.Windows.Media.Brushes.White
+        label.Content = message
+        label.Margin = System.Windows.Thickness(2, 0, 0, 0)
+        stack_panel.Children.Add(label)
+
+        # Create WrapPanel for command options
+        self.button_list = System.Windows.Controls.WrapPanel()
+        self.button_list.Margin = System.Windows.Thickness(5)
+        stack_panel.Children.Add(self.button_list)
+
+        for switch in switches:
+            my_button = System.Windows.Controls.Button()
+            my_button.BorderBrush = System.Windows.Media.Brushes.Black
+            my_button.BorderThickness = System.Windows.Thickness(0)
+            my_button.Content = switch
+            my_button.Margin = System.Windows.Thickness(5, 0, 5, 5)
+            my_button.Padding = System.Windows.Thickness(5, 0, 5, 0)
+            my_button.Click += self.processSwitch
+            self.button_list.Children.Add(my_button)
+
+
+    def handleEsc(self, sender, args):
+        if (args.Key == System.Windows.Input.Key.Escape):
+            self.my_window.Close()
+
+
+    def processSwitch(self, sender, args):
+        self.my_window.Close()
+        self.selected_switch = sender.Content
+
+    def pickCommandSwitch(self):
+        self.my_window.ShowDialog()
+        return self.selected_switch
+
+
 def get_script_info(script_file_addr):
     script_dir = op.dirname(script_file_addr)
     for component_type in get_all_subclasses([GenericCommand]):
--- packages/pyRevit.package/pyRevit.tab/Selection.panel/Pick.splitpushbutton/Pick.pushbutton/script.py
+++ packages/pyRevit.package/pyRevit.tab/Selection.panel/Pick.splitpushbutton/Pick.pushbutton/script.py
@@ -19,7 +19,8 @@ https://github.com/eirannejad/pyRevit/blob/master/LICENSE
 
 __doc__ = 'Activates selection tool that picks a specific type of element.'
 
-__window__.Close()
+
+from pyrevit.scriptutils import commandSwitches
 
 from Autodesk.Revit.UI.Selection import ISelectionFilter
 from System.Collections.Generic import List
@@ -27,71 +28,6 @@ from Autodesk.Revit.DB import ElementId
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
-
-import clr
-clr.AddReferenceByPartialName('PresentationCore')
-clr.AddReferenceByPartialName("PresentationFramework")
-clr.AddReferenceByPartialName('System.Windows.Forms')
-clr.AddReferenceByPartialName('WindowsBase')
-import System.Windows
-
-class commandSwitches:
-    def __init__(self, switches, message = 'Pick a command option:'):
-        # Create window
-        self.my_window = System.Windows.Window()
-        self.my_window.WindowStyle = System.Windows.WindowStyle.None
-        self.my_window.AllowsTransparency = True
-        self.my_window.Background = None
-        self.my_window.Title = 'Command Options'
-        self.my_window.Width = 600
-        self.my_window.SizeToContent = System.Windows.SizeToContent.Height
-        self.my_window.ResizeMode = System.Windows.ResizeMode.CanMinimize
-        self.my_window.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
-        self.my_window.PreviewKeyDown += self.handleEsc
-        border = System.Windows.Controls.Border()
-        border.CornerRadius  = System.Windows.CornerRadius(15)
-        border.Background = System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(220,55,50,50))
-        self.my_window.Content = border
-
-        # Create StackPanel to Layout UI elements
-        stack_panel = System.Windows.Controls.StackPanel()
-        stack_panel.Margin = System.Windows.Thickness(5)
-        border.Child = stack_panel
-
-        label = System.Windows.Controls.Label()
-        label.Foreground = System.Windows.Media.Brushes.White
-        label.Content = message
-        label.Margin = System.Windows.Thickness(2, 0, 0, 0)
-        stack_panel.Children.Add(label)
-
-        # Create WrapPanel for command options
-        self.button_list = System.Windows.Controls.WrapPanel()
-        self.button_list.Margin = System.Windows.Thickness(5)
-        stack_panel.Children.Add(self.button_list)
-
-        for switch in switches:
-            my_button = System.Windows.Controls.Button()
-            my_button.BorderBrush = System.Windows.Media.Brushes.Black
-            my_button.BorderThickness = System.Windows.Thickness(0)
-            my_button.Content = switch
-            my_button.Margin = System.Windows.Thickness(5, 0, 5, 5)
-            my_button.Padding = System.Windows.Thickness(5, 0, 5, 0)
-            my_button.Click += self.processSwitch
-            self.button_list.Children.Add(my_button)
-
-
-    def handleEsc(self, sender, args):
-        if (args.Key == System.Windows.Input.Key.Escape):
-            self.my_window.Close()
-
-
-    def processSwitch(self, sender, args):
-        self.my_window.Close()
-        global selected_switch
-        selected_switch = sender.Content
-
-    def pickCommandSwitch(self):
-        self.my_window.ShowDialog()
 
 
 class PickByCategorySelectionFilter(ISelectionFilter):
@@ -121,24 +57,22 @@ def pickbycategory(catname):
     except:
         pass
 
-selected_switch = ''
-
-commandSwitches(sorted(['Area',
-                 'Column',
-                 'Dimension',
-                 'Door',
-                 'Floor',
-                 'Framing',
-                 'Furniture',
-                 'Grid',
-                 'Room',
-                 'Room Tag',
-                 'Truss',
-                 'Wall',
-                 'Window',
-                 'Ceiling',
-                 'Section Box',
-                 'Elevation Mark',
-                 'Parking',]), 'Pick only elements of type:').pickCommandSwitch()
+selected_switch = commandSwitches(sorted(['Area',
+                                         'Column',
+                                         'Dimension',
+                                         'Door',
+                                         'Floor',
+                                         'Framing',
+                                         'Furniture',
+                                         'Grid',
+                                         'Room',
+                                         'Room Tag',
+                                         'Truss',
+                                         'Wall',
+                                         'Window',
+                                         'Ceiling',
+                                         'Section Box',
+                                         'Elevation Mark',
+                                         'Parking',]), 'Pick only elements of type:').pickCommandSwitch()
 if selected_switch is not '':
     pickbycategory(selected_switch)
