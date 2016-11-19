b37d695370e2e3bc671ee1770fdb220c0c45411f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-11-19 13:10:17 -0800

    Tests

--- packages/pyRevit.package/pyRevit.tab/Testing.panel/Test2.pushbutton/script.py
+++ packages/pyRevit.package/pyRevit.tab/Testing.panel/Test2.pushbutton/script.py
@@ -6,3 +6,19 @@ if __shiftclick__:
 import pyrevit.scriptutils as su
 script = su.get_script_info(__file__)
 print script.cmd_context
+
+
+from System import AppDomain as ad
+for a in ad.CurrentDomain.GetAssemblies():
+	if 'pyrevit' in str(a.FullName).lower():
+		print a
+
+
+# from System import AppDomain
+# from System.Reflection import AssemblyName
+# dom = AppDomain.CreateDomain("some")
+# assemblyName = AssemblyName()
+# assemblyName.CodeBase = r'C:\Users\eirannejad\Desktop\pyRevit\loader\pyRevitLoader\pyRevitClasses.dll'
+# assembly = dom.Load(assemblyName)
+# types = assembly.GetTypes()
+# AppDomain.Unload(dom)
