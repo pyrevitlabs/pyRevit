e55df542bcf1f09a23359cdd2f2ff56b023eb761
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-12-12 20:48:43 -0800

    WIP Beautiful about window

--- extensions/pyRevit.extension/pyRevit.tab/pyRevit.panel/About.pushbutton/script.py
+++ extensions/pyRevit.extension/pyRevit.tab/pyRevit.panel/About.pushbutton/script.py
@@ -19,135 +19,63 @@ See this link for a copy of the GNU General Public License protecting this packa
 https://github.com/eirannejad/pyRevit/blob/master/LICENSE
 """
 
+
+import sys
+import clr
+import os
+from pyrevit.repo import PYREVIT_VERSION
+
+clr.AddReference('PresentationCore')
+clr.AddReferenceByPartialName("PresentationFramework")
+clr.AddReference('IronPython.Wpf')
+
+# noinspection PyUnresolvedReferences
+from System import Uri
+# noinspection PyUnresolvedReferences
+from System.Windows import Application, Window
+# noinspection PyUnresolvedReferences
+from System.Windows.Media.Imaging import BitmapImage
+# noinspection PyUnresolvedReferences
+import wpf
+
+
+folder = os.path.dirname(__file__)
+
+
 __doc__ = 'About pyrevit. Opens the pyrevit blog website. You can find detailed information on how pyrevit works, ' \
           'updates about the new tools and changes, and a lot of other information there.'
 
-import os
-import sys
 
-import clr
-from pyrevit.repo import PYREVIT_VERSION
-
-clr.AddReferenceByPartialName('PresentationCore')
-clr.AddReferenceByPartialName("PresentationFramework")
-clr.AddReferenceByPartialName('System.Windows.Forms')
-clr.AddReferenceByPartialName('WindowsBase')
-# noinspection PyUnresolvedReferences
-import System.Windows
-
-
-# fixme: Update about using wpf
-
-class aboutWindow:
+class MyWindow(Window):
     def __init__(self):
-        # Create window
-        self.my_window = System.Windows.Window()
-        self.my_window.WindowStyle = System.Windows.WindowStyle.None
-        self.my_window.AllowsTransparency = True
-        self.my_window.Background = None
-        self.my_window.Title = 'About pyrevit'
-        self.my_window.Width = 580
-        self.my_window.Height = 315
-        self.my_window.ResizeMode = System.Windows.ResizeMode.CanMinimize
-        self.my_window.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
-        self.my_window.PreviewKeyDown += self.handle_esc_key
-        self.my_window.MouseUp += self.handle_click
-        border = System.Windows.Controls.Border()
-        border.CornerRadius = System.Windows.CornerRadius(15)
-        border.Background = System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(248, 240, 240, 240))
-        self.my_window.Content = border
-
-        fontfam = System.Windows.Media.FontFamily('Courier New')
-
-        # Create StackPanel to Layout UI elements
-        self.my_stack = System.Windows.Controls.StackPanel()
-        self.my_stack.Margin = System.Windows.Thickness(5)
-        border.Child = self.my_stack
-
-        self.reponame = System.Windows.Controls.Label()
-        self.reponame.Content = 'pyRevit'
-        self.reponame.FontFamily = fontfam
-        self.reponame.FontSize = 44.0
-        self.reponame.Margin = System.Windows.Thickness(30, 20, 30, 0)
-
-        self.versionlabel = System.Windows.Controls.Label()
-        tag_line = 'IronPython Library and Scripts for Autodesk Revit'
-        sub_title = '{}\nv {}\n'.format(tag_line, PYREVIT_VERSION.get_formatted())
-        self.versionlabel.Content = sub_title
-        self.versionlabel.FontFamily = fontfam
-        self.versionlabel.FontSize = 16.0
-        self.versionlabel.Margin = System.Windows.Thickness(30, -10, 30, 0)
-
-        self.ipyversion = System.Windows.Controls.Label()
-        running_on = 'Running on IronPython {}.{}.{}'.format(sys.version_info.major,
-                                                             sys.version_info.minor,
-                                                             sys.version_info.micro)
-        self.ipyversion.Content = running_on
-        self.ipyversion.FontFamily = fontfam
-        self.ipyversion.FontSize = 14.0
-        self.ipyversion.Margin = System.Windows.Thickness(30, -10, 30, 0)
-
-        self.my_stack.Children.Add(self.reponame)
-        self.my_stack.Children.Add(self.versionlabel)
-        self.my_stack.Children.Add(self.ipyversion)
-
-        self.my_button_openwebsite = System.Windows.Controls.Button()
-        self.my_button_openwebsite.Content = 'Open pyrevit website'
-        self.my_button_openwebsite.Margin = System.Windows.Thickness(30, 10, 30, 0)
-        self.my_button_openwebsite.Click += self.openwebsite
-        self.my_button_openwebsite.Cursor = System.Windows.Input.Cursors.Hand
-
-        self.my_button_opencredits = System.Windows.Controls.Button()
-        self.my_button_opencredits.Content = 'Open Credits webpage'
-        self.my_button_opencredits.Margin = System.Windows.Thickness(30, 10, 30, 0)
-        self.my_button_opencredits.Click += self.opencredits
-        self.my_button_opencredits.Cursor = System.Windows.Input.Cursors.Hand
-
-        self.my_button_openrevisionhistory = System.Windows.Controls.Button()
-        self.my_button_openrevisionhistory.Content = 'Open Revision History webpage'
-        self.my_button_openrevisionhistory.Margin = System.Windows.Thickness(30, 10, 30, 0)
-        self.my_button_openrevisionhistory.Click += self.openrevisionhistory
-        self.my_button_openrevisionhistory.Cursor = System.Windows.Input.Cursors.Hand
-
-        self.my_button_opengithubrepopage = System.Windows.Controls.Button()
-        self.my_button_opengithubrepopage.Content = 'Open Github Repository for this library'
-        self.my_button_opengithubrepopage.Margin = System.Windows.Thickness(30, 10, 30, 0)
-        self.my_button_opengithubrepopage.Click += self.opengithubrepopage
-        self.my_button_opengithubrepopage.Cursor = System.Windows.Input.Cursors.Hand
-
-        self.my_stack.Children.Add(self.my_button_openwebsite)
-        self.my_stack.Children.Add(self.my_button_opencredits)
-        self.my_stack.Children.Add(self.my_button_openrevisionhistory)
-        self.my_stack.Children.Add(self.my_button_opengithubrepopage)
+        wpf.LoadComponent(self, os.path.join(folder, 'AboutWindow.xaml'))
+        uri = Uri(os.path.join(folder, 'credits.png'))
+        self.image_credits.Source = BitmapImage(uri)
+        self.version_info.Text = PYREVIT_VERSION.get_formatted()
+        self.pyrevit_subtitle.Text += '\nRunning on IronPython {}.{}.{}'.format(sys.version_info.major,
+                                                                                sys.version_info.minor,
+                                                                                sys.version_info.micro)
 
     # noinspection PyUnusedLocal
-    def handle_click(self, sender, args):
-        self.my_window.Close()
-
-    # noinspection PyUnusedLocal
-    def handle_esc_key(self, sender, args):
-        if args.Key == System.Windows.Input.Key.Escape:
-            self.my_window.Close()
-
-    # noinspection PyUnusedLocal
-    def openwebsite(self, sender, args):
-        os.system('start http://eirannejad.github.io/pyRevit/')
-
-    # noinspection PyUnusedLocal
-    def opencredits(self, sender, args):
-        os.system('start http://eirannejad.github.io/pyRevit/credits/')
-
-    # noinspection PyUnusedLocal
-    def openrevisionhistory(self, sender, args):
-        os.system('start http://eirannejad.github.io/pyRevit/releasenotes/')
-
-    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
     def opengithubrepopage(self, sender, args):
         os.system('start https://github.com/eirannejad/pyRevit')
 
     # noinspection PyUnusedLocal
-    def showwindow(self):
-        self.my_window.ShowDialog()
+    # noinspection PyMethodMayBeStatic
+    def openrevisionhistory(self, sender, args):
+        os.system('start http://eirannejad.github.io/pyRevit/releasenotes/')
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def opencredits(self, sender, args):
+        os.system('start http://eirannejad.github.io/pyRevit/credits/')
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def handleclick(self, sender, args):
+        self.Close()
 
 
-aboutWindow().showwindow()
+if __name__ == '__main__':
+    MyWindow().ShowDialog()
