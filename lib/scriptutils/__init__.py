67bdc0c50b8a4be36d4d91596415e1262e6ebdf3
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-12-17 09:52:34 -0800

    WIP Scriptutils

--- lib/scriptutils/__init__.py
+++ lib/scriptutils/__init__.py
@@ -1,39 +1,39 @@
-# import os.path as op
-#
-# from pyrevit.coreutils.coreutils import get_all_subclasses
-#
-# from pyrevit.coreutils.logger import get_logger
-# from pyrevit.extensions.components import GenericCommand
-# from ..core.exceptions import PyRevitException
-#
-#
-# logger = get_logger(__name__)
-#
-#
-# def get_script_info(script_file_addr):
-#     script_dir = op.dirname(script_file_addr)
-#     for component_type in get_all_subclasses([GenericCommand]):
-#         logger.debug('Testing sub_directory {} for {}'.format(script_dir, component_type))
-#         try:
-#             # if cmp_class can be created for this sub-dir, the add to list
-#             # cmp_class will raise error if full_path is not of cmp_class type.
-#             component = component_type()
-#             component.__init_from_dir__(script_dir)
-#             logger.debug('Successfuly created component: {} from: {}'.format(component, script_dir))
-#             return component
-#         except PyRevitException:
-#             logger.debug('Can not create component of type: {} from: {}'.format(component_type, script_dir))
-#     return None
-#
-#
-# def get_ui_button():
-#     # fixme: implement get_ui_button
-#     pass
-#
-#
-# def get_temp_file():
-#     """Returns a filename to be used by a user script to store temporary information.
-#     Temporary files are saved in PYREVIT_APP_DIR.
-#     """
-#     # fixme temporary file
-#     pass
+import os.path as op
+
+from pyrevit import PyRevitException
+from pyrevit.coreutils import get_all_subclasses
+
+from pyrevit.coreutils.logger import get_logger
+from pyrevit.extensions.genericcomps import GenericUICommand
+
+
+logger = get_logger(__name__)
+
+
+def get_script_info(script_file_addr):
+    script_dir = op.dirname(script_file_addr)
+    for component_type in get_all_subclasses([GenericUICommand]):
+        logger.debug('Testing sub_directory {} for {}'.format(script_dir, component_type))
+        try:
+            # if cmp_class can be created for this sub-dir, the add to list
+            # cmp_class will raise error if full_path is not of cmp_class type.
+            component = component_type()
+            component.__init_from_dir__(script_dir)
+            logger.debug('Successfuly created component: {} from: {}'.format(component, script_dir))
+            return component
+        except PyRevitException:
+            logger.debug('Can not create component of type: {} from: {}'.format(component_type, script_dir))
+    return None
+
+
+def get_ui_button():
+    # fixme: implement get_ui_button
+    pass
+
+
+def get_temp_file():
+    """Returns a filename to be used by a user script to store temporary information.
+    Temporary files are saved in PYREVIT_APP_DIR.
+    """
+    # fixme temporary file
+    pass
