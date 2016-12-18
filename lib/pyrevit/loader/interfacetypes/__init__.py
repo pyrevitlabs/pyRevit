555d075adbb6d04d4bdf5363ab66303d28e52b65
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-12-18 15:29:17 -0800

    Support for loading previously compiled command assemblies

--- lib/pyrevit/loader/interfacetypes/__init__.py
+++ lib/pyrevit/loader/interfacetypes/__init__.py
@@ -5,12 +5,12 @@ import clr
 
 from pyrevit import PyRevitException
 from pyrevit.coreutils import make_canonical_name, find_loaded_asm, load_asm_file, calculate_dir_hash, get_str_hash
-from pyrevit.coreutils.appdata import PYREVIT_APP_DIR, get_data_file, is_data_file_available
 from pyrevit.coreutils.dotnetcompiler import compile_csharp
 from pyrevit.coreutils.logger import get_logger
 from pyrevit.extensions import PYTHON_LANG, CSHARP_LANG, VB_LANG
 from pyrevit.versionmgr import PYREVIT_VERSION
 from pyrevit.loader import ASSEMBLY_FILE_TYPE, HASH_CUTOFF_LENGTH
+import pyrevit.coreutils.appdata as appdata
 
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.Attributes import RegenerationAttribute, RegenerationOption, TransactionAttribute, TransactionMode
@@ -32,7 +32,7 @@ LOADER_DIR = op.dirname(op.dirname(__file__))
 ADDIN_DIR = op.join(LOADER_DIR, 'addin')
 
 sys.path.append(ADDIN_DIR)
-sys.path.append(PYREVIT_APP_DIR)
+sys.path.append(appdata.PYREVIT_APP_DIR)
 
 ADDIN_RESOURCE_DIR = op.join(ADDIN_DIR, 'Source', 'pyRevitLoader', 'Resources')
 INTERFACE_TYPES_DIR = op.join(LOADER_DIR, 'interfacetypes')
@@ -57,7 +57,7 @@ CMD_AVAIL_CLS_NAME_SELECTION = make_canonical_name(LOADER_BASE_NAMESPACE, 'PyRev
 source_file_filter = '(\.cs)'
 BASE_CLASSES_DIR_HASH = calculate_dir_hash(INTERFACE_TYPES_DIR, '', source_file_filter)[:HASH_CUTOFF_LENGTH]
 BASE_CLASSES_ASM_FILE_ID = '{}_{}'.format(BASE_CLASSES_DIR_HASH, LOADER_BASE_NAMESPACE)
-BASE_CLASSES_ASM_FILE = get_data_file(BASE_CLASSES_ASM_FILE_ID, ASSEMBLY_FILE_TYPE)
+BASE_CLASSES_ASM_FILE = appdata.get_data_file(BASE_CLASSES_ASM_FILE_ID, ASSEMBLY_FILE_TYPE)
 # taking the name of the generated data file and use it as assembly name
 BASE_CLASSES_ASM_NAME = op.splitext(op.basename(BASE_CLASSES_ASM_FILE))[0]
 logger.debug('Interface types assembly file is: {}'.format(BASE_CLASSES_ASM_NAME))
@@ -167,7 +167,7 @@ def _generate_base_classes_asm():
 
 
 def _get_base_classes_asm():
-    if is_data_file_available(file_id=BASE_CLASSES_ASM_FILE_ID, file_ext=ASSEMBLY_FILE_TYPE):
+    if appdata.is_data_file_available(file_id=BASE_CLASSES_ASM_FILE_ID, file_ext=ASSEMBLY_FILE_TYPE):
         return load_asm_file(BASE_CLASSES_ASM_FILE)
     else:
         return _generate_base_classes_asm()
@@ -196,6 +196,16 @@ CMD_EXECUTOR_CLASS = _find_pyrevit_base_type(BASE_CLASSES_ASM, CMD_EXECUTOR_CLAS
 CMD_AVAIL_CLS = _find_pyrevit_base_type(BASE_CLASSES_ASM, CMD_AVAIL_CLS_NAME)
 CMD_AVAIL_CLS_CATEGORY = _find_pyrevit_base_type(BASE_CLASSES_ASM, CMD_AVAIL_CLS_NAME_CATEGORY)
 CMD_AVAIL_CLS_SELECTION = _find_pyrevit_base_type(BASE_CLASSES_ASM, CMD_AVAIL_CLS_NAME_SELECTION)
+
+
+def _create_ext_command_attrs():
+    regen_const_info = clr.GetClrType(RegenerationAttribute).GetConstructor(Array[Type]((RegenerationOption,)))
+    regen_attr_builder = CustomAttributeBuilder(regen_const_info, Array[object]((RegenerationOption.Manual,)))
+    # add TransactionAttribute to type
+    trans_constructor_info = clr.GetClrType(TransactionAttribute).GetConstructor(Array[Type]((TransactionMode,)))
+    trans_attrib_builder = CustomAttributeBuilder(trans_constructor_info, Array[object]((TransactionMode.Manual,)))
+
+    return [regen_attr_builder, trans_attrib_builder]
 
 
 def _create_base_type(modulebuilder, type_class, class_name, custom_attr_list, *args):
@@ -266,20 +276,13 @@ def _create_cmd_loader_type(module_builder, cmd_params):
     """
     logger.debug('Creating loader class type for: {}'.format(cmd_params))
 
-    # add RegenerationAttribute to type
-    regen_const_info = clr.GetClrType(RegenerationAttribute).GetConstructor(Array[Type]((RegenerationOption,)))
-    regen_attr_builder = CustomAttributeBuilder(regen_const_info, Array[object]((RegenerationOption.Manual,)))
-    # add TransactionAttribute to type
-    trans_constructor_info = clr.GetClrType(TransactionAttribute).GetConstructor(Array[Type]((TransactionMode,)))
-    trans_attrib_builder = CustomAttributeBuilder(trans_constructor_info, Array[object]((TransactionMode.Manual,)))
-
     _create_base_type(module_builder, CMD_EXECUTOR_CLASS, cmd_params.class_name,
-                      [regen_attr_builder, trans_attrib_builder],
+                      _create_ext_command_attrs(),
                       cmd_params.script_file_address,
                       cmd_params.config_script_file_address,
                       cmd_params.search_paths_str,
                       cmd_params.cmd_name,
-                      PYREVIT_APP_DIR)
+                      appdata.PYREVIT_APP_DIR)
 
 
 def _create_vb_type(module_builder, cmd_params):
@@ -293,24 +296,18 @@ def _create_csharp_type(module_builder, cmd_params):
     script_hash = get_str_hash(source)[:HASH_CUTOFF_LENGTH]
 
     command_assm_file_id = '{}_{}'.format(script_hash, cmd_params.class_name)
-    command_assm_file = get_data_file(command_assm_file_id, ASSEMBLY_FILE_TYPE)
-    logger.debug('Compiling script to: {}'.format(command_assm_file))
+    compiled_assm_path = appdata.is_data_file_available(file_id=command_assm_file_id, file_ext=ASSEMBLY_FILE_TYPE)
 
-    regen_const_info = clr.GetClrType(RegenerationAttribute).GetConstructor(Array[Type]((RegenerationOption,)))
-    regen_attr_builder = CustomAttributeBuilder(regen_const_info, Array[object]((RegenerationOption.Manual,)))
-    # add TransactionAttribute to type
-    trans_constructor_info = clr.GetClrType(TransactionAttribute).GetConstructor(Array[Type]((TransactionMode,)))
-    trans_attrib_builder = CustomAttributeBuilder(trans_constructor_info, Array[object]((TransactionMode.Manual,)))
+    if not compiled_assm_path:
+        command_assm_file = appdata.get_data_file(file_id=command_assm_file_id, file_ext=ASSEMBLY_FILE_TYPE)
+        logger.debug('Compiling script to: {}'.format(command_assm_file))
+        compiled_assm_path = compile_csharp([source], command_assm_file, reference_list=_get_references())
 
-    compiled_assm_path = compile_csharp([source], command_assm_file, reference_list=_get_references())
     compiled_assm = load_asm_file(compiled_assm_path)
 
     for compiled_type in compiled_assm.GetTypes():
         if IExternalCommand in compiled_type.GetInterfaces():
-            _create_base_type(module_builder,
-                              compiled_type,
-                              cmd_params.class_name,
-                              [regen_attr_builder, trans_attrib_builder])
+            _create_base_type(module_builder, compiled_type, cmd_params.class_name, _create_ext_command_attrs())
             return
 
     logger.error('Could not find command interface: {}'.format(cmd_params))
