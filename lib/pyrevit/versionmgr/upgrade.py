80cafb19621eea4cfa2b3ddddfa045e84118c226
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-12-18 16:15:49 -0800

    updated upgrade function

--- lib/pyrevit/versionmgr/upgrade.py
+++ lib/pyrevit/versionmgr/upgrade.py
@@ -1,3 +1,9 @@
 # perform upgrades between versions here, e.g. adding a new config parameter
+from pyrevit.userconfig import user_config
+
 def upgrade_existing_pyrevit():
-    pass
+    try:
+        temp = user_config.init.filelogging
+    except:
+        user_config.init.filelogging = False
+        user_config.save_changes()
