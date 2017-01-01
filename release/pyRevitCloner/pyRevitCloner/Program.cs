0c17d67bf143c89e22ccffd137859bfb5aab81c5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2016-12-31 18:39:01 -0800

    Created a standalone cloner for pyRevit using libgit2sharp
    Reduced installer size from 150MB to 1.5Mb :D

---
+++ release/pyRevitCloner/pyRevitCloner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace pyRevitCloner
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            var cops = new CloneOptions();
+            cops.Checkout = true;
+            cops.BranchName = "master";
+            Repository.Clone(args[0], args[1]);
+        }
+    }
+}
