1eb8be3eee1fd07d530e85bf462cbc36eac442ed
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-01 16:57:07 -0800

    Added support for git user credentials
    Added support for reading repo access username and password from userconfig

--- pyrevitlib/pyrevit/versionmgr/updater.py
+++ pyrevitlib/pyrevit/versionmgr/updater.py
@@ -1,8 +1,9 @@
 from pyrevit import HOST_APP
-from pyrevit.versionmgr import get_pyrevit_repo
-from pyrevit.coreutils.logger import get_logger
 import pyrevit.coreutils.git as git
+from pyrevit.coreutils.logger import get_logger
+from pyrevit.versionmgr import get_pyrevit_repo
 
+from pyrevit.userconfig import user_config
 from pyrevit.extensions.extensionmgr import get_installed_extension_data
 
 # noinspection PyUnresolvedReferences
@@ -12,24 +13,46 @@ from System import DateTime, DateTimeOffset
 logger = get_logger(__name__)
 
 
-# noinspection PyUnusedLocal
-def hndlr(url, uname, types):
+def _credentials_hndlr(username, password):
     up = git.libgit.UsernamePasswordCredentials()
-    up.Username = ''
-    up.Password = ''
+    up.Username = username
+    up.Password = password
     return up
 
 
-def _make_pull_options():
+def _hndlr(url, uname, types):
+    up = git.libgit.UsernamePasswordCredentials()
+    up.Username = 'eirannejad@gmail.com'
+    up.Password = 'testpassword1891'
+    return up
+
+
+def _get_credentials_hndlr(username, password):
+    return git.libgit.Handlers.CredentialsHandler(lambda url, uname, types: _credentials_hndlr(username, password))
+
+
+def _get_credentials(repo_info):
+    try:
+        repo_config = getattr(user_config, repo_info.name)
+        return repo_config.username, repo_config.password
+    except:
+        return None, None
+
+
+def _make_pull_options(repo_info):
     pull_ops = git.libgit.PullOptions()
     pull_ops.FetchOptions = git.libgit.FetchOptions()
-    # pull_ops.FetchOptions.CredentialsProvider = git.libgit.Handlers.CredentialsHandler(hndlr)
+    username, password = _get_credentials(repo_info)
+    if username and password:
+        pull_ops.FetchOptions.CredentialsProvider = _get_credentials_hndlr(username, password)
     return pull_ops
 
 
-def _make_fetch_options():
+def _make_fetch_options(repo_info):
     fetch_ops = git.libgit.FetchOptions()
-    # fetch_ops.CredentialsProvider = git.libgit.Handlers.CredentialsHandler(hndlr)
+    username, password = _get_credentials(repo_info)
+    if username and password:
+        fetch_ops.CredentialsProvider = _get_credentials_hndlr(username, password)
     return fetch_ops
 
 
@@ -70,7 +93,7 @@ def update_pyrevit(repo_info):
     head_msg = str(repo.Head.Tip.Message).replace('\n', '')
     logger.debug('Current head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
     try:
-        repo.Network.Pull(_make_pull_signature(), _make_pull_options())
+        repo.Network.Pull(_make_pull_signature(), _make_pull_options(repo_info))
         logger.debug('Successfully updated repo: {}'.format(repo_info.directory))
         head_msg = str(repo.Head.Tip.Message).replace('\n', '')
         logger.debug('New head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
@@ -91,7 +114,7 @@ def has_pending_updates(repo_info):
     for remote in repo.Network.Remotes:
         logger.debug('Fetching remote: {} of {}'.format(remote.Name, repo_info.directory))
         try:
-            repo.Network.Fetch(remote, _make_fetch_options())
+            repo.Network.Fetch(remote, _make_fetch_options(repo_info))
         except Exception as fetch_err:
             logger.error('Failed fetching: {} | {}'.format(repo_info.directory, str(fetch_err).replace('\n', '')))
 
