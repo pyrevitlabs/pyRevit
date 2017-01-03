28ab34432a5b4680498975efaa64645d6daeac1d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-02 18:48:53 -0800

    Moving git core functions into coreutils.git
    ready for testing

--- pyrevitlib/pyrevit/coreutils/git.py
+++ pyrevitlib/pyrevit/coreutils/git.py
@@ -10,10 +10,14 @@ import clr
 import importlib
 import os.path as op
 
+from pyrevit import HOST_APP
+from pyrevit.coreutils.logger import get_logger
+
 # noinspection PyUnresolvedReferences
 import System
+# noinspection PyUnresolvedReferences
+from System import DateTime, DateTimeOffset
 
-from pyrevit.coreutils.logger import get_logger
 
 GIT_LIB = 'LibGit2Sharp'
 
@@ -41,9 +45,45 @@ class RepoInfo:
         self.head_name = repo.Head.Name
         self.last_commit_hash = repo.Head.Tip.Id.Sha
         self.repo = repo
+        self.username = self.password = None
 
     def __repr__(self):
         return '<type \'RepoInfo\' head \'{}\' @ {}>'.format(self.last_commit_hash, self.directory)
+
+
+def _credentials_hndlr(username, password):
+    up = libgit.UsernamePasswordCredentials()
+    up.Username = username
+    up.Password = password
+    return up
+
+
+def _get_credentials_hndlr(username, password):
+    return libgit.Handlers.CredentialsHandler(lambda url, uname, types: _credentials_hndlr(username, password))
+
+
+def _make_pull_options(repo_info):
+    logger.debug('Making pull options: {}'.format(repo_info))
+    pull_ops = libgit.PullOptions()
+    pull_ops.FetchOptions = libgit.FetchOptions()
+    if repo_info.username and repo_info.password:
+        logger.debug('Username and password are available but cant\' log private info. Making Credentials handler.')
+        pull_ops.FetchOptions.CredentialsProvider = _get_credentials_hndlr(repo_info.username, repo_info.password)
+    return pull_ops
+
+
+def _make_fetch_options(repo_info):
+    logger.debug('Making fetch options: {}'.format(repo_info))
+    fetch_ops = libgit.FetchOptions()
+    if repo_info.username and repo_info.password:
+        logger.debug('Username and password are available but cant\' log private info. Making Credentials handler.')
+        fetch_ops.CredentialsProvider = _get_credentials_hndlr(repo_info.username, repo_info.password)
+    return fetch_ops
+
+
+def _make_pull_signature():
+    logger.debug('Creating pull signature for username: {}'.format(HOST_APP.username))
+    return libgit.Signature(HOST_APP.username, HOST_APP.username, DateTimeOffset(DateTime.Now))
 
 
 def get_repo(repo_dir):
@@ -57,3 +97,30 @@ def get_repo(repo_dir):
     """
     repo = libgit.Repository(repo_dir)
     return RepoInfo(repo)
+
+
+def git_pull(repo_info):
+    repo = repo_info.repo
+    try:
+        repo.Network.Pull(_make_pull_signature(), _make_pull_options(repo_info))
+        logger.debug('Successfully pulled repo: {}'.format(repo_info.directory))
+        head_msg = str(repo.Head.Tip.Message).replace('\n', '')
+        logger.debug('New head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
+        return RepoInfo(repo)
+
+    except Exception as pull_err:
+        logger.error('Failed git pull: {} | {}'.format(repo_info.directory, pull_err))
+        return None
+
+
+def git_fetch(repo_info):
+    repo = repo_info.repo
+    try:
+        repo.Network.Pull(_make_pull_signature(), _make_pull_options(repo_info))
+        logger.debug('Successfully pulled repo: {}'.format(repo_info.directory))
+        head_msg = str(repo.Head.Tip.Message).replace('\n', '')
+        logger.debug('New head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
+        return RepoInfo(repo)
+
+    except Exception as fetch_err:
+        logger.error('Failed git fetch: {} | {}'.format(repo_info.directory, fetch_err))
--- pyrevitlib/pyrevit/versionmgr/updater.py
+++ pyrevitlib/pyrevit/versionmgr/updater.py
@@ -1,4 +1,3 @@
-from pyrevit import HOST_APP
 import pyrevit.coreutils.git as git
 from pyrevit.coreutils.logger import get_logger
 from pyrevit.versionmgr import get_pyrevit_repo
@@ -6,25 +5,11 @@ from pyrevit.versionmgr import get_pyrevit_repo
 from pyrevit.userconfig import user_config
 from pyrevit.extensions.extensionmgr import get_installed_extension_data
 
-# noinspection PyUnresolvedReferences
-from System import DateTime, DateTimeOffset
-
 
 logger = get_logger(__name__)
 
 
-def _credentials_hndlr(username, password):
-    up = git.libgit.UsernamePasswordCredentials()
-    up.Username = username
-    up.Password = password
-    return up
-
-
-def _get_credentials_hndlr(username, password):
-    return git.libgit.Handlers.CredentialsHandler(lambda url, uname, types: _credentials_hndlr(username, password))
-
-
-def _get_credentials(repo_info):
+def _get_extension_credentials(repo_info):
     try:
         repo_config = user_config.get_section(repo_info.name)
         return repo_config.username, repo_config.password
@@ -32,30 +17,14 @@ def _get_credentials(repo_info):
         return None, None
 
 
-def _make_pull_options(repo_info):
-    logger.debug('Making pull options: {}'.format(repo_info))
-    pull_ops = git.libgit.PullOptions()
-    pull_ops.FetchOptions = git.libgit.FetchOptions()
-    username, password = _get_credentials(repo_info)
+def _fetch_remote(remote, repo_info):
+    logger.debug('Fetching remote: {} of {}'.format(remote.Name, repo_info.directory))
+    username, password = _get_extension_credentials(repo_info)
     if username and password:
-        logger.debug('Username and password are available but cant\' log private info. Making Credentials handler.')
-        pull_ops.FetchOptions.CredentialsProvider = _get_credentials_hndlr(username, password)
-    return pull_ops
+        repo_info.username = username
+        repo_info.password = password
 
-
-def _make_fetch_options(repo_info):
-    logger.debug('Making fetch options: {}'.format(repo_info))
-    fetch_ops = git.libgit.FetchOptions()
-    username, password = _get_credentials(repo_info)
-    if username and password:
-        logger.debug('Username and password are available but cant\' log private info. Making Credentials handler.')
-        fetch_ops.CredentialsProvider = _get_credentials_hndlr(username, password)
-    return fetch_ops
-
-
-def _make_pull_signature():
-    logger.debug('Creating pull signature for username: {}'.format(HOST_APP.username))
-    return git.libgit.Signature(HOST_APP.username, HOST_APP.username, DateTimeOffset(DateTime.Now))
+    git.git_fetch(repo_info)
 
 
 def get_thirdparty_ext_repos():
@@ -92,31 +61,30 @@ def update_pyrevit(repo_info):
     logger.debug('Updating repo: {}'.format(repo_info.directory))
     head_msg = str(repo.Head.Tip.Message).replace('\n', '')
     logger.debug('Current head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
-    try:
-        repo.Network.Pull(_make_pull_signature(), _make_pull_options(repo_info))
+    username, password = _get_extension_credentials(repo_info)
+    if username and password:
+        repo_info.username = username
+        repo_info.password = password
+
+    updated_repo_info = git.git_pull(repo_info)
+
+    if not updated_repo_info:
+        logger.error('Failed updating: {}'.format(repo_info.directory))
+        return False
+    else:
         logger.debug('Successfully updated repo: {}'.format(repo_info.directory))
-        head_msg = str(repo.Head.Tip.Message).replace('\n', '')
-        logger.debug('New head is: {} > {}'.format(repo.Head.Tip.Id.Sha, head_msg))
-        return git.RepoInfo(repo)
-
-    except Exception as pull_err:
-        logger.error('Failed updating: {} | {}'.format(repo_info.directory, pull_err))
-
-    return False
+        return updated_repo_info
 
 
 def has_pending_updates(repo_info):
     repo = repo_info.repo
     logger.debug('Fetching updates for: {}'.format(repo_info.directory))
-    repo_branches = repo.Branches
-    logger.debug('Repo branches: {}'.format([b for b in repo_branches]))
 
     for remote in repo.Network.Remotes:
-        logger.debug('Fetching remote: {} of {}'.format(remote.Name, repo_info.directory))
-        try:
-            repo.Network.Fetch(remote, _make_fetch_options(repo_info))
-        except Exception as fetch_err:
-            logger.error('Failed fetching: {} | {}'.format(repo_info.directory, str(fetch_err).replace('\n', '')))
+        _fetch_remote(remote, repo_info)
+
+    repo_branches = repo.Branches
+    logger.debug('Repo branches: {}'.format([b for b in repo_branches]))
 
     for branch in repo_branches:
         if not branch.IsRemote:
