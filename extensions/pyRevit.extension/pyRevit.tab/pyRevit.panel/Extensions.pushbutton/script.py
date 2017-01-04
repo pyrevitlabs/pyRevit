1beb89d12b054c393b7e5ef8a82cb8c911003f35
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-04 08:07:48 -0800

    WIP Extensions

--- extensions/pyRevit.extension/pyRevit.tab/pyRevit.panel/Extensions.pushbutton/script.py
+++ extensions/pyRevit.extension/pyRevit.tab/pyRevit.panel/Extensions.pushbutton/script.py
@@ -1,5 +1,29 @@
 __doc__ = 'Add or remove pyRevit extensions.'
 
-from Autodesk.Revit.UI import TaskDialog
+import os
+import os.path as op
+import json
 
-TaskDialog.Show('pyRevit', 'Work in progress.')
+from pyrevit.userconfig import user_config
+
+ext_dirs = user_config.get_ext_root_dirs()
+
+ext_def_dict = {'extensions':[]}
+
+for ext_dir in ext_dirs:
+    ext_def_file = op.join(ext_dir, 'extensions.json')
+    if op.exists(ext_def_file):
+        with open(ext_def_file, 'r') as ext_def:
+            ext_def_dict['extensions'].extend(json.load(ext_def)['extensions'])
+
+for ext in ext_def_dict['extensions']:
+    print """Extension:
+    {name}
+    {description}
+
+    Extension git repo: {url}
+    Extension website: {website}
+    Extension website: {image}
+
+
+    """.format(**ext)
