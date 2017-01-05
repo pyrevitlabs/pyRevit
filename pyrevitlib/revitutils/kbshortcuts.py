ef6a458ed6310a2a9431fcc6f2dd6d8b5b70469d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-05 15:22:42 -0800

    kbshortcut notes

---
+++ pyrevitlib/revitutils/kbshortcuts.py
@@ -0,0 +1,4 @@
+# files are under %AppData%\Roaming\Autodesk\Revit\Autodesk Revit 20XX
+# shortcut file is KeyboardShortcuts.xml
+# import export shortcuts from other revit versions
+# querying shortcuts if API does not provide this info
