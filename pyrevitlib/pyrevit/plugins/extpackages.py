ce59be6ae07a62d69cda84642dfcb125ebf62b62
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-10 11:34:37 -0800

    Added author and author profile to Extension system

--- pyrevitlib/pyrevit/plugins/extpackages.py
+++ pyrevitlib/pyrevit/plugins/extpackages.py
@@ -37,8 +37,11 @@ class ExtensionPackage:
         try:
             self.website = info_dict['website']
             self.image = info_dict['image']
+            self.author = info_dict['author']
+            self.author_profile = info_dict['author-url']
         except Exception as ext_info_err:
             self.website = self.image = None
+            self.author = self.author_profile = None
             logger.debug('Missing extended plugin ext info. | {}'.format(ext_info_err))
 
     def __repr__(self):
