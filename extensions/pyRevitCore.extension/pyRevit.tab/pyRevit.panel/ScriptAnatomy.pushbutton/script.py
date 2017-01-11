0e8f9611d4b732d2e977a7ce22a50402065d5990
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-10 22:32:45 -0800

    Removed Sample command and added a link to Anatomy of a Script page

---
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/ScriptAnatomy.pushbutton/script.py
@@ -0,0 +1,6 @@
+__doc__ = 'Opens the Anatomy of a Script page in the default browser.'
+
+__title__ = 'Script\nAnatomy'
+
+from scriptutils import open_url
+open_url('http://eirannejad.github.io/pyRevit/anatomyofpyrevitscript/')
