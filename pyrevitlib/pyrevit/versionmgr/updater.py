b52f7e518573635dbda62e883308b2544ef6ed4f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-01-14 17:03:03 -0800

    Fixed bug in returning credential tuple

--- pyrevitlib/pyrevit/versionmgr/updater.py
+++ pyrevitlib/pyrevit/versionmgr/updater.py
@@ -14,6 +14,7 @@ def _get_extension_credentials(repo_info):
         repo_config = user_config.get_section(repo_info.name)
         if repo_config.private_repo:
             return repo_config.username, repo_config.password
+        return None, None
     except:
         return None, None
 
