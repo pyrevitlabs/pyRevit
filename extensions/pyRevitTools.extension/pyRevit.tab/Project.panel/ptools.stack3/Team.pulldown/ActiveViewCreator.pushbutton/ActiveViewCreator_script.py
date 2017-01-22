128f23cc2d4817e5c5d6ad397c1cd994301bec7a
frederic-beaupere <beau.git@gmx.de>
2017-01-22 19:42:34 +0100

    added @Team: ActiveViewCreator

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack3/Team.pulldown/ActiveViewCreator.pushbutton/ActiveViewCreator_script.py
@@ -0,0 +1,33 @@
+﻿"""
+Active View Creator
+Shows the creator of the active view.
+
+Copyright (c) 2017 Frederic Beaupere
+github.com/frederic-beaupere
+
+--------------------------------------------------------
+PyRevit Notice:
+Copyright (c) 2014-2017 Ehsan Iran-Nejad
+pyRevit: repository at https://github.com/eirannejad/pyRevit
+
+"""
+
+__title__ = 'Active View Creator'
+__author__ = 'Frederic Beaupere'
+__contact__ = 'github.com/frederic-beaupere'
+__credits__ = 'http://eirannejad.github.io/pyRevit/credits/'
+__doc__ = """Shows the creator of the active view."""
+
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import WorksharingUtils
+from revitutils import doc
+
+active_view = doc.ActiveView
+view_id = active_view.Id.ToString()
+view_name = active_view.Name
+view_creator = WorksharingUtils.GetWorksharingTooltipInfo(doc, active_view.Id).Creator
+
+print("Creator of the current view: " + view_name)
+print("with the id: " + view_id)
+print("is: " + view_creator)
