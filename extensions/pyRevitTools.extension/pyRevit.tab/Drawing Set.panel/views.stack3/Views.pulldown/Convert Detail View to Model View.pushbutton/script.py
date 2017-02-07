e0a2417f950bac887b37b1bdecaef1ecc9290802
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-07 15:11:30 -0800

    Modified the view conversion tool for better reporting

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Convert Detail View to Model View.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Convert Detail View to Model View.pushbutton/script.py
@@ -1,7 +1,7 @@
 import sys
 import clr
 
-from scriptutils import logger
+from scriptutils import logger, this_script, print_md
 from scriptutils.userinput import CommandSwitchWindow
 from revitutils import doc, selection
 
@@ -9,12 +9,13 @@ from System.Collections.Generic import List
 from Autodesk.Revit.DB import Element, ElementId, View, ViewType, FilteredElementCollector, BuiltInCategory, \
                               CopyPasteOptions, ElementTransformUtils, ElevationMarker, \
                               Transaction, TransactionGroup, IDuplicateTypeNamesHandler, ViewDuplicateOption, \
-                              ViewPlan, ViewDrafting, ViewSection, BoundingBoxXYZ, ElementTypeGroup, XYZ
+                              ViewPlan, ViewDrafting, ViewSection, BoundingBoxXYZ, ElementTypeGroup, XYZ, \
+                              IFailuresPreprocessor, FailureProcessingResult
 from Autodesk.Revit.UI import TaskDialog
 
 
 __doc__ = "Converts selected detail views to selected model view type." \
-          "The resulting model views will not show the model and their associated tags are only visible in 1:1 views."
+          "The resulting model views will not show the model, and their associated tags are only visible in 1:1 views."
 
 
 selected_level = None
@@ -23,14 +24,39 @@ view_types_dict = {'Floor Plan': ViewPlan,
                    'Section': ViewSection,
                    'Reflected Ceiling Plan': ViewPlan,
                    'Elevation': ViewSection,
-                #    'Area Plan': ViewPlan,
-                #    'Structural Plan': ViewPlan,
+                   # 'Area Plan': ViewPlan,
+                   # 'Structural Plan': ViewPlan,
                    }
 
 
 class CopyUseDestination(IDuplicateTypeNamesHandler):
     def OnDuplicateTypeNamesFound(self, args):
+        logger.debug('Duplicate types found. Using destination types.')
         return DuplicateTypeAction.UseDestinationTypes
+
+
+class ViewConverterFailurePreProcessor(IFailuresPreprocessor):
+    def __init__(self, trans_name, view_name):
+        self.transaction_name = trans_name
+        self.src_viewname = view_name
+
+    def PreprocessFailures(self, failures_accessor):
+        trans_name = failures_accessor.GetTransactionName()
+        fmas = list(failures_accessor.GetFailureMessages())
+
+        if len(fmas) == 0:
+            return FailureProcessingResult.Continue
+
+        # We already know the transaction name.
+        if trans_name == self.transaction_name:
+            # DeleteWarning mimics clicking 'Ok' button
+            for fma in fmas:
+                logger.warning('Revit failure occured | {}'.format(fma.GetDescriptionText()))
+                failures_accessor.DeleteWarning(fma)
+
+            return FailureProcessingResult.ProceedWithCommit
+
+        return FailureProcessingResult.Continue
 
 
 def process_selection():
@@ -122,54 +148,76 @@ def get_copyable_elements(src_view):
         else:
             logger.debug('Skipping Element with id: {0}'.format(el.Id))
 
-    if len(elements_to_copy) < 1:
-        logger.debug('Skipping {0}. No copyable elements where found.'.format(src_view.ViewName))
-
     return elements_to_copy
 
 
 def copy_paste_elements_btwn_views(src_view, dest_view):
     elements_to_copy = get_copyable_elements(src_view)
-    # copying and pasting elements
-    try:
-        with Transaction(doc, 'Copy and Paste elements') as t:
-            t.Start()
-            options = CopyPasteOptions()
-            options.SetDuplicateTypeNamesHandler(CopyUseDestination())
-            copied_element = ElementTransformUtils.CopyElements(src_view,
-                                                                List[ElementId](elements_to_copy),
-                                                                dest_view,
-                                                                None,
-                                                                options)
+    if len(elements_to_copy) >= 1:
+        # copying and pasting elements
+        trans_name = 'Copy and Paste elements'
+        try:
+            with Transaction(doc, trans_name) as t:
+                t.Start()
 
-            # matching element graphics overrides and view properties
-            for dest, src in zip(copied_element, elements_to_copy):
-                dest_view.SetElementOverrides(dest, src_view.GetElementOverrides(src))
+                failure_ops = t.GetFailureHandlingOptions()
+                failure_ops.SetFailuresPreprocessor(ViewConverterFailurePreProcessor(trans_name, src_view.ViewName))
+                # failure_ops.SetForcedModalHandling(False)
+                t.SetFailureHandlingOptions(failure_ops)
 
-            t.Commit()
-            return True
-    except Exception as err:
-        logger.error(err)
-        return False
+                options = CopyPasteOptions()
+                options.SetDuplicateTypeNamesHandler(CopyUseDestination())
+                copied_element = ElementTransformUtils.CopyElements(src_view,
+                                                                    List[ElementId](elements_to_copy),
+                                                                    dest_view,
+                                                                    None,
+                                                                    options)
+
+                # matching element graphics overrides and view properties
+                if len(copied_element) != len(elements_to_copy):
+                    logger.error('Some elements were not copied from view: {}'.format(src_view.ViewName))
+                for dest, src in zip(copied_element, elements_to_copy):
+                    dest_view.SetElementOverrides(dest, src_view.GetElementOverrides(src))
+
+                t.Commit()
+                return len(copied_element)
+        except Exception as err:
+            logger.error('Error occured while copying elements from {} | {}'.format(src_view.ViewName, err))
+            return 0
+    else:
+        print('No copyable elements where found.')
 
 
 drafting_views = process_selection()
 dest_view_type = get_modelview_type()
+successfully_converted = 0
 
 # iterate over drafting views
 with TransactionGroup(doc, 'Convert Drafting to Model') as tg:
     tg.Start()
+    tg.IsFailureHandlingForcedModal = False
+
     for src_view in drafting_views:
-        logger.info('Converting: {}'.format(src_view.ViewName))
+        print_md('-----\n**Converting: {}**'.format(src_view.ViewName))
         dest_view_successfully_setup = False
         try:
             dest_view = create_dest_view(dest_view_type, src_view.ViewName, src_view.Scale)
             dest_view_successfully_setup = True
         except Exception as err:
-            logger.error('Error converting: {} | {}'.format(src_view.ViewName, err))
-            logger.debug(err)
+            logger.error('Error creating model view for: {}. Conversion unsuccessful. | {}'.format(src_view.ViewName,
+                                                                                                   err))
 
         if dest_view_successfully_setup:
-            copy_paste_elements_btwn_views(src_view, dest_view)
+            print('Copying contents from {} to {}'.format(this_script.output.linkify(src_view.Id),
+                                                          this_script.output.linkify(dest_view.Id)))
+            el_count = copy_paste_elements_btwn_views(src_view, dest_view)
+            if el_count:
+                print('Conversion completed. {} elements were copied.\n\n'.format(el_count))
+                successfully_converted += 1
+            else:
+                print('Conversion cancelled. No elements were copied.\n\n')
+
 
     tg.Assimilate()
+
+print_md('**{} out of {} views were successfully converted.**'.format(successfully_converted, len(drafting_views)))
