82a7c80e933d32eaa61a01fc58e936bac5308bfc
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-14 08:29:16 -0800

    Script cleanup

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Revision.pulldown/Find All Revised Sheets.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Revision.pulldown/Find All Revised Sheets.pushbutton/script.py
@@ -1,40 +1,20 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+"""Lists all sheets revised under any revision."""
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
-
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
-
-__doc__ = 'Lists all sheets revised under any revision.'
-
+from revitutils import doc
+from scriptutils import this_script
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
 
-doc = __revit__.ActiveUIDocument.Document
-
-print('LIST OF REVISIONS:')
+this_script.output.print_md('**LIST OF REVISIONS:**')
 cl = FilteredElementCollector(doc)
 revs = cl.OfCategory(BuiltInCategory.OST_Revisions).WhereElementIsNotElementType()
 for rev in revs:
-    print('{0}\tREV#: {1}DATE: {2}TYPE:{3}DESC: {4}'.format(rev.SequenceNumber, str(rev.RevisionNumber).ljust(5),
-                                                            str(rev.RevisionDate).ljust(10),
-                                                            str(rev.NumberType.ToString()).ljust(15), rev.Description))
+    print('{0}\tREV#: {1}\tDATE: {2}\tTYPE:{3}\tDESC: {4}'.format(rev.SequenceNumber,
+                                                                  str(rev.RevisionNumber).ljust(5),
+                                                                  str(rev.RevisionDate).ljust(10),
+                                                                  str(rev.NumberType.ToString()).ljust(15),
+                                                                  rev.Description))
 
-print('\n\n'
-      'REVISED SHEETS:\n'
-      '\n'
-      'NAME\tNUMBER\n--------------------------------------------------------------------------')
+this_script.output.print_md('*****\n\n\n###REVISED SHEETS:\n')
 
 sheetsnotsorted = FilteredElementCollector(doc).OfCategory(
     BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
@@ -43,9 +23,9 @@ sheets = sorted(sheetsnotsorted, key=lambda x: x.SheetNumber)
 for sht in sheets:
     revs = sht.GetAllRevisionIds()
     if len(revs) > 0:
-        print('{0}\t{1}'.format(sht.LookupParameter('Sheet Number').AsString(),
-                                sht.LookupParameter('Sheet Name').AsString()
-                                ))
+        print('{}\t{}\t{}'.format(this_script.output.linkify(sht.Id),
+                                  sht.LookupParameter('Sheet Number').AsString(),
+                                  sht.LookupParameter('Sheet Name').AsString()))
         for rev in revs:
             rev = doc.GetElement(rev)
             print('\tREV#: {0}\t\tDATE: {1}\t\tDESC:{2}'.format(rev.RevisionNumber,
