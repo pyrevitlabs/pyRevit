2667ffbeaef325cf73d1105910ec7d1c2c62cfb7
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-15 13:32:01 -0800

    WIP: Added selection utils to revitutils
    Scripts can ask user to pick an element by using:
    from revitutils import selection
    selection.utils.pick_element()

--- pyrevitlib/revitutils/__init__.py
+++ pyrevitlib/revitutils/__init__.py
@@ -1,20 +1,39 @@
 from pyrevit import HOST_APP
 
+# noinspection PyUnresolvedReferences
+from Autodesk.Revit.DB import ElementId
+# noinspection PyUnresolvedReferences
+from Autodesk.Revit.UI.Selection import ObjectType
+
+class SelectionUtils:
+    def __init__(self, document, uidocument):
+        self._doc = document
+        self._uidoc = uidocument
+        self._uidoc_selection = self._uidoc.Selection
+
+    def pick_element(self, pick_message=''):
+        try:
+            return doc.GetElement(self._uidoc_selection.PickObject(ObjectType.Element, pick_message))
+        except:
+            return None
+
 
 class CurrentElementSelection:
     def __init__(self, document, uidocument):
-        self.doc = document
+        self._doc = document
         self._uidoc = uidocument
         self._uidoc_selection = self._uidoc.Selection
 
         self.element_ids = list(self._uidoc_selection.GetElementIds())
-        self.elements = [self.doc.GetElement(el_id) for el_id in self.element_ids]
+        self.elements = [self._doc.GetElement(el_id) for el_id in self.element_ids]
 
         self.count = len(self.element_ids)
         self.first = self.last = None
         if self.count > 0:
             self.first = self.elements[0]
             self.last = self.elements[self.count-1]
+
+        self.utils = SelectionUtils(self._doc, self._uidoc)
 
     @property
     def is_empty(self):
