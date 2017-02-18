eea1399d8c2e6940fcef4c51e4217c895bce09a6
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-17 18:40:26 -0800

    Revised log viewer filters and added command information

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/Logs.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/Logs.pushbutton/script.py
@@ -25,24 +25,33 @@ from System.Windows.Controls import ListViewItem
 
 
 log_entry_parser = re.compile('(\d{4}-\d{2}-\d{2})\s{1}(\d{2}:\d{2}:\d{2},\d{3})\s{1}(.*)\:\s{1}\[(.*?)\]\s{1}(.+)')
+logging_command_parser = re.compile('<(.*)>\s(.*)')
 
 
 class EntryFilter:
     type_id = ''
+    name_template = ''
 
-    def __init__(self, filter_type):
-        self.filter_type = filter_type
+    def __init__(self, filter_value):
+        self.filter_value = filter_value
+        self.filter_name = self.name_template.format(self.filter_value)
 
     def __eq__(self, other):
-        return self.filter_type == other.filter_type
+        return self.filter_value == other.filter_value
+
+    def __ne__(self, other):
+        return self.filter_value != other.filter_value
 
     def __hash__(self):
-        return hash((self.filter_type))
+        return hash(self.filter_value)
+
+    def __lt__(self, other):
+        return self.filter_value < other.filter_value
 
     def filter_entries(self, entry_list, search_term=None):
         filtered_list = []
         for entry in entry_list:
-            if getattr(entry, self.type_id) == self.filter_type:
+            if getattr(entry, self.type_id) == self.filter_value:
                 if search_term:
                     if search_term.lower() in entry.message.lower() \
                     or search_term.lower() in entry.clean_msg.lower():
@@ -54,8 +63,10 @@ class EntryFilter:
 
 class EntryNoneFilter(EntryFilter):
     type_id = 'none'
+
     def __init__(self):
-        self.filter_type = 'None'
+        self.filter_value = 'None'
+        self.filter_name = 'No Filter'
 
     def filter_entries(self, entry_list, search_term=None):
         if search_term:
@@ -69,26 +80,42 @@ class EntryNoneFilter(EntryFilter):
             return entry_list
 
 
-class EntryTypeFilter(EntryFilter):
-    type_id = 'type'
+class EntryLevelFilter(EntryFilter):
+    type_id = 'level'
+    name_template = 'Level: {}'
 
 
 class EntryDateFilter(EntryFilter):
     type_id = 'date'
+    name_template = 'Date: {}'
 
 
 class EntryModuleFilter(EntryFilter):
     type_id = 'module'
+    name_template = 'Module: {}'
+
+
+class EntryCommandFilter(EntryFilter):
+    type_id = 'command'
+    name_template = 'Command: {}'
 
 
 class LogMessageListItem(object):
     def __init__(self, log_entry):
         self._log_entry = log_entry
-        self.date, self.time, self.type, self.module, self.message = log_entry_parser.findall(self._log_entry)[0]
+        self.date, self.time, self.level, self.module, self.message = log_entry_parser.findall(self._log_entry)[0]
+        try:
+            self.command, self.module = logging_command_parser.findall(self.module)[0]
+        except Exception as err:
+            self.command = None
 
     @property
     def clean_msg(self):
         return self.message.replace('&clt;', '<').replace('&cgt;', '>')
+
+    @property
+    def is_command(self):
+        return self.command
 
 
 class LogViewerWindow(WPFWindow):
@@ -120,17 +147,20 @@ class LogViewerWindow(WPFWindow):
         entry_list = []
         log_file_line = 1
         prev_entry = None
-        with open(file_path, 'r') as log_file:
-            for log_entry in log_file.readlines():
-                try:
-                    new_entry = LogMessageListItem(log_entry)
-                    entry_list.append(new_entry)
-                    log_file_line += 1
-                    prev_entry = new_entry
-                except Exception as err:
-                    logger.debug('Error processing entry at {}:{}'.format(op.basename(file_path), log_file_line))
-                    prev_entry.message += log_entry
-                    log_file_line += 1
+        try:
+            with open(file_path, 'r') as log_file:
+                for log_entry in log_file.readlines():
+                    try:
+                        new_entry = LogMessageListItem(log_entry)
+                        entry_list.append(new_entry)
+                        log_file_line += 1
+                        prev_entry = new_entry
+                    except Exception as err:
+                        logger.debug('Error processing entry at {}:{}'.format(op.basename(file_path), log_file_line))
+                        prev_entry.message += log_entry
+                        log_file_line += 1
+        except Exception as read_err:
+            logger.error('Error reading log file: {} | {}'.format(file_path, read_err))
 
         return entry_list
 
@@ -146,15 +176,19 @@ class LogViewerWindow(WPFWindow):
         entry_types = set()
         entry_modules = set()
         entry_dates = set()
+        entry_commands = set()
 
         for log_entry in log_entry_list:
-            entry_types.add(EntryTypeFilter(log_entry.type))
+            entry_types.add(EntryLevelFilter(log_entry.level))
             entry_modules.add(EntryModuleFilter(log_entry.module))
             entry_dates.add(EntryDateFilter(log_entry.date))
+            if log_entry.command:
+                entry_commands.add(EntryCommandFilter(log_entry.command))
 
-        filter_list.extend(entry_types)
-        filter_list.extend(entry_modules)
-        filter_list.extend(entry_dates)
+        filter_list.extend(sorted(entry_types))
+        filter_list.extend(sorted(entry_modules))
+        filter_list.extend(sorted(entry_dates))
+        filter_list.extend(sorted(entry_commands))
 
         return filter_list
 
@@ -162,13 +196,14 @@ class LogViewerWindow(WPFWindow):
     # noinspection PyMethodMayBeStatic
     def log_file_changed(self, sender, args):
         self._current_entry_list = self._read_log_file(self._log_files[self.current_log_file])
-        filter_list = self._extract_filters(self._current_entry_list)
+        if self._current_entry_list:
+            filter_list = self._extract_filters(self._current_entry_list)
 
-        self.logitems_lb.ItemsSource = self._current_entry_list
-        self.logitems_lb.ScrollIntoView(self.logitems_lb.Items[0])
+            self.logitems_lb.ItemsSource = self._current_entry_list
+            self.logitems_lb.ScrollIntoView(self.logitems_lb.Items[0])
 
-        self.filter_cb.ItemsSource = filter_list
-        self.filter_cb.SelectedIndex = 0
+            self.filter_cb.ItemsSource = filter_list
+            self.filter_cb.SelectedIndex = 0
 
     # noinspection PyUnusedLocal
     # noinspection PyMethodMayBeStatic
