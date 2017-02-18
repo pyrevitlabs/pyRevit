66bd45fcf8fe21e03173c4582e49fa67820d0da5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-17 18:46:40 -0800

    Cleaned up reload tool

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/Reload.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/Reload.pushbutton/script.py
@@ -1,11 +1,7 @@
-from pyrevit.coreutils.logger import get_logger
+"""Searches the script folders and create buttons for the new script or newly installed extensions."""
+
+from scriptutils import logger
 from pyrevit.loader.sessionmgr import load_session
-
-# noinspection PyUnresolvedReferences
-logger = get_logger(__commandname__)
-
-__doc__ = 'Searches the script folders and create buttons for the new script or newly installed extensions.'
-
 
 # re-load pyrevit session.
 logger.info('Reloading....')
