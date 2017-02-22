7db6de55bec5a75d85f112619948e039cf864b66
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-21 17:39:31 -0800

    Code cleanups

--- pyrevitlib/revitutils/__init__.py
+++ pyrevitlib/revitutils/__init__.py
@@ -1,4 +1,5 @@
 from pyrevit import HOST_APP
+from pyrevit.coreutils.logger import get_logger
 
 # noinspection PyUnresolvedReferences
 from System.Collections.Generic import List
@@ -6,6 +7,9 @@ from System.Collections.Generic import List
 from Autodesk.Revit.DB import ElementId
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.UI.Selection import ObjectType
+
+
+logger = get_logger(__name__)
 
 
 class SelectionUtils:
@@ -17,6 +21,12 @@ class SelectionUtils:
     def pick_element(self, pick_message=''):
         try:
             return doc.GetElement(self._uidoc_selection.PickObject(ObjectType.Element, pick_message))
+        except:
+            return None
+
+    def pick_point(self, pick_message=''):
+        try:
+            return self._uidoc_selection.PickPoint(pick_message)
         except:
             return None
 
@@ -46,11 +56,10 @@ class CurrentElementSelection:
         return len(self.elements) == 0
 
 
-doc = uidoc = selection = None
 try:
     doc = HOST_APP.uiapp.ActiveUIDocument.Document
     uidoc = HOST_APP.uiapp.ActiveUIDocument
     all_docs = HOST_APP.uiapp.Application.Documents
     selection = CurrentElementSelection(doc, uidoc)
-except:
-    pass
+except Exception as ru_setup_err:
+    logger.error('Error setting up revitutils | {}'.format(ru_setup_err))
