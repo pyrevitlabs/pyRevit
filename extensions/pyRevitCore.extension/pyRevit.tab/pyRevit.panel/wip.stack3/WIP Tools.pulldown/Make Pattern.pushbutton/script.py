fe41901233e8c5e92cbc71cf9e72f8cd2becaac0
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-02-23 19:47:36 -0800

    WIP hatchmaker
    works on standard angles
    need to resolve angles and approximation

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/WIP Tools.pulldown/Make Pattern.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/WIP Tools.pulldown/Make Pattern.pushbutton/script.py
@@ -1,18 +1,46 @@
+from itertools import tee, izip
+
 from scriptutils import this_script, logger
 from revitutils import doc, selection, patmaker
 
 # noinspection PyUnresolvedReferences
-from Autodesk.Revit.DB import DetailLine, UnitType
+from Autodesk.Revit.DB import DetailLine, DetailArc, DetailEllipse, DetailNurbSpline
+# noinspection PyUnresolvedReferences
+from Autodesk.Revit.DB import Line, Arc, Ellipse, NurbSpline
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.UI import TaskDialog
 
 
-det_lines = []
+
+accpeted_lines = [DetailLine, DetailArc, DetailEllipse, DetailNurbSpline]
+accpeted_curves = [Arc, Ellipse, NurbSpline]
 
 
-for element in selection.elements:
-    if isinstance(element, DetailLine):
-        det_lines.append(element)
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+
+def make_pattern_line(rvt_start_p, rvt_end_p, rvt_origin):
+    relative_start_p = rvt_start_p - rvt_origin
+    relative_end_p = rvt_end_p - rvt_origin
+    start_p = patmaker.PatternPoint(relative_start_p.X, relative_start_p.Y)
+    end_p = patmaker.PatternPoint(relative_end_p.X, relative_end_p.Y)
+    return patmaker.PatternLine(start_p, end_p, line_id=det_line.Id.IntegerValue)
+
+
+def cleanup_selection():
+    lines = []
+    for element in selection.elements:
+        if type(element) in accpeted_lines:
+            lines.append(element)
+    return lines
+
+
+det_lines = cleanup_selection()
+
 
 if len(det_lines) > 0:
     # ask user for origin and max domain points
@@ -26,16 +54,20 @@ if len(det_lines) > 0:
             pat_lines = []
             for det_line in det_lines:
                 geom_curve = det_line.GeometryCurve
-                relative_start_p = geom_curve.GetEndPoint(0) - pat_bottomleft
-                relative_end_p = geom_curve.GetEndPoint(1) - pat_bottomleft
-                start_p = patmaker.PatternPoint(relative_start_p.X, relative_start_p.Y)
-                end_p = patmaker.PatternPoint(relative_end_p.X, relative_end_p.Y)
-                pat_lines.append(patmaker.PatternLine(start_p, end_p, line_id=det_line.Id.IntegerValue))
+                if type(geom_curve) in accpeted_curves:
+                    tes_points = [tp for tp in geom_curve.Tessellate()]
+                    for p1, p2 in pairwise(tes_points):
+                        pat_lines.append(make_pattern_line(p1, p2, pat_bottomleft))
+
+                elif isinstance(geom_curve, Line):
+                    pat_lines.append(make_pattern_line(geom_curve.GetEndPoint(0),
+                                                       geom_curve.GetEndPoint(1),
+                                                       pat_bottomleft))
 
             logger.debug('Pattern domain is: {}'.format(pat_domain))
             logger.debug('Pattern lines are: {}'.format(pat_lines))
 
-            patmaker.make_pattern('Test Pattern 6', pat_lines, pat_domain, model_pattern=True)
+            patmaker.make_pattern('Test Pattern 17', pat_lines, pat_domain, model_pattern=True)
 
 else:
     TaskDialog.Show('pyRevit', 'At least one Detail Line must be selected.')
--- pyrevitlib/revitutils/patmaker.py
+++ pyrevitlib/revitutils/patmaker.py
@@ -1,4 +1,4 @@
-from math import sqrt, cos, pi, tan
+from math import sqrt, pi, sin, cos, tan
 
 from pyrevit import PyRevitException
 from pyrevit.coreutils.logger import get_logger
@@ -14,21 +14,28 @@ from Autodesk.Revit.DB import Transaction, FillPattern, FillPatternElement, Fill
 logger = get_logger(__name__)
 
 
-RESOLUTION = 4
 PI = pi
-HALF_PI = pi/2
-MAX_SPAN_TRY = 20000
+HALF_PI = PI/2.0
+RESOLUTION = 4
+COORD_RESOLUTION = 10
 ZERO_TOL = 5 / 10.0**RESOLUTION
+MAX_TRY = 20000
+MAX_DOMAIN = 200
+MAX_JIGGLE = 16
 
 
 class CanNotDetermineSpanException(PyRevitException):
     pass
 
 
+class CanNotDetermineNextGridException(PyRevitException):
+    pass
+
+
 class PatternPoint:
     def __init__(self, u_point, v_point):
-        self.u = u_point
-        self.v = v_point
+        self.u = round(u_point, COORD_RESOLUTION)
+        self.v = round(v_point, COORD_RESOLUTION)
 
     def __repr__(self):
         return '<PatternPoint U:{} V:{}>'.format(self.u, self.v)
@@ -69,11 +76,11 @@ class PatternLine:
     def length(self):
         return abs(sqrt(self.direction.u**2 + self.direction.v**2))
 
-    def point_on_line(self, point):
+    def point_on_line(self, point, tolerance=0.0):
         a = self.start_point
         b = self.end_point
         c = point
-        if (a.u - c.u) * (b.v - c.v) - (a.v - c.v) * (b.u - c.u) == 0:
+        if 0.0 <= abs((a.u - c.u) * (b.v - c.v) - (a.v - c.v) * (b.u - c.u)) <= tolerance:
             return True
         else:
             return False
@@ -98,24 +105,86 @@ class PatternLine:
 
 class PatternGridAxis:
     def __init__(self, line, pat_domain):
-        self._init_line = line
+        # setting up domain bounds
         self._pat_domain = pat_domain
+        self._dom_diag = PatternLine(PatternPoint(0.0, 0.0), PatternPoint(self._pat_domain.u, self._pat_domain.v))
+        self._quarter_angle = self._dom_diag.angle
+        self._threequart_angle = PI - self._dom_diag.angle
 
-        start_point, end_point = self._intersect_with_domain(self._init_line)
-        self.axis = PatternLine(start_point, end_point)
-        self.angle = self.axis.angle
-        self.small_angle_u_side = (self.axis.angle <= HALF_PI/2)
+        # setup initial line to base axis on
+        self._init_line = line
+        self._pivot_point = self._init_line.center_point
 
-        self.span = self._get_span()
-        self.shift = self._get_shift()
-        self.offset = self._get_offset()
+        # set default values
+        self.tile_count_prep = self.tile_count_base = 1
+        self._shift_direction = self._offset_direction = 1
+
+        # try finding axis
+        try_count = 0
+        self._setup_axis()
+        while not self._determite_tri_params() and try_count < MAX_JIGGLE:
+            # if cant jiggle init line and try again
+            try_count += 1
+            self._jiggle_axis(try_count)
+            self._setup_axis()
+
+        if try_count >= MAX_JIGGLE:
+            raise PyRevitException('Can not find a decent approximate axis.')
+        else:
+            self.jiggle_count = try_count
 
         self.segment_lines = [self._init_line]
 
     def __repr__(self):
-        return '<PatternGridAxis Axis:{} Angle:{} Offset:{} Shift:{} ' \
-               'Span:{} Origin:{} Segments:{}>'.format(self.axis, self.angle, self.offset, self.shift,
-                                                       self.span, self.origin, self.segments)
+        return '<PatternGridAxis Axis:{} AxisAngle:{} uSide:{} ' \
+               'Angle:{} Offset:{} Shift:{} Jiggle:{} ' \
+               'Span:{} Tiles:{} Origin:{} Segments:{}>'.format(self.axis, self.axis.angle, self._is_small_angle_u_side(),
+                                                                self.angle, self.offset, self.shift, self.jiggle_count,
+                                                                self.span, self.tile_count_prep, self.origin, self.segments)
+
+    def _is_small_angle_u_side(self):
+        return (self.axis.angle <= self._quarter_angle) or (self.axis.angle >= self._threequart_angle)
+
+    def _is_almost_right_angle(self):
+        if self._is_small_angle_u_side():
+            return 0.0 <= self.axis.angle < ZERO_TOL or PI - ZERO_TOL < self.axis.angle <= PI
+        else:
+            return HALF_PI - ZERO_TOL < abs(HALF_PI - self.axis.angle) < HALF_PI + ZERO_TOL
+
+    def _determine_relative_vectors(self):
+        if self._is_small_angle_u_side():
+            self.dom_base_length = self._pat_domain.u
+            self.dom_prep_length = self._pat_domain.v
+
+            if self.axis.angle <= self._quarter_angle:
+                self._offset_direction = -1.0
+                self._shift_direction = 1.0
+                if self._is_almost_right_angle():
+                    self.angle = 0.0
+                else:
+                    self.angle = abs(self.axis.angle)
+            elif self.axis.angle >= self._threequart_angle:
+                self._offset_direction = 1.0
+                self._shift_direction = 1.0
+                if self._is_almost_right_angle():
+                    self.angle = 0.0
+                else:
+                    self.angle = abs(PI - self.axis.angle)
+        else:
+            self.dom_base_length = self._pat_domain.v
+            self.dom_prep_length = self._pat_domain.u
+
+            if self._quarter_angle < self.axis.angle < HALF_PI:
+                self._offset_direction = 1.0
+                self._shift_direction = 1.0
+            elif HALF_PI < self.axis.angle < self._threequart_angle:
+                self._offset_direction = -1.0
+                self._shift_direction = 1.0
+
+            if self._is_almost_right_angle():
+                self.angle = 0.0
+            else:
+                self.angle = abs(HALF_PI - self.axis.angle)
 
     def _intersect_with_domain(self, line):
         boundary_lines = [PatternLine(PatternPoint(0, 0), PatternPoint(self._pat_domain.u, 0)),
@@ -142,56 +211,108 @@ class PatternGridAxis:
         return sorted(extent_points)
 
     def _get_span(self):
-        if self.small_angle_u_side:
-            if 0.0 <= self.angle < ZERO_TOL:
-                return self._pat_domain.u
-            else:
-                small_angle_dom_length = self._pat_domain.u
-                large_angle_dom_length = self._pat_domain.v
-                inside_ang = HALF_PI - self.angle
-        else:
-            if 0.0 <= HALF_PI - self.angle < ZERO_TOL:
-                return self._pat_domain.v
-            else:
-                large_angle_dom_length = self._pat_domain.u
-                small_angle_dom_length = self._pat_domain.v
-                inside_ang = self.angle
+        if self.angle == 0.0:
+            return self.dom_base_length
+        elif round(self.angle, RESOLUTION) == round(self._quarter_angle, RESOLUTION):
+            return self._dom_diag.length
+
+        def calc_span_rem(rep_count):
+            dom_prep_rep = self.dom_prep_length * rep_count
+            return dom_prep_rep * tan(HALF_PI - self.angle)
+
+        tolerance = self.dom_base_length / 100.0
+        pos_tolerance = tolerance
+        neg_tolerance = self.dom_base_length - tolerance
+        def is_not_within_tolerance(dom_base_rep):
+            rem = dom_base_rep % self.dom_base_length
+            return not (neg_tolerance <= rem <= self.dom_base_length or 0.0 <= rem <= pos_tolerance)
 
         rep_count = 1
-        tolerance = small_angle_dom_length / 50.0
-        large_angle_side_rep = large_angle_dom_length * rep_count
-        small_angle_side_rep = large_angle_side_rep * tan(inside_ang)
-        while round(small_angle_side_rep, RESOLUTION) % small_angle_dom_length > tolerance and rep_count < MAX_SPAN_TRY:
+        while is_not_within_tolerance(calc_span_rem(rep_count)) and rep_count < MAX_TRY:
             rep_count += 1
-            large_angle_side_rep = large_angle_dom_length * rep_count
-            small_angle_side_rep = large_angle_side_rep * tan(inside_ang)
 
-        if rep_count < MAX_SPAN_TRY:
-            self.tile_count = rep_count
-            return abs(round(large_angle_side_rep / cos(inside_ang), RESOLUTION))
+        if rep_count < MAX_TRY:
+            self.tile_count_prep = rep_count
+            self.tile_count_base = round(calc_span_rem(rep_count) / self.dom_base_length, 0)
+            return abs(calc_span_rem(rep_count) / cos(self.angle))
         else:
             raise CanNotDetermineSpanException()
 
     def _get_offset(self):
-        if self.small_angle_u_side:
-            if 0.0 <= self.angle < ZERO_TOL:
-                return self._pat_domain.u
-            else:
-                return abs(round(self._pat_domain.u * cos(HALF_PI - self.angle), RESOLUTION))
-        else:
-            if 0.0 <= HALF_PI - self.angle < ZERO_TOL:
-                return self._pat_domain.v
-            else:
-                return abs(round(self._pat_domain.v * cos(PI - self.angle), RESOLUTION))
+        if self.angle == 0.0:
+            return self.dom_prep_length
+
+        # offset_value = abs(self.dom_base_length * self.tile_count_base * sin(self.angle) / self.tile_count_prep)
+        # return  offset_value * self._offset_direction
+        return  abs(self.dom_base_length * sin(self.angle) / self.tile_count_prep) * self._offset_direction
 
     def _get_shift(self):
-        if self.small_angle_u_side:
-            return -round(self._pat_domain.u * cos(self.angle), RESOLUTION)
+        def find_nxt_grid_point(offset_line, domain_u, domain_v, tol=5.0/10**RESOLUTION):
+            u_mult = 0
+            u_max = MAX_TRY
+            while u_mult < u_max:
+                for v_mult in range(0, MAX_TRY):
+                    grid_point = PatternPoint(domain_u * u_mult, domain_v * v_mult)
+                    if offset_line.point_on_line(grid_point, tolerance=tol):
+                        return grid_point
+                u_mult +=1
+            if u_mult >= u_max:
+                raise CanNotDetermineNextGridException()
+
+        offset_u = abs(self.offset * sin(self.angle))
+        offset_v = -abs(self.offset * cos(self.angle))
+        offset_vector_start = PatternPoint(self.axis.start_point.u + offset_u, self.axis.start_point.v + offset_v)
+        offset_vector_end = PatternPoint(self.axis.end_point.u + offset_u, self.axis.end_point.v + offset_v)
+        offset_vector = PatternLine(offset_vector_start, offset_vector_end)
+
+        if self.tile_count_prep > 1:
+            nxt_grid_point = find_nxt_grid_point(offset_vector,
+                                                 self.dom_base_length,
+                                                 self.dom_prep_length)
+
+            total_shift = offset_vector_start.distance_to(nxt_grid_point)
+            return total_shift * self._shift_direction
         else:
-            return -round(self._pat_domain.v * cos(HALF_PI - self.angle), RESOLUTION)
+            return abs(self.dom_base_length * cos(self.angle)) * self._shift_direction
+
+    def _setup_axis(self):
+        start_point, end_point = self._intersect_with_domain(self._init_line)
+        self.axis = PatternLine(start_point, end_point)
+        self._determine_relative_vectors()
+
+    def _determite_tri_params(self):
+        try:
+            self.span = self._get_span()
+            self.offset = self._get_offset()
+            self.shift = self._get_shift()
+            if self.span > MAX_DOMAIN or self.shift > MAX_DOMAIN:
+                logger.debug('Calculated span is too wide for line id: {} | {}'.format(self._init_line.id, self.span))
+                return False
+            else:
+                return True
+        except Exception as calc_err:
+            logger.debug('Error calculating tri params | {}'.format(calc_err))
+            return False
+
+    def _jiggle_axis(self, try_count):
+        def rotate_point(origin, point, angle):
+            qx = origin.u + cos(angle) * (point.u - origin.u) - sin(angle) * (point.v - origin.v)
+            qy = origin.v + sin(angle) * (point.u - origin.u) + cos(angle) * (point.v - origin.v)
+            return PatternPoint(qx, qy)
+
+        if try_count%2 == 0:
+            jiggle_mult = -try_count
+        else:
+            jiggle_mult = try_count
+
+        rotate_increment = 0.01 * jiggle_mult
+        new_start = rotate_point(self._init_line.center_point, self._init_line.start_point, rotate_increment)
+        new_end = rotate_point(self._init_line.center_point, self._init_line.end_point, rotate_increment)
+        self._init_line = PatternLine(new_start, new_end)
 
     def _overlap_line(self, pat_line):
         # see if pat_line overlaps, if yes:
+        return False
         start_check = self.axis.point_on_line(pat_line.start_point)
         end_check = self.axis.point_on_line(pat_line.end_point)
         if start_check and end_check:
@@ -223,6 +344,7 @@ class PatternGridAxis:
     @property
     def segments(self):
         line = self._get_merged_lines()[0]
+        # segment_list = [0.05, 0.05, line.length - 0.1, self.span - line.length]
         segment_list = [line.length, self.span - line.length]
         return segment_list
 
@@ -231,7 +353,7 @@ class PatternGridAxis:
 
     def get_fill_grid(self, scale):
         fg = FillGrid()
-        fg.Angle = self.angle
+        fg.Angle = self.axis.angle
         fg.Origin = UV(self.origin.u * scale, self.origin.v * scale)
         fg.Offset = self.offset * scale
         fg.Shift = self.shift * scale
@@ -266,8 +388,8 @@ def make_pattern(pat_name, line_list, pat_domain, model_pattern=True, dot_thresh
                 print new_axis
                 logger.debug('New pattern axis: {}'.format(new_axis))
                 grid_axes_list.append(new_axis)
-            except CanNotDetermineSpanException:
-                logger.error('Error determining span on line id: {}'.format(line.id))
+            except Exception as gridaxis_err:
+                logger.error('Error determining axis on line id: {} | {}'.format(line.id, gridaxis_err))
 
     # get list of FillGrids
     fill_grids = [seg.get_fill_grid(scale) for seg in grid_axes_list]
