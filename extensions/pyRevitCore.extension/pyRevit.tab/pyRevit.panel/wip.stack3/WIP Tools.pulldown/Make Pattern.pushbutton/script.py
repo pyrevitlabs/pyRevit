98c0f51b3b7fa3b6a1daccbf0b418ed0d208e3eb
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-01 16:53:31 -0800

    Cleaned up hatch maker front-end

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/WIP Tools.pulldown/Make Pattern.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/WIP Tools.pulldown/Make Pattern.pushbutton/script.py
@@ -1,4 +1,6 @@
-from itertools import tee, izip
+import re
+
+from pyrevit.coreutils import pairwise
 
 from scriptutils import this_script, logger
 from scriptutils.userinput import WPFWindow
@@ -12,15 +14,19 @@ from Autodesk.Revit.DB import Line, Arc, Ellipse, NurbSpline
 from Autodesk.Revit.UI import TaskDialog
 
 
-
 accpeted_lines = [DetailLine, DetailArc, DetailEllipse, DetailNurbSpline]
 accpeted_curves = [Arc, Ellipse, NurbSpline]
 
 
-
 class MakePatternWindow(WPFWindow):
-    def __init__(self, xaml_file_name):
+    def __init__(self, xaml_file_name, rvt_elements):
+        # cleanup selection (pick only acceptable curves)
+        self.selected_lines = self._cleanup_selection(rvt_elements)
+
+        # create pattern maker window and process options
         WPFWindow.__init__(self, xaml_file_name)
+        self.dottypes_cb.ItemsSource = patmaker.DOT_TYPES
+        self.dottypes_cb.SelectedIndex = 0
         self.pat_name_tb.Focus()
 
     @property
@@ -35,78 +41,78 @@ class MakePatternWindow(WPFWindow):
     def pat_name(self):
         return self.pat_name_tb.Text
 
+    @property
+    def allow_jiggle(self):
+        return self.allow_jiggle_cb.IsChecked
+
+    @property
+    def create_filledregion(self):
+        return self.createfilledregion_cb.IsChecked
+
+    @staticmethod
+    def _cleanup_selection(rvt_elements):
+        lines = []
+        for element in rvt_elements:
+            if type(element) in accpeted_lines:
+                lines.append(element)
+        return lines
+
+    def _pick_domain(self):
+        # ask user for origin and max domain points
+        pat_bottomleft = selection.utils.pick_point('Pick origin point (bottom-right corner of the pattern area):')
+        if pat_bottomleft:
+            pat_topright = selection.utils.pick_point('Pick top-right corner of the pattern area:')
+            if pat_topright:
+                return pat_bottomleft, pat_topright
+
+        return False
+
+    def _make_pattern_line(self, start_xyz, end_xyz):
+        return (start_xyz.X, start_xyz.Y), (end_xyz.X, end_xyz.Y)
+
+    def _create_pattern(self, domain):
+        pat_lines = []
+        for det_line in self.selected_lines:
+            geom_curve = det_line.GeometryCurve
+            if type(geom_curve) in accpeted_curves:
+                tes_points = [tp for tp in geom_curve.Tessellate()]
+                for xyz1, xyz2 in pairwise(tes_points):
+                    pat_lines.append(self._make_pattern_line(xyz1, xyz2))
+
+            elif isinstance(geom_curve, Line):
+                pat_lines.append(self._make_pattern_line(geom_curve.GetEndPoint(0), geom_curve.GetEndPoint(1)))
+
+        # patmaker.make_pattern(self.pat_name, pat_lines, domain, model_pattern=self.is_model_pat)
+        print(self.pat_name, pat_lines, domain, self.is_model_pat, self.allow_jiggle, self.create_filledregion)
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def open_help(self, sender, args):
+        pass
+
     # noinspection PyUnusedLocal
     # noinspection PyMethodMayBeStatic
     def make_pattern(self, sender, args):
-        self.Close()
+        if not self.pat_name:
+            TaskDialog.Show('pyRevit', 'Type a name for the pattern first')
+        elif not re.search('[a-zA-Z0-9]', self.pat_name):
+            TaskDialog.Show('pyRevit', 'Pattern name must have at least one character or digit')
+        else:
+            self.Hide()
+            domain = self._pick_domain()
+            if domain:
+                self._create_pattern(domain)
+            self.Close()
 
     # noinspection PyUnusedLocal
     # noinspection PyMethodMayBeStatic
     def fix_angles(self, sender, args):
-        self.Close()
-
-
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return izip(a, b)
-
-
-def make_pattern_line(rvt_start_p, rvt_end_p, rvt_origin, id):
-    relative_start_p = rvt_start_p - rvt_origin
-    relative_end_p = rvt_end_p - rvt_origin
-    start_p = patmaker.PatternPoint(relative_start_p.X, relative_start_p.Y)
-    end_p = patmaker.PatternPoint(relative_end_p.X, relative_end_p.Y)
-    return patmaker.PatternLine(start_p, end_p, line_id=id)
-
-
-def cleanup_selection():
-    lines = []
-    for element in selection.elements:
-        if type(element) in accpeted_lines:
-            lines.append(element)
-    return lines
-
-
-def create_pattern(det_lines, pat_name, is_model_pat):
-    # ask user for origin and max domain points
-    pat_bottomleft = selection.utils.pick_point('Pick origin point (bottom-right corner of the pattern area):')
-    if pat_bottomleft:
-        pat_topright = selection.utils.pick_point('Pick top-right corner of the pattern area:')
-        if pat_topright:
-            print('Calculating safe angles for this tile size...')
-            print('Maximum pattern size is {}'.format(patmaker.MAX_DOMAIN))
-            domain = pat_topright - pat_bottomleft
-            pat_domain = patmaker.PatternDomain(domain.X, domain.Y)
-            print('Pattern domain is {0:.6f} x {1:.6f}'.format(pat_domain.max_u, pat_domain.max_v))
-            print pat_domain
-
-            pat_lines = []
-            for det_line in det_lines:
-                geom_curve = det_line.GeometryCurve
-                if type(geom_curve) in accpeted_curves:
-                    tes_points = [tp for tp in geom_curve.Tessellate()]
-                    for p1, p2 in pairwise(tes_points):
-                        pat_lines.append(make_pattern_line(p1, p2, pat_bottomleft, det_line.Id.IntegerValue))
-
-                elif isinstance(geom_curve, Line):
-                    pat_lines.append(make_pattern_line(geom_curve.GetEndPoint(0),
-                                                       geom_curve.GetEndPoint(1),
-                                                       pat_bottomleft,
-                                                       det_line.Id.IntegerValue))
-
-            logger.debug('Pattern domain is: {}'.format(pat_domain))
-            logger.debug('Pattern lines are: {}'.format(pat_lines))
-
-            patmaker.make_pattern(pat_name, pat_lines, pat_domain, model_pattern=is_model_pat)
+        # self.Close()
+        TaskDialog.Show('pyRevit', 'Work in progress...')
 
 
 if __name__ == '__main__':
-    det_lines = cleanup_selection()
-    if len(det_lines) > 0:
-        pat_info = MakePatternWindow('MakePatternWindow.xaml')
-        pat_info.ShowDialog()
-        create_pattern(det_lines, pat_info.pat_name, pat_info.is_model_pat)
+    if len(selection.elements) > 0:
+        MakePatternWindow('MakePatternWindow.xaml', selection.elements).ShowDialog()
     else:
         TaskDialog.Show('pyRevit', 'At least one Detail Line must be selected.')
