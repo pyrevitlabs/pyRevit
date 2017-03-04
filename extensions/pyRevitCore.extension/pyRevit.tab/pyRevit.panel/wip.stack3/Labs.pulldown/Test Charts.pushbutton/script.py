a89621e15c0aad9f20cd41c3aa98dfe9378c6fa6
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-04 13:13:59 -0800

    Added charts tests

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/Labs.pulldown/Test Charts.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/wip.stack3/Labs.pulldown/Test Charts.pushbutton/script.py
@@ -1,129 +1,94 @@
-import os
-import os.path as op
-from pyrevit import MAIN_LIB_DIR
+from pyrevit.coreutils.console import charts
 
 from scriptutils import this_script
 this_script.output.set_width(500)
-
-def get_head():
-    return this_script.output.__winhandle__.txtStdOut.Document.GetElementsByTagName('head')[0]
-
-
-def make_script_element(script):
-    scr_el = this_script.output.__winhandle__.txtStdOut.Document.CreateElement("<script></script>")
-    if script:
-        scr_el.InnerHtml = script
-    return scr_el
-
-
-def make_canvas_element(canvas_id):
-    print('\nTesting {}...'.format(canvas_id))
-    canvas = """<canvas id="{}"></canvas><br/>""".format(canvas_id)
-    this_script.output.print_html(canvas)
 
 
 print('Testing Charts.js charts...')
 
 
-head = get_head()
-# setup charts.js header
-charts_sc_el = make_script_element('')
-charts_path = op.join(MAIN_LIB_DIR, 'pyrevit', 'coreutils', 'console', 'charts', 'Chart.js')
-charts_sc_el.SetAttribute('src', charts_path)
-head.AppendChild(charts_sc_el)
+test1_types = [charts.LINE_CHART,
+               charts.BAR_CHART,
+               charts.RADAR_CHART]
+
+test2_types = [charts.BUBBLE_CHART]
+
+test3_types = [charts.POLAR_CHART,
+               charts.PIE_CHART,
+               charts.DOUGHNUT_CHART]
 
 
-make_canvas_element('linechart')
-line_chart_script = """
-var ctx = document.getElementById('linechart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    datasets: [{
-      label: 'apples',
-      data: [12, 19, 3, 17, 6, 3, 7],
-      backgroundColor: "rgba(153,255,51,0.4)"
-    }, {
-      label: 'oranges',
-      data: [2, 29, 5, 5, 2, 3, 10],
-      backgroundColor: "rgba(255,153,0,0.4)"
-    }]
-  }
-});
-"""
-head.AppendChild(make_script_element(line_chart_script))
+def get_test_chart(chart_type):
+    print('\nTesting {} charts...'.format(chart_type))
+    chart = this_script.output.make_chart()
+    chart.type = chart_type
+    return chart
 
 
-make_canvas_element('barchart')
-bar_chart_script = """
-var ctx = document.getElementById("barchart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["M", "T", "W", "R", "F", "S", "S"],
-    datasets: [{
-      label: 'apples',
-      data: [12, 19, 3, 17, 28, 24, 7],
-      backgroundColor: "rgba(153,255,51,0.4)"
-    }, {
-      label: 'oranges',
-      data: [30, 29, 5, 5, 20, 3, 10],
-      backgroundColor: "rgba(255,153,0,0.4)"
-    }]
-  }
-});
-"""
-head.AppendChild(make_script_element(bar_chart_script))
+def test1_chart(chart_type):
+    chart = get_test_chart(chart_type)
+
+    chart.data.labels = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+
+    apples = chart.data.new_dataset('apples')
+    apples.data = [12, 19, 3, 17, 6, 3, 7]
+    apples.set_background(153, 255, 51, 0.4)
+
+    oranges = chart.data.new_dataset('oranges')
+    oranges.data = [2, 29, 5, 5, 2, 3, 10]
+    oranges.set_background(255, 153, 0, 0.4)
+    oranges.fill = False
+
+    pears = chart.data.new_dataset('pears')
+    pears.data = [55, 12, 2, 20, 18, 6, 22]
+    pears.set_background(0, 153, 255, 0.4)
+
+    chart.draw()
 
 
-make_canvas_element('polarchart')
-polar_chart_script = """
-var ctx = document.getElementById("polarchart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'polarArea',
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f",
-        "#e74c3c",
-        "#34495e"
-      ],
-      data: [12, 19, 3, 17, 28, 24, 7]
-    }]
-  }
-});
-"""
-head.AppendChild(make_script_element(polar_chart_script))
+def test2_chart(chart_type):
+    chart = get_test_chart(chart_type)
+
+    apples = chart.data.new_dataset('apples')
+    apples.data = [{'x': 1, 'y': 2, 'r': 3}, {'x': 4, 'y': 5, 'r': 6}, {'x': 7, 'y': 8, 'r': 9}]
+    apples.set_background(153, 255, 51, 0.4)
+
+    oranges = chart.data.new_dataset('oranges')
+    oranges.data = [{'x': 10, 'y': 11, 'r': 12}, {'x': 13, 'y': 14, 'r': 15}, {'x': 16, 'y': 17, 'r': 18}]
+    oranges.set_background(255, 153, 0, 0.4)
+
+    pears = chart.data.new_dataset('pears')
+    pears.data = [{'x': 19, 'y': 20, 'r': 21}, {'x': 22, 'y': 23, 'r': 24}]
+    pears.set_background(0, 153, 255, 0.4)
+
+    chart.draw()
 
 
-make_canvas_element('piechart')
-pie_chart_script = """
-var ctx = document.getElementById("piechart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f",
-        "#e74c3c",
-        "#34495e"
-      ],
-      data: [12, 19, 3, 17, 28, 24, 7]
-    }]
-  }
-});
-"""
-head.AppendChild(make_script_element(pie_chart_script))
+def test3_chart(chart_type):
+    chart = get_test_chart(chart_type)
 
-# this_script.output.__winhandle__.txtStdOut.Document.InvokeScript("drawStuff")
+    chart.data.labels = ['Red', 'Blue', 'Green']
+
+    apples = chart.data.new_dataset('apples')
+    apples.data = [300, 20, 50]
+    apples.backgroundColor = ["#FF6384", "#36A2EB", "#FFCE56"]
+
+    oranges = chart.data.new_dataset('oranges')
+    oranges.data = [50, 30, 80]
+    oranges.backgroundColor = ["#FF6384", "#36A2EB", "#FFCE56"]
+
+    pears = chart.data.new_dataset('pears')
+    pears.data = [40, 20, 10]
+    pears.backgroundColor = ["#FF6384", "#36A2EB", "#FFCE56"]
+
+    chart.draw()
+
+
+for ct_type in test1_types:
+    test1_chart(ct_type)
+
+for ct_type in test2_types:
+    test2_chart(ct_type)
+
+for ct_type in test3_types:
+    test3_chart(ct_type)
