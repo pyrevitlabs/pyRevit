c4d1568eb8fac08900cc2f43df478e909f37a648
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-11 13:45:08 -0800

    Created command tool to upgrade pyRevit

---
+++ release/pyRevitCoreUpdater/pyRevitCoreUpdater/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace pyRevitCoreUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new Repository(args[0]);
+            PullOptions options = new PullOptions();
+            options.FetchOptions = new FetchOptions();
+            Commands.Pull(repo, new Signature("pyRevitCoreUpdater", "eirannejad@gmail.com", new DateTimeOffset(DateTime.Now)), options);
+        }
+    }
+}
