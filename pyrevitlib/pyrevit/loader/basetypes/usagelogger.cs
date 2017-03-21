2128b595d7539b433bbd3c7fd59c432a8db38e86
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-21 16:41:04 -0700

    WIP Usage logging to server in good shape

--- pyrevitlib/pyrevit/loader/basetypes/usagelogger.cs
+++ pyrevitlib/pyrevit/loader/basetypes/usagelogger.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Reflection;
 using System.Linq;
+using System.Net;
+using System.IO;
 using System.Windows.Forms;
 using Autodesk.Revit;
 using Autodesk.Revit.UI;
@@ -38,14 +40,60 @@ namespace PyRevitBaseClasses
             _pyRevitVersion = pyRevitVersion;
         }
 
+        public string MakeFileLogEntry()
+        {
+            return String.Format("{0}, {1}, {2}:{3}, {4}, {5}, {6}, {7}, {8}, {9}",
+                   DateTime.Now.ToString("yyyy/MM/dd@HH:mm:ss:ffff"),
+                   _username, _revitVerNum, _revitBuild, _pyRevitVersion,
+                   _forcedDebugMode, _altScriptMode, _cmdName, _execResult, _scriptSource);
+        }
+
+        public string MakeJSONLogEntry()
+        {
+            return '{' +
+                   String.Format("\"date\":\"{0}\"", DateTime.Now.ToString("yyyy/MM/dd")) +
+                   String.Format(", \"time\":\"{0}\"", DateTime.Now.ToString("HH:mm:ss:ffff")) +
+                   String.Format(", \"username\":\"{0}\"", _username) +
+                   String.Format(", \"revit\":\"{0}\"", _revitVerNum) +
+                   String.Format(", \"revitbuild\":\"{0}\"", _revitBuild) +
+                   String.Format(", \"pyrevit\":\"{0}\"", _pyRevitVersion) +
+                   String.Format(", \"debug\":\"{0}\"", _forcedDebugMode) +
+                   String.Format(", \"alternate\":\"{0}\"", _altScriptMode) +
+                   String.Format(", \"name\":\"{0}\"", _cmdName) +
+                   String.Format(", \"result\":\"{0}\"", _execResult) +
+                   String.Format(", \"source\":\"{0}\"", _scriptSource.Replace("\\", "\\\\")) +
+                   '}';
+        }
+
+        public void PostUsageLogToServer(string serverUrl)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(serverUrl);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = MakeJSONLogEntry();
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+
+        public void WriteUsageLogToFile(string logFilePath)
+        {
+        }
+
         public void LogUsage()
         {
-            string timeStamp = DateTime.Now.ToString("yyyy/MM/dd@HH:mm:ss:ffff");
-            string logEntry = String.Format("{0}, {1}, {2}:{3}, {4}, {5}, {6}, {7}, {8}, {9}",
-                timeStamp, _username, _revitVerNum, _revitBuild, _pyRevitVersion,
-                _forcedDebugMode, _altScriptMode, _cmdName, _execResult, _scriptSource);
-
-            // MessageBox.Show(logEntry, "Log Entry");
+            // PostUsageLogToServer(" ");
         }
     }
 }
