f7bc0a76d6451d39330ee6e17c261478171f1416
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-22 16:48:39 -0700

    Revised usage logger to properly json serialize an entry object

--- pyrevitlib/pyrevit/loader/basetypes/usagelogger.cs
+++ pyrevitlib/pyrevit/loader/basetypes/usagelogger.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Web.Script.Serialization;
 using System.IO;
 using System.Windows.Forms;
 using Autodesk.Revit;
@@ -14,79 +15,80 @@ using IronPython.Runtime;
 
 namespace PyRevitBaseClasses
 {
+    public class LogEntry
+    {
+        public string date;
+        public string time;
+        public string username;
+        public string revit;
+        public string revitbuild;
+        public int sessionid;
+        public string pyrevit;
+        public bool debug;
+        public bool alternate;
+        public string commandname;
+        public int resultcode;
+        public Dictionary<String, String> commandresults;
+        public string scriptpath;
+
+        public LogEntry(string revitUsername, string revitVersion, string revitBuild, int revitProcessId,
+                        string pyRevitVersion, bool debugModeEnabled, bool alternateModeEnabled,
+                        string pyRevitCommandName, int executorResultCode, ref Dictionary<String, String> resultDict,
+                        string pyRevitCommandPath)
+        {
+            // date = logDate;
+            // time = logTime;
+            username = revitUsername;
+            revit = revitVersion;
+            revitbuild = revitBuild;
+            sessionid = revitProcessId;
+            pyrevit = pyRevitVersion;
+            debug = debugModeEnabled;
+            alternate = alternateModeEnabled;
+            commandname = pyRevitCommandName;
+            resultcode = executorResultCode;
+            commandresults = resultDict;
+            scriptpath = pyRevitCommandPath;
+        }
+    }
+
     public class ScriptUsageLogger
     {
         private readonly UIApplication _revit;
-        public string _cmdName = "";
-        public string _scriptSource = "";
-        public string _revitVerNum = "";
-        public string _revitBuild = "";
-        public string _username = "";
-        public string _pyRevitVersion = "";
-        public string _usageLogFilePath = "";
-        public string _serverUrl = "";
-        public bool _altScriptMode = false;
-        public bool _forcedDebugMode = false;
-        public int _execResult = 0;
-        public int _processid = 0;
-        public Dictionary<String, String> _resultDict;
+        public LogEntry _entry;
+        public string _usageLogFilePath;
+        public string _usageLogServerUrl;
 
         public ScriptUsageLogger(ExternalCommandData commandData,
                                  string cmdName, string scriptSource,
                                  bool forcedDebugMode, bool altScriptMode, int execResult,
                                  ref Dictionary<String, String> resultDict)
         {
-            _cmdName = cmdName;
-            _scriptSource = scriptSource;
             _revit = commandData.Application;
-            _revitVerNum = _revit.Application.VersionNumber;
-            _revitBuild = _revit.Application.VersionBuild;
-            _processid = Process.GetCurrentProcess().Id;
-            _username = _revit.Application.Username;
-            _forcedDebugMode = forcedDebugMode;
-            _altScriptMode = altScriptMode;
-            _execResult = execResult;
-            _resultDict = resultDict;
 
             // get live data from python dictionary saved in appdomain
             var envdict = new EnvDictionary();
-            _pyRevitVersion = envdict.GetPyRevitVersion();
             _usageLogFilePath = envdict.GetUsageLogFilePath();
-            _serverUrl = envdict.GetUsageLogFilePath();
+            _usageLogServerUrl = envdict.GetUsageLogServerUrl();
+
+            _entry = new LogEntry(_revit.Application.Username,
+                                  _revit.Application.VersionNumber, _revit.Application.VersionBuild,
+                                  Process.GetCurrentProcess().Id, envdict.GetPyRevitVersion(),
+                                  forcedDebugMode, altScriptMode, cmdName, execResult, ref resultDict,
+                                  scriptSource.Replace("\\", "\\\\"));
         }
 
         public string MakeJSONLogEntry()
         {
-            // Create json package and add the standard log data
-            var json_log_pkg =  '{' +
-                   String.Format("\"date\":\"{0}\"", DateTime.Now.ToString("yyyy/MM/dd")) +
-                   String.Format(", \"time\":\"{0}\"", DateTime.Now.ToString("HH:mm:ss:ffff")) +
-                   String.Format(", \"username\":\"{0}\"", _username) +
-                   String.Format(", \"revit\":\"{0}\"", _revitVerNum) +
-                   String.Format(", \"revitbuild\":\"{0}\"", _revitBuild) +
-                   String.Format(", \"sessionid\":\"{0}\"", _processid) +
-                   String.Format(", \"pyrevit\":\"{0}\"", _pyRevitVersion) +
-                   String.Format(", \"debug\":\"{0}\"", _forcedDebugMode) +
-                   String.Format(", \"alternate\":\"{0}\"", _altScriptMode) +
-                   String.Format(", \"commandname\":\"{0}\"", _cmdName) +
-                   String.Format(", \"result\":\"{0}\"", _execResult) +
-                   String.Format(", \"source\":\"{0}\"", _scriptSource.Replace("\\", "\\\\"));
+            _entry.date = DateTime.Now.ToString("yyyy/MM/dd");
+            _entry.time = DateTime.Now.ToString("HH:mm:ss:ffff");
 
-           foreach(KeyValuePair<string, string> entry in _resultDict)
-           {
-               // Add custom script data to log package
-               json_log_pkg += String.Format(", \"{0}\":\"{1}\"", entry.Key, entry.Value);
-           }
-
-           // Close the json package
-           json_log_pkg += '}';
-
-           return json_log_pkg;
+             return new JavaScriptSerializer().Serialize(_entry);
         }
 
-        public void PostUsageLogToServer()
+        public void PostUsageLogToServer(string usageLogServerUrl)
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(_serverUrl);
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(usageLogServerUrl);
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
 
@@ -106,13 +108,17 @@ namespace PyRevitBaseClasses
             }
         }
 
-        public void WriteUsageLogToFile()
+        public void WriteUsageLogToFile(string usageLogFilePath)
         {
+            string json = MakeJSONLogEntry();
         }
 
         public void LogUsage()
         {
-
+            if(!String.IsNullOrEmpty(_usageLogServerUrl))
+                PostUsageLogToServer(_usageLogServerUrl);
+            if(!String.IsNullOrEmpty(_usageLogServerUrl))
+                WriteUsageLogToFile(_usageLogFilePath);
         }
     }
 }
