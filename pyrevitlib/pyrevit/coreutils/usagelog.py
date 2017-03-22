a6c5664591b3fc9e5825d0f0ac0341a1e6de1a7e
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-22 15:15:58 -0700

    Revised usagelog to accept custom file and server values

--- pyrevitlib/pyrevit/coreutils/usagelog.py
+++ pyrevitlib/pyrevit/coreutils/usagelog.py
@@ -2,6 +2,7 @@ import os.path as op
 
 from pyrevit import EXEC_PARAMS
 from pyrevit import PYREVIT_ADDON_NAME, PYREVIT_VERSION_APP_DIR, PYREVIT_FILE_PREFIX_STAMPED
+from pyrevit.coreutils import is_blank
 from pyrevit.coreutils.logger import get_logger
 from pyrevit.coreutils.envvars import set_pyrevit_env_var
 
@@ -9,12 +10,14 @@ from pyrevit.coreutils.envvars import set_pyrevit_env_var
 logger = get_logger(__name__)
 
 
-FILELOG_ISC_NAME = PYREVIT_ADDON_NAME + '_usagelogISC'
+FILELOG_ISC_NAME = PYREVIT_ADDON_NAME + '_usagelogfileISC'
+SERVERLOG_ISC_NAME = PYREVIT_ADDON_NAME + '_usagelogserverISC'
 
 FILE_LOG_FILENAME = '{}_usage.log'.format(PYREVIT_FILE_PREFIX_STAMPED)
 FILE_LOG_FILEPATH = op.join(PYREVIT_VERSION_APP_DIR, FILE_LOG_FILENAME)
 
-STANDARD_USAGE_LOG_PARAMS = ['time', 'username', 'revit', 'revitbuild', 'pyrevit', 'debug',
+
+STANDARD_USAGE_LOG_PARAMS = ['time', 'username', 'revit', 'revitbuild', 'sessionid', 'pyrevit', 'debug',
                              'alternate', 'commandname', 'result', 'source']
 
 
@@ -37,5 +40,13 @@ class CommandCustomResults:
             EXEC_PARAMS.result_dict.Add(key, value)
 
 
-def setup_usage_logfile():
-    set_pyrevit_env_var(FILELOG_ISC_NAME, FILE_LOG_FILEPATH)
+def setup_usage_logfile(logfilepath=None, logserverurl=None):
+    if not logfilepath or is_blank(logfilepath):
+        set_pyrevit_env_var(FILELOG_ISC_NAME, FILE_LOG_FILEPATH)
+    else:
+        set_pyrevit_env_var(FILELOG_ISC_NAME, logfilepath)
+
+    if not logserverurl or is_blank(logserverurl):
+        set_pyrevit_env_var(SERVERLOG_ISC_NAME, '')
+    else:
+        set_pyrevit_env_var(SERVERLOG_ISC_NAME, logserverurl)
