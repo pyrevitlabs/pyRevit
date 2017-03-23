955d3da18fcdaae2ba0e0197ff77a7013050d530
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-23 15:04:04 -0700

    Added url verification method

--- pyrevitlib/pyrevit/coreutils/__init__.py
+++ pyrevitlib/pyrevit/coreutils/__init__.py
@@ -492,3 +492,15 @@ def is_blank(input_string):
     if input_string and input_string.strip():
         return False
     return True
+
+
+def is_url_valid(url_string):
+    regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return regex.match(url_string)
