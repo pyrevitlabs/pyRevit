3152fab1762fefd55a970249f693158e7222b1b2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-24 14:09:17 -0700

    Added SelectFromCheckBoxes to default userinput windows

--- pyrevitlib/scriptutils/userinput.py
+++ pyrevitlib/scriptutils/userinput.py
@@ -21,6 +21,8 @@ from System.Windows.Forms import FolderBrowserDialog, DialogResult, OpenFileDial
 # noinspection PyUnresolvedReferences
 from System.Windows.Controls import SelectionMode
 # noinspection PyUnresolvedReferences
+from System.Windows.Media import SolidColorBrush, Color
+# noinspection PyUnresolvedReferences
 from System.Windows.Media.Imaging import BitmapImage
 # noinspection PyUnresolvedReferences
 from System.IO import StringReader
@@ -62,7 +64,36 @@ class WPFWindow(Window):
             wpf_element.Visibility = System.Windows.Visibility.Visible
 
 
-class SelectFromList(WPFWindow):
+class TemplateUserInputWindow(WPFWindow):
+    layout = """
+    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+            ShowInTaskbar="False" ResizeMode="NoResize"
+            WindowStartupLocation="CenterScreen" HorizontalContentAlignment="Center">
+    </Window>
+    """
+    def __init__(self, context, title, width, height, **kwargs):
+        WPFWindow.__init__(self, self.layout, literal_string=True)
+        self.Title = title
+        self.Width = width
+        self.Height = height
+
+        self._context = context
+        self.response = None
+
+        self._setup(**kwargs)
+
+    def _setup(self, **kwargs):
+        pass
+
+    @classmethod
+    def show(cls, context, title='User Input', width=300, height=400, **kwargs):
+        dlg = cls(context, title, width, height, **kwargs)
+        dlg.ShowDialog()
+        return dlg.response
+
+
+class SelectFromList(TemplateUserInputWindow):
     layout = """
     <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -74,49 +105,47 @@ class SelectFromList(WPFWindow):
                 </Style>
             </Window.Resources>
             <DockPanel Margin="10">
-                <WrapPanel DockPanel.Dock="Top" Margin="0,0,0,10">
-                    <TextBlock FontSize="14" Margin="0,3,10,0">Filter:</TextBlock>
-                    <TextBox x:Name="search_tb" Width="220px" Height="25px" TextChanged="search_txt_changed"/>
-                    <Button Style="{StaticResource ClearButton}"
-                            x:Name="clrsearch_b" Content="x" Margin="-25,0,0,0" Padding="0,-4,0,0"
-                            Click="clear_search" Width="15px" Height="15px"/>
-                </WrapPanel>
+                <DockPanel DockPanel.Dock="Top" Margin="0,0,0,10">
+                    <TextBlock FontSize="14" Margin="0,3,10,0" DockPanel.Dock="Left">Filter:</TextBlock>
+                    <StackPanel>
+                        <TextBox x:Name="search_tb" Height="25px" TextChanged="search_txt_changed"/>
+                        <Button Style="{StaticResource ClearButton}" HorizontalAlignment="Right"
+                                x:Name="clrsearch_b" Content="x" Margin="0,-25,5,0" Padding="0,-4,0,0"
+                                Click="clear_search" Width="15px" Height="15px"/>
+                    </StackPanel>
+                </DockPanel>
                 <Button Content="Select" Click="button_select" DockPanel.Dock="Bottom" Margin="0,10,0,0"/>
                 <ListView x:Name="list_lb" />
             </DockPanel>
     </Window>
     """
-    def __init__(self, option_list, title, width, height, multiselect):
-        WPFWindow.__init__(self, SelectFromList.layout, literal_string=True)
-        self.Title = title
-        self.Width = width
-        self.Height = height
-
-        self._option_list = option_list
-
+    def _setup(self, **kwargs):
         self.hide_element(self.clrsearch_b)
         self.clear_search(None, None)
         self.search_tb.Focus()
 
-        self.list_lb.SelectionMode = SelectionMode.Extended if multiselect else SelectionMode.Single
+        if 'multiselect' in kwargs and not kwargs['multiselect']:
+            self.list_lb.SelectionMode = SelectionMode.Single
+        else:
+            self.list_lb.SelectionMode = SelectionMode.Extended
+
         self._list_options()
-        self.selected = None
 
     def _list_options(self, option_filter=None):
         if option_filter:
             option_filter = option_filter.lower()
-            self.list_lb.ItemsSource = [str(option) for option in self._option_list
+            self.list_lb.ItemsSource = [str(option) for option in self._context
                                                     if option_filter in str(option).lower()]
         else:
-            self.list_lb.ItemsSource = [str(option) for option in self._option_list]
+            self.list_lb.ItemsSource = [str(option) for option in self._context]
 
     def _get_options(self):
-        return [option for option in self._option_list if str(option) in self.list_lb.SelectedItems]
+        return [option for option in self._context if str(option) in self.list_lb.SelectedItems]
 
     # noinspection PyUnusedLocal
     # noinspection PyMethodMayBeStatic
     def button_select(self, sender, args):
-        self.selected = self._get_options()
+        self.response = self._get_options()
         self.Close()
 
     # noinspection PyUnusedLocal
@@ -134,13 +163,138 @@ class SelectFromList(WPFWindow):
     def clear_search(self, sender, args):
         self.search_tb.Text = ' '
         self.search_tb.Clear()
-        self.list_lb.ItemsSource = self._option_list
+        self.list_lb.ItemsSource = self._context
 
-    @classmethod
-    def show(cls, option_list, title='Select from list', width=300, height=400, multiselect=True):
-        dlg = cls(option_list, title, width, height, multiselect)
-        dlg.ShowDialog()
-        return dlg.selected
+
+class SelectFromCheckBoxes(TemplateUserInputWindow):
+    layout = """
+    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+            ShowInTaskbar="False" ResizeMode="NoResize"
+            WindowStartupLocation="CenterScreen" HorizontalContentAlignment="Center">
+            <Window.Resources>
+                <Style x:Key="ClearButton" TargetType="Button">
+                    <Setter Property="Background" Value="White"/>
+                </Style>
+            </Window.Resources>
+            <DockPanel Margin="10">
+                <DockPanel DockPanel.Dock="Top" Margin="0,0,0,10">
+                    <TextBlock FontSize="14" Margin="0,3,10,0" DockPanel.Dock="Left">Filter:</TextBlock>
+                    <StackPanel>
+                        <TextBox x:Name="search_tb" Height="25px" TextChanged="search_txt_changed"/>
+                        <Button Style="{StaticResource ClearButton}" HorizontalAlignment="Right"
+                                x:Name="clrsearch_b" Content="x" Margin="0,-25,5,0" Padding="0,-4,0,0"
+                                Click="clear_search" Width="15px" Height="15px"/>
+                    </StackPanel>
+                </DockPanel>
+                <StackPanel DockPanel.Dock="Bottom">
+                    <Grid>
+                        <Grid.RowDefinitions>
+                            <RowDefinition Height="Auto" />
+                        </Grid.RowDefinitions>
+                        <Grid.ColumnDefinitions>
+                            <ColumnDefinition Width="*" />
+                            <ColumnDefinition Width="*" />
+                            <ColumnDefinition Width="*" />
+                        </Grid.ColumnDefinitions>
+                        <Button x:Name="checkall_b" Grid.Column="0" Grid.Row="0"
+                                Content="Check" Click="check_all" Margin="0,10,3,0"/>
+                        <Button x:Name="uncheckall_b" Grid.Column="1" Grid.Row="0"
+                                Content="Uncheck" Click="uncheck_all" Margin="3,10,3,0"/>
+                        <Button x:Name="toggleall_b" Grid.Column="2" Grid.Row="0"
+                                Content="Toggle" Click="toggle_all" Margin="3,10,0,0"/>
+                    </Grid>
+                    <Button x:Name="select_b" Content="" Click="button_select" Margin="0,10,0,0"/>
+                </StackPanel>
+                <ListView x:Name="list_lb">
+                    <ListView.ItemTemplate>
+                         <DataTemplate>
+                           <StackPanel>
+                             <CheckBox Content="{Binding name}" IsChecked="{Binding state}"/>
+                           </StackPanel>
+                         </DataTemplate>
+                   </ListView.ItemTemplate>
+                </ListView>
+            </DockPanel>
+    </Window>
+    """
+    def _setup(self, **kwargs):
+        self.hide_element(self.clrsearch_b)
+        self.clear_search(None, None)
+        self.search_tb.Focus()
+
+        if 'button_name' in kwargs:
+            self.select_b.Content = kwargs['button_name']
+        # if 'button_color' in kwargs:
+        #     color_tuple = kwargs['button_color']
+        #     color_brush = SolidColorBrush(Color.FromRgb(color_tuple[0], color_tuple[1], color_tuple[2]));
+        #     self.select_b.Background = color_brush
+
+        self._list_options()
+
+    def _list_options(self, checkbox_filter=None):
+        if checkbox_filter:
+            self.checkall_b.Content = 'Check'
+            self.uncheckall_b.Content = 'Uncheck'
+            self.toggleall_b.Content = 'Toggle'
+            checkbox_filter = checkbox_filter.lower()
+            self.list_lb.ItemsSource = [checkbox for checkbox in self._context
+                                                 if checkbox_filter in checkbox.name.lower()]
+        else:
+            self.checkall_b.Content = 'Check All'
+            self.uncheckall_b.Content = 'Uncheck All'
+            self.toggleall_b.Content = 'Toggle All'
+            self.list_lb.ItemsSource = self._context
+
+    def _set_states(self, state=True, flip=False):
+        current_list = self.list_lb.ItemsSource
+        for checkbox in current_list:
+            if flip:
+                checkbox.state = not checkbox.state
+            else:
+                checkbox.state = state
+
+        # push list view to redraw
+        self.list_lb.ItemsSource = None
+        self.list_lb.ItemsSource = current_list
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def toggle_all(self, sender, args):
+        self._set_states(flip=True)
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def check_all(self, sender, args):
+        self._set_states(state=True)
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def uncheck_all(self, sender, args):
+        self._set_states(state=False)
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def button_select(self, sender, args):
+        self.response = self._context
+        self.Close()
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def search_txt_changed(self, sender, args):
+        if self.search_tb.Text == '':
+            self.hide_element(self.clrsearch_b)
+        else:
+            self.show_element(self.clrsearch_b)
+
+        self._list_options(checkbox_filter=self.search_tb.Text)
+
+    # noinspection PyUnusedLocal
+    # noinspection PyMethodMayBeStatic
+    def clear_search(self, sender, args):
+        self.search_tb.Text = ' '
+        self.search_tb.Clear()
+        self.list_lb.ItemsSource = self._context
 
 
 class CommandSwitchWindow:
