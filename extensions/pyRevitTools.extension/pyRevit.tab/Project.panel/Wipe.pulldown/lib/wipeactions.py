9d0c6c5ad6714f986f27bddef67ee58e75901102
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-25 18:49:16 -0700

    WIP Wipe Components tool

--- extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/Wipe.pulldown/lib/wipeactions.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/Wipe.pulldown/lib/wipeactions.py
@@ -1,7 +1,6 @@
 import clr
 from scriptutils import this_script, logger
-from revitutils import doc, uidoc
-from revitutils.transaction import Action
+from revitutils import doc, uidoc, Action, ActionGroup
 
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.DB import *
@@ -22,22 +21,31 @@ def print_header(header):
     this_script.output.print_md('### {}'.format(header))
 
 
-def print_info(message):
+def log_debug(message):
     logger.debug(message)
 
 
-def print_error(el_type='', el_id=0, delete_err=None):
+def log_error(el_type='', el_id=0, delete_err=None):
     err_msg = str(delete_err).replace('\n', ' ').replace('\r', '')
     logger.error('Error Removing Element with Id: {} Type: {} | {}'.format(el_type, el_id, err_msg))
 
 
 def remove_action(action_title, action_cat, elements_to_remove, validity_func=None):
     def remove_element(rem_el):
-        try:
-            doc.Delete(rem_el.Id)
-            return True
-        except Exception as e:
-            return False
+        if rem_el:
+            try:
+                log_debug('Removing element:{} id:{}'.format(rem_el, rem_el.Id))
+                doc.Delete(rem_el.Id)
+                return True
+            except Exception as e:
+                if hasattr(rem_el, 'Id'):
+                    log_error(el_type=action_cat, el_id=rem_el.Id, delete_err=e)
+                else:
+                    log_error(el_type=action_cat, delete_err=e)
+        else:
+            log_debug('Element does not have value. It might have been already removed by other actions.')
+
+        return False
 
     with Action(action_title):
         for element in elements_to_remove:
@@ -60,316 +68,227 @@ def call_purge():
 @dependent
 def remove_all_constraints():
     """Remove All Constraints"""
-    t = Transaction(doc, 'Remove All Constraints')
-    t.Start()
-    print_header('REMOVING ALL CONSTRAINTS')
+
+    def confirm_removal(cnst):
+        return cnst.View is not None
+
     cl = FilteredElementCollector(doc)
-    clconst = list(cl.OfCategory(BuiltInCategory.OST_Constraints).WhereElementIsNotElementType())
-    for cnst in clconst:
-        if cnst.View is not None:
-            try:
-                doc.Delete(cnst.Id)
-            except Exception as e:
-                print_error('Constraint', None, e)
-                continue
-    t.Commit()
+    consts = list(cl.OfCategory(BuiltInCategory.OST_Constraints).WhereElementIsNotElementType())
+
+    print_header('REMOVING ALL CONSTRAINTS')
+    remove_action('Remove All Constraints', 'Constraint', consts, validity_func=confirm_removal)
 
 
 @notdependent
 def remove_all_groups():
     """Remove (and Explode) All Groups"""
-    t = Transaction(doc, 'Remove All Groups')
-    t.Start()
-    grpTypes = list(FilteredElementCollector(doc).OfClass(clr.GetClrType(GroupType)).ToElementIds())
+
+    def confirm_removal(group_typeid):
+        group_type = doc.GetElement(group_typeid)
+        return group_type and group_type.Category.Name != 'Attached Detail Groups'
+
+    group_types = list(FilteredElementCollector(doc).OfClass(clr.GetClrType(GroupType)).ToElementIds())
     groups = list(FilteredElementCollector(doc).OfClass(clr.GetClrType(Group)).ToElements())
 
-    print_header('EXPLODING GROUPS')
-    # ungroup all groups
-    for grp in groups:
-        grp.UngroupMembers()
+    print_header('EXPLODING GROUPS')         # ungroup all groups
 
-    print_header('REMOVING GROUPS')
-    # delete group types
-    for gtid in grpTypes:
-        gtype = doc.GetElement(gtid)
-        if gtype:
-            if gtype.Category.Name != 'Attached Detail Groups':
-                try:
-                    doc.Delete(gtid)
-                except Exception as e:
-                    print_error('Group Type', None, e)
-                    continue
-    t.Commit()
+    with ActionGroup('Remove All Groups', assimilate=True):
+        with Action('Exploding All Groups'):
+            for grp in groups:
+                grp.UngroupMembers()
+
+        print_header('REMOVING GROUPS')          # delete group types
+        remove_action('Remove All Groups', 'Group Type', group_types, validity_func=confirm_removal)
 
 
 @notdependent
 def remove_all_external_links():
     """Remove All External Links"""
-    t = Transaction(doc, 'Remove All External Links')
-    t.Start()
-    print_header('REMOVE ALL EXTERNAL LINKS')
+
+    def confirm_removal(refId):
+        link_el = doc.GetElement(refId)
+        return isinstance(link_el, RevitLinkType) or isinstance(link_el, CADLinkType)
+
     if doc.PathName:
         modelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(doc.PathName)
         transData = TransmissionData.ReadTransmissionData(modelPath)
         externalReferences = transData.GetAllExternalFileReferenceIds()
         cl = FilteredElementCollector(doc)
-        impInstances = list(cl.OfClass(clr.GetClrType(ImportInstance)).ToElements())
-        imported = []
-        for refId in externalReferences:
-            try:
-                lnk = doc.GetElement(refId)
-                if isinstance(lnk, RevitLinkType) or isinstance(lnk, CADLinkType):
-                    doc.Delete(refId)
-            except Exception as e:
-                print_error('External Link', None, e)
-                continue
+        import_instances = list(cl.OfClass(clr.GetClrType(ImportInstance)).ToElements())
+
+        print_header('REMOVE ALL EXTERNAL LINKS')
+        remove_action('Remove All External Links', 'External Link', import_instances, validity_func=confirm_removal)
     else:
-        print_error('Model must be saved for external links to be removed.')
-    t.Commit()
+        log_error('Model must be saved for external links to be removed.')
 
 
 @notdependent
 def remove_all_sheets():
     """Remove All Sheets"""
-    t = Transaction(doc, 'Remove All Sheets')
-    t.Start()
-    print_header('REMOVING SHEETS')
+
     cl = FilteredElementCollector(doc)
     sheets = cl.OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
-    openUIViews = uidoc.GetOpenUIViews()
-    openViews = [x.ViewId.IntegerValue for x in openUIViews]
-    for s in sheets:
-        if s.Id.IntegerValue in openViews:
-            continue
-        else:
-            try:
-                print_info('{2}{0}  {1}'.format(s.LookupParameter('Sheet Number').AsString().rjust(10),
-                                            s.LookupParameter('Sheet Name').AsString().ljust(50), s.Id))
-                doc.Delete(s.Id)
-            except Exception as e:
-                print_error('Sheet', None, e)
-                continue
-    t.Commit()
+    open_UIViews = uidoc.GetOpenUIViews()
+    open_views = [ov.ViewId.IntegerValue for ov in open_UIViews]
+
+    def confirm_removal(sht):
+        return sht.Id.IntegerValue in open_views
+
+    print_header('REMOVING SHEETS')
+    remove_action('Remove All Sheets', 'Sheet', sheets, validity_func=confirm_removal)
 
 
 @dependent
 def remove_all_rooms():
     """Remove All Rooms"""
-    t = Transaction(doc, 'Remove All Rooms')
-    t.Start()
-    print_header('REMOVING ROOMS')
+
     cl = FilteredElementCollector(doc)
     rooms = cl.OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements()
-    for r in rooms:
-        try:
-            print_info('{2}{1}{0}'.format(
-                r.LookupParameter('Name').AsString().ljust(30),
-                r.LookupParameter('Number').AsString().ljust(20),
-                r.Id
-            ))
-            doc.Delete(r.Id)
-        except Exception as e:
-            print_error('Room', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING ROOMS')
+    remove_action('Remove All Rooms', 'Room', rooms)
 
 
 @dependent
 def remove_all_areas():
     """Remove All Areas"""
-    t = Transaction(doc, 'Remove All Areas')
-    t.Start()
-    print_header('REMOVING AREAS')
+
     cl = FilteredElementCollector(doc)
     areas = cl.OfCategory(BuiltInCategory.OST_Areas).WhereElementIsNotElementType().ToElements()
-    for a in areas:
-        try:
-            print_info('{2}{1}{0}'.format(
-                a.ParametersMap['Name'].AsString().ljust(30),
-                a.ParametersMap['Number'].AsString().ljust(10),
-                a.Id))
-            doc.Delete(a.Id)
-        except Exception as e:
-            print_error('Area', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING AREAS')
+    remove_action('Remove All Areas', 'Area', areas)
 
 
 @notdependent
 def remove_all_room_separation_lines():
     """Remove All Room Separation Lines"""
-    t = Transaction(doc, 'Remove All Room Separation Lines')
-    t.Start()
-    print_header('REMOVING ROOM SEPARATIONS LINES')
+
     cl = FilteredElementCollector(doc)
     rslines = cl.OfCategory(BuiltInCategory.OST_RoomSeparationLines).WhereElementIsNotElementType().ToElements()
-    for line in rslines:
-        try:
-            # print_info('ID: {0}'.format( line.Id ))
-            doc.Delete(line.Id)
-        except Exception as e:
-            print_error('Room Separation Line', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING ROOM SEPARATIONS LINES')
+    remove_action('Remove All Room Separation Lines', 'Room Separation Line', rslines)
 
 
 @notdependent
 def remove_all_area_separation_lines():
     """Remove All Area Separation Lines"""
-    t = Transaction(doc, 'Remove All Area Separation Lines')
-    t.Start()
-    print_header('REMOVING AREA SEPARATIONS LINES')
+
     cl = FilteredElementCollector(doc)
     aslines = cl.OfCategory(BuiltInCategory.OST_AreaSchemeLines).WhereElementIsNotElementType().ToElements()
-    for line in aslines:
-        try:
-            doc.Delete(line.Id)
-        except Exception as e:
-            print_error('Area Separation Line', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING AREA SEPARATIONS LINES')
+    remove_action('Remove All Area Separation Lines', 'Area Separation Line', aslines)
 
 
 @notdependent
 def remove_all_scope_boxes():
     """Remove All Scope Boxes"""
-    t = Transaction(doc, 'Remove All ScopeBoxes')
-    t.Start()
-    print_header('REMOVING SCOPE BOXES')
+
     cl = FilteredElementCollector(doc)
     scopeboxes = cl.OfCategory(BuiltInCategory.OST_VolumeOfInterest).WhereElementIsNotElementType().ToElements()
-    for s in scopeboxes:
-        try:
-            print_info('ID: {0}'.format(s.Id))
-            doc.Delete(s.Id)
-        except Exception as e:
-            print_error('Scope Box', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING SCOPE BOXES')
+    remove_action('Remove All ScopeBoxes', 'Scope Box', scopeboxes)
 
 
 @notdependent
 def remove_all_reference_planes():
     """Remove All Reference Planes"""
-    t = Transaction(doc, 'Remove All Reference Planes')
-    t.Start()
-    print_header('REMOVING REFERENCE PLANES')
+
     cl = FilteredElementCollector(doc)
     refplanes = cl.OfCategory(BuiltInCategory.OST_CLines).WhereElementIsNotElementType().ToElements()
-    for s in refplanes:
-        try:
-            print_info('ID: {0}'.format(s.Id))
-            doc.Delete(s.Id)
-        except Exception as e:
-            print_error('Reference Plane', None, e)
-            continue
-    t.Commit()
+
+    print_header('REMOVING REFERENCE PLANES')
+    remove_action('Remove All Reference Planes', 'Reference Plane', refplanes)
 
 
 @notdependent
 def remove_all_materials():
     """Remove All Materials"""
-    t = Transaction(doc, 'Remove All Materials')
-    t.Start()
-    print_header('REMOVING MATERIALS')
+
+    def confirm_removal(mat):
+        if 'poche' in mat.Name.lower():
+            return False
+        return True
+
     cl = FilteredElementCollector(doc)
     mats = cl.OfCategory(BuiltInCategory.OST_Materials).WhereElementIsNotElementType().ToElements()
-    for m in mats:
-        if 'poche' in m.Name.lower():
-            continue
-        try:
-            doc.Delete(m.Id)
-        except Exception as e:
-            print_error('Material', None, e)
-            continue
-    t.Commit()
 
+    print_header('REMOVING MATERIALS')
+    remove_action('Remove All Materials', 'Material', mats, validity_func=confirm_removal)
+
+
+READONLY_VIEWS = [ViewType.ProjectBrowser,
+                  ViewType.SystemBrowser,
+                  ViewType.Undefined,
+                  ViewType.DrawingSheet,
+                  ViewType.Internal]
 
 @notdependent
 def remove_all_views():
     """Remove All Views"""
-    t = Transaction(doc, 'Remove All Views')
-    t.Start()
-    print_header('REMOVING VIEWS / LEGENDS / SCHEDULES')
+
     cl = FilteredElementCollector(doc)
     views = set(cl.OfClass(View).WhereElementIsNotElementType().ToElementIds())
-    openUIViews = uidoc.GetOpenUIViews()
-    openViews = [x.ViewId.IntegerValue for x in openUIViews]
-    for vid in views:
-        v = doc.GetElement(vid)
+    open_UIViews = uidoc.GetOpenUIViews()
+    open_views = [ov.ViewId.IntegerValue for ov in open_UIViews]
+
+    def confirm_removal(v):
         if isinstance(v, View):
-            if v.ViewType in [ViewType.ProjectBrowser,
-                              ViewType.SystemBrowser,
-                              ViewType.Undefined,
-                              ViewType.DrawingSheet,
-                              ViewType.Internal,
-                              ]:
-                continue
-            elif ViewType.ThreeD == v.ViewType and '{3D}' == v.ViewName:
-                continue
+            if ViewType.ThreeD == v.ViewType and '{3D}' == v.ViewName:
+                return False
             elif '<' in v.ViewName or v.IsTemplate:
-                continue
-            elif vid.IntegerValue in openViews:
-                continue
+                return False
+            elif vid.IntegerValue in open_views:
+                return False
             else:
-                print_info('{2}{1}{0}'.format(v.ViewName.ljust(50), str(v.ViewType).ljust(15), str(v.Id).ljust(10)))
-                try:
-                    doc.Delete(v.Id)
-                except Exception as e:
-                    print_error('View', None, e)
-                    continue
-    t.Commit()
+                return True
+        else:
+            return False
+
+    print_header('REMOVING VIEWS / LEGENDS / SCHEDULES')
+    remove_action('Remove All Views', 'View', views, validity_func=confirm_removal)
 
 
 @notdependent
 def remove_all_view_templates():
     """Remove All View Templates"""
-    t = Transaction(doc, 'Remove All View Templates')
-    t.Start()
-    print_header('REMOVING VIEW TEMPLATES')
+
+    def confirm_removal(v):
+        if isinstance(v, View) \
+        and v.IsTemplate \
+        and v.ViewType not in READONLY_VIEWS:
+                return True
+        else:
+            return False
+
     cl = FilteredElementCollector(doc)
-    views = set(cl.OfClass(View).WhereElementIsNotElementType().ToElementIds())
-    for vid in views:
-        v = doc.GetElement(vid)
-        if isinstance(v, View):
-            if v.ViewType in [ViewType.ProjectBrowser,
-                              ViewType.SystemBrowser,
-                              ViewType.Undefined,
-                              ViewType.DrawingSheet,
-                              ViewType.Internal,
-                              ]:
-                continue
-            elif v.IsTemplate:
-                print_info('{2}{1}{0}'.format(v.ViewName.ljust(50), str(v.ViewType).ljust(15), str(v.Id).ljust(10)))
-                try:
-                    doc.Delete(v.Id)
-                except Exception as e:
-                    print_error('View Template', None, e)
-                    continue
-    t.Commit()
+    views = set(cl.OfClass(View).WhereElementIsNotElementType().ToElements())
+
+    print_header('REMOVING ALL VIEW TEMPLATES')
+    remove_action('Remove All View Templates', 'View Templates', views, validity_func=confirm_removal)
 
 
 @notdependent
 def remove_all_elevation_markers():
     """Remove All Elevation Markers"""
-    t = Transaction(doc, 'Remove All Elevation Markers')
-    t.Start()
-    print_header('REMOVING ELEVATION MARKERS')
+
+    def confirm_removal(elev_marker):
+        return elev_marker.CurrentViewCount == 0
+
     cl = FilteredElementCollector(doc)
-    elevMarkers = cl.OfClass(ElevationMarker).WhereElementIsNotElementType().ToElements()
-    for em in elevMarkers:
-        try:
-            if em.CurrentViewCount == 0:
-                doc.Delete(em.Id)
-        except Exception as e:
-            print_error('Elevation Marker', None, e)
-            continue
-    t.Commit()
+    elev_markers = cl.OfClass(ElevationMarker).WhereElementIsNotElementType().ToElements()
+
+    print_header('REMOVING ELEVATION MARKERS')
+    remove_action('Remove All Elevation Markers', 'Elevation Marker', elev_markers, validity_func=confirm_removal)
 
 
 @notdependent
 def remove_all_filters():
     """Remove All Filters"""
+
     cl = FilteredElementCollector(doc)
     filters = cl.OfClass(FilterElement).WhereElementIsNotElementType().ToElements()
 
