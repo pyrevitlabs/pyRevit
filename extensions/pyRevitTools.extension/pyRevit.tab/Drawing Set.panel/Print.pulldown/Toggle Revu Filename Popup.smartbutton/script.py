9ee564e2ae2c4d8ef24ddadbf277f8aa036d0eef
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-26 12:35:35 -0700

    Fixed Revu popup toggle tool

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Print.pulldown/Toggle Revu Filename Popup.smartbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Print.pulldown/Toggle Revu Filename Popup.smartbutton/script.py
@@ -1,7 +1,7 @@
 # noinspection PyUnresolvedReferences
 import _winreg as wr
 
-from scriptutils import this_script
+from scriptutils import this_script, logger
 from pyrevit.coreutils.ribbon import ICON_MEDIUM
 
 
@@ -10,7 +10,10 @@ __doc__ = 'Enables and disables the Revu PDF Printer prompt for filename option.
 
 
 def get_reg_key():
-    return wr.OpenKey(wr.HKEY_CURRENT_USER, r'Software\Bluebeam Software\Brewery\V45\Printer Driver', 0, wr.KEY_WRITE)
+    try:
+        return wr.OpenKey(wr.HKEY_CURRENT_USER, r'Software\Bluebeam Software\Brewery\V45\Printer Driver', 0, wr.KEY_WRITE)
+    except:
+        return None
 
 
 # noinspection PyUnusedLocal
@@ -19,13 +22,16 @@ def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
     off_icon = script_cmp.get_bundle_file('off.png')
 
     reg_key = get_reg_key()
-    curval = wr.QueryValueEx(reg_key, r'PromptForFileName')
-    wr.FlushKey(reg_key)
-    reg_key.Close()
+    if reg_key:
+        curval = wr.QueryValueEx(reg_key, r'PromptForFileName')
+        wr.FlushKey(reg_key)
+        reg_key.Close()
 
-    if curval[0] == '1':
-        ui_button_cmp.set_icon(on_icon, icon_size=ICON_MEDIUM)
-    elif curval[0] == '0':
+        if curval[0] == '1':
+            ui_button_cmp.set_icon(on_icon, icon_size=ICON_MEDIUM)
+        elif curval[0] == '0':
+            ui_button_cmp.set_icon(off_icon, icon_size=ICON_MEDIUM)
+    else:
         ui_button_cmp.set_icon(off_icon, icon_size=ICON_MEDIUM)
 
 
@@ -34,15 +40,19 @@ def toggle_state():
     off_icon = this_script.get_bundle_file('off.png')
 
     reg_key = get_reg_key()
-    curval = wr.QueryValueEx(reg_key, r'PromptForFileName')
-    if curval[0] == '1':
-        wr.SetValueEx(reg_key, r'PromptForFileName', 0, wr.REG_SZ, '0')
+    if reg_key:
+        curval = wr.QueryValueEx(reg_key, r'PromptForFileName')
+        if curval[0] == '1':
+            wr.SetValueEx(reg_key, r'PromptForFileName', 0, wr.REG_SZ, '0')
+            this_script.ui_button.set_icon(off_icon, icon_size=ICON_MEDIUM)
+        elif curval[0] == '0':
+            wr.SetValueEx(reg_key, r'PromptForFileName', 0, wr.REG_SZ, '1')
+            this_script.ui_button.set_icon(on_icon, icon_size=ICON_MEDIUM)
+        wr.FlushKey(reg_key)
+        reg_key.Close()
+    else:
+        logger.error('Bluebeam Revu is not installed on this machine. Can not find registry key.')
         this_script.ui_button.set_icon(off_icon, icon_size=ICON_MEDIUM)
-    elif curval[0] == '0':
-        wr.SetValueEx(reg_key, r'PromptForFileName', 0, wr.REG_SZ, '1')
-        this_script.ui_button.set_icon(on_icon, icon_size=ICON_MEDIUM)
-    wr.FlushKey(reg_key)
-    reg_key.Close()
 
 
 if __name__ == '__main__':
