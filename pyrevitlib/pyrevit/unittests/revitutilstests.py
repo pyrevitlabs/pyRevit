9f6ccc89019136298e908a2a163069234eaf2c88
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-26 07:54:04 -0700

    Combined revitutils unittests

--- pyrevitlib/pyrevit/unittests/revitutilstests.py
+++ pyrevitlib/pyrevit/unittests/revitutilstests.py
@@ -1,40 +1,32 @@
 from unittest import TestCase
 
 
-class TestDocumentAccess(TestCase):
-    def test_action(self):
+class TestRevitUtils(TestCase):
+    def test_docAccess(self):
         try:
             from revitutils import doc, uidoc, all_docs
         except Exception as e:
             raise e
 
-
-class TestProjectInfo(TestCase):
-    def test_action(self):
+    def test_projectInfo(self):
         try:
             from revitutils import project
         except Exception as e:
             raise e
 
-
-class TestSelection(TestCase):
-    def test_action(self):
+    def test_selection(self):
         try:
             from revitutils import selection
         except Exception as e:
             raise e
 
-
-class TestTransaction(TestCase):
-    def test_action(self):
+    def test_transaction(self):
         try:
             from revitutils import Action
         except Exception as e:
             raise e
 
-
-class TestTransactionGroup(TestCase):
-    def test_action(self):
+    def test_transcationGroup(self):
         try:
             from revitutils import ActionGroup
         except Exception as e:
