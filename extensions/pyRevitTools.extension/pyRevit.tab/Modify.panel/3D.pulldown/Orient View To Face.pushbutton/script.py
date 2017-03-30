83586b6aa8f0193e3b59ce73925ed10c70fcade1
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-03-30 16:24:46 -0700

    Upgraded the orient to face tool to use new methods in 2017

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient View To Face.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient View To Face.pushbutton/script.py
@@ -1,45 +1,26 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+from pyrevit import HOST_APP
+from revitutils import doc, uidoc, selection, curview
+from revitutils import Action
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+# noinspection PyUnresolvedReferences
+from Autodesk.Revit.DB import View3D, SketchPlane, Plane, UV
 
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
 
 __doc__ = 'Reorients the current 3D view camera, perpendicular to the selected face. ' \
           'This tool will set a sketch plane over the selected face for 3d drawing.'
 
-__window__.Close()
-from Autodesk.Revit.DB import Transaction, View3D, SketchPlane, Plane
-from Autodesk.Revit.UI.Selection import ObjectType
 
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-selection = [doc.GetElement(elId) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+face = selection.utils.pick_face()
 
-curview = uidoc.ActiveView
-try:
-    ref = uidoc.Selection.PickObject(ObjectType.Face)
-    el = doc.GetElement(ref.ElementId)
-    face = el.GetGeometryObjectFromReference(ref)
-    if isinstance(curview, View3D):
-        t = Transaction(doc, 'Orient to Selected Face')
-        t.Start()
-        sp = SketchPlane.Create(doc, Plane(face.Normal, face.Origin))
-        curview.OrientTo(face.Normal.Negate())
+
+if face and isinstance(curview, View3D):
+    with Action('Orient to Selected Face'):
+        if HOST_APP.is_newer_than(2015):
+            normal_vec = face.ComputeNormal(UV(0,0))
+            sp = SketchPlane.Create(doc, Plane(normal_vec, face.Origin))
+            curview.OrientTo(normal_vec.Negate())
+        else:
+            sp = SketchPlane.Create(doc, Plane(face.Normal, face.Origin))
+            curview.OrientTo(face.Normal.Negate())
         uidoc.ActiveView.SketchPlane = sp
         uidoc.RefreshActiveView()
-        t.Commit()
-except:
-    pass
