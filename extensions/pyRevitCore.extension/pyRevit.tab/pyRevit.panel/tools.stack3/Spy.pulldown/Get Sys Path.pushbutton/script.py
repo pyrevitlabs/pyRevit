4d6e8b7d7504cdb7348b867d72d8997c0c17ebdf
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-04-12 15:28:42 -0700

    Upgraded spy tools

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Get Sys Path.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Get Sys Path.pushbutton/script.py
@@ -1,31 +1,14 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+"""Prints the IronPython sys.path paths."""
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
-
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
-
-__doc__ = 'Prints the IronPython sys.path paths.'
-
-import sys
 import os
+import sys
+
+
+__context__ = 'zerodoc'
+
 
 folder = os.path.dirname(__file__)
 print('Home directory of this script:\n{0}\n'.format(folder))
-
-# sys.path.append(folder)
 
 print('Printing sys.path directories:')
 for p in sys.path:
--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Open Journals Folder.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Open Journals Folder.pushbutton/script.py
@@ -1,41 +1,13 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
-
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
-
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
-
-__doc__ = 'Opens the Revit journals folder for current user.'
-
-__window__.Close()
-
-import os, subprocess
-import os.path as op
+"""Opens the Revit journals folder for current user."""
 
 
-# def getjournalsfolder():
-# appdataFolder = os.getenv('localappdata')
-# journalsFolder = op.join( appdataFolder, 'Autodesk\\Revit\\Autodesk Revit {0}\\Journals'.format( __revit__.Application.VersionNumber ))
-# os.chdir(journalsFolder)
-# return journalsFolder
-
-def getjournalsfolder():
-    journalsfolder = op.dirname(__revit__.Application.RecordingJournalFilename)
-    os.chdir(journalsfolder)
-    return journalsfolder
+import os
+from scriptutils import coreutils
+import revitutils.journals as journals
 
 
-journalsFolder = getjournalsfolder()
-subprocess.Popen(r'explorer "{0}"'.format(journalsFolder))
+__context__ = 'zerodoc'
+
+
+journals_folder = journals.get_journals_folder()
+coreutils.open_folder_in_explorer(journals_folder)
