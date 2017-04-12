2e968755db9e9f65e437e227d09d012c3e8ab54f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-04-12 16:31:47 -0700

    Upgraded more spy tools

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Find All Loaded Assemblies.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Spy.pulldown/Find All Loaded Assemblies.pushbutton/script.py
@@ -1,32 +1,16 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+"""List all currently loaded assemblies"""
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
-
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
-
-__doc__ = 'List all currently loaded assemblies'
-
+# noinspection PyUnresolvedReferences
 from System import AppDomain
 
-__window__.Width = 1500
+
+__context__ = 'zerodoc'
+
 
 userScriptsAssemblies = []
 for loadedAssembly in AppDomain.CurrentDomain.GetAssemblies():
     loc = ''
-    if 'pyrevit' in loadedAssembly.FullName:
+    if 'pyrevit' in unicode(loadedAssembly.FullName).lower():
         userScriptsAssemblies.append(loadedAssembly)
         continue
     try:
@@ -35,8 +19,7 @@ for loadedAssembly in AppDomain.CurrentDomain.GetAssemblies():
         pass
     print('\n{0}{1}{2}'.format(loadedAssembly.GetName().Name.ljust(50),
                                str(loadedAssembly.GetName().Version).ljust(20),
-                               loc
-                               ))
+                               loc))
     try:
         print('External Applications:')
         print([x.FullName for x in loadedAssembly.GetTypes() if x.GetInterface("IExternalApplication") is not None])
@@ -57,8 +40,7 @@ for loadedAssembly in userScriptsAssemblies:
         pass
     print('{0}{1}{2}'.format(loadedAssembly.GetName().Name.ljust(50),
                              str(loadedAssembly.GetName().Version).ljust(20),
-                             loc
-                             ))
+                             loc))
     try:
         print('External Commands:')
         print([x.FullName for x in loadedAssembly.GetTypes() if x.GetInterface("IExternalCommand") is not None])
