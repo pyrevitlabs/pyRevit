7f1fd8761ae7eed97ddf216ae3b49fe5a4bd0fed
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-04-14 15:06:40 -0700

    Revised Docdecorator to work on functions as well

--- pyrevitlib/revitutils/_project.py
+++ pyrevitlib/revitutils/_project.py
@@ -17,12 +17,20 @@ project = CurrentProject(doc)
 curview = uidoc.ActiveView
 
 
-def DocDecorator(orig_cls):
+def DocDecorator(orig_obj):
     global doc
 
-    class DerivedDocProvider(orig_cls):
-        def __init__(self, *args, **kwargs):
-            self.doc = doc
-            orig_cls.__init__(self, *args, **kwargs)
+    # if given object is a function, wrap it in FuncDocProvider
+    if hasattr(orig_obj, '__closure__'):
+        def FuncDocProvider(*args, **kwargs):
+            orig_obj(*args, **kwargs)
 
-    return DerivedDocProvider
+        return FuncDocProvider
+    # if given object is a class, create a dervided class and add doc attribute
+    else:
+        class DerivedDocProvider(orig_obj):
+            def __init__(self, *args, **kwargs):
+                self.doc = doc
+                orig_obj.__init__(self, *args, **kwargs)
+
+        return DerivedDocProvider
