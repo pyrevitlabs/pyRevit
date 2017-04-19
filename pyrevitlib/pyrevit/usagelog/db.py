88236b28e7a5c47a6f77af0241fbdd518e0605e2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-04-18 21:22:14 -0700

    PEP8 / 80col

--- pyrevitlib/pyrevit/usagelog/db.py
+++ pyrevitlib/pyrevit/usagelog/db.py
@@ -1,13 +1,15 @@
 """
-This module is used to access the usage logging records saved in usage log files.
-It is not used to access the usage log records sent to a remote logging server.
+This module is used to access the usage logging records saved in usage
+log files. It is not used to access the usage log records sent to a 
+remote logging server.
 
     It has only one public function:
         >>> get_records()
-        This function accepts, source directory, filters, and search terms and returns
-        sorted results. See more on function documentation.
+        This function accepts, source directory, filters, and search terms
+        and returns sorted results. See more on function documentation.
 
 """
+
 import os
 import os.path as op
 import json
@@ -15,6 +17,7 @@ import json
 from pyrevit import PyRevitException
 from pyrevit.coreutils import calculate_dir_hash
 from pyrevit.coreutils.logger import get_logger
+
 
 from pyrevit.usagelog import FILE_LOG_EXT, get_current_usage_logpath
 from pyrevit.usagelog.record import UsageRecord
@@ -25,16 +28,16 @@ logger = get_logger(__name__)
 
 
 def _logobj_decoder(logobj):
-    # decoder function that receives object dictionary from json.load method and
-    # returns the object that owns that data
+    # decoder function that receives object dictionary from json.load method
+    # and returns the object that owns that data
     if 'revit' in logobj:
         # if 'revit' is in the keys, it means its a UsageRecord object.
         new_rec = UsageRecord()
         new_rec.update(logobj)
         return new_rec
     else:
-        # else it is not UsageRecord. It could be the command results dictionary or other dictionaries,
-        # saved in command results.
+        # else it is not UsageRecord. It could be the command results
+        # dictionary or other dictionaries, saved in command results.
         return logobj
 
 
@@ -48,7 +51,8 @@ def _collect_records_from_file(usagelog_file):
                 record.logfilename = usagelog_file
             return record_list
     except Exception as err:
-        raise PyRevitException('Error reading usage log for: {} | {}'.format(usagelog_file, err))
+        raise PyRevitException('Error reading usage log for: {} | {}'
+                               .format(usagelog_file, err))
 
 
 _records = []
@@ -57,7 +61,8 @@ _current_path_hash = None
 
 def _is_record_info_current(source_path):
     # calculates a hash from the log files in the directory and compares it
-    # with the _current_path_hash (to determine if the current _records list is up-to-date)
+    # with the _current_path_hash
+    # (to determine if the current _records list is up-to-date)
     dir_hash = calculate_dir_hash(source_path, '', FILE_LOG_EXT)
     if dir_hash == _current_path_hash:
         return True
@@ -65,7 +70,8 @@ def _is_record_info_current(source_path):
 
 
 def _get_all_usagelogfiles(usagelog_path=None):
-    # returns a list of all usage log files in the default or provided directory
+    # returns a list of all usage log files in the
+    # default or provided directory
     usagelog_file_list = []
     for usagelog_file in os.listdir(usagelog_path):
         if usagelog_file.endswith(FILE_LOG_EXT):
@@ -78,7 +84,8 @@ def _collect_all_records(source_path=None):
     global _records
 
     # use the default usage log file path if source_path is not provided
-    current_log_path = get_current_usage_logpath() if not source_path else source_path
+    current_log_path = get_current_usage_logpath() \
+        if not source_path else source_path
 
     if current_log_path:
         # todo: implement per file hash check to improve performance
@@ -96,30 +103,37 @@ def _collect_all_records(source_path=None):
         return None
 
 
-def get_records(source_path=None, record_filter=None, search_term=None, reverse=True):
+def get_records(source_path=None, record_filter=None,
+                search_term=None, reverse=True):
     """
-    Returns a list of UsageRecord objects. Each UsageRecord object represents a usage log entry.
-    if record_filter argument is provided with a RecordFilter (or any of the subclasses) object, this
-    function will filter the records as well. Filter objects are provided in pyrevit.usagelog.filters module.
+    Returns a list of UsageRecord objects. Each UsageRecord object represents
+    a usage log entry. if record_filter argument is provided with a
+    RecordFilter (or any of the subclasses) object, this function will filter
+    the records as well. Filter objects are provided in
+    pyrevit.usagelog.filters module.
 
-    Returned record list is sorterd using sorted() function. The UsageRecord object provides necessary
-    methods to allow sorting based on time and date. So the result will be sorted by date:time.
+    Returned record list is sorterd using sorted() function.
+    The UsageRecord object provides necessary methods to allow sorting
+    based on time and date. So the result will be sorted by date:time.
 
     Args:
         source_path (str): Directory address of usage log file locations.
-        record_filter (RecordFilter): RecordFilter object or one of its subclasses.
+        record_filter (RecordFilter): RecordFilter object or its subclasses.
         search_term (str): Only return the results that include this string.
         reverse (bool): Reverse the result list order.
 
     Returns:
-        list: returns a list of UsageRecord objects, sorted by date:time; Empty list if no records
+        list: returns a list of UsageRecord objects, sorted by date:time;
+              Empty list if no records
     """
 
     all_records = _collect_all_records(source_path)
 
     if all_records:
         if record_filter:
-            return sorted(record_filter.filter_records(all_records, search_term=search_term), reverse=reverse)
+            return sorted(record_filter.filter_records(all_records,
+                                                       search_term=search_term),
+                          reverse=reverse)
         else:
             return sorted(all_records, reverse=reverse)
     else:
--- pyrevitlib/pyrevit/usagelog/record.py
+++ pyrevitlib/pyrevit/usagelog/record.py
@@ -21,55 +21,72 @@ TIMESTAMP_FORMAT = '%Y/%m/%d %H:%M:%S:%f'
 
 class _CommandCustomResults(object):
     """
-    This class provides an interface wrapper around the EXEC_PARAMS.result_dict dictionary that is
-    provided by the ScriptExecutor C# object. ScriptExecutor provides this results dictionary to all
-    scripts, and scripts can add key:value pairs to the dictionary. But since the provided dictionary is
-    a C# dictionary, this class provides a very easy to use wrapper around it.
+    This class provides an interface wrapper around the EXEC_PARAMS.result_dict
+    dictionary that is provided by the ScriptExecutor C# object.
+    ScriptExecutor provides this results dictionary to all scripts, and scripts
+    can add key:value pairs to the dictionary. But since the provided
+    dictionary is a C# dictionary, this class provides a very easy
+    to use wrapper around it.
 
     Example:
-        >>> _CommandCustomResults().returnparam = value
+        >>> _CommandCustomResults().returnparam = 'some return value'
 
-    Attributes:
-        any (str): This is a smart class and can accept any attribute other than the reserved ones.
     """
 
-    # list of standard/default usage log record params provided by the c-sharp logger
-    # scripts should not use these names
-    RESERVED_NAMES = ['time', 'username', 'revit', 'revitbuild', 'sessionid', 'pyrevit', 'debug',
-                      'alternate', 'commandname', 'result', 'source']
+    # list of standard/default usage log record params provided
+    # by the c-sharp logger scripts should not use these names
+    RESERVED_NAMES = ['time', 'username', 'revit', 'revitbuild', 'sessionid',
+                      'pyrevit', 'debug', 'alternate', 'commandname',
+                      'result', 'source']
 
     def __getattr__(self, key):
-        # return value of the given key, let it raise exception if the value is not there
+        # return value of the given key,
+        # let it raise exception if the value is not there
         return unicode(EXEC_PARAMS.result_dict[key])
 
     def __setattr__(self, key, value):
         if key in _CommandCustomResults.RESERVED_NAMES:
             # making sure the script is not using a reserved name
-            logger.error('{} is a standard log param. Can not override this value.'.format(key))
+            logger.error('{} is a standard log param. '
+                         'Can not override this value.'.format(key))
         else:
             # if all is okay lets add the key:value to the return dict
             EXEC_PARAMS.result_dict.Add(key, unicode(value))
 
 
 class UsageRecord:
-    """Usage record object. This is created by ``pyrevit.usagelog.db`` module when reading records.
+    """
+    Usage record object. This is created by ``pyrevit.usagelog.db``
+    module when reading records.
 
     Attributes:
-        date (str): Date of usage log entry e.g. '2017/03/28'
-        time (str): Time of usage log entry e.g. '18:35:22:1235'
-        username (str): Usename if user that used this command e.g. 'eirannejad'
-        revit (str): Year of Revit used to run the command e.g. '2017'
-        revitbuild (str): Build of Revit used to run the command e.g. '20160720_1515(x64)'
-        sessionid (str): pyRevit session id that his command was executed in e.g. 'f5351ce1-13fc-11e7-97e9-7323ba0b5bf7'
-        pyrevit (str): pyRevit version that his command was executed by e.g. '4.2:dcf4090'
+        date (str): Date of usage log entry
+                    e.g. '2017/03/28'
+        time (str): Time of usage log entry
+                    e.g. '18:35:22:1235'
+        username (str): Usename if user that used this command
+                        e.g. 'eirannejad'
+        revit (str): Year of Revit used to run the command
+                     e.g. '2017'
+        revitbuild (str): Build of Revit used to run the command
+                          e.g. '20160720_1515(x64)'
+        sessionid (str): pyRevit session id that his command was executed in
+                         e.g. 'f5351ce1-13fc-11e7-97e9-7323ba0b5bf7'
+        pyrevit (str): pyRevit version that his command was executed by
+                       e.g. '4.2:dcf4090'
         debug (bool): Was command run in debug mode?
         alternate (bool): Was command run while holding down the SHIFT key?
-        commandname (str): Name of the executed command e.g. 'Usage Records'
-        commandbundle (str): Bundle name of the executed command e.g. 'Usage Records.pushbutton'
-        commandextension (str): pyRevit extension name of the executed command e.g. 'pyRevitDevTools'
+        commandname (str): Name of the executed command
+                           e.g. 'Usage Records'
+        commandbundle (str): Bundle name of the executed command
+                             e.g. 'Usage Records.pushbutton'
+        commandextension (str): pyRevit extension name of the executed command
+                                e.g. 'pyRevitDevTools'
         resultcode (int): Result code return from the executed command e.g. 3
-        commandresults (dict): Custom data returned from command e.g. {'param1': 'value'}
-        scriptpath (str): Path of the executed command e.g. 'C:\Users\eirannejad\...\Usage Records.pushbutton\script.py'
+        commandresults (dict): Custom data returned from command
+                               e.g. {'param1': 'value'}
+        scriptpath (str): Path of the executed command
+                          e.g. 'C:\...\SomeButton.pushbutton\script.py'
     """
 
     def __init__(self):
@@ -99,14 +116,19 @@ class UsageRecord:
         # less-than operator overload for object comparison
         # compares objects based on date:time
         # whichever that got logged earlier (is older) is less
-        mydate = datetime.datetime.strptime('{} {}'.format(self.date, self.time), TIMESTAMP_FORMAT)
-        otherdate = datetime.datetime.strptime('{} {}'.format(other.date, other.time), TIMESTAMP_FORMAT)
+        mydate = datetime.datetime.strptime('{} {}'
+                                            .format(self.date, self.time),
+                                            TIMESTAMP_FORMAT)
+        otherdate = datetime.datetime.strptime('{} {}'
+                                               .format(other.date, other.time),
+                                               TIMESTAMP_FORMAT)
         return mydate < otherdate
 
     @property
     def logfilename(self):
-        """Returns the log file name that this record has been saved to. The file path will be setup by
-        the ``pyrevit.usagelog.db`` module when parsing the log records.
+        """Returns the log file name that this record has been saved to.
+        The file path will be setup by the ``pyrevit.usagelog.db`` module
+        when parsing the log records.
 
         Returns:
             str: Full file path of the usage log file containing this record
@@ -116,18 +138,21 @@ class UsageRecord:
 
     @logfilename.setter
     def logfilename(self, logfile_path):
-        """Sets the log file name that this record has been saved to. The file path will be set by
-        the ``pyrevit.usagelog.db`` module when parsing the log records.
+        """Sets the log file name that this record has been saved to.
+        The file path will be set by the ``pyrevit.usagelog.db`` module
+        when parsing the log records.
         """
 
         self._logfilename = logfile_path
 
     @property
     def result(self):
-        """Command execution result in human readable format (e.g. CompileException)
+        """Command execution result in human readable format
+        (e.g. CompileException)
 
         Returns:
-            str: Command execution result in human readable format (e.g. CompileException)
+            str: Command execution result in human readable format
+                 (e.g. CompileException)
         """
 
         return RESULT_DICT[self.resultcode]
@@ -140,14 +165,18 @@ class UsageRecord:
             dict: original dictionary used to create/update this record.
         """
         rec_string = str(self._src_dict)
-        rec_string = rec_string.replace('\'', '\"').replace('False', 'false').replace('True', 'true')
+        rec_string = rec_string \
+            .replace('\'', '\"') \
+            .replace('False', 'false') \
+            .replace('True', 'true')
         return rec_string
 
     def has_term(self, search_term):
         """Checks all parameters for the search_term.
 
         Returns:
-            bool: Returns True if search_term is found in any of the parameter values
+            bool: Returns True if search_term is found in any of the
+                  parameter values
         """
 
         for value in self._src_dict.values():
@@ -155,12 +184,14 @@ class UsageRecord:
                 return True
 
     def update(self, src_dict):
-        """Updates all parameters in this record object based on the give src_dict.
-        This is the promary method for the ``pyrevit.usagelog.db`` module to create the records from
+        """Updates all parameters in this record object based on the
+        given src_dict. This is the promary method for the
+        ``pyrevit.usagelog.db`` module to create the records from
         a dictionary return by the record file reader.
 
         Args:
-            src_dict (dict): Source dictionary containing values for record parameters
+            src_dict (dict): Source dictionary containing values
+                             for record parameters
         """
 
         self.__dict__.update(src_dict)
