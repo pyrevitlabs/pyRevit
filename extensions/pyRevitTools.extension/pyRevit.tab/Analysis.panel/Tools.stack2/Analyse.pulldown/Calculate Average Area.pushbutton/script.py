bdf8dc9d736af4cf17692a014f0130237c2a9ed5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-05-03 15:36:55 -0700

    Fixed average area calcs

--- extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack2/Analyse.pulldown/Calculate Average Area.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack2/Analyse.pulldown/Calculate Average Area.pushbutton/script.py
@@ -21,38 +21,46 @@ rms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms)\
 spaces = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces) \
                                       .WhereElementIsNotElementType().ToElements()
 
-total = 0.0
-average = 0.0
-count = 0
+processed_items = {Area: [], Room: [], Space: []}
 
 for el in selection.elements:
+    count = 0
+    total = 0.0
+    average = 0.0
     if isinstance(el, Area):
         selareaname = el.LookupParameter('Name').AsString()
-        print("AREA TYPE IS: {}".format(selareaname))
-        for area in areas:
-            areaname = area.LookupParameter('Name').AsString()
-            if area.AreaScheme.Name == el.AreaScheme.Name and selareaname == areaname:
-                total += area.LookupParameter('Area').AsDouble()
-                count += 1
-        print("TOTAL OF {} AREAS WERE FOUND.".format(count))
+        if selareaname not in processed_items[Area]:
+            print("AREA TYPE IS: {}".format(selareaname))
+            for area in areas:
+                areaname = area.LookupParameter('Name').AsString()
+                if area.AreaScheme.Name == el.AreaScheme.Name and selareaname == areaname:
+                    total += area.LookupParameter('Area').AsDouble()
+                    count += 1
+            print("TOTAL OF {} AREAS WERE FOUND.".format(count))
+            processed_items[Area].append(selareaname)
     elif isinstance(el, Room):
         selroomname = el.LookupParameter('Name').AsString()
-        print("ROOM TYPE IS: {}".format(selroomname))
-        for room in rms:
-            roomname = room.LookupParameter('Name').AsString()
-            if selroomname == roomname:
-                total += room.LookupParameter('Area').AsDouble()
-                count += 1
-        print("TOTAL OF {} ROOMS WERE FOUND.".format(count))
+        if selroomname not in processed_items[Room]:
+            print("ROOM TYPE IS: {}".format(selroomname))
+            for room in rms:
+                roomname = room.LookupParameter('Name').AsString()
+                if selroomname == roomname:
+                    total += room.LookupParameter('Area').AsDouble()
+                    count += 1
+            print("TOTAL OF {} ROOMS WERE FOUND.".format(count))
+            processed_items[Room].append(selroomname)
     elif isinstance(el, Space):
         selspacename = el.LookupParameter('Name').AsString()
-        print("SPACE TYPE IS: {}".format(selspacename))
-        for space in spaces:
-            spacename = space.LookupParameter('Name').AsString()
-            if selspacename == spacename:
-                total += space.LookupParameter('Area').AsDouble()
-                count += 1
-        print("TOTAL OF {} SPACES WERE FOUND.".format(count))
+        if selspacename not in processed_items[Space]:
+            print("SPACE TYPE IS: {}".format(selspacename))
+            for space in spaces:
+                spacename = space.LookupParameter('Name').AsString()
+                if selspacename == spacename:
+                    total += space.LookupParameter('Area').AsDouble()
+                    count += 1
+            print("TOTAL OF {} SPACES WERE FOUND.".format(count))
+            processed_items[Space].append(selspacename)
 
-    average = total / count
-    print("AVERAGE AREA OF THE SELECTED TYPE IS:\n{0} SQFT\n{1} ACRE".format(average, average / 43560))
+    if count != 0:
+        average = total / count
+        print("AVERAGE AREA OF THE SELECTED TYPE IS:\n{0} SQFT\n{1} ACRE".format(average, average / 43560))
