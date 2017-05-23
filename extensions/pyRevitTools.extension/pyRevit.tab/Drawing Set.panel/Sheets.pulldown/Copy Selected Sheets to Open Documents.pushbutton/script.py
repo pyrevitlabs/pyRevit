5b1cc428908cb41ac273533b4b5e88189b8c9403
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-05-22 17:09:53 -0700

    Copy sheets WIP

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Sheets.pulldown/Copy Selected Sheets to Open Documents.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Sheets.pulldown/Copy Selected Sheets to Open Documents.pushbutton/script.py
@@ -3,7 +3,7 @@
 import sys
 
 from revitutils import doc as activedoc
-from revitutils import uidoc, selection
+from revitutils import uidoc, selection, curview, Action
 
 from System.Collections.Generic import List
 
@@ -16,63 +16,78 @@ class CopyUseDestination(IDuplicateTypeNamesHandler):
         return DuplicateTypeAction.UseDestinationTypes
 
 
-def error(msg):
+def error_and_close(msg):
     TaskDialog.Show('pyrevit', msg)
     sys.exit(0)
 
 
-# find open documents other than the active doc
-openDocs = [d for d in __revit__.Application.Documents if not d.IsLinked]
-openDocs.remove(activeDoc)
-if len(openDocs) < 1:
-    error('Only one active document is found. At least two documents must be open. Operation cancelled.')
+def get_dest_docs():
+    # find open documents other than the active doc
+    open_docs = [d for d in __revit__.Application.Documents if not d.IsLinked]
+    open_docs.remove(activedoc)
+    if len(open_docs) < 1:
+        error_and_close('Only one active document is found. ' \
+                        'At least two documents must be open. ' \
+                        'Operation cancelled.')
+    return open_docs
 
-# get a list of selected legends
-selection = [activeDoc.GetElement(elId) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds() if
-             activeDoc.GetElement(elId).ViewType == ViewType.Legend]
 
-if len(selection) > 0:
-    for doc in openDocs:
-        print('\n---PROCESSING DOCUMENT {0}---'.format(doc.Title))
-        # finding first available legend view
-        allViews = FilteredElementCollector(doc).OfClass(View)
-        baseLegendView = None
-        for v in allViews:
-            if v.ViewType == ViewType.Legend:
-                baseLegendView = v
-                break
-        if None == baseLegendView:
-            error('Document\n{0}\nmust have at least one Legend view.'.format(doc.Title))
-        # iterate over interfacetypes legend views
-        for srcView in selection:
-            print('\nCOPYING {0}'.format(srcView.ViewName))
-            # get legend view elements and exclude non-copyable elements
-            viewElements = FilteredElementCollector(activeDoc, srcView.Id).ToElements()
-            elementList = []
-            for el in viewElements:
-                if isinstance(el, Element) and el.Category:
-                    elementList.append(el.Id)
-                else:
-                    print('SKIPPING ELEMENT WITH ID: {0}'.format(el.Id))
-            if len(elementList) < 1:
-                print('SKIPPING {0}. NO ELEMENTS FOUND.'.format(srcView.ViewName))
-                continue
-            # start creating views and copying elements
-            with Transaction(doc, 'Copy Legends to this document') as t:
-                t.Start()
-                destView = doc.GetElement(baseLegendView.Duplicate(ViewDuplicateOption.Duplicate))
-                options = CopyPasteOptions()
-                options.SetDuplicateTypeNamesHandler(CopyUseDestination())
-                copiedElement = ElementTransformUtils.CopyElements(srcView,
-                                                                   List[ElementId](elementList),
-                                                                   destView,
-                                                                   None,
-                                                                   options)
-                # matching element graphics overrides and view properties
-                for dest, src in zip(copiedElement, elementList):
-                    destView.SetElementOverrides(dest, srcView.GetElementOverrides(src))
-                destView.ViewName = srcView.ViewName
-                destView.Scale = srcView.Scale
-                t.Commit()
-else:
-    error('At least one Legend view must be selected.')
+def copy_sheet_view(activedoc, source_sheet, dest_doc):
+    print('copy_sheet_view')
+
+    t = Transaction(dest_doc, 'Create Sheet')
+    t.Start()
+    new_sheet = ViewSheet.Create(dest_doc, ElementId.InvalidElementId)
+    new_sheet.Name = source_sheet.Name
+    new_sheet.SheetNumber = source_sheet.SheetNumber
+    t.Commit()
+
+    return new_sheet
+
+
+def copy_viewport_contents(activedoc, source_view, dest_doc, new_view):
+    print('copy_sheet_contents')
+    view_elements = FilteredElementCollector(activedoc, source_view.Id).ToElements()
+    elements_ids_to_copy = []
+    for element in view_elements:
+        if isinstance(element, Viewport):
+            continue
+        else:
+            elements_ids_to_copy.append(element.Id)
+
+    options = CopyPasteOptions()
+    options.SetDuplicateTypeNamesHandler(CopyUseDestination())
+
+    t = Transaction(dest_doc, 'Copy Legends to this document')
+    t.Start()
+    copiedElement = ElementTransformUtils.CopyElements(source_view,
+                                                       List[ElementId](elements_ids_to_copy),
+                                                       new_view, None, options)
+    t.Commit()
+
+
+def copy_sheet_contents(*args):
+    copy_viewport_contents(*args)
+
+
+def copy_sheet_viewports(activedoc, source_sheet, dest_doc, new_sheet):
+    print('copy_sheet_viewports')
+    for vp in source_sheet.GetAllViewports():
+        
+
+
+for dest_doc in get_dest_docs():
+    print('\nCopying Sheet to Document: {0}'.format(dest_doc.Title))
+    sheets_to_be_copied = []
+    if selection.is_empty and isinstance(curview, ViewSheet):
+        sheets_to_be_copied.append(curview)
+    else:
+        sheets_to_be_copied = [x for x in selection if isinstance(x, ViewSheet)]
+
+    if not sheets_to_be_copied:
+        error_and_close('At least one sheet must be selection or be active.')
+
+    for source_sheet in sheets_to_be_copied:
+        new_sheet = copy_sheet_view(activedoc, source_sheet, dest_doc)
+        copy_sheet_contents(activedoc, source_sheet, dest_doc, new_sheet)
+        copy_sheet_viewports(activedoc, source_sheet, dest_doc, new_sheet)
