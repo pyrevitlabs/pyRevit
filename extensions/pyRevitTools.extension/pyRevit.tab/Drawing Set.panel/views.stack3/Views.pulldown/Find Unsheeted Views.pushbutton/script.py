55315c37da35dbe1d2313c73d6c599ab417ee532
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-06-06 08:48:14 -0700

    A little improvement on Find Unsheeted views tool

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Find Unsheeted Views.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Find Unsheeted Views.pushbutton/script.py
@@ -12,7 +12,7 @@ from Autodesk.Revit.DB import BuiltInCategory, View, ViewType
 dviews = []
 mviews = []
 lviews = []
-all_sheeted_view_ids = []
+all_sheeted_view_ids = set()
 
 views = Fec(doc).OfCategory(BuiltInCategory.OST_Views).WhereElementIsNotElementType().ToElements()
 for v in views:
@@ -26,35 +26,35 @@ for v in views:
 
 sheets = Fec(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
 for sht in sheets:
-    vp_ids = [doc.GetElement(x).ViewId for x in sht.GetAllViewports()]
-    all_sheeted_view_ids.extend(vp_ids)
+    vp_ids = [doc.GetElement(x).ViewId.IntegerValue for x in sht.GetAllViewports()]
+    all_sheeted_view_ids = all_sheeted_view_ids.union(vp_ids)
 
 
 this_script.output.print_md('### DRAFTING VIEWS NOT ON ANY SHEETS')
-for v in dviews:
-    if v.Id in all_sheeted_view_ids:
+for dv in dviews:
+    if dv.Id.IntegerValue in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
-                                                   v.ViewType,
-                                                   this_script.output.linkify(v.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(dv.ViewName,
+                                                   dv.ViewType,
+                                                   this_script.output.linkify(dv.Id)))
 
 
 this_script.output.print_md('### MODEL VIEWS NOT ON ANY SHEETS')
-for v in mviews:
-    if v.Id in all_sheeted_view_ids:
+for mv in mviews:
+    if mv.Id.IntegerValue in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
-                                                   v.ViewType,
-                                                   this_script.output.linkify(v.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(mv.ViewName,
+                                                   mv.ViewType,
+                                                   this_script.output.linkify(mv.Id)))
 
 
 this_script.output.print_md('### LEGENDS NOT ON ANY SHEETS')
-for v in lviews:
-    if v.Id in all_sheeted_view_ids:
+for lv in lviews:
+    if lv.Id.IntegerValue in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
-                                                   v.ViewType,
-                                                   this_script.output.linkify(v.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(lv.ViewName,
+                                                   lv.ViewType,
+                                                   this_script.output.linkify(lv.Id)))
