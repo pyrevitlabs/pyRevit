855b6c75f88dd304d00df9d0e933f6fe7f4f0209
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-07-04 10:42:13 -0700

    Resolved issue #209

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Find Unsheeted Views.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Find Unsheeted Views.pushbutton/script.py
@@ -1,20 +1,27 @@
 """Lists views that have not been placed on any sheets."""
 
 from scriptutils import this_script
-from revitutils import doc, uidoc
 
+from rpw import doc
+
+import clr
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.DB import FilteredElementCollector as Fec
 # noinspection PyUnresolvedReferences
-from Autodesk.Revit.DB import BuiltInCategory, View, ViewType
+from Autodesk.Revit.DB import BuiltInCategory, View, ViewType, \
+                              ScheduleSheetInstance, ViewSchedule
 
 
+# COLELCTING DATA --------------------------------------------------------------
 dviews = []
 mviews = []
 lviews = []
-all_sheeted_view_ids = set()
+scheduleviews = []
+all_sheeted_view_ids = []
 
-views = Fec(doc).OfCategory(BuiltInCategory.OST_Views).WhereElementIsNotElementType().ToElements()
+# Collecting all the model, drafting, and legend views
+views = Fec(doc).OfCategory(BuiltInCategory.OST_Views)\
+                .WhereElementIsNotElementType().ToElements()
 for v in views:
     if not v.IsTemplate:
         if v.ViewType == ViewType.DraftingView:
@@ -24,37 +31,65 @@ for v in views:
         else:
             mviews.append(v)
 
-sheets = Fec(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
+# Schedules need to be collected separately
+schedule_views = Fec(doc).OfClass(clr.GetClrType(ViewSchedule))\
+                         .WhereElementIsNotElementType().ToElements()
+for sv in schedule_views:
+    scheduleviews.append(sv)
+
+
+# Now collecting all sheets and find all sheeted views
+sheets = Fec(doc).OfCategory(BuiltInCategory.OST_Sheets)\
+                 .WhereElementIsNotElementType().ToElements()
 for sht in sheets:
-    vp_ids = [doc.GetElement(x).ViewId.IntegerValue for x in sht.GetAllViewports()]
-    all_sheeted_view_ids = all_sheeted_view_ids.union(vp_ids)
+    vp_ids = [doc.GetElement(x).ViewId for x in sht.GetAllViewports()]
+    all_sheeted_view_ids.extend(vp_ids)
 
 
-this_script.output.print_md('### DRAFTING VIEWS NOT ON ANY SHEETS')
-for dv in dviews:
-    if dv.Id.IntegerValue in all_sheeted_view_ids:
+# Find all sheeted schedule views and add them to the list as well
+allSheetedSchedules = Fec(doc).OfClass(ScheduleSheetInstance).ToElements()
+for ss in allSheetedSchedules:
+    all_sheeted_view_ids.append (ss.ScheduleId)
+
+# NOW LET'S REPORT -------------------------------------------------------------
+
+out = this_script.output
+
+out.print_md('### DRAFTING VIEWS NOT ON ANY SHEETS')
+for v in dviews:
+    if v.Id in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(dv.ViewName,
-                                                   dv.ViewType,
-                                                   this_script.output.linkify(dv.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
+                                                   v.ViewType,
+                                                   out.linkify(v.Id)))
 
 
-this_script.output.print_md('### MODEL VIEWS NOT ON ANY SHEETS')
-for mv in mviews:
-    if mv.Id.IntegerValue in all_sheeted_view_ids:
+out.print_md('### MODEL VIEWS NOT ON ANY SHEETS')
+for v in mviews:
+    if v.Id in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(mv.ViewName,
-                                                   mv.ViewType,
-                                                   this_script.output.linkify(mv.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
+                                                   v.ViewType,
+                                                   out.linkify(v.Id)))
 
 
-this_script.output.print_md('### LEGENDS NOT ON ANY SHEETS')
-for lv in lviews:
-    if lv.Id.IntegerValue in all_sheeted_view_ids:
+out.print_md('### LEGENDS NOT ON ANY SHEETS')
+for v in lviews:
+    if v.Id in all_sheeted_view_ids:
         continue
     else:
-        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(lv.ViewName,
-                                                   lv.ViewType,
-                                                   this_script.output.linkify(lv.Id)))
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
+                                                   v.ViewType,
+                                                   out.linkify(v.Id)))
+
+
+out.print_md('### SCHEDULES NOT ON ANY SHEETS')
+for v in scheduleviews:
+    if v.Id in all_sheeted_view_ids:
+        continue
+    else:
+        print('TYPE: {1}\t\tID: {2}\t\t{0}'.format(v.ViewName,
+                                                   v.ViewType,
+                                                   out.linkify(v.Id)))
