f82909b80c94b63903529f4c3345cbbfa2dc42f9
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-07-21 15:46:57 -0700

    Fixed error when using doc on a Family Document
    Family Documents don't have ProjectInformation

--- pyrevitlib/revitutils/_project.py
+++ pyrevitlib/revitutils/_project.py
@@ -4,8 +4,11 @@ from pyrevit import HOST_APP
 class CurrentProject:
     def __init__(self, document):
         self._doc = document
-        self._info = self._doc.ProjectInformation
-        self.name = self._info.Name
+
+        if not self._doc.IsFamilyDocument:
+            self._info = self._doc.ProjectInformation
+            self.name = self._info.Name
+
         self.location = self._doc.PathName
 
 
