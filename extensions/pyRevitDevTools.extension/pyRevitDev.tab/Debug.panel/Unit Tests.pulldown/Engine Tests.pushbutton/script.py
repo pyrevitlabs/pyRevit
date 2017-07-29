df8cd4dfb8fa7c9d54e9b74bd2d84637ed90cffd
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-07-29 14:43:52 -0700

    Added __cleanengine__ test and Updated Todo list

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Engine Tests.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Engine Tests.pushbutton/script.py
@@ -4,6 +4,11 @@
 __context__ = 'zerodoc'
 
 
-import pyrevit.unittests as prtests
+# import pyrevit.unittests as prtests
+#
+# prtests.perform_engine_tests()
 
-prtests.perform_engine_tests()
+__cleanengine__ = True
+
+import sys
+print len(sys.modules)
