bffb75c48deffa8fefd588acff66551d6d9157db
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-09-16 16:35:31 -0700

    Updated rpw test tool

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test RPW.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test RPW.pushbutton/script.py
@@ -1,32 +1,8 @@
-# import sys
-# from pyrevit.coreutils.logger import get_logger
-# logger = get_logger(__commandname__)
-
 from pyrevit.coreutils import Timer
 
-# timer = Timer()
-# import imp, sys
-# for mod_name, mod in sys.modules.items():
-#     if mod_name != 'imp':
-#         try:
-#             imp.reload(mod)
-#             # print('Succeeded reloading: {}'.format(mod_name))
-#         except:
-#             # print('------- errr reloading: {}'.format(mod_name))
-#             pass
-# endtime = timer.get_time()
-# print('module reload time: {} seconds'.format(endtime))
-
-
+# measure import time
 timer = Timer()
 import rpw
 from rpw import db, DB, extras
 endtime = timer.get_time()
 print('rpw load time: {} seconds'.format(endtime))
-
-from scriptutils import this_script
-print this_script.info
-
-import sys
-# print globals().keys()
-print '\n'.join(sorted(sys.modules.keys()))
