41be2405bcc57e2d32d917860bffafa1b3055203
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-24 10:20:11 -0700

    Corrected Installer paths for the new location of loader

--- release/install_addin.bat
+++ release/install_addin.bat
@@ -16,7 +16,7 @@ echo ^<?xml version="1.0" encoding="utf-8" standalone="no"?^>
 echo ^<RevitAddIns^>
 echo   ^<AddIn Type="Application"^>
 echo     ^<Name^>PyRevitLoader^</Name^>
-echo     ^<Assembly^>%installpath%\pyrevitlib\pyrevit\loader\addin\PyRevitLoader.dll^</Assembly^>
+echo     ^<Assembly^>%installpath%\pyrevitlib\pyrevit\loader\addin\277\PyRevitLoader.dll^</Assembly^>
 echo     ^<AddInId^>B39107C3-A1D7-47F4-A5A1-532DDF6EDB5D^</AddInId^>
 echo     ^<FullClassName^>PyRevitLoader.PyRevitLoaderApplication^</FullClassName^>
 echo   ^<VendorId^>eirannejad^</VendorId^>
