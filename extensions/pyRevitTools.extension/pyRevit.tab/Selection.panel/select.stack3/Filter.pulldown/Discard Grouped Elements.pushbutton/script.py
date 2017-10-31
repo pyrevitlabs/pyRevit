d46d40e36e7225f93e4f5cab27b16923f71b986d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-30 19:33:37 -0700

    Fixed Filter Pulldown tools

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack3/Filter.pulldown/Discard Grouped Elements.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack3/Filter.pulldown/Discard Grouped Elements.pushbutton/script.py
@@ -1,17 +1,17 @@
 """Discards (not delete) grouped elements from the current selection."""
 
-from revitutils import doc, uidoc, selection
-
-# noinspection PyUnresolvedReferences
-from Autodesk.Revit.DB import ElementId, Group
+from pyrevit import revit, DB
 
 
 __context__ = 'Selection'
 
+selection = revit.get_selection()
+
 
 filtered_elements = []
 for el in selection:
-    if el.GroupId == ElementId.InvalidElementId and not isinstance(el, Group):
+    if el.GroupId == DB.ElementId.InvalidElementId \
+            and not isinstance(el, DB.Group):
         filtered_elements.append(el.Id)
 
 selection.set_to(filtered_elements)
