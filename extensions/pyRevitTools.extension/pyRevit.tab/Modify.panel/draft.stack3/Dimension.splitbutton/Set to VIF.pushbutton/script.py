aac619722e11c3748ae6a67629a9d57498ea76f4
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-31 20:38:51 -0700

    Fixed Dimension pulldown

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/draft.stack3/Dimension.splitbutton/Set to VIF.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/draft.stack3/Dimension.splitbutton/Set to VIF.pushbutton/script.py
@@ -1,40 +1,13 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+"""Sets dimension value to VERIFY IN FIELD on selected dimensions."""
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+from pyrevit import revit, DB
 
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
-
-__doc__ = 'Sets dimension value to VERIFY IN FIELD on selected dimensions.'
-
-__window__.Close()
-from Autodesk.Revit.DB import Transaction, Dimension
-
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-
-t = Transaction(doc, 'VIF dimensions')
-t.Start()
-
-for elId in uidoc.Selection.GetElementIds():
-    el = doc.GetElement(elId)
-    if isinstance(el, Dimension):
-        if len(list(el.Segments)) > 0:
-            for seg in el.Segments:
-                seg.Below = 'VIF'
-        else:
-            el.Below = 'VIF'
-
-t.Commit()
+with revit.Transaction('VIF dimensions'):
+    for elId in revit.uidoc.Selection.GetElementIds():
+        el = revit.doc.GetElement(elId)
+        if isinstance(el, DB.Dimension):
+            if len(list(el.Segments)) > 0:
+                for seg in el.Segments:
+                    seg.Below = 'VIF'
+            else:
+                el.Below = 'VIF'
