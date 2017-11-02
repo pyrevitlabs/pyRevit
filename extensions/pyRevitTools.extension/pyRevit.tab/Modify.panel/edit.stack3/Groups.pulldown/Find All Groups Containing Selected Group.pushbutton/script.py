a6a8471dcb5ca2af33dee585738b7f2981d93280
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-01 19:40:32 -0700

    Fixed group pulldown

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit.stack3/Groups.pulldown/Find All Groups Containing Selected Group.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit.stack3/Groups.pulldown/Find All Groups Containing Selected Group.pushbutton/script.py
@@ -1,52 +1,33 @@
-"""
-Copyright (c) 2014-2017 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+"""List all groups that includethe selected group element as a nested group."""
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
+from pyrevit import framework
+from pyrevit import revit, DB
 
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+selection = revit.get_selection()
 
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
-"""
 
-__doc__ = 'List all groups that includethe selected group element as a nested group.'
-
-import clr
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Element, Group, GroupType, Transaction
-
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-selection = list(uidoc.Selection.GetElementIds())
-
-cl = FilteredElementCollector(doc)
-grps = list(cl.OfClass(clr.GetClrType(Group)).ToElements())
+grps = list(DB.FilteredElementCollector(revit.doc)
+              .OfClass(framework.get_type(DB.Group))
+              .ToElements())
 
 grpTypes = set()
 
+
 if len(selection) > 0:
-    elid = selection.pop()
-    el = doc.GetElement(elid)
-    if isinstance(el, Group):
-        selectedgtid = el.GroupType.Id
-        for g in grps:
-            mems = g.GetMemberIds()
-            for memid in mems:
-                mem = doc.GetElement(memid)
-                if isinstance(mem, Group):
-                    memgtid = mem.GroupType.Id
-                    if memgtid == selectedgtid:
-                        grpTypes.add(g.GroupType.Id)
+    for el in selection:
+        if isinstance(el, DB.Group):
+            selectedgtid = el.GroupType.Id
+            for g in grps:
+                mems = g.GetMemberIds()
+                for memid in mems:
+                    mem = revit.doc.GetElement(memid)
+                    if isinstance(mem, DB.Group):
+                        memgtid = mem.GroupType.Id
+                        if memgtid == selectedgtid:
+                            grpTypes.add(g.GroupType.Id)
 
-        for gtId in grpTypes:
-            print(Element.Name.GetValue(doc.GetElement(gtId)))
-
-else:
-    pass
+            for gtId in grpTypes:
+                wrapped_gtype = \
+                    revit.ElementWrapper(revit.doc.GetElement(gtId))
+                print(wrapped_gtype.name)
