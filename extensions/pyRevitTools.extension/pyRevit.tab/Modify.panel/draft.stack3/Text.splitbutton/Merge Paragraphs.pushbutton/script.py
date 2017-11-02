927b1910a7f7dcf8cfc2f8d2570e862da56d40b2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-01 18:29:25 -0700

    Added tests for empty selection

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/draft.stack3/Text.splitbutton/Merge Paragraphs.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/draft.stack3/Text.splitbutton/Merge Paragraphs.pushbutton/script.py
@@ -10,13 +10,14 @@ selection = revit.get_selection()
 with revit.Transaction('Merge Paragraphs'):
     tnotes = sorted(selection, key=lambda txnote: 0 - txnote.Coord.Y)
 
-    mtxt = tnotes[0]
-    mtxtwidth = mtxt.Width
-    for txt in tnotes[1:]:
-        if txt.Text[0] == '\r\n\r\n':
-            mtxt.Text = mtxt.Text + txt.Text
-        else:
-            mtxt.Text = mtxt.Text + '\r\n\r\n' + txt.Text
-        revit.doc.Delete(txt.Id)
+    if len(tnotes) > 1:
+        mtxt = tnotes[0]
+        mtxtwidth = mtxt.Width
+        for txt in tnotes[1:]:
+            if txt.Text[0] == '\r\n\r\n':
+                mtxt.Text = mtxt.Text + txt.Text
+            else:
+                mtxt.Text = mtxt.Text + '\r\n\r\n' + txt.Text
+            revit.doc.Delete(txt.Id)
 
-    mtxt.Width = mtxtwidth
+        mtxt.Width = mtxtwidth
