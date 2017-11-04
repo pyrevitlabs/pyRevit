3a60bbdf074cc146817102436105eb59217adf24
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-04 15:28:53 -0700

    Fixed View pulldown

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Hide All Grid Bubbles in Current View.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack3/Views.pulldown/Hide All Grid Bubbles in Current View.pushbutton/script.py
@@ -1,18 +1,18 @@
 """Hides all grid bubbles in the active view."""
 
 
-from pyrevit.revit import doc, DB
+from pyrevit import revit, DB
 
 
-grids = DB.FilteredElementCollector(__activedoc__)\
+grids = DB.FilteredElementCollector(revit.doc)\
           .OfCategory(DB.BuiltInCategory.OST_Grids)\
           .WhereElementIsNotElementType().ToElements()
 
 
-with doc.Transaction('Hide Grid Bubbles'):
-    activeview = __activeuidoc__.ActiveView
+with revit.Transaction('Hide Grid Bubbles'):
+    activeview = revit.activeview
     for grid in grids:
         grid.HideBubbleInView(DB.DatumEnds.End0, activeview)
         grid.HideBubbleInView(DB.DatumEnds.End1, activeview)
 
-__activeuidoc__.RefreshActiveView()
+revit.uidoc.RefreshActiveView()
