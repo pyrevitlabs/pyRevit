843e4b5174a670d17aaa717e74aed2c09998bcc5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-03 20:58:46 -0700

    Figured out how to get the proper ribbon button obj

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test Extended Tooltips.pushbutton/Tests/tooltiptest.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test Extended Tooltips.pushbutton/Tests/tooltiptest.py
@@ -11,23 +11,15 @@ from pyrevit.coreutils import ribbon
 
 ui = ribbon.get_current_ui()
 rb = ui.ribbon_tab('Tesla').ribbon_panel('General').ribbon_item('Tesla').get_rvtapi_object()
-# meth = rb.GetType().GetMethod('get_ToolTip', BindingFlags.Public | BindingFlags.Instance)
-# r = meth.Invoke(rb, None)
-
-# tt = AdWindows.RibbonToolTip()
-# tt.Title = "This is important button1"
-# tt.ExpandedContent = "tooltip text"
-# tt.ExpandedVideo = Uri('path/to/tooltip.swf')
-
-# r.ExpandedVideo = Uri(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev\pyRevitDev\extensions\pyRevitDevTools.extension\pyRevitDev.tab\Debug.panel\Unit Tests.pulldown\Test Extended Tooltips.pushbutton\tooltip.swf')
-# r.HelpSource = Uri('https://www.google.com/')
-# r.HelpTopic = ''
-
+meth = rb.GetType().GetMethod('getRibbonItem', BindingFlags.NonPublic | BindingFlags.Instance)
+ri = meth.Invoke(rb, None)
 
 tt = AdWindows.RibbonToolTip()
+tt.IsHelpEnabled = True
+tt.HelpTopic = ''
 tt.HelpSource = Uri('https://www.google.com/')
 tt.Title = "This is important button1"
 tt.ExpandedContent = "tooltip text"
 tt.ExpandedVideo = Uri(r'C:\Users\eirannejad\AppData\Roaming\pyRevit\pyRevitDev\pyRevitDev\extensions\pyRevitDevTools.extension\pyRevitDev.tab\Debug.panel\Unit Tests.pulldown\Test Extended Tooltips.pushbutton\tooltip.swf')
 
-rb.ToolTip = tt
+ri.ToolTip = tt
