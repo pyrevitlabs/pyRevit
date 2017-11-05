9950738bd5eebb16627b02a8f0fa91b3fa511996
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-04 18:02:02 -0800

    Fixed family pulldown

--- extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack3/Family.pulldown/Purge And Reset All Unused Family Parameters.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack3/Family.pulldown/Purge And Reset All Unused Family Parameters.pushbutton/script.py
@@ -1,34 +1,39 @@
-import sys
-import clr
-from Autodesk.Revit.DB import Transaction, ElementId, StorageType, FilteredElementCollector, Dimension, FamilyParameter
-from Autodesk.Revit.UI import TaskDialog, TaskDialogCommonButtons, TaskDialogResult
+from pyrevit import revit, DB, UI
+from pyrevit import script
 
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
 
 __doc__ = 'This script removes all custom parameters that has not been used '\
           'in dimensions as labels and also resets the value for the other ' \
           'parameters to zero or null.'
 
-res = TaskDialog.Show('pyRevit',
-                      'Make sure your models are saved and synced. '
-                      'Hit OK to continue...',
-                      TaskDialogCommonButtons.Ok | TaskDialogCommonButtons.Cancel)
 
-if not res == TaskDialogResult.Ok:
-    sys.exit()
+logger = script.get_logger()
 
-if doc.IsFamilyDocument:
-    params = doc.FamilyManager.GetParameters()
-    dims = FilteredElementCollector(doc).OfClass(Dimension).WhereElementIsNotElementType()
-    allelements = FilteredElementCollector(doc).WhereElementIsNotElementType()
+res = \
+    UI.TaskDialog.Show('pyRevit',
+                       'Make sure your models are saved and synced. '
+                       'Hit OK to continue...',
+                       UI.TaskDialogCommonButtons.Ok
+                       | UI.TaskDialogCommonButtons.Cancel)
+
+if not res == UI.TaskDialogResult.Ok:
+    script.exit()
+
+if revit.doc.IsFamilyDocument:
+    params = revit.doc.FamilyManager.GetParameters()
+    dims = DB.FilteredElementCollector(revit.doc)\
+             .OfClass(DB.Dimension)\
+             .WhereElementIsNotElementType()
+
+    allelements = DB.FilteredElementCollector(revit.doc)\
+                    .WhereElementIsNotElementType()
 
     labelParams = set()
     for d in dims:
         try:
-            if isinstance(d.FamilyLabel, FamilyParameter):
+            if isinstance(d.FamilyLabel, DB.FamilyParameter):
                 labelParams.add(d.FamilyLabel.Id.IntegerValue)
-        except:
+        except Exception:
             continue
 
     visibParams = set()
@@ -36,43 +41,51 @@ if doc.IsFamilyDocument:
         try:
             visibparam = el.LookupParameter('Visible')
             if visibparam is not None:
-                famvisibparam = doc.FamilyManager.GetAssociatedFamilyParameter(visibparam)
-                if famvisibparam is not None and isinstance(famvisibparam, FamilyParameter):
+                famvisibparam = \
+                    revit.doc.FamilyManager.GetAssociatedFamilyParameter(
+                        visibparam
+                        )
+
+                if famvisibparam is not None \
+                        and isinstance(famvisibparam, DB.FamilyParameter):
                     visibParams.add(famvisibparam.Id.IntegerValue)
-        except:
+        except Exception:
             continue
 
     print('STARTING CLEANUP...')
-    t = Transaction(doc, 'Remove all family parameters')
-    t.Start()
 
-    for param in params:
-        try:
-            print('\nREMOVING FAMILY PARAMETER:\nID: {0}\tNAME: {1}'.format(param.Id, param.Definition.Name))
-            if param.Id.IntegerValue not in labelParams and param.Id.IntegerValue not in visibParams:
-                doc.FamilyManager.RemoveParameter(param)
-                print('REMOVED.')
-            else:
-                print('NOT REMOVED. PARAMETER IS BEING USED AS A LABEL.')
-        except:
-            print('-- CAN NOT DELETE --')
+    with revit.Transaction('Remove all family parameters'):
+        for param in params:
             try:
-                if param.CanAssignFormula:
-                    doc.FamilyManager.SetFormula(param, None)
-                if param.StorageType == StorageType.Integer or param.StorageType == StorageType.Double:
-                    doc.FamilyManager.Set(param, 0)
-                    print('-- PARAMETER VALUE SET TO INTEGER 0')
-                elif param.StorageType == StorageType.String:
-                    doc.FamilyManager.Set(param, '')
-                    print('-- PARAMETER VALUE SET TO EMPTY STRING.')
+                print('\nREMOVING FAMILY PARAMETER:\nID: {0}\tNAME: {1}'
+                      .format(param.Id, param.Definition.Name))
+                if param.Id.IntegerValue not in labelParams \
+                        and param.Id.IntegerValue not in visibParams:
+                    revit.doc.FamilyManager.RemoveParameter(param)
+                    print('REMOVED.')
                 else:
-                    print('-- PARAMETER TYPE IS UNKNOWN. CAN NOT RESET VALUE.')
-            except Exception as e:
-                print e
+                    print('NOT REMOVED. PARAMETER IS BEING USED AS A LABEL.')
+            except Exception:
+                print('-- CAN NOT DELETE --')
+                try:
+                    if param.CanAssignFormula:
+                        revit.doc.FamilyManager.SetFormula(param, None)
+                    if param.StorageType == DB.StorageType.Integer \
+                            or param.StorageType == DB.StorageType.Double:
+                        revit.doc.FamilyManager.Set(param, 0)
+                        print('-- PARAMETER VALUE SET TO INTEGER 0')
+                    elif param.StorageType == DB.StorageType.String:
+                        revit.doc.FamilyManager.Set(param, '')
+                        print('-- PARAMETER VALUE SET TO EMPTY STRING.')
+                    else:
+                        print('-- PARAMETER TYPE IS UNKNOWN. '
+                              'CAN NOT RESET VALUE.')
+                except Exception as e:
+                    logger.error(e)
+                    continue
                 continue
-            continue
+
     print('\n\nALL DONE.....................................')
-    t.Commit()
 else:
-    __window__.Close()
-    TaskDialog.Show('pyrevit', 'This script works only on an active family editor.')
+    UI.TaskDialog.Show('pyrevit',
+                       'This script works only on an active family editor.')
