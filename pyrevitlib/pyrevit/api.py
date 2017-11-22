dbf435d9f30b32e5a034b49fb83a503c5cad6f46
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-21 21:29:25 -0800

    Added imports to a couple more modules shipped with Revit

--- pyrevitlib/pyrevit/api.py
+++ pyrevitlib/pyrevit/api.py
@@ -3,8 +3,18 @@ from pyrevit.framework import clr
 clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI')
 clr.AddReference('AdWindows')
+clr.AddReference('UIFramework')
+clr.AddReference('UIFrameworkServices')
+clr.AddReference('Newtonsoft.Json')
 
+import Autodesk.Internal as AdInternal
+import Autodesk.Private as AdPrivate
 import Autodesk.Windows as AdWindows
+
+import UIFramework
+import UIFrameworkServices
+
+import Newtonsoft.Json as NSJson
 
 import Autodesk.Revit.Attributes as Attributes
 
