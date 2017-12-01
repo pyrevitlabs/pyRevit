7b3ad53c6819c5c24461c6576dfaee3af37f0ea6
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-30 16:49:41 -0800

    Resolved issue #235

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Sheets.pulldown/Reorder Selected Viewport.pushbutton/script.py
@@ -0,0 +1,58 @@
+from collections import namedtuple
+
+from pyrevit import revit, DB
+from pyrevit import forms
+
+
+ViewportConfig = namedtuple('ViewportConfig', ['box_center',
+                                               'type_id',
+                                               'view_id'])
+
+
+def get_selected_viewports():
+    selection = revit.get_selection()
+    return [x for x in selection if isinstance(x, DB.Viewport)]
+
+
+def get_nonselected_viewports(sheet):
+    svps = get_selected_viewports()
+    svp_ids = [x.Id for x in svps]
+    vps = sheet.GetAllViewports()
+    if svp_ids:
+        return [revit.doc.GetElement(x) for x in vps if x not in svp_ids]
+    else:
+        return []
+
+
+def capture_vp_config(viewport):
+    return ViewportConfig(viewport.GetBoxCenter(),
+                          viewport.GetTypeId(),
+                          viewport.ViewId)
+
+
+def bring_to_front(sheet, vp_list):
+    with revit.Transaction('Reorder Viewports'):
+        for vp in vp_list:
+            config = capture_vp_config(vp)
+            sheet.DeleteViewport(vp)
+            new_vp = DB.Viewport.Create(revit.doc,
+                                        sheet.Id,
+                                        config.view_id,
+                                        config.box_center)
+            new_vp.ChangeTypeId(config.type_id)
+
+
+selected_option = \
+    forms.CommandSwitchWindow.show(
+        ['Bring to Front', 'Send to Back'],
+        message='Select reordering method:'
+    )
+
+
+if selected_option:
+    if selected_option == 'Bring to Front':
+        viewports = get_selected_viewports()
+    elif selected_option == 'Send to Back':
+        viewports = get_nonselected_viewports(revit.activeview)
+
+    bring_to_front(revit.activeview, viewports)
