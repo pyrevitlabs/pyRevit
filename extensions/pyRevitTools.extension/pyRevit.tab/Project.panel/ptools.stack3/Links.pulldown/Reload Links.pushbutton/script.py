ac51e297e74a82223680e55ad7c87318da9919fe
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-12-02 14:39:50 -0800

    Improved link tools

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack3/Links.pulldown/Reload Links.pushbutton/script.py
@@ -0,0 +1,51 @@
+"""Reloads all linked Revit models."""
+
+from pyrevit import HOST_APP
+from pyrevit import revit, DB, UI
+from pyrevit import forms
+
+
+def reload_links(linktype=DB.ExternalFileReferenceType.RevitLink):
+    location = revit.doc.PathName
+    try:
+        modelPath = \
+            DB.ModelPathUtils.ConvertUserVisiblePathToModelPath(location)
+
+        transData = DB.TransmissionData.ReadTransmissionData(modelPath)
+
+        externalReferences = transData.GetAllExternalFileReferenceIds()
+        for refId in externalReferences:
+            extRef = transData.GetLastSavedReferenceData(refId)
+            if extRef.ExternalFileReferenceType == \
+                    DB.ExternalFileReferenceType.RevitLink:
+                link = revit.doc.GetElement(refId)
+                path = \
+                    DB.ModelPathUtils.ConvertModelPathToUserVisiblePath(
+                        extRef.GetPath()
+                        )
+
+                if not path:
+                    path = '--NOT ASSIGNED--'
+
+                fref = str(extRef.ExternalFileReferenceType) + ':'
+                print("Reloading...\n{0}{1}".format(fref, path))
+                link.Reload()
+                print('Done\n')
+    except Exception:
+        UI.TaskDialog.Show('pyRevit',
+                           'Model is not saved yet. Can not acquire location.')
+
+
+linktypes = {'Revit Links': DB.ExternalFileReferenceType.RevitLink,
+             'Keynote Table': DB.ExternalFileReferenceType.KeynoteTable,
+             'Images': DB.ExternalFileReferenceType.Decal}
+
+if HOST_APP.is_newer_than(2017):
+    linktypes['CAD Links'] = DB.ExternalFileReferenceType.CADLink
+
+selected_option = \
+    forms.CommandSwitchWindow.show(linktypes.keys(),
+                                   message='Select link type:')
+
+if selected_option:
+    reload_links(linktypes[selected_option])
