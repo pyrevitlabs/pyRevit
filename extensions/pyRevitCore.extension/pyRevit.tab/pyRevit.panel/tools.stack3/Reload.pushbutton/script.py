451937ea33c27ded15e47f9f709796c875759ede
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-12-11 13:37:24 -0800

    Cleand up reload script

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Reload.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/tools.stack3/Reload.pushbutton/script.py
@@ -1,12 +1,14 @@
-__cleanengine__ = True
-__context__ = 'zerodoc'
-__doc__ = 'Searches the script folders and create buttons ' \
-          'for the new script or newly installed extensions.'
-
+"""Reload pyRevit into new session."""
 
 from pyrevit import script
 from pyrevit.loader import sessionmgr
 from pyrevit.loader import sessioninfo
+
+
+__cleanengine__ = True
+__context__ = 'zerodoc'
+__doc__ = 'Searches the script folders and create buttons ' \
+          'for the new script or newly installed extensions.'
 
 
 logger = script.get_logger()
