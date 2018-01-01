acaa791d63711d88652967fc5e00c432e73a62b0
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-01-01 11:17:43 -0800

    Copyright update to 2018

--- pyrevitlib/pyrevit/versionmgr/about.py
+++ pyrevitlib/pyrevit/versionmgr/about.py
@@ -10,4 +10,4 @@ PyRevitAbout = namedtuple('PyRevitAbout', ['subtitle',
 def get_pyrevit_about():
     return PyRevitAbout(subtitle='python tools for Autodesk Revit®',
                         madein="['pdx', 'hio', 'rno']",
-                        copyright='© 2015-2017 Ehsan Iran-Nejad')
+                        copyright='© 2014-2018 Ehsan Iran-Nejad')
