e7ce9c98450d58c9fe7f1604f25f11eb8ee51f25
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-01-01 11:21:35 -0800

    Updated rpw import test

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test RPW.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test RPW.pushbutton/script.py
@@ -4,5 +4,7 @@ from pyrevit.coreutils import Timer
 timer = Timer()
 import rpw
 from rpw import db, DB, extras
+from rpw import __version__
 endtime = timer.get_time()
 print('rpw load time: {} seconds'.format(endtime))
+print('rpw version: {}'.format(__version__))
