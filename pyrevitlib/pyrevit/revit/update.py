c73976f48ab6b3be6e69c8710f34b793d7896c42
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-02-28 15:27:10 -0800

    Fixed document bug in revision update method and cleanups

--- pyrevitlib/pyrevit/revit/update.py
+++ pyrevitlib/pyrevit/revit/update.py
@@ -7,36 +7,38 @@ from pyrevit import DB
 from pyrevit.revit import query
 
 
-def update_sheet_revisions(revisions, sheets, state=True, doc=None):
+def update_sheet_revisions(revisions, sheets=None, state=True, doc=None):
     doc or HOST_APP.doc
 
     # get revisions if not set
-    revisions = revisions or query.get_revisions()
-    if type(revisions) is not list:
+    revisions = revisions or query.get_revisions(doc=doc)
+    if not isinstance(revisions, list):
         revisions = [revisions]
     # get sheets if not available
-    sheets = sheets or query.get_sheets()
+    sheets = sheets or query.get_sheets(doc=doc)
 
-    cloudedsheets = []
+    # cloudedsheets = []
     updated_sheets = []
     for s in sheets:
         for r in revisions:
-            revs = set([x.IntegerValue for x in s.GetAllRevisionIds()])
-            addrevs = set([x.IntegerValue
-                           for x in s.GetAdditionalRevisionIds()])
-            cloudrevs = revs - addrevs
-            if r.Id.IntegerValue in cloudrevs:
-                cloudedsheets.append(s)
-                continue
+            # skip issued revisions
+            if not r.Issued:
+                # revs = set([x.IntegerValue for x in s.GetAllRevisionIds()])
+                addrevs = set([x.IntegerValue
+                               for x in s.GetAdditionalRevisionIds()])
+                # cloudrevs = revs - addrevs
+                # if r.Id.IntegerValue in cloudrevs:
+                #     cloudedsheets.append(s)
+                #     continue
 
-            if state:
-                addrevs.add(r.Id.IntegerValue)
-            elif r.Id.IntegerValue in addrevs:
-                addrevs.remove(r.Id.IntegerValue)
+                if state:
+                    addrevs.add(r.Id.IntegerValue)
+                elif r.Id.IntegerValue in addrevs:
+                    addrevs.remove(r.Id.IntegerValue)
 
-            rev_elids = [DB.ElementId(x) for x in addrevs]
-            s.SetAdditionalRevisionIds(List[DB.ElementId](rev_elids))
-            updated_sheets.append(s)
+                rev_elids = [DB.ElementId(x) for x in addrevs]
+                s.SetAdditionalRevisionIds(List[DB.ElementId](rev_elids))
+                updated_sheets.append(s)
 
     return updated_sheets
 
