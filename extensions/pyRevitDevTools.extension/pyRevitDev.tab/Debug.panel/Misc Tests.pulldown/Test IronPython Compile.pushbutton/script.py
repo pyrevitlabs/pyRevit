a8cfcf4622846edc183ec18b2a4ae505b9ffb3b0
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-03-14 18:01:31 -0700

    Updated test script to know if the file exists

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test IronPython Compile.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test IronPython Compile.pushbutton/script.py
@@ -4,11 +4,17 @@ import clr
 
 from pyrevit import USER_SYS_TEMP
 from pyrevit import script
+from pyrevit.framework import IO
 
 # compile
-source = script.get_bundle_file('ipycompiletest.py')
-dest = op.join(USER_SYS_TEMP, 'compiledipytest.dll')
-clr.CompileModules(dest, source)
+try:
+    source = script.get_bundle_file('ipycompiletest.py')
+    dest = op.join(USER_SYS_TEMP, 'compiledipytest.dll')
+    clr.CompileModules(dest, source)
+except IO.IOException as ioerr:
+    print('DLL file already exists...')
+except Exception as cerr:
+    print('Compilation failed: {}'.format(cerr))
 
 # import test
 sys.path.append(USER_SYS_TEMP)
