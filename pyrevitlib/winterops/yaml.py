5b8ace81582bdf0d42a6b35a2ba0709995dd0c41
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-05-24 12:02:21 -0700

    Updated yaml to return python dict as well
    Updated docs

--- pyrevitlib/winterops/yaml.py
+++ pyrevitlib/winterops/yaml.py
@@ -1,3 +1,4 @@
+"""Wrapper for YamlDotNet."""
 # pylama:ignore=E402,W0611
 from winterops import clr, System
 
@@ -6,9 +7,47 @@ clr.AddReferenceByName('YamlDotNet')
 import YamlDotNet as libyaml
 
 
+def _convert_yamldotnet_to_dict(ynode, level=0):
+    # logger.debug('{}* Processing: {}'.format(' '*level, type(ynode)))
+    if hasattr(ynode, 'Children'):
+        d = dict()
+        value_childs = []
+        for child in ynode.Children:
+            # logger.debug('{}+ Child: {}'.format(' '*level, child))
+            if hasattr(child, 'Key') and hasattr(child, 'Value'):
+                d[child.Key.Value] = \
+                    _convert_yamldotnet_to_dict(child.Value, level=level+1)
+            else:
+                value_childs.append(child.Value)
+        return value_childs or d
+    else:
+        # logger.debug('{}- Child: {}'.format(' '*level, ynode.Value))
+        return ynode.Value
+
+
 def load(yaml_file):
+    """Load Yaml file into YamlDotNet object.
+
+    Args:
+        yaml_file (str): fiel path to yaml file
+
+    Returns:
+        obj`YamlDotNet.RepresentationModel.YamlMappingNode`: yaml node
+    """
     with open(yaml_file, 'r') as yamlfile:
         yamlstr = libyaml.RepresentationModel.YamlStream()
         yamlstr.Load(System.IO.StringReader(yamlfile.read()))
         if yamlstr.Documents.Count >= 1:
             return yamlstr.Documents[0].RootNode
+
+
+def load_as_dict(yaml_file):
+    """Load Yaml file into python dict object.
+
+    Args:
+        yaml_file (str): fiel path to yaml file
+
+    Returns:
+        obj`dict`: dictionary representing yaml data
+    """
+    return _convert_yamldotnet_to_dict(load(yaml_file))
