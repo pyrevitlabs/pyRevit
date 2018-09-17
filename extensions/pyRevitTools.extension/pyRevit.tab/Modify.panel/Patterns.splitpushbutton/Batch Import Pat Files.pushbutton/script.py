15b7f7c9ea52f79df92bbcaf35c0ab4cb733ec2c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-09-17 15:39:12 -0700

    Finished and tested pattern batch import tool

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/Patterns.splitpushbutton/Batch Import Pat Files.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/Patterns.splitpushbutton/Batch Import Pat Files.pushbutton/script.py
@@ -1,8 +1,9 @@
 """Batch import AutoCAD PAT files into Revit model."""
 
 import math
+from collections import namedtuple
 
-from pyrevit.framework import List
+from pyrevit import framework
 from pyrevit import revit, DB
 from pyrevit import forms
 from pyrevit import script
@@ -13,34 +14,76 @@ logger = script.get_logger()
 output = script.get_output()
 
 
-def extract_patstrings(patfile):
-    patstrings = []
-    active_patstring = ''
+MM_SCALE = 0.00328084
+INCH_SCALE = 1/12.0
+
+# namedtuple for patter contents
+PatDef = namedtuple('PatDef', ['name', 'comments',
+                               'version', 'units', 'type',
+                               'grids'])
+
+
+def extract_patdefs(patfile):
+    patdefs = []
+    active_name = ''
+    active_comments= ''
+    active_version = ''
+    active_units = 'MM'
+    active_type = 'DRAFTING'
+    active_grids = []
     print('Extracting patterns from {}'.format(patfile))
     with open(patfile, 'r') as patf:
         for patline in patf.readlines():
             # skip if patline is a comment line
             if patline.startswith(';'):
-                continue
+                cleanedpatline = patline.replace('\n', '')
+                if '%VERSION' in cleanedpatline:
+                    active_version = cleanedpatline.split('=')[1]
+                elif '%UNITS' in cleanedpatline:
+                    active_units = cleanedpatline.split('=')[1].upper()
+                elif '%TYPE' in cleanedpatline:
+                    active_type = cleanedpatline.split('=')[1].upper()
+                else:
+                    continue
 
             # start a new patstring if starts with *
             elif patline.startswith('*'):
-                if active_patstring:
-                    patstrings.append(active_patstring)
-                active_patstring = patline
-            
-            # treat as pattern definition if 
-            elif ',' in patline:
-                active_patstring += patline
+                if active_grids:
+                    patdefs.append(
+                        PatDef(name=active_name,
+                               comments=active_comments,
+                               version=active_version,
+                               units=active_units,
+                               type=active_type,
+                               grids=active_grids)
+                    )
+                # grab name and comments of new pattern
+                # effectively starting a new capture
+                active_name, active_comments = \
+                    patline.replace('\n', '')[1:].split(',')
+                active_version = ''
+                active_units = 'MM'
+                active_type = 'DRAFTING'
+                active_grids = []
+                
+            # treat as pattern grid if split by , has 5 or more elements
+            elif len(patline.split(',')) >= 5:
+                active_grids.append(patline)
 
     # make sure last pattern is also added
-    if active_patstring:
-        patstrings.append(active_patstring)
+    if active_grids:
+        patdefs.append(PatDef(name=active_name,
+                              comments=active_comments,
+                              version=active_version,
+                              units=active_units,
+                              type=active_type,
+                              grids=active_grids))
 
-    return patstrings
+    return patdefs
 
 
 def make_fillgrid(gridstring, scale=0.00328084):
+    logger.debug(gridstring, scale)
     rvt_fill_grid = DB.FillGrid()
     griddata = [float(x.strip()) for x in gridstring.split(',') if x]
     if griddata:
@@ -56,32 +99,62 @@ def make_fillgrid(gridstring, scale=0.00328084):
         return rvt_fill_grid
 
 
-def make_pattern(patstring):
-    # grab the name
-    datastrings = patstring.split('\n')
-    patname = datastrings[0][1:].split(',')[0]
+def get_existing_fillpatternelement(fpname, fptarget):
+    existing_fillpatternelements = \
+        DB.FilteredElementCollector(revit.doc)\
+        .OfClass(framework.get_type(DB.FillPatternElement))
+
+    for exfpe in existing_fillpatternelements:
+        exfp = exfpe.GetFillPattern()
+        if fpname == exfp.Name \
+                and fptarget == exfp.Target:
+            return exfpe
+
+
+def make_pattern(patdef):
+    logger.debug(patdef)
+
+    # make the grids
     fill_grids = []
-    for dstring in  datastrings[1:]:
-        fgrid = make_fillgrid(dstring)
+    for pgrid in patdef.grids:
+        fgrid = make_fillgrid(pgrid,
+                              scale=MM_SCALE if patdef.units=='MM'
+                                    else INCH_SCALE)
         if fgrid:
             fill_grids.append(fgrid)
-    fp_target = DB.FillPatternTarget.Drafting # .Model
-    rvt_fill_pat = DB.FillPattern(patname,
-                                  fp_target,
-                                  DB.FillPatternHostOrientation.ToHost)
+    # determine pattern type
+    fp_target = \
+        DB.FillPatternTarget.Model if patdef.type=='MODEL' \
+        else DB.FillPatternTarget.Drafting
 
-    rvt_fill_pat.SetFillGrids(List[DB.FillGrid](fill_grids))
-    fill_pat_element = DB.FillPatternElement.Create(revit.doc, rvt_fill_pat)
+    # check if fillpattern element exists
+    existing_fpelement = get_existing_fillpatternelement(patdef.name, fp_target)
 
-    logger.debug('Fill Pattern:{}'.format(fill_pat_element.Name))
-    fp = fill_pat_element.GetFillPattern()
-    logger.debug('Fill Grids Count: {}'.format(len(fp.GetFillGrids())))
-    for idx, fg in enumerate(fp.GetFillGrids()):
-        logger.debug('FillGrid #{} '
-                        'Origin:{} Angle:{} Shift:{} Offset:{} Segments:{}'
-                        .format(idx, fg.Origin,
-                                fg.Angle, fg.Shift,
-                                fg.Offset, fg.GetSegments()))
+    # make fillpattern
+    rvt_fill_pat = DB.FillPattern(patdef.name,
+                                fp_target,
+                                DB.FillPatternHostOrientation.ToHost)
+    rvt_fill_pat.SetFillGrids(framework.List[DB.FillGrid](fill_grids))
+
+    if existing_fpelement:
+        existing_fpelement.SetFillPattern(rvt_fill_pat)
+        logger.debug('Updated FillPatternElement with id:{}'
+                     .format(existing_fpelement.Id))
+    else:
+        # make pattern element
+        logger.debug('Creating new fillpattern element...')
+        fill_pat_element = DB.FillPatternElement.Create(revit.doc, rvt_fill_pat)
+
+        # log results
+        logger.debug('Fill Pattern:{}'.format(fill_pat_element.Name))
+        fp = fill_pat_element.GetFillPattern()
+        logger.debug('Fill Grids Count: {}'.format(len(fp.GetFillGrids())))
+        for idx, fg in enumerate(fp.GetFillGrids()):
+            logger.debug('FillGrid #{} '
+                            'Origin:{} Angle:{} Shift:{} Offset:{} Segments:{}'
+                            .format(idx, fg.Origin,
+                                    fg.Angle, fg.Shift,
+                                    fg.Offset, fg.GetSegments()))
 
 
 def get_files():
@@ -92,6 +165,9 @@ patfiles = get_files()
 if patfiles:
     with revit.Transaction("Batch import PAT files"):
         for patfile in patfiles:
-            patstrings = extract_patstrings(patfile)
-            for patstring in patstrings:
-                make_pattern(patstring)
+            patdefs = extract_patdefs(patfile)
+            for patdef in patdefs:
+                print('Processing "{}" @ "{}" ({}, {})'
+                      .format(patdef.name, patfile, patdef.units, patdef.type))
+                make_pattern(patdef)
+    print('Done...')
