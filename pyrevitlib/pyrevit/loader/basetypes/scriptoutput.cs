d326d1d205aeb72dc66b59331df6b3796613b27c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-10-01 12:39:58 -0700

    Added print preview button to output window

--- pyrevitlib/pyrevit/loader/basetypes/scriptoutput.cs
+++ pyrevitlib/pyrevit/loader/basetypes/scriptoutput.cs
@@ -233,6 +233,13 @@ namespace PyRevitBaseClasses {
             saveButton.Click += Save_Contents_Button_Clicked;
             RightWindowCommands.Items.Insert(0, saveButton);
 
+            var printButton = new Button() { ToolTip = "Print Contents", Focusable = false };
+            printButton.Width = 32;
+            printButton.Content =
+                MakeButtonPath("M18,3H6V7H18M19,12A1,1 0 0,1 18,11A1,1 0 0,1 19,10A1,1 0 0,1 20,11A1,1 0 0,1 19,12M16,19H8V14H16M19,8H5A3,3 0 0,0 2,11V17H6V21H18V17H22V11A3,3 0 0,0 19,8Z");
+            printButton.Click += PrintButton_Click; ;
+            RightWindowCommands.Items.Insert(0, printButton);
+
             var openButton = new Button() { ToolTip = "Open in Browser", Focusable = false };
             openButton.Width = 32;
             openButton.Content =
@@ -632,6 +639,10 @@ namespace PyRevitBaseClasses {
 
         private void OpenButton_Click(object sender, RoutedEventArgs e) {
             Process.Start(string.Format("file:///{0}", SaveContentsToTemp()));
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e) {
+            renderer.ShowPrintPreviewDialog();
         }
 
         public void Dispose() {
