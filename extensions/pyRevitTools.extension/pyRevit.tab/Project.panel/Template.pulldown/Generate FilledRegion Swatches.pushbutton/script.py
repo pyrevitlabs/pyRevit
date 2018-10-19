25d71158a47ee3f1a6eb4b1370cd99b38c72630f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-10-18 17:30:14 -0700

    Added create filled region swatch tool

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/Template.pulldown/Generate FilledRegion Swatches.pushbutton/script.py
@@ -0,0 +1,69 @@
+"""Generate FilledRegion Swatches"""
+#pylint: disable=E0401,C0103
+from pyrevit.framework import List
+from pyrevit import revit, DB
+
+
+MAX_WIDTH = 16
+DIRECTION = 1       # OR -1
+
+
+def make_title(base_point, fr_type):
+    tnote_typeid = \
+        revit.doc.GetDefaultElementTypeId(DB.ElementTypeGroup.TextNoteType)
+    DB.TextNote.Create(revit.doc,
+                       revit.activeview.Id,
+                       DB.XYZ(base_point.X, base_point.Y + 1, base_point.Z),
+                       1/12.0,
+                       revit.ElementWrapper(fr_type).name,
+                       tnote_typeid
+                      )
+
+
+def make_filledregion_element(base_point, fr_type):
+    cloop = DB.CurveLoop()
+    cloop.Append(
+        DB.Line.CreateBound(
+            DB.XYZ(base_point.X, base_point.Y, base_point.Z),
+            DB.XYZ(base_point.X + 1, base_point.Y, base_point.Z)
+            )
+    )
+    cloop.Append(
+        DB.Line.CreateBound(
+            DB.XYZ(base_point.X + 1, base_point.Y, base_point.Z),
+            DB.XYZ(base_point.X + 1, base_point.Y + DIRECTION, base_point.Z)
+            )
+    )
+    cloop.Append(
+        DB.Line.CreateBound(
+            DB.XYZ(base_point.X + 1, base_point.Y + DIRECTION, base_point.Z),
+            DB.XYZ(base_point.X, base_point.Y + DIRECTION, base_point.Z)
+            )
+    )
+    cloop.Append(
+        DB.Line.CreateBound(
+            DB.XYZ(base_point.X, base_point.Y + DIRECTION, base_point.Z),
+            DB.XYZ(base_point.X, base_point.Y, base_point.Z)
+            )
+    )
+
+    DB.FilledRegion.Create(revit.doc,
+                           fr_type.Id,
+                           revit.activeview.Id,
+                           List[DB.CurveLoop]([cloop]))
+
+
+def make_swatch(index, fr_type):
+    row = 0 + (index / MAX_WIDTH)
+    col = index - (MAX_WIDTH * row)
+    base_point = DB.XYZ(col, row * DIRECTION, 0)
+    make_title(base_point, fr_type)
+    make_filledregion_element(base_point, fr_type)
+
+
+filledregion_types = revit.query.get_types_by_class(DB.FilledRegionType)
+with revit.Transaction('Generate FilledRegion Swatched'):
+    for idx, filledregion_type in enumerate(
+            sorted(filledregion_types,
+                   key=lambda x: revit.ElementWrapper(x).name)):
+        make_swatch(idx, filledregion_type)
