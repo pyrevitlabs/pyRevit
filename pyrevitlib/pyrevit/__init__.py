8e7dbf3a7d8b89ab53b896e582d33af3057c8e9b
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-11-24 09:44:47 -0800

    Tested changes and upped to 4.6.8

--- pyrevitlib/pyrevit/__init__.py
+++ pyrevitlib/pyrevit/__init__.py
@@ -38,7 +38,7 @@ except ImportError:
 PYREVIT_ADDON_NAME = 'pyRevit'
 VERSION_MAJOR = 4
 VERSION_MINOR = 6
-BUILD_METADATA = '.7'
+BUILD_METADATA = '.8'
 
 # -----------------------------------------------------------------------------
 # config environment paths
