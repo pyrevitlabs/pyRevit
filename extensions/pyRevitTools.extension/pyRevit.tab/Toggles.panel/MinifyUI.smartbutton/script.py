433a4286a8c2c4b88b83d3805fa54b6cc8d9e94d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-01-03 09:28:18 -0800

    Split minify to module and config scripts

--- extensions/pyRevitTools.extension/pyRevit.tab/Toggles.panel/MinifyUI.smartbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Toggles.panel/MinifyUI.smartbutton/script.py
@@ -3,11 +3,8 @@
 Shift+Click:
 Customize which tabs should be hidden in minified mode.
 """
-
-from pyrevit import revit, DB
-from pyrevit import forms
+#pylint: disable=C0103,E0401
 from pyrevit import script
-from pyrevit.coreutils import ribbon
 from pyrevit.coreutils.ribbon import ICON_MEDIUM
 
 
@@ -16,60 +13,6 @@ __context__ = 'zerodoc'
 
 
 config = script.get_config()
-logger = script.get_logger()
-
-
-MINIFYUI_ENV_VAR = 'MINIFYUIACTIVE'
-
-
-class TabOption(forms.TemplateListItem):
-    def __init__(self, uitab, hidden_tabs_list):
-        super(TabOption, self).__init__(uitab)
-        self.state = self.name in hidden_tabs_list
-
-    @property
-    def name(self):
-        return self.item.name
-
-
-def set_minifyui_config(hidden_tabs_list):
-    config.hidden_tabs = hidden_tabs_list
-    script.save_config()
-
-
-def get_minifyui_config():
-    return config.get_option('hidden_tabs', [])
-
-
-def config_minifyui():
-    this_ext_name = script.get_extension_name()
-    hidden_tabs = get_minifyui_config()
-    tabs = forms.SelectFromList.show(
-        [TabOption(x, hidden_tabs) for x in ribbon.get_current_ui()
-         if x.name not in this_ext_name],
-        title='Minify UI Config',
-        button_name='Hide Selected Tabs in Minified Mode',
-        multiselect=True
-        )
-
-    if tabs:
-        set_minifyui_config([x.name for x in tabs if x])
-
-
-def update_ui():
-    # Minify or unminify the ui here
-    hidden_tabs = get_minifyui_config()
-    for tab in ribbon.get_current_ui():
-        if tab.name in hidden_tabs:
-            # not new state since the visible value is reverse
-            tab.visible = not script.get_envvar(MINIFYUI_ENV_VAR)
-
-
-def toggle_minifyui():
-    new_state = not script.get_envvar(MINIFYUI_ENV_VAR)
-    script.set_envvar(MINIFYUI_ENV_VAR, new_state)
-    script.toggle_icon(new_state)
-    update_ui()
 
 
 # FIXME: need to figure out a way to fix the icon sizing of toggle buttons
@@ -79,8 +22,5 @@ def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
 
 
 if __name__ == '__main__':
-    if __shiftclick__: # noqa
-        config_minifyui()
-        update_ui()
-    else:
-        toggle_minifyui()
+    import minifyui
+    minifyui.toggle_minifyui(config)
