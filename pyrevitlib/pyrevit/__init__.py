e1e6e5b9334ff997e819e3ec2c4f608f62a7cc58
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-01-12 12:16:29 -0800

    Upped to 4.6.11

--- pyrevitlib/pyrevit/__init__.py
+++ pyrevitlib/pyrevit/__init__.py
@@ -38,7 +38,7 @@ except ImportError:
 PYREVIT_ADDON_NAME = 'pyRevit'
 VERSION_MAJOR = 4
 VERSION_MINOR = 6
-BUILD_METADATA = '.10'
+BUILD_METADATA = '.11'
 
 # -----------------------------------------------------------------------------
 # config environment paths
