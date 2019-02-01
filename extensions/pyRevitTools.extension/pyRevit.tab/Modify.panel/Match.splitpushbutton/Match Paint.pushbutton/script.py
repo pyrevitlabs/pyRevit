cb1029dd144fc18916d2d31de6165206dee21062
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-02-01 14:16:43 -0800

    Implemented issue #498

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/Match.splitpushbutton/Match Paint.pushbutton/script.py
@@ -0,0 +1,34 @@
+"""Pickup painted surface material and apply to other surfaces."""
+#pylint: disable=E0401,C0111,W0613,C0103,broad-except
+from pyrevit import revit, UI
+from pyrevit import forms
+
+
+with forms.WarningBar(title='Pick source object:'):
+    source_face = revit.pick_face()
+
+
+if source_face:
+    material_id = source_face.MaterialElementId
+    material = revit.doc.GetElement(material_id)
+
+    with forms.WarningBar(title='Pick faces to match materials:'):
+        while True:
+            try:
+                dest_ref = \
+                    revit.uidoc.Selection.PickObject(
+                        UI.Selection.ObjectType.Face
+                        )
+            except Exception:
+                break
+
+            if not dest_ref:
+                break
+
+            dest_element = revit.doc.GetElement(dest_ref)
+            dest_face = dest_element.GetGeometryObjectFromReference(dest_ref)
+
+            with revit.Transaction('Match Painted Materials'):
+                revit.doc.Paint(dest_element.Id,
+                                dest_face,
+                                material_id)
