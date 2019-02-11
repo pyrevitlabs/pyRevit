00978b6f04406b28d38d03cf1126bb6dbdcd7bd3
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-02-11 13:42:18 -0800

    Updated cpython test

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test CPython Command.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test CPython Command.pushbutton/script.py
@@ -5,12 +5,34 @@
 import sys
 print('\n'.join(sys.path))
 
+
+def print_html(output_str):
+    print(output_str.replace('<', '&clt;').replace('>', '&cgt;'))
+
+
+# trst numpy
 try:
     import numpy as np
     print(repr(np.arange(15).reshape(3, 5)))
 except Exception as ex:
     print('numpy load error: {}'.format(ex))
 
+# trst pandas
+try:
+    import pandas as pd
+
+    df_dict = {'key 1': 1, 'key 2': 2, 'key 3': 3}
+    df = pd.DataFrame([df_dict])
+
+    print("pandas dataframe:")
+    print(print_html(df.to_html()))
+except Exception as ex:
+    print('pandas load error: {}'.format(ex))
+
+
+
+def print_html(output_str):
+	print(output_str.replace('<', '&clt;').replace('>', '&cgt;'))
 
 import clr
 # clr.AddReference('Autodesk.Revit.DB')
