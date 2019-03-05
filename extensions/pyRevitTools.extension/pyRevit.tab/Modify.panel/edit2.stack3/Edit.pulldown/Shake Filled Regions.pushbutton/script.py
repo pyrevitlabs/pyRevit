2ccabcb3c9ec27a4b194626bc5fb7e2e54cfb462
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-03-05 12:03:44 -0800

    Cleaned up shake filled regions

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit2.stack3/Edit.pulldown/Shake Filled Regions.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit2.stack3/Edit.pulldown/Shake Filled Regions.pushbutton/script.py
@@ -1,7 +1,6 @@
-from pyrevit import framework
+#pylint: disable=missing-docstring,import-error,invalid-name,unused-argument
 from pyrevit import revit, DB
 from pyrevit import script
-
 
 __doc__ = 'Moves all Filled Regions on the seleced views back-and-forth by '\
           'a tiny amount to push Revit to regenerate the filled region and '\
@@ -12,28 +11,34 @@ __doc__ = 'Moves all Filled Regions on the seleced views back-and-forth by '\
 
 
 output = script.get_output()
+
+# collect target views
 selection = revit.get_selection()
+target_views = selection.elements
+if not target_views:
+    target_views = [revit.activeview]
 
 
-def shake_filled_regions(view):
-    fregions = DB.FilteredElementCollector(revit.doc)\
-                 .OwnedByView(view.Id)\
-                 .OfClass(framework.get_type(DB.FilledRegion))\
-                 .WhereElementIsNotElementType()\
-                 .ToElements()
+def shake_filled_regions(target_view):
+    filled_regions = \
+        DB.FilteredElementCollector(target_view.Document, target_view.Id)\
+          .OfClass(DB.FilledRegion)\
+          .WhereElementIsNotElementType()\
+          .ToElements()
 
-    print('Shaking Filled Regions in: {}'.format(revit.query.get_name(view)))
+    print('Shaking Filled Regions in: {}'
+          .format(revit.query.get_name(target_view)))
 
-    for i, fr in enumerate(fregions):
-        with revit.Transaction('Shake Filled Region #{}'.format(i)):
-            fr.Location.Move(DB.XYZ(0.01, 0, 0))
-            fr.Location.Move(DB.XYZ(-0.01, 0, 0))
+    for fregion in filled_regions:
+        with revit.Transaction('Shake Filled Region'):
+            fregion.Location.Move(DB.XYZ(0.1, 0, 0))
+            fregion.Location.Move(DB.XYZ(-0.1, 0, 0))
 
 
-print('Shaking Filled Regions in {} views'.format(len(selection)))
+print('Shaking Filled Regions in {} views'.format(len(target_views)))
 with revit.TransactionGroup('Shake Filled Regions'):
-    for i, view in enumerate(selection.elements):
+    for idx, view in enumerate(target_views):
         shake_filled_regions(view)
-        output.update_progress(i+1, len(selection))
+        output.update_progress(idx+1, len(target_views))
 
 print('All Filled Regions where shaken...')
