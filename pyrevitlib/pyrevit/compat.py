3801fb9d4ae58712eaa867a49b00265d7fe527ce
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-05-01 11:18:13 -0700

    revised safe_strtype wrapper per issue #577

--- pyrevitlib/pyrevit/compat.py
+++ pyrevitlib/pyrevit/compat.py
@@ -11,10 +11,12 @@ PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
 IRONPY273 = sys.version_info[:3] == (2, 7, 3)
 IRONPY277 = sys.version_info[:3] == (2, 7, 7)
+IRONPY279 = sys.version_info[:3] == (2, 7, 9)
 
 
 #pylint: disable=C0103
 safe_strtype = str
 
 if PY2:
-    safe_strtype = unicode  #pylint: disable=E0602
+    # https://gist.github.com/gornostal/1f123aaf838506038710
+    safe_strtype = lambda x: unicode(x)  #pylint: disable=E0602,unnecessary-lambda
