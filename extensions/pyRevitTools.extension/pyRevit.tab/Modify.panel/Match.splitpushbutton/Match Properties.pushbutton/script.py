a3d447a81c4dcd5da4ae26bf5c32326de20ba90f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-05-15 09:12:33 -0700

    Added support for matching view properties

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/Match.splitpushbutton/Match Properties.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/Match.splitpushbutton/Match Properties.pushbutton/script.py
@@ -1,7 +1,7 @@
 """Match instance or type properties between elements and their types.
 
 Shift+Click:
-Reapply the previous match properties.
+Reapply the previous matched properties.
 
 """
 #pylint: disable=import-error,invalid-name,broad-except
@@ -36,6 +36,40 @@ MEMFILE = script.get_document_data_file(
     )
 
 
+def match_prop(dest_inst, dest_type, src_props):
+    """Match given properties on target instance or type"""
+    for pkv in src_props:
+        logger.debug("Applying %s", pkv.name)
+
+        # determine target
+        target = dest_type if pkv.istype else dest_inst
+        # ensure target is valid if it is type
+        if pkv.istype and not target:
+            logger.warning("Element type is not accessible.")
+            continue
+        logger.debug("Target is %s", target)
+
+        # find parameter
+        dparam = target.LookupParameter(pkv.name)
+        if dparam and pkv.datatype == dparam.StorageType:
+            try:
+                if dparam.StorageType == DB.StorageType.Integer:
+                    dparam.Set(pkv.value or 0)
+                elif dparam.StorageType == DB.StorageType.Double:
+                    dparam.Set(pkv.value or 0.0)
+                elif dparam.StorageType == DB.StorageType.ElementId:
+                    dparam.Set(DB.ElementId(pkv.value))
+                else:
+                    dparam.Set(pkv.value or "")
+            except Exception as setex:
+                logger.warning(
+                    "Error applying value to: %s | %s",
+                    pkv.name, setex)
+                continue
+        else:
+            logger.debug("Parameter \"%s\"not found on target.", pkv.name)
+
+
 def get_source_properties(src_element):
     """Return info on selected properties."""
     props = []
@@ -44,16 +78,16 @@ def get_source_properties(src_element):
 
     selected_params = forms.select_parameter(
         src_element,
-        title='Select Parameters',
+        title="Select Parameters",
         multiple=True,
         include_instance=True,
         include_type=True
     ) or []
 
-    logger.debug('Selected parameters: %s', [x.name for x in selected_params])
+    logger.debug("Selected parameters: %s", [x.name for x in selected_params])
 
     for sparam in selected_params:
-        logger.debug('Reading %s', sparam.name)
+        logger.debug("Reading %s", sparam.name)
         target = src_type if sparam.istype else src_element
         tparam = target.LookupParameter(sparam.name)
         if tparam:
@@ -79,30 +113,15 @@ def get_source_properties(src_element):
 
 def pick_and_match_types(src_props):
     """Match property values for selected types."""
-    with forms.WarningBar(title='Pick objects to match type properties:'):
+    with forms.WarningBar(title="Pick objects to match type properties:"):
         while True:
             dest_element = revit.pick_element()
-
             if not dest_element:
                 break
 
             dest_type = revit.query.get_type(dest_element)
-            if dest_type:
-                with revit.Transaction('Match Type Properties'):
-                    for pkv in src_props:
-                        logger.debug('Applying %s', pkv.name)
-                        target = dest_type if pkv.istype else dest_element
-                        logger.debug('Target is %s', target)
-                        dparam = target.LookupParameter(pkv.name)
-                        if dparam and pkv.datatype == dparam.StorageType:
-                            if dparam.StorageType == DB.StorageType.Integer:
-                                dparam.Set(pkv.value or 0)
-                            elif dparam.StorageType == DB.StorageType.Double:
-                                dparam.Set(pkv.value or 0.0)
-                            elif dparam.StorageType == DB.StorageType.ElementId:
-                                dparam.Set(DB.ElementId(pkv.value))
-                            else:
-                                dparam.Set(pkv.value or "")
+            with revit.Transaction("Match Type Properties"):
+                match_prop(dest_element, dest_type, src_props)
 
 
 def recall():
@@ -113,7 +132,7 @@ def recall():
             data = pickle.load(mf)
     except Exception as ex:
         logger.debug(
-            'Failed loading matched properties from memory | %s', str(ex)
+            "Failed loading matched properties from memory | %s", str(ex)
             )
     return data
 
@@ -127,18 +146,42 @@ def remember(src_props):
 # main
 source_props = []
 if __shiftclick__:    #pylint: disable=undefined-variable
-    source_props = recall()
-    logger.debug('Recalled data: %s', source_props)
+    target_type, source_props = recall()
+    logger.debug("Recalled data: %s", source_props)
 
 if not source_props:
-    with forms.WarningBar(title='Pick source object:'):
-        source_element = revit.pick_element()
+    source_element = None
 
+    # ask for type of elements to match
+    # some are not selectable in graphical views
+    target_type = \
+        forms.CommandSwitchWindow.show(
+            ["Elements", "Views"],
+            message="Pick type of targets:")
+
+    # determine source element
+    if target_type == "Elements":
+        with forms.WarningBar(title="Pick source object:"):
+            source_element = revit.pick_element()
+    elif target_type == "Views":
+        source_element = \
+            forms.select_views(title="Select Source View", multiple=False)
+
+    # grab properties from source element
     if source_element:
         if not source_props:
             source_props = get_source_properties(source_element)
-            remember(source_props)
+            remember((target_type, source_props))
 
-
+# apply values
 if source_props:
-    pick_and_match_types(source_props)
+    if target_type == "Elements":
+        pick_and_match_types(source_props)
+    elif target_type == "Views":
+        target_views = \
+            forms.select_views(title="Select Target Views", multiple=True)
+        if target_views:
+            with revit.Transaction("Match Type Properties"):
+                for tview in target_views:
+                    tview_type = revit.query.get_type(tview)
+                    match_prop(tview, tview_type, source_props)
