bcd15ddbc66604554b8411f8338ac26a753c60e5
melnikovalex <alex@hiteca.ru>
2019-05-30 13:22:05 +0200

    Pick: works with non-english Revit, consistently

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/Pick.splitpushbutton/Pick.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/Pick.splitpushbutton/Pick.pushbutton/script.py
@@ -4,11 +4,10 @@ Shift-Click:
 Pick from all available categories.
 """
 
-#pylint: disable=E0401,W0703,C0103
-from pyrevit import revit, UI
+# pylint: disable=E0401,W0703,C0103
+from pyrevit import revit, UI, DB
 from pyrevit import forms
 from pyrevit import script
-
 
 logger = script.get_logger()
 
@@ -19,13 +18,13 @@ class PickByCategorySelectionFilter(UI.Selection.ISelectionFilter):
 
     # standard API override function
     def AllowElement(self, element):
-        if self.category in element.Category.Name:
+        if self.category == element.Category.Name:
             return True
         else:
             return False
 
     # standard API override function
-    def AllowReference(self, refer, point): #pylint: disable=W0613
+    def AllowReference(self, refer, point):  # pylint: disable=W0613
         return False
 
 
@@ -42,28 +41,35 @@ def pickbycategory(catname):
         logger.debug(err)
 
 
-
-if __shiftclick__:  #pylint: disable=E0602
+if __shiftclick__:  # pylint: disable=E0602
     options = sorted([x.Name for x in revit.doc.Settings.Categories])
 else:
-    options = sorted(['Area',
-                      'Area Boundary',
-                      'Column',
-                      'Dimension',
-                      'Door',
-                      'Floor',
-                      'Framing',
-                      'Furniture',
-                      'Grid',
-                      'Rooms',
-                      'Room Tag',
-                      'Truss',
-                      'Wall',
-                      'Window',
-                      'Ceiling',
-                      'Section Box',
-                      'Elevation Mark',
-                      'Parking'])
+    categories_shortlist = [
+        DB.BuiltInCategory.OST_Areas,
+        DB.BuiltInCategory.OST_AreaSchemeLines,
+        DB.BuiltInCategory.OST_Columns,
+        DB.BuiltInCategory.OST_StructuralColumns,
+        DB.BuiltInCategory.OST_Dimensions,
+        DB.BuiltInCategory.OST_Doors,
+        DB.BuiltInCategory.OST_Floors,
+        DB.BuiltInCategory.OST_StructuralFraming,
+        DB.BuiltInCategory.OST_Furniture,
+        DB.BuiltInCategory.OST_Grids,
+        DB.BuiltInCategory.OST_Rooms,
+        DB.BuiltInCategory.OST_RoomTags,
+        DB.BuiltInCategory.OST_Truss,
+        DB.BuiltInCategory.OST_Walls,
+        DB.BuiltInCategory.OST_Windows,
+        DB.BuiltInCategory.OST_Ceilings,
+        DB.BuiltInCategory.OST_SectionBox,
+        DB.BuiltInCategory.OST_ElevationMarks,
+        DB.BuiltInCategory.OST_Parking
+    ]
+    categories_shortlist_ids = [int(x) for x in categories_shortlist]
+    categories_filtered = [
+        x for x in revit.doc.Settings.Categories if (
+            x.Id.IntegerValue in categories_shortlist_ids)]
+    options = sorted([x.Name for x in categories_filtered])
 
 selected_switch = \
     forms.CommandSwitchWindow.show(options,
