943ea7393886b09f2bb96b6169e73e50c0129f2d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-05 12:04:23 -0700

    added c# event handler template

--- extensions/pyRevitDevTools.extension/hooks/view-activated.cs
+++ extensions/pyRevitDevTools.extension/hooks/view-activated.cs
@@ -1,8 +1,24 @@
 // executor needs to find the correct function signature and executes that
+using System;
+using System.IO;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
 namespace MyEvents {
     public static class MyEventMgr {
         public static void MyEventMgr_UiApp_ViewActivated(object sender, Autodesk.Revit.UI.Events.ViewActivatedEventArgs e) {
-
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            File.AppendAllText(
+                Path.Combine(desktopPath, "hooks.log"),
+                string.Format("[view-activate-csharp] doc:\"{0}\" active_view:\"{1}\" prev_view:\"{2}\" status:\"{3}\"",
+                    e.Document.ToString(),
+                    e.CurrentActiveView.ToString(),
+                    e.PreviousActiveView.ToString(),
+                    e.Status.ToString()
+                )
+            );
         }
     }
 }
