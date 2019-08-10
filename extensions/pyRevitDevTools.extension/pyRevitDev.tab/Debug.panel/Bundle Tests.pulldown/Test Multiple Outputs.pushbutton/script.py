43d9b566ffeca1b39447ab258db6f32880f66494
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-09 17:58:49 -0700

    Cleaned up test multiple outputs script

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Bundle Tests.pulldown/Test Multiple Outputs.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Bundle Tests.pulldown/Test Multiple Outputs.pushbutton/script.py
@@ -1,13 +1,10 @@
+"""Test opening multiple output windows."""
+#pylint: disable=import-error,line-too-long
 from pyrevit.loader.sessionmgr import execute_command
-
 
 __context__ = 'zerodoc'
 
-
-commandlist = \
-    ["pyRevitDevToolspyRevitDevDebugMiscTestsTestRPW",
-     "pyRevitToolspyRevitSelectionselectSelectListSelectionasClickableLinks"]
-
-for cmd in commandlist:
+for cmd in ["pyrevitdevtools-pyrevitdev-debug-misctests-testrpw",
+            "pyrevittools-pyrevit-selection-select-select-listselectionasclickablelinks"]:
     for i in range(5):
         execute_command(cmd)
