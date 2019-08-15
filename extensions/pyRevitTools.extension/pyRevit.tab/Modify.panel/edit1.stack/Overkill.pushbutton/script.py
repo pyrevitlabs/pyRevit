a7389f0f4da75c23a759b2205561c1c501eec4dd
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-14 19:34:26 -0700

    First pass at refactoring overkill

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit1.stack/Overkill.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit1.stack/Overkill.pushbutton/script.py
@@ -15,59 +15,87 @@ __authors__ = ["tylerk", "{{author}}"]
 logger = script.get_logger()
 output = script.get_output()
 
-INCLUDE_STYLE = True
-VIEW_SPECIFIC = True
-
-DomainBound = namedtuple('DomainBound', ['x', 'y', 'dlid'])
+# configs
+RESOLUTION = '{:.3f}'
 
 
-class DetailLineDomain(object):
-    def __init__(self, dline, include_style=False):
-        # calculate domain parameters
-        dvector = dline.GeometryCurve.Direction
-        p1 = dline.GeometryCurve.GetEndPoint(0)
-        p2 = dline.GeometryCurve.GetEndPoint(1)
-        self.offset = \
-            self.shortest_dist(p1.X, p1.Y, p2.X, p2.Y, dvector.X, dvector.Y)
-        self.direction = dvector.X, dvector.Y
-        self.reverse = (dvector.X * dvector.Y) < 0
-        self.weight = None
-        if include_style:
-            self.weight = DetailLineDomain.get_style_weight(dline)
+CurvePoint = namedtuple('CurvePoint', ['x', 'y', 'cid'])
 
-        # set domain boundary
-        self.max = DomainBound(x=p1.X, y=p1.Y, dlid=dline.Id.IntegerValue)
-        self.min = DomainBound(x=p2.X, y=p2.Y, dlid=dline.Id.IntegerValue)
-        if self.max.x < self.min.x \
-                or self.min.x > self.max.x:
-            self.min, self.max = self.max, self.min
+
+class LinearCurveGroup(object):
+    supported_geoms = (DB.Line)
+
+    def __init__(self, curve, include_style=False):
+        self.points = set()
+        # calculate cgroup parameters
+        p1 = curve.GeometryCurve.GetEndPoint(0)
+        p2 = curve.GeometryCurve.GetEndPoint(1)
+        p1x = LinearCurveGroup.get_float(p1.X)
+        p1y = LinearCurveGroup.get_float(p1.Y)
+        p2x = LinearCurveGroup.get_float(p2.X)
+        p2y = LinearCurveGroup.get_float(p2.Y)
+        # get the direction vector
+        curve_dir = curve.GeometryCurve.Direction
+        self.dir_x, self.dir_y = \
+            LinearCurveGroup.get_float(curve_dir.X), \
+                LinearCurveGroup.get_float(curve_dir.Y)
+        # unify the direction
+        if (self.dir_x < 0.0 and self.dir_y <= 0.0) \
+                or (self.dir_x > 0.0 and self.dir_y < 0.0):
+            self.dir_x, self.dir_y = -self.dir_x, -self.dir_y
+        # make sure there are no -0.0
+        self.dir_x = 0.0 if self.dir_x == 0.0 else self.dir_x
+        self.dir_y = 0.0 if self.dir_y == 0.0 else self.dir_y
+        # calc curve offset from cgroup
+        self.dir_offset = \
+            self.shortest_dist(p1x, p1y,
+                               p2x, p2y,
+                               self.dir_x, self.dir_y)
+        # get curve weight
+        self.weight = \
+            LinearCurveGroup.get_style_weight(curve) if include_style else None
+
+        # set cgroup boundary
+        self.dir_cid = curve.Id.IntegerValue
+        self.add_points([
+            CurvePoint(x=p1x, y=p1y, cid=self.dir_cid),
+            CurvePoint(x=p2x, y=p2y, cid=self.dir_cid)
+        ])
+
+        # create identity
+        # identity is a tuple (dir_x, dir_y, dir_offset, weight)
+        self.identity = \
+            (LinearCurveGroup.get_float(self.dir_x),
+             LinearCurveGroup.get_float(self.dir_y),
+             LinearCurveGroup.get_float(self.dir_offset),
+             self.weight)
 
         # prepare a list for overlapping curves
         self.bounded = set()
 
     def __repr__(self):
-        return '<%s max=%s, min=%s weight=%s>' % (
+        return '<%s max=%s, min=%s points=%s weight=%s id=%s hash=%s>' % (
             self.__class__.__name__,
-            self.max,
-            self.min,
-            self.weight)
+            self.max_point,
+            self.min_point,
+            len(self.points),
+            self.weight,
+            self.identity,
+            hash(self))
 
     def __hash__(self):
-        d1 = self.direction[0]
-        d2 = self.direction[1]
-        if d1 < 0 and d2 < 0:
-            d1 = abs(d1)
-            d2 = abs(d2)
-        elif d1 < 0 and d2 > 0:
-            d1 = abs(d1)
-            d2 = -d2
-        return hash(('{:.3f}'.format(d1),
-                     '{:.3f}'.format(d2),
-                     '{:.3f}'.format(self.offset),
-                     self.weight))
+        return hash(self.identity)
 
     def __eq__(self, other):
-        return hash(self) == hash(other)
+        # if same direction vector
+        if isinstance(other, LinearCurveGroup) \
+                and hash(self) == hash(other):
+            # and any of the points is inside the bounds of this curve group
+            for cgroup_point in [other.min_point, other.max_point]:
+                x_in = self.min_point.x <= cgroup_point.x <= self.max_point.x
+                y_in = self.min_point.y <= cgroup_point.y <= self.max_point.y
+                return x_in or y_in
+        return False
 
     @staticmethod
     def shortest_dist(x1, y1, x2, y2, x0, y0):
@@ -78,123 +106,173 @@ class DetailLineDomain(object):
                 math.sqrt((y2-y1)**2 + (x2-x1)**2))
 
     @staticmethod
-    def get_style_weight(dline):
-        graphic_style_cat = dline.LineStyle.GraphicsStyleCategory
-        graphic_style_type = dline.LineStyle.GraphicsStyleType
+    def get_style_weight(curve):
+        graphic_style_cat = curve.LineStyle.GraphicsStyleCategory
+        graphic_style_type = curve.LineStyle.GraphicsStyleType
         return graphic_style_cat.GetLineWeight(graphic_style_type)
 
-    def dline_ids(self):
-        return list(set([self.max.dlid, self.min.dlid]))
+    @staticmethod
+    def get_float(float_value):
+        return float(RESOLUTION.format(float_value))
 
-    def extends_max(self, dbound):
-        if self.reverse:
-            if dbound.x > self.max.x:
-                if dbound.y < self.max.y:
-                    return True
-        else:
-            if dbound.x > self.max.x:
-                if dbound.y > self.max.y:
-                    return True
-        return False
+    @property
+    def max_point(self):
+        return max(self.points, key=lambda dp: abs(dp.x))
 
-    def extends_min(self, dbound):
-        if self.reverse:
-            if dbound.x < self.min.x:
-                if dbound.y > self.min.y:
-                    return True
-        else:
-            if dbound.x < self.min.x:
-                if dbound.y < self.min.y:
-                    return True
-        return False
+    @property
+    def min_point(self):
+        return min(self.points, key=lambda dp: abs(dp.x))
 
-    def merge(self, dldomain):
-        extends_max = self.extends_max(dldomain.max)
-        extends_min = self.extends_min(dldomain.min)
-        if extends_max and extends_min:
-            logger.debug('Domain overlaps existing: %s', dldomain)
-            logger.debug('Mark as bounded: %s', self.dline_ids())
-            self.bounded.update(self.dline_ids())
-            logger.debug('Extending max: %s -> %s', self.max, dldomain.max)
-            self.max = dldomain.max
-            logger.debug('Extending min: %s -> %s', self.min, dldomain.min)
-            self.min = dldomain.min
-        elif not extends_max and not extends_min:
-            logger.debug('Domain is contained: %s', dldomain)
-            logger.debug('Mark as bounded: %s', dldomain.dline_ids())
-            self.bounded.update(dldomain.dline_ids())
+    def add_points(self, domain_points):
+        for domain_point in domain_points:
+            if isinstance(domain_point, CurvePoint):
+                logger.debug('Adding cgroup point: %s', domain_point)
+                self.points.add(domain_point)
+
+    def merge(self, cgroup):
+        if isinstance(cgroup, LinearCurveGroup):
+            self.add_points(cgroup.points)
+
+    def overkill(self, doc=None):
+        bounded_curve_ids = set()
+        if doc and len(self.points) > 2:
+            # extend the root curve to cover the full cgroup
+            root_curve = doc.GetElement(DB.ElementId(self.dir_cid))
+            min_dpoint = self.min_point
+            max_dpoint = self.max_point
+            # create new geometry
+            curve_z = root_curve.GeometryCurve.GetEndPoint(0).Z
+            start_point = DB.XYZ(min_dpoint.x, min_dpoint.y, curve_z)
+            end_point = DB.XYZ(max_dpoint.x, max_dpoint.y, curve_z)
+            reset_dir_curve = False
+            try:
+                geom_curve = DB.Line.CreateBound(start_point, end_point)
+                root_curve.GeometryCurve = geom_curve
+                reset_dir_curve = True
+            except Exception as set_ex:
+                logger.debug('Failed re-setting root curve | %s', set_ex)
+            # now delete the overlapping lines
+            if reset_dir_curve:
+                bounded_curve_ids = \
+                    {x.cid for x in self.points if x.cid != self.dir_cid}
+                if bounded_curve_ids:
+                    bounded_curve_ids = \
+                        [DB.ElementId(x) for x in bounded_curve_ids]
+                    logger.debug('Deleting %s', bounded_curve_ids)
+                    doc.Delete(List[DB.ElementId](bounded_curve_ids))
+        return len(bounded_curve_ids)
 
 
-class DetailLineDomainCollection(object):
+CURVE_GROUP_TYPES = [
+    LinearCurveGroup,
+]
+
+
+class CurveGroupCollection(object):
     def __init__(self, include_style=False):
-        self.domains = []
+        self.curve_groups = []
         self._include_style = include_style
 
     def __iter__(self):
-        return iter(self.domains)
+        return iter(self.curve_groups)
 
-    def extend(self, dline):
-        dldomain = DetailLineDomain(dline, include_style=self._include_style)
-        if dldomain not in self.domains:
-            logger.debug('New domain %s', dldomain)
-            self.domains.append(dldomain)
-        else:
-            idx = self.domains.index(dldomain)
-            ext_dldomain = self.domains[idx]
-            logger.debug('Merging into existing domain %s', ext_dldomain)
-            ext_dldomain.merge(dldomain)
+    def extend(self, curve_element):
+        logger.debug('Extending cruve group...')
+        for cgroup_type in CURVE_GROUP_TYPES:
+            logger.debug('Trying cgroup type %s', cgroup_type)
+            if isinstance(curve_element.GeometryCurve,
+                          cgroup_type.supported_geoms):
+                logger.debug('cruve group type supports %s',
+                             curve_element.GeometryCurve)
+                cgroup = cgroup_type(curve_element,
+                                     include_style=self._include_style)
+                logger.debug('Domain is %s', cgroup)
+                if cgroup in self.curve_groups:
+                    logger.debug('Domain exists %s', cgroup)
+                    idx = self.curve_groups.index(cgroup)
+                    exst_domain = self.curve_groups[idx]
+                    logger.debug('Merging into existing cgroup %s', exst_domain)
+                    exst_domain.merge(cgroup)
+                else:
+                    logger.debug('New cgroup %s', cgroup)
+                    self.curve_groups.append(cgroup)
+                return True
 
 
-def filter_lines(lines, view_specific):
+def filter_curves(elements, view_specific=None):
+    """Filter given curves for view specific."""
+    filtered_elements = \
+        revit.query.get_elements_by_class(DB.CurveElement, elements=elements) \
+            if elements else []
     if view_specific is None:
-        return lines
+        return filtered_elements
     else:
-        return [x for x in lines if x.ViewSpecific == view_specific]
+        return [x for x in filtered_elements if x.ViewSpecific == view_specific]
 
 
-# ask user for options and process
-options = ['All Lines', 'Detail Lines', 'Model Lines']
-switches = {'Consider Line Weights': True}
-selected_option, switches = \
-    forms.CommandSwitchWindow.show(
-        options,
-        switches=switches,
-        message='Pick overkill option:'
-        )
+def ask_for_curve_type():
+    # ask user for options and process
+    options = ['All Lines', 'Detail Lines', 'Model Lines']
+    switches = {'Consider Line Weights': True}
+    selected_option, switches = \
+        forms.CommandSwitchWindow.show(
+            options,
+            switches=switches,
+            message='Pick overkill option:'
+            )
 
-if not switches['Consider Line Weights']:
-    INCLUDE_STYLE = False
+    return (selected_option, switches['Consider Line Weights'])
 
-if selected_option == 'All Lines':
-    VIEW_SPECIFIC = None
-elif selected_option == 'Model Lines':
-    VIEW_SPECIFIC = False
-elif selected_option == 'Detail Lines':
-    VIEW_SPECIFIC = True
 
-if selected_option:
-    # collect all detail-lines in active view
-    logger.debug('Collecting detail lines in current view...')
-    detline_collector = \
-        revit.query.get_elements_by_class(DB.CurveElement,
-                                        view_id=revit.active_view.Id)
+def ask_consider_weight():
+    # ask user for options and process
+    return True
 
+
+def overkill_curves(curve_elements,
+                    view_specific=None,
+                    include_style=True):
     # collect comparison info on each detail-lines geomtery
-    logger.debug('Extracting 2d domains...')
-    detline_domaincl = DetailLineDomainCollection(include_style=INCLUDE_STYLE)
-    for detline in filter_lines(detline_collector, view_specific=VIEW_SPECIFIC):
-        logger.debug('Adding detail line to domains: %s', detline.Id.IntegerValue)
-        detline_domaincl.extend(detline)
+    logger.debug('Extracting 2d cruve group...')
+    cgroup_collection = CurveGroupCollection(include_style=include_style)
+    for curve_element in filter_curves(curve_elements,
+                                       view_specific=view_specific):
+        logger.debug('Adding curve to cruve group: %s', curve_element.Id)
+        cgroup_collection.extend(curve_element)
 
     del_count = 0
     with revit.Transaction('Overkill'):
-        for detline_domain in detline_domaincl:
-            bounded_detline_ids = \
-                [DB.ElementId(x) for x in detline_domain.bounded]
-            del_count += len(bounded_detline_ids)
-            revit.doc.Delete(List[DB.ElementId](bounded_detline_ids))
+        # process linear cruve group
+        for cgroup in cgroup_collection:
+            del_count += cgroup.overkill(doc=revit.doc)
 
     if del_count > 0:
         forms.alert('{} lines were removed.'.format(del_count))
     else:
         forms.alert('Pretty clean! No lines were removed.')
+
+
+
+selected_curves = filter_curves(revit.get_selection())
+if selected_curves:
+    overkill_curves(selected_curves, include_style=ask_consider_weight())
+else:
+    selected_opt, incl_style = ask_for_curve_type()
+
+    if selected_opt == 'All Lines':
+        view_spec = None
+    elif selected_opt == 'Model/Symbolic Lines':
+        view_spec = False
+    elif selected_opt == 'Detail Lines':
+        view_spec = True
+
+    if selected_opt:
+        # collect all detail-lines in active view
+        logger.debug('Collecting detail lines in current view...')
+        curve_collector = \
+            revit.query.get_elements_by_class(DB.CurveElement,
+                                            view_id=revit.active_view.Id)
+        overkill_curves(list(curve_collector),
+                        view_specific=view_spec,
+                        include_style=incl_style)
+
