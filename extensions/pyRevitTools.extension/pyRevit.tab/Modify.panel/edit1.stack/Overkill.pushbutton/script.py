af4ff0455f474b9aca45ea2978af2b7e66fe2af5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-15 11:40:25 -0700

    Another attempt at improving overkill
    It might be easier to collect overlapping lines form Revit warnings

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit1.stack/Overkill.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit1.stack/Overkill.pushbutton/script.py
@@ -16,7 +16,9 @@ logger = script.get_logger()
 output = script.get_output()
 
 # configs
-RESOLUTION = '{:.3f}'
+POINT_RESOLUTION = '{:.4f}'
+DIRECTION_RESOLUTION = '{:.2f}'
+OFFSET_RESOLUTION = '{:.4f}'
 
 
 CurvePoint = namedtuple('CurvePoint', ['x', 'y', 'cid'])
@@ -30,80 +32,59 @@ class LinearCurveGroup(object):
         # calculate cgroup parameters
         p1 = curve.GeometryCurve.GetEndPoint(0)
         p2 = curve.GeometryCurve.GetEndPoint(1)
-        p1x = LinearCurveGroup.get_float(p1.X)
-        p1y = LinearCurveGroup.get_float(p1.Y)
-        p2x = LinearCurveGroup.get_float(p2.X)
-        p2y = LinearCurveGroup.get_float(p2.Y)
         # get the direction vector
-        curve_dir = curve.GeometryCurve.Direction
-        self.dir_x, self.dir_y = \
-            LinearCurveGroup.get_float(curve_dir.X), \
-                LinearCurveGroup.get_float(curve_dir.Y)
-        # unify the direction
-        if (self.dir_x < 0.0 and self.dir_y <= 0.0) \
-                or (self.dir_x > 0.0 and self.dir_y < 0.0):
-            self.dir_x, self.dir_y = -self.dir_x, -self.dir_y
-        # make sure there are no -0.0
-        self.dir_x = 0.0 if self.dir_x == 0.0 else self.dir_x
-        self.dir_y = 0.0 if self.dir_y == 0.0 else self.dir_y
+        self.dir_x, self.dir_y = self.get_direction(curve)
         # calc curve offset from cgroup
-        self.dir_offset = \
-            self.shortest_dist(p1x, p1y,
-                               p2x, p2y,
-                               self.dir_x, self.dir_y)
+        self.dir_offset = self.get_offset(p1, p2)
         # get curve weight
-        self.weight = \
-            LinearCurveGroup.get_style_weight(curve) if include_style else None
+        self.weight = self.get_weight(curve) if include_style else None
+        # create curve group identity
+        # identity is a tuple (dir_x, dir_y, dir_offset, weight)
+        self.cgroup_id = (self.dir_x, self.dir_y, self.dir_offset, self.weight)
 
         # set cgroup boundary
         self.dir_cid = curve.Id.IntegerValue
         self.add_points([
-            CurvePoint(x=p1x, y=p1y, cid=self.dir_cid),
-            CurvePoint(x=p2x, y=p2y, cid=self.dir_cid)
+            self.get_point(p1.X, p1.Y),
+            self.get_point(p2.X, p2.Y),
         ])
-
-        # create identity
-        # identity is a tuple (dir_x, dir_y, dir_offset, weight)
-        self.identity = \
-            (LinearCurveGroup.get_float(self.dir_x),
-             LinearCurveGroup.get_float(self.dir_y),
-             LinearCurveGroup.get_float(self.dir_offset),
-             self.weight)
 
         # prepare a list for overlapping curves
         self.bounded = set()
 
     def __repr__(self):
-        return '<%s max=%s, min=%s points=%s weight=%s id=%s hash=%s>' % (
+        return '<%s max=%s, min=%s points=%s id(dir=[%s,%s],offset=%s,weight=%s)>' % (
             self.__class__.__name__,
             self.max_point,
             self.min_point,
             len(self.points),
-            self.weight,
-            self.identity,
-            hash(self))
-
-    def __hash__(self):
-        return hash(self.identity)
-
-    def __eq__(self, other):
-        # if same direction vector
-        if isinstance(other, LinearCurveGroup) \
-                and hash(self) == hash(other):
-            # and any of the points is inside the bounds of this curve group
-            for cgroup_point in [other.min_point, other.max_point]:
-                x_in = self.min_point.x <= cgroup_point.x <= self.max_point.x
-                y_in = self.min_point.y <= cgroup_point.y <= self.max_point.y
-                return x_in or y_in
-        return False
+            self.dir_x,
+            self.dir_y,
+            self.dir_offset,
+            self.weight)
 
     @staticmethod
     def shortest_dist(x1, y1, x2, y2, x0, y0):
         # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
         # line passes thru (x1, y1), (x2, y2)
         # point is (x0, y0)
-        return (abs(x0*(y2-y1)-y0*(x2-x1)+x2*y1-y2*x1) /
-                math.sqrt((y2-y1)**2 + (x2-x1)**2))
+        return (
+            abs(x0*(y2-y1) - y0*(x2-x1) + x2*y1 - y2*x1) /
+            math.sqrt((y2-y1)**2 + (x2-x1)**2)
+        )
+
+    @staticmethod
+    def is_inside(min_cpoint, max_cpoint, cpoint):
+        x_range = max_cpoint.x - min_cpoint.x
+        y_range = max_cpoint.y - min_cpoint.y
+        if x_range == 0.0:
+            return min_cpoint.y <= cpoint.y <= max_cpoint.y
+        elif y_range == 0.0:
+            return min_cpoint.x <= cpoint.x <= max_cpoint.x
+        else:
+            xr = LinearCurveGroup.get_float((cpoint.x - min_cpoint.x) / x_range)
+            yr = LinearCurveGroup.get_float((cpoint.y - min_cpoint.y) / y_range)
+            return xr == yr
 
     @staticmethod
     def get_style_weight(curve):
@@ -112,26 +93,63 @@ class LinearCurveGroup(object):
         return graphic_style_cat.GetLineWeight(graphic_style_type)
 
     @staticmethod
-    def get_float(float_value):
-        return float(RESOLUTION.format(float_value))
+    def get_float(float_value, res=POINT_RESOLUTION):
+        return float(res.format(float_value))
 
     @property
     def max_point(self):
-        return max(self.points, key=lambda dp: abs(dp.x))
+        return max(self.points, key=lambda p: p.x + p.y)
 
     @property
     def min_point(self):
-        return min(self.points, key=lambda dp: abs(dp.x))
+        return min(self.points, key=lambda p: p.x + p.y)
 
-    def add_points(self, domain_points):
-        for domain_point in domain_points:
-            if isinstance(domain_point, CurvePoint):
-                logger.debug('Adding cgroup point: %s', domain_point)
-                self.points.add(domain_point)
+    def get_point(self, x, y):
+        return CurvePoint(x=LinearCurveGroup.get_float(x, res=POINT_RESOLUTION),
+                          y=LinearCurveGroup.get_float(y, res=POINT_RESOLUTION),
+                          cid=self.dir_cid)
+
+    def get_direction(self, curve):
+        dir_x, dir_y = \
+            curve.GeometryCurve.Direction.X, curve.GeometryCurve.Direction.Y
+        # unify the direction
+        if (dir_x <= 0.0 and dir_y <= 0.0) \
+                or (dir_x > 0.0 and dir_y < 0.0):
+            dir_x, dir_y = -dir_x, -dir_y
+        # make sure there are no -0.0
+        dir_x = 0.0 if dir_x == 0.0 else dir_x
+        dir_y = 0.0 if dir_y == 0.0 else dir_y
+        # bring direction within tolerance
+        return LinearCurveGroup.get_float(dir_x, res=DIRECTION_RESOLUTION), \
+            LinearCurveGroup.get_float(dir_y, res=DIRECTION_RESOLUTION)
+
+    def get_offset(self, p1, p2):
+        dir_offset = \
+            LinearCurveGroup.shortest_dist(
+                LinearCurveGroup.get_float(p1.X, res=POINT_RESOLUTION),
+                LinearCurveGroup.get_float(p1.Y, res=POINT_RESOLUTION),
+                LinearCurveGroup.get_float(p2.X, res=POINT_RESOLUTION),
+                LinearCurveGroup.get_float(p2.Y, res=POINT_RESOLUTION),
+                self.dir_x, self.dir_y)
+        return LinearCurveGroup.get_float(dir_offset, res=OFFSET_RESOLUTION)
+
+    def get_weight(self, curve):
+        return LinearCurveGroup.get_style_weight(curve)
+
+    def add_points(self, curve_points):
+        for curve_point in curve_points:
+            if isinstance(curve_point, CurvePoint):
+                self.points.add(curve_point)
 
     def merge(self, cgroup):
         if isinstance(cgroup, LinearCurveGroup):
-            self.add_points(cgroup.points)
+            # and any of the points is inside the bounds of this curve group
+            for cgroup_point in cgroup.points:
+                if LinearCurveGroup.is_inside(
+                        self.min_point, self.max_point, cgroup_point):
+                    self.add_points(cgroup.points)
+                    return True
+        return False
 
     def overkill(self, doc=None):
         bounded_curve_ids = set()
@@ -142,12 +160,12 @@ class LinearCurveGroup(object):
             max_dpoint = self.max_point
             # create new geometry
             curve_z = root_curve.GeometryCurve.GetEndPoint(0).Z
-            start_point = DB.XYZ(min_dpoint.x, min_dpoint.y, curve_z)
-            end_point = DB.XYZ(max_dpoint.x, max_dpoint.y, curve_z)
+            min_point = DB.XYZ(min_dpoint.x, min_dpoint.y, curve_z)
+            max_point = DB.XYZ(max_dpoint.x, max_dpoint.y, curve_z)
             reset_dir_curve = False
             try:
-                geom_curve = DB.Line.CreateBound(start_point, end_point)
-                root_curve.GeometryCurve = geom_curve
+                geom_curve = DB.Line.CreateBound(min_point, max_point)
+                root_curve.SetGeometryCurve(geom_curve, overrideJoins=True)
                 reset_dir_curve = True
             except Exception as set_ex:
                 logger.debug('Failed re-setting root curve | %s', set_ex)
@@ -158,7 +176,6 @@ class LinearCurveGroup(object):
                 if bounded_curve_ids:
                     bounded_curve_ids = \
                         [DB.ElementId(x) for x in bounded_curve_ids]
-                    logger.debug('Deleting %s', bounded_curve_ids)
                     doc.Delete(List[DB.ElementId](bounded_curve_ids))
         return len(bounded_curve_ids)
 
@@ -176,25 +193,25 @@ class CurveGroupCollection(object):
     def __iter__(self):
         return iter(self.curve_groups)
 
+    def merge(self, cgroup):
+        matching_cgroups = \
+            [x for x in self.curve_groups if x.cgroup_id == cgroup.cgroup_id]
+        if matching_cgroups:
+            first_matching = matching_cgroups[0]
+            matching_cgroups.remove(first_matching)
+            if matching_cgroups:
+                for matching_cgroup in matching_cgroups:
+                    cgroup.merge(matching_cgroup)
+                    self.curve_groups.remove(matching_cgroup)
+            return first_matching.merge(cgroup)
+
     def extend(self, curve_element):
-        logger.debug('Extending cruve group...')
         for cgroup_type in CURVE_GROUP_TYPES:
-            logger.debug('Trying cgroup type %s', cgroup_type)
             if isinstance(curve_element.GeometryCurve,
                           cgroup_type.supported_geoms):
-                logger.debug('cruve group type supports %s',
-                             curve_element.GeometryCurve)
                 cgroup = cgroup_type(curve_element,
                                      include_style=self._include_style)
-                logger.debug('Domain is %s', cgroup)
-                if cgroup in self.curve_groups:
-                    logger.debug('Domain exists %s', cgroup)
-                    idx = self.curve_groups.index(cgroup)
-                    exst_domain = self.curve_groups[idx]
-                    logger.debug('Merging into existing cgroup %s', exst_domain)
-                    exst_domain.merge(cgroup)
-                else:
-                    logger.debug('New cgroup %s', cgroup)
+                if not self.merge(cgroup):
                     self.curve_groups.append(cgroup)
                 return True
 
@@ -233,16 +250,14 @@ def overkill_curves(curve_elements,
                     view_specific=None,
                     include_style=True):
     # collect comparison info on each detail-lines geomtery
-    logger.debug('Extracting 2d cruve group...')
     cgroup_collection = CurveGroupCollection(include_style=include_style)
     for curve_element in filter_curves(curve_elements,
                                        view_specific=view_specific):
-        logger.debug('Adding curve to cruve group: %s', curve_element.Id)
         cgroup_collection.extend(curve_element)
 
     del_count = 0
-    with revit.Transaction('Overkill'):
-        # process linear cruve group
+    with revit.Transaction('Overkill', swallow_errors=True):
+        # process linear curve group
         for cgroup in cgroup_collection:
             del_count += cgroup.overkill(doc=revit.doc)
 
@@ -250,7 +265,6 @@ def overkill_curves(curve_elements,
         forms.alert('{} lines were removed.'.format(del_count))
     else:
         forms.alert('Pretty clean! No lines were removed.')
-
 
 
 selected_curves = filter_curves(revit.get_selection())
@@ -268,7 +282,6 @@ else:
 
     if selected_opt:
         # collect all detail-lines in active view
-        logger.debug('Collecting detail lines in current view...')
         curve_collector = \
             revit.query.get_elements_by_class(DB.CurveElement,
                                             view_id=revit.active_view.Id)
