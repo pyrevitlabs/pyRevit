fb55ae46f6cf81564d988a07d8fa53da7c57218e
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-27 12:47:30 -0700

    Improved test tool output

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Bundle Tests.pulldown/Test Cached Engines.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Bundle Tests.pulldown/Test Cached Engines.pushbutton/script.py
@@ -6,7 +6,7 @@ from pyrevit.runtime.types import ScriptOutputManager, EngineManager
 __context__ = 'zerodoc'
 
 
-print('Active Engines:')
+print(':fire_engine: Active Engines:')
 for engine_info in EngineManager.EngineDict:
     print('Engine Type Id: %s' % engine_info.Key)
     print('Engine: %s' % engine_info.Value)
@@ -14,10 +14,10 @@ for engine_info in EngineManager.EngineDict:
     print('Engine Assembly: %s' % engine_info.Value.GetType().Assembly.Location)
     print('\n\n')
 
-print('Cached engine? {}'.format('YES' if __cachedengine__ else 'NO'))
+print(':rocket: Cached engine? {}'.format('YES' if __cachedengine__ else 'NO'))
 print('\n\n')
 
-print('Active Output Windows:')
+print(':page_facing_up: Active Output Windows:')
 for so in ScriptOutputManager.ActiveOutputWindows:
     print('Output Id: %s' % so.OutputId)
     print('Output Unique Id: %s' % so.OutputUniqueId)
