6122d3fc8d1d4dbe54ec30c8e8b1490107df6192
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-28 12:10:04 -0700

    Switched to master for final compile

--- dev/pyRevitLabs/pyRevitLabs.Common/Consts.cs
+++ dev/pyRevitLabs/pyRevitLabs.Common/Consts.cs
@@ -14,8 +14,8 @@ namespace pyRevitLabs.Common {
         public const string DefaultRemoteName = "origin";
         public const string OriginalRepoDefaultBranch = "master";
 
-        //public const string TragetBranch = OriginalRepoDefaultBranch;
-        public const string TragetBranch = @"develop/newexec";
+        public const string TragetBranch = OriginalRepoDefaultBranch;
+        //public const string TragetBranch = @"develop/newexec";
 
         // consts for the official pyRevit repo
         public const string OriginalRepoName = ProductName;
