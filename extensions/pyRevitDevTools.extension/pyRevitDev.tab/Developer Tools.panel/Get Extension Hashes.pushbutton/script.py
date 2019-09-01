d515ceb8dae8c4ece99922218d06e2f142f14491
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-09-01 11:06:55 -0700

    Brought back the original code

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Developer Tools.panel/Get Extension Hashes.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Developer Tools.panel/Get Extension Hashes.pushbutton/script.py
@@ -5,29 +5,16 @@ __highlight_updated__= True
 __context__ = 'zero-doc'
 # __beta__ = True
 
-from pyrevit import HOST_APP
 from pyrevit import script
 from pyrevit.extensions import extensionmgr
-from pyrevit.runtime import types
-
-# logger = script.get_logger()
-# logger.set_quiet_mode()
 
 
-# for ui_ext in extensionmgr.get_installed_ui_extensions():
-#     print('{}\t\tExtension: {}'.format(ui_ext.dir_hash_value, ui_ext.name))
+logger = script.get_logger()
+logger.set_quiet_mode()
 
 
-# logger.reset_level()
+for ui_ext in extensionmgr.get_installed_ui_extensions():
+    print('{}\t\tExtension: {}'.format(ui_ext.dir_hash_value, ui_ext.name))
 
-if __shiftclick__:
-    print("Active: %s" % types.DocumentTabEventUtils.IsUpdatingDocumentTabs)
-    if types.DocumentTabEventUtils.DocumentBrushes:
-        print("Count: %s" % types.DocumentTabEventUtils.DocumentBrushes.Count)
-        for k,v in dict(types.DocumentTabEventUtils.DocumentBrushes).items():
-            print(k, v)
-else:
-    if types.DocumentTabEventUtils.IsUpdatingDocumentTabs:
-        types.DocumentTabEventUtils.StopGroupingDocumentTabs(HOST_APP.uiapp)
-    else:
-        types.DocumentTabEventUtils.StartGroupingDocumentTabs(HOST_APP.uiapp)
+
+logger.reset_level()
