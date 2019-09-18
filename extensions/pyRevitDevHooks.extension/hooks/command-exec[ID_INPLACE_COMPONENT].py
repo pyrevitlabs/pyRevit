9923bf92ec40566d64bd7d9234a9dbc7bed5c64f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-09-18 12:06:17 -0700

    Updated command exec test script

--- extensions/pyRevitDevHooks.extension/hooks/command-exec[ID_INPLACE_COMPONENT].py
+++ extensions/pyRevitDevHooks.extension/hooks/command-exec[ID_INPLACE_COMPONENT].py
@@ -1,4 +1,7 @@
 # pylint: skip-file
+import os.path as op
+from pyrevit import script
+
 import hooks_logger as hl
 hl.log_hook(__file__,
     {
@@ -6,4 +9,9 @@ hl.log_hook(__file__,
         "command_id": str(__eventargs__.CommandId.Name),
         "doc_title": str(__eventargs__.ActiveDocument.Title),
     }
+)
+
+output = script.get_output()
+output.print_image(
+    op.join(op.dirname(__file__), op.basename(__file__).replace('.py', '.gif'))
 )
