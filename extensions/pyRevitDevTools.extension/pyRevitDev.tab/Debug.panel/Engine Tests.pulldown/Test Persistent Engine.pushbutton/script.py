735572731bf4538f8fd04baab82727d80bbf647d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-09-20 13:02:57 -0700

    Resolved issue with unsubscribing events on non-modal windows

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Engine Tests.pulldown/Test Persistent Engine.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Engine Tests.pulldown/Test Persistent Engine.pushbutton/script.py
@@ -13,6 +13,35 @@ from pyrevit.runtime import types as runtime_types
 __context__ = 'zero-doc'
 
 
+# with non-modal windows, Revit API call to command execute returns after
+# creating the window, and thus, while window is running, Revit is NOT in
+# API context and all the api calls needs be handled through external events.
+# The event subscription (+=) happens on the API context inside the window
+# __init__ method while window is being created, but
+# the unsibscribe needs to happen on API context and thus an event handler
+# is necessary. The event handler needs to hold a reference to window so
+# it can unsibscribe the subscribed events
+class EventHandlerRemover(UI.IExternalEventHandler):
+    def __init__(self, window):
+        self.name = 'EventHandlerRemover'
+        self.target_window = window
+
+    def Execute(self, uiapp):
+        if self.target_window:
+            HOST_APP.app.DocumentChanged -= \
+                self.target_window.docchanged_hndlr
+            HOST_APP.app.DocumentClosed -= \
+                self.target_window.docclosed_hndlr
+            HOST_APP.app.DocumentOpened -= \
+                self.target_window.docopened_hndlr
+            HOST_APP.uiapp.ViewActivated -= \
+                self.target_window.viewactivated_hndlr
+            self.target_window = None
+
+    def GetName(self):
+        return self.name
+
+
 class NonModalWindow(forms.WPFWindow):
     def __init__(self, xaml_file_name, ext_event_handler):
         forms.WPFWindow.__init__(self, xaml_file_name, set_owner=False)
@@ -24,6 +53,27 @@ class NonModalWindow(forms.WPFWindow):
         self.prev_title = "Title Changed..."
 
         # activate document updator
+        self.docchanged_hndlr = \
+            framework.EventHandler[DB.Events.DocumentChangedEventArgs](
+                self.uiupdator_eventhandler
+            )
+        self.docclosed_hndlr = \
+            framework.EventHandler[DB.Events.DocumentClosedEventArgs](
+                self.uiupdator_eventhandler
+            )
+        self.docopened_hndlr = \
+            framework.EventHandler[DB.Events.DocumentOpenedEventArgs](
+                self.uiupdator_eventhandler
+            )
+        self.viewactivated_hndlr = \
+            framework.EventHandler[UI.Events.ViewActivatedEventArgs](
+                self.uiupdator_eventhandler
+            )
+
+        self.remover = UI.ExternalEvent.Create(
+            EventHandlerRemover(self)
+            )
+
         self.update_ui()
         self.start_listening()
 
@@ -31,16 +81,13 @@ class NonModalWindow(forms.WPFWindow):
         self.update_ui()
 
     def start_listening(self):
-        HOST_APP.app.DocumentClosed += self.uiupdator_eventhandler
-        HOST_APP.app.DocumentOpened += self.uiupdator_eventhandler
-        HOST_APP.app.DocumentChanged += self.uiupdator_eventhandler
-        HOST_APP.uiapp.ViewActivated += self.uiupdator_eventhandler
+        HOST_APP.app.DocumentChanged += self.docchanged_hndlr
+        HOST_APP.app.DocumentClosed += self.docclosed_hndlr
+        HOST_APP.app.DocumentOpened += self.docopened_hndlr
+        HOST_APP.uiapp.ViewActivated += self.viewactivated_hndlr
 
     def stop_listening(self):
-        # FIXME: this crashes Revit
-        # HOST_APP.app.DocumentChanged -= self.docchanged_handler
-        # HOST_APP.uiapp.ViewActivated -= self.viewactivated_handler
-        pass
+        self.remover.Raise()
 
     def action(self, sender, args):
         if __shiftclick__:
