9d33f0316037a62f78ec1fc9b89e0763aac3df70
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-09-19 18:09:45 -0700

    Tested event handlers on non-modal window
    The window reacts safely to document changes and is safe during reloads as well

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Engine Tests.pulldown/Test Persistent Engine.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Engine Tests.pulldown/Test Persistent Engine.pushbutton/script.py
@@ -4,17 +4,43 @@ Shift-Click:
 Run window as Modal
 """
 # pylint: skip-file
+from pyrevit import HOST_APP, framework
 from pyrevit import forms
-from pyrevit import UI
+from pyrevit import revit, DB, UI
 from pyrevit.runtime import types as runtime_types
 
 
+__context__ = 'zero-doc'
+
+
 class NonModalWindow(forms.WPFWindow):
-    def __init__(self, xaml_file_name, ext_event, ext_event_handler):
-        forms.WPFWindow.__init__(self, xaml_file_name)
-        self.ext_event = ext_event
+    def __init__(self, xaml_file_name, ext_event_handler):
+        forms.WPFWindow.__init__(self, xaml_file_name, set_owner=False)
+
         self.ext_event_handler = ext_event_handler
+        self.ext_event = \
+            UI.ExternalEvent.Create(self.ext_event_handler)
+
         self.prev_title = "Title Changed..."
+
+        # activate document updator
+        self.update_ui()
+        self.start_listening()
+
+    def uiupdator_eventhandler(self, sender, args):
+        self.update_ui()
+
+    def start_listening(self):
+        HOST_APP.app.DocumentClosed += self.uiupdator_eventhandler
+        HOST_APP.app.DocumentOpened += self.uiupdator_eventhandler
+        HOST_APP.app.DocumentChanged += self.uiupdator_eventhandler
+        HOST_APP.uiapp.ViewActivated += self.uiupdator_eventhandler
+
+    def stop_listening(self):
+        # FIXME: this crashes Revit
+        # HOST_APP.app.DocumentChanged -= self.docchanged_handler
+        # HOST_APP.uiapp.ViewActivated -= self.viewactivated_handler
+        pass
 
     def action(self, sender, args):
         if __shiftclick__:
@@ -28,11 +54,20 @@ class NonModalWindow(forms.WPFWindow):
     def other_action(self, sender, args):
         self.Title, self.prev_title = self.prev_title, self.Title
 
+    def update_ui(self):
+        if revit.doc:
+            self.doc_tb.Text = 'Document: {}'.format(revit.doc.Title)
+            elements = revit.query.get_all_elements(doc=revit.doc)
+            self.elements_tb.Text = 'Element Count: {}'.format(len(elements))
+        else:
+            self.doc_tb.Text = 'No Documents'
+            self.elements_tb.Text = 'N/A'
 
-handler = runtime_types.PlaceKeynoteExternalEventHandler()
+    def window_closing(self, sender, args):
+        self.stop_listening()
+
 
 NonModalWindow(
     'NonModalWindow.xaml',
-    ext_event=UI.ExternalEvent.Create(handler),
-    ext_event_handler=handler
+    ext_event_handler=runtime_types.PlaceKeynoteExternalEventHandler(),
     ).show(modal=__shiftclick__)
