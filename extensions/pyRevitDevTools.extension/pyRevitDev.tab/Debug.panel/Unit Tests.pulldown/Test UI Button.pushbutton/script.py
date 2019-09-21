45b9c4d660f4030590fbaead6e1e1a72a6b5bc67
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-09-20 18:42:56 -0700

    Added draft if simple ui tool

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test UI Button.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test UI Button.pushbutton/script.py
@@ -3,3 +3,25 @@ from pyrevit import EXEC_PARAMS
 print(__uibutton__)
 
 print(EXEC_PARAMS.command_uibutton)
+
+
+# from pyrevit import revit
+# from pyrevit import script
+
+# ui = script.get_ui()
+
+# ui.button.Content = "Start"
+
+# @revit.events.handle('doc-changed')
+# def doc_changed():
+#     pass
+
+# @ui.handle_close
+# def window_closing():
+#     pass
+
+# @ui.handle_click('button')
+# def do_things():
+#     pass
+
+# ui.show()
