d9f838b2b1bca0b276fa4cf126c2d72542aeabf4
melnikovalex <alex@hiteca.ru>
2019-10-04 16:54:11 +0200

    Toggle Imported - fix if overrided by template

--- extensions/pyRevitTools.extension/pyRevit.tab/Toggles.panel/toggles2.stack/Imported.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Toggles.panel/toggles2.stack/Imported.pushbutton/script.py
@@ -1,11 +1,26 @@
 """Toggles visibility of imported categories on current view."""
 
-from pyrevit import revit
+from pyrevit import revit, forms, DB
 
+def vg_parameter_controlled_by_template(view, built_in_parameter):
+    """
+    Checks if certain view parameter is overrided by template
+    """
+    if view.ViewTemplateId == DB.ElementId.InvalidElementId:
+        return False
+    view_template = view.Document.GetElement(view.ViewTemplateId)
+    if DB.ElementId(int(built_in_parameter)) in view_template.GetNonControlledTemplateParameterIds():
+        return False
+    else:
+        return True
 
 @revit.carryout('Toggle Imported')
 def toggle_imported():
     aview = revit.active_view
+    # check if 'Imported' is overrided by Template. If so, enable temporary view
+    if vg_parameter_controlled_by_template(aview, DB.BuiltInParameter.VIS_GRAPHICS_IMPORT):
+        if not aview.IsTemporaryViewPropertiesModeEnabled():
+            aview.EnableTemporaryViewPropertiesMode(aview.ViewTemplateId)
     aview.AreImportCategoriesHidden = \
         not aview.AreImportCategoriesHidden
 
