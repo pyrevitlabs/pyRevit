afb551843a85ab4f2c48663d966a6094107b28fb
melnikovalex <alex@hiteca.ru>
2019-10-10 13:59:45 +0200

    Flip tool improvements

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit2.stack/Flip.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit2.stack/Flip.pushbutton/script.py
@@ -1,25 +1,49 @@
 """Provides options for flipping (Hand/Face) selected elements."""
 
-from pyrevit import revit, DB
+from pyrevit import revit, DB, script
 from pyrevit import forms
 
 
 __context__ = 'selection'
 
+def can_flip_wall(el):
+    return isinstance(el, DB.Wall)
+
+
+def can_flip_facing(el):
+    return (hasattr(el, 'CanFlipFacing') and el.CanFlipFacing) or hasattr(el, 'rotate')
+
+
+def can_flip_hand(el):
+    return hasattr(el, 'CanFlipHand') and el.CanFlipHand
+
+
+def can_flip(el):
+    return hasattr(el, 'Flip')
+
+
+def flip():
+    with revit.Transaction('Flip Selected'):
+        for el in revit.get_selection():
+            if hasattr(el, 'Flip'):
+                el.Flip()
+
 
 def flip_facing():
     with revit.Transaction('Flip Facing Selected'):
         for el in revit.get_selection():
-            if hasattr(el, 'flipFacing'):
+            if hasattr(el, 'CanFlipFacing') and el.CanFlipFacing:
                 el.flipFacing()
-            elif hasattr(el, 'Flip'):
+            elif can_flip(el):
                 el.Flip()
+            elif hasattr(el, 'rotate'):
+                el.rotate()
 
 
 def flip_hand():
     with revit.Transaction('Flip Hand Selected'):
         for el in revit.get_selection():
-            if hasattr(el, 'flipHand'):
+            if hasattr(el, 'CanFlipHand') and el.CanFlipHand:
                 el.flipHand()
             elif hasattr(el, 'Flip'):
                 el.Flip()
@@ -36,7 +60,7 @@ location_line_values = {'Wall Centerline': (0, 0),
 def flip_wall_location():
     with revit.TransactionGroup("Flip Wall On CenterLine"):
         for el in revit.get_selection():
-            if isinstance(el, DB.Wall):
+            if can_flip_wall(el):
                 param = el.Parameter[DB.BuiltInParameter.WALL_KEY_REF_PARAM]
                 current_value = param.AsInteger()
                 with revit.Transaction('Change Wall Location Line'):
@@ -49,7 +73,7 @@ def flip_wall_location():
 
 def flip_wall_location_line():
     for el in revit.get_selection():
-        if isinstance(el, DB.Wall):
+        if can_flip_wall(el):
             param = el.Parameter[DB.BuiltInParameter.WALL_KEY_REF_PARAM]
             current_value = param.AsInteger()
             for pair in location_line_values.values():
@@ -59,16 +83,32 @@ def flip_wall_location_line():
                     return
 
 
+def selection_can(func):
+    for el in revit.get_selection():
+        if func(el):
+            return True
+    return False
 
-options_dict = {'Flip Facing': flip_facing,
-                'Flip Hand': flip_hand,
-                'Flip Wall On CenterLine': flip_wall_location,
-                'Flip Wall Location Line': flip_wall_location_line}
 
-selected_switch = \
-    forms.CommandSwitchWindow.show(options_dict.keys())
+options_dict = {}
 
-option_func = options_dict.get(selected_switch, None)
+if selection_can(can_flip_wall):
+    options_dict.update({'Flip Wall On CenterLine': flip_wall_location,
+                         'Flip Wall Location Line': flip_wall_location_line})
+if selection_can(can_flip_facing):
+    options_dict.update({'Flip Facing': flip_facing})
+if selection_can(can_flip_hand):
+    options_dict.update({'Flip Hand': flip_hand})
+if not options_dict and selection_can(can_flip):
+    options_dict.update({'Flip': flip})
 
-if option_func:
-    option_func()
+if options_dict:
+    selected_switch = \
+        forms.CommandSwitchWindow.show(options_dict.keys())
+
+    option_func = options_dict.get(selected_switch, None)
+
+    if option_func:
+        option_func()
+else:
+    forms.alert("Selection cannot be flipped")
