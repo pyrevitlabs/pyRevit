5db77ef3e8a47733abc38a9e6270d3be09942bc5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-10-23 11:36:19 -0700

    Updated test xaml tool with multi-databinding
    per response to my question on SO
    https://stackoverflow.com/a/58522061/2350244

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test XAML.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test XAML.pushbutton/script.py
@@ -2,8 +2,10 @@
 #pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
 import urllib2
 import json
+from time import sleep
 
 from pyrevit import framework
+from pyrevit import coreutils
 from pyrevit import revit, DB
 from pyrevit import forms
 from pyrevit import script
@@ -43,7 +45,7 @@ class ButtonData(forms.Reactive):
 class EmployeeInfo(forms.Reactive):
     def __init__(self, name, job, supports):
         self._name = name
-        self.job = job
+        self._job = job
         self.supports = supports
 
     @forms.reactive
@@ -53,6 +55,14 @@ class EmployeeInfo(forms.Reactive):
     @name.setter
     def name(self, value):
         self._name = value
+
+    @forms.reactive
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        self._job = value
 
 
 class Server(forms.Reactive):
@@ -69,20 +79,18 @@ class Server(forms.Reactive):
         self._status = value
 
 
+class DataSelectorConverter(framework.Windows.Data.IMultiValueConverter):
+    def Convert(self, values, target_types, parameter, culture):
+        return values[0][int(values[1]) - 1]
+
+    def ConvertBack(self, value, target_types, parameter, culture):
+        pass
+
+
 # code-behind for the window
 class UI(forms.WPFWindow):
     def __init__(self):
-        self.nested_data = NestedObject(text="<nested text>")
-        self.data = \
-            ButtonData(
-                title="<bound title>",
-                nested=self.nested_data
-                )
-        self.server = Server(r'https://status.epicgames.com/api/v2/status.json')
-
-    def setup(self):
-        self.textbox.DataContext = self.nested_data
-        self.empinfo.DataContext = [
+        self.employee_data = [
             EmployeeInfo(
                 name="Ehsan",
                 job="Architect",
@@ -104,6 +112,26 @@ class UI(forms.WPFWindow):
                     "Core"
                 ]),
         ]
+        self.nested_data = NestedObject(text="Text in Data Object")
+        self.data = \
+            ButtonData(
+                title="Title in Data Object",
+                nested=self.nested_data
+                )
+        self.server = Server(r'https://status.epicgames.com/api/v2/status.json')
+
+    def setup(self):
+        mbinding = framework.Windows.Data.MultiBinding()
+        mbinding.Converter = DataSelectorConverter()
+        mbinding.Bindings.Add(framework.Windows.Data.Binding("."))
+        binding = framework.Windows.Data.Binding("Value")
+        binding.ElementName = "selector"
+        mbinding.Bindings.Add(binding)
+        # mbinding.NotifyOnSourceUpdated = True
+
+        self.textbox.DataContext = self.nested_data
+        self.emppanel.SetBinding(self.emppanel.DataContextProperty, mbinding)
+        self.empinfo.DataContext = self.employee_data
         self.textblock.DataContext = self.data
         self.button.DataContext = self.data
         self.statuslight.DataContext = self.server
@@ -112,19 +140,21 @@ class UI(forms.WPFWindow):
         self.server.status = status is not None
 
     def check_status(self):
-        status = json.loads(urllib2.urlopen(self.server.url).read())
+        status = json.loads(coreutils.read_url(self.server.url))
+        sleep(2)    # fake slow io
         self.dispatch(self.set_status, status)
 
     def check_fortnite_status(self, sender, args):
         self.dispatch(self.check_status)
 
     def button_click(self, sender, args):
-        self.data.title = "<updated bound title>"
-        self.nested_data.text = "<updated nested text>"
+        self.data.title = "Title in Data Object (Updated)"
+        self.nested_data.text = "Text in Data Object (Updated)"
+        for emp in self.employee_data:
+            emp.job = emp.job.replace(" (Updated)", "") + " (Updated)"
 
     def read_data(self, sender, args):
         forms.alert(self.nested_data.text)
-
 
 # init ui
 ui = script.load_ui(UI(), 'ui.xaml')
