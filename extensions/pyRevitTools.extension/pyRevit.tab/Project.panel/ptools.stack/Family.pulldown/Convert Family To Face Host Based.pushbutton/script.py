97efb1a6390f5460606f840e95c9cbd9bcf91bfd
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-11-21 11:41:38 -0800

    Revised script per issue #681

--- extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack/Family.pulldown/Convert Family To Face Host Based.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack/Family.pulldown/Convert Family To Face Host Based.pushbutton/script.py
@@ -1,3 +1,4 @@
+#pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
 from pyrevit import framework
 from pyrevit import revit, DB, UI
 from pyrevit import script
@@ -13,37 +14,44 @@ __doc__ = 'This script removes all instances of the selected element '\
           'SecurityDevices, Sprinklers, TelephoneDevices can be converted.'
 
 
-def deleteallinstances(family):
-    matchlist = []
+def delete_all_instances(target_family):
+    family_instances = []
     try:
-        symbolidset = family.GetFamilySymbolIds()
-        for symid in symbolidset:
-            for faminstance in DB.FilteredElementCollector(revit.doc)\
+        symbol_ids = target_family.GetFamilySymbolIds()
+        for symbol_id in symbol_ids:
+            for family_instance in DB.FilteredElementCollector(revit.doc)\
                                  .WherePasses(
-                                     DB.FamilyInstanceFilter(revit.doc,
-                                                             symid))\
+                                         DB.FamilyInstanceFilter(
+                                             revit.doc,
+                                             symbol_id))\
                                  .ToElements():
-                matchlist.append(faminstance.Id)
+                family_instances.append(family_instance.Id)
     except Exception:
         raise Exception
 
-    for elid in matchlist:
-        revit.doc.Delete(elid)
+    for element_id in family_instances:
+        revit.doc.Delete(element_id)
 
 
 if forms.alert('All instances of the selected families '
                'will be removed for this conversion. '
                'Are you ready to proceed?', cancel=True, yes=True):
-    for el in revit.get_selection():
-        fam = el.Symbol.Family
-        famid = el.Symbol.Family.Id
-        print('\nStarting conversion for family: {0}'.format(fam.Name))
-        try:
-            with revit.Transaction('Convert to Face Host Based'):
-                deleteallinstances(fam)
-                DB.FamilyUtils.ConvertFamilyToFaceHostBased(revit.doc, famid)
+    for selected_element in revit.get_selection():
+        family = selected_element.Symbol.Family
+        if family:
+            if not DB.FamilyUtils.FamilyCanConvertToFaceHostBased(revit.doc,
+                                                                  family.Id):
+                forms.alert("Revit API does not support converting this "
+                            "family into a face-hosted family.",
+                            exitscript=True)
 
-            print('Conversion Successful.')
-        except Exception as e:
-            print('Conversion failed for family: {0}'.format(fam.Name))
-            print('Exception Description:\n{0}'.format(e))
+            print('\nStarting conversion for family: {0}'.format(family.Name))
+            try:
+                with revit.Transaction('Convert to Face Host Based'):
+                    delete_all_instances(family)
+                    DB.FamilyUtils.ConvertFamilyToFaceHostBased(revit.doc,
+                                                                family.Id)
+                print('Conversion Successful.')
+            except Exception as e:
+                print('Conversion failed for family: {0}'.format(family.Name))
+                print('Exception Description:\n{0}'.format(e))
