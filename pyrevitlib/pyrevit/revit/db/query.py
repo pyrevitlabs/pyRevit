c9ff4bf26041c0f8dde235c1280c5d9b7f9682bb
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-03-13 10:38:56 -0700

    Resolved issue #833

--- pyrevitlib/pyrevit/revit/db/query.py
+++ pyrevitlib/pyrevit/revit/db/query.py
@@ -258,7 +258,7 @@ def get_elements_by_categories(element_bicats, elements=None, doc=None):
                 in element_bicats]
 
     # otherwise collect from model
-    cat_filters = [DB.ElementCategoryFilter(x) for x in element_bicats]
+    cat_filters = [DB.ElementCategoryFilter(x) for x in element_bicats if x]
     elcats_filter = \
         DB.LogicalOrFilter(framework.List[DB.ElementFilter](cat_filters))
     return DB.FilteredElementCollector(doc or HOST_APP.doc)\
