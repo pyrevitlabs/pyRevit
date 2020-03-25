eb6d30f575f1bd86ceb33e8fed4b9fab0c129a36
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-03-25 12:05:34 -0700

    Fixed issue #760

--- dev/pyRevitLabs/pyRevitLabs.Common/CommonUtils.cs
+++ dev/pyRevitLabs/pyRevitLabs.Common/CommonUtils.cs
@@ -395,7 +395,7 @@ namespace pyRevitLabs.Common {
             string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
             string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
             string Name = "";
-            Name += consonants[r.Next(consonants.Length)].ToUpper();
+            Name += consonants[r.Next(consonants.Length)].ToUpperInvariant();
             Name += vowels[r.Next(vowels.Length)];
             int b = 2; //b tells how many times a new letter has been added. It's 2 right now because the first two letters are already in the name.
             while (b < len) {
