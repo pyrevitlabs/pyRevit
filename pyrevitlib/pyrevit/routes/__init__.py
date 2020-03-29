1fa6f32691495e7488e1f4a561cba532362fec53
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-03-29 12:14:51 -0700

    added support for route server per revit instance

--- pyrevitlib/pyrevit/routes/__init__.py
+++ pyrevitlib/pyrevit/routes/__init__.py
@@ -1,7 +1,9 @@
 """Handles http api routing and serving with usage similar to flask."""
 #pylint: disable=import-error,invalid-name,broad-except,dangerous-default-value,missing-docstring
+from pyrevit import HOST_APP, PyRevitException
 from pyrevit.api import DB, UI
 from pyrevit.coreutils import envvars
+from pyrevit.coreutils.logger import get_logger
 from pyrevit.userconfig import user_config
 
 from pyrevit.routes import router
@@ -11,6 +13,9 @@ from pyrevit.routes.server import Request, Response
 
 __all__ = ('API', 'Request', 'Response',
            'activate_routes', 'deactivate_routes')
+
+
+mlogger = get_logger(__name__)
 
 
 class API(object):
@@ -48,13 +53,19 @@ def activate_routes():
     routes_server = envvars.get_pyrevit_env_var(envvars.ROUTES_SERVER)
     if not routes_server:
         host = user_config.core.get_option("routes_host", default_value='')
-        port = user_config.core.get_option("routes_port", default_value=48884)
-        routes_server = server.RoutesServer(ip=host, port=port)
-        envvars.set_pyrevit_env_var(envvars.ROUTES_SERVER, routes_server)
-        routes_server.start()
+        ports_dict = \
+            user_config.core.get_option("routes_ports", default_value={})
+        port = ports_dict.get(HOST_APP.version, None)
+        if port:
+            routes_server = server.RoutesServer(ip=host, port=port)
+            envvars.set_pyrevit_env_var(envvars.ROUTES_SERVER, routes_server)
+            routes_server.start()
+        else:
+            mlogger.error("Port is not configured for Routes server")
 
 
 def deactivate_routes():
     routes_server = envvars.get_pyrevit_env_var(envvars.ROUTES_SERVER)
-    envvars.set_pyrevit_env_var(envvars.ROUTES_SERVER, None)
-    routes_server.stop()
+    if routes_server:
+        envvars.set_pyrevit_env_var(envvars.ROUTES_SERVER, None)
+        routes_server.stop()
