bf7a39eb7d8973d53c7759041d2f99926681f939
melnikovalex <alex@hiteca.ru>
2020-05-10 18:23:58 +0200

    Fix CopyState - ViewZoomPanState

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/lib/copypastestate/actions.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/lib/copypastestate/actions.py
@@ -72,7 +72,7 @@ class ViewZoomPanStateAction(basetypes.CopyPasteStateAction):
         dir_orient = vzps_data.dir_orient
 
         active_ui_view = revit.uidoc.GetOpenUIViews()[0]
-        if revit.active_view.ViewType == view_type:
+        if revit.active_view.ViewType != view_type:
             raise PyRevitException(
                 'Saved view type (%s) is different from active view (%s)'
                 % (vzps_data.view_type, type(revit.active_view).__name__))
