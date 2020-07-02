cfe3ff8402fabf7012b1b6815d5f192cc0c5055b
jmcouffin <jmcouffin@users.noreply.github.com>
2020-07-02 12:11:49 +0200

    Adding SaveAs postable command functions
    I added 3 postable commands to allows users to have icons and keyboard shortcuts for these common functions

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools3.stack/SaveAs Family.pushbutton/script.py
@@ -0,0 +1,31 @@
+"""Save As Family
+This tool uses the postable command to save your families with the SaveAs command
+Copyright (c) 2020 Jean-Marc Couffin
+https://github.com/jmcouffin
+--------------------------------------------------------
+pyRevit Notice:
+Copyright (c) 2014-2020 Ehsan Iran-Nejad
+pyRevit: repository at https://github.com/eirannejad/pyRevit
+"""
+
+__title__ = "SaveAs Family"
+__author__ = 'Jean-Marc Couffin'
+__contact__ = 'https://github.com/jmcouffin'
+__credits__ = 'http://eirannejad.github.io/pyRevit/credits/'
+__doc__ = 'Allows you to set the save as command as an icon and attribute a shortcut'
+
+import clr
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+import Autodesk
+from Autodesk.Revit.UI import RevitCommandId
+from Autodesk.Revit.UI import UIApplication
+from Autodesk.Revit.UI import ExternalCommandData
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+
+uiapp = DocumentManager.Instance.CurrentUIApplication
+CmndID = RevitCommandId.LookupCommandId('ID_SAVE_FAMILY_ANY')
+CmId = CmndID.Id
+uiapp.PostCommand(CmndID)
