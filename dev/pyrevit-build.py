0f1be3ef424bcf079ef1c7817ee6678c32c4dfa2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-08 13:02:13 -0700

    more cleanups

--- dev/pyrevit-build.py
+++ dev/pyrevit-build.py
@@ -1,43 +1,3 @@
-#! bin/python
-# tasks
-# X docs
-# - labs + cli
-# - engines: ironpython loader and runners
-# - engines: cpython
-# - telemetry server, and autocomplete
-    # echo "prepare git for go packages..."
-    # git config --global http.https://pkg.re.followRedirects true
-    # echo "formatting autocomplete helper code"
-    # go fmt $AUTOCOMP
-    # go get "github.com/lib/pq"
-    # go get "gopkg.in/mgo.v2"
-    # go get "github.com/denisenkom/go-mssqldb"
-    # go get "github.com/go-sql-driver/mysql"
-    # go get "github.com/mattn/go-sqlite3"
-    # go get "github.com/gorilla/mux"
-    # go get "github.com/satori/go.uuid"
-    # go get "github.com/gofrs/uuid"
-    # go get "pkg.re/essentialkaos/ek.v10/fmtc"
-    # go get "github.com/asaskevich/govalidator"
-    # go get "github.com/posener/complete"
-# - installer: pyrevit (and update pyrevit_products.json)
-# - installer: pyrevit cli (and update pyrevit_products.json)
-    # echo "setting version "$PYREVIT_VERSION" on "$PYREVIT_AIPFILE
-    # advancedinstaller.com "//edit" $PYREVIT_AIPFILE "//SetVersion" $PYREVIT_VERSION
-    # AIPVersion=$(advancedinstaller.com "//edit" $PYREVIT_AIPFILE "//GetProperty" "ProductVersion")
-    # echo $AIPVersion > $PYREVIT_VERSION_FILE
-    # advancedinstaller.com "//build" $PYREVIT_AIPFILE
-# utils:
-# - add new revit version
-# X collect changes
-# - run tests?
-# - start telemetry server for testing (mongo db docker)
-# - push translation files into pyRevit (airtable api)
-
-# TODO: primary goals
-# - make my life easier building a new release of pyRevit
-# - build docs for quick testing of doc build errors
-# pylint: disable=broad-except,invalid-name
 """Utility to support pyRevit build and release workflows
 
 Usage:
@@ -59,6 +19,7 @@ Options:
         Advanced Installer (installer builder)
                                 https://www.advancedinstaller.com
 """
+# pylint: disable=invalid-name,broad-except
 import sys
 import os
 import os.path as op
@@ -71,6 +32,7 @@ from docopt import docopt
 
 # dev scripts
 from scripts import utils
+from scripts.utils import Command
 from scripts import configs
 
 # cli info
@@ -78,24 +40,10 @@ __binname__ = os.environ["PYREVIT_BIN"] or op.splitext(op.basename(__file__))[0]
 __version__ = "1.0"
 
 
-class Command:
-    """CLI command type"""
-
-    def __init__(self, name, target, args, run):
-        self.name = name
-        self.target = target
-        self.args = args
-        self.help = run.__doc__
-        self.template = " ".join(
-            [x for x in [self.name, self.target, " ".join(self.args)] if x]
-        )
-        self.run = run
-
-
 Change = namedtuple("Change", ["hash", "message"])
 
 
-def install(args: Dict[str, str]):
+def install(_: Dict[str, str]):
     """Prepare build environment"""
     print("running install")
 
@@ -104,16 +52,15 @@ def report_changes_since(args: Dict[str, str]):
     """List commit messages from given tag to HEAD"""
     tag_hash = utils.system(["git", "rev-parse", f"{args['<tag>']}"])
     changes = []
-    for cline in utils.system(["git", "log", "--oneline", f"{tag_hash}..HEAD"]).split(
-        "\n"
-    ):
+    changelines = utils.system(["git", "log", "--oneline", f"{tag_hash}..HEAD"])
+    for cline in changelines.split("\n"):
         chash, cmsg = cline.split(" ", 1)
         changes.append(Change(hash=chash, message=cmsg))
 
     print("\n".join([x.message for x in changes]))
 
 
-def build_docs(args: Dict[str, str]):  # pylint: disable=unused-argument
+def build_docs(_: Dict[str, str]):
     """Build the python docs"""
     report = utils.system(
         ["pipenv", "run", "sphinx-build", "-b", "html", ".", configs.DOCS_BUILD],
@@ -122,7 +69,7 @@ def build_docs(args: Dict[str, str]):  # pylint: disable=unused-argument
     print(report)
 
 
-def open_docs(args: Dict[str, str]):  # pylint: disable=unused-argument
+def open_docs(_: Dict[str, str]):
     """Open the local python docs in browser"""
     if op.exists(configs.DOCS_INDEX):
         if sys.platform == "darwin":
@@ -134,7 +81,7 @@ def open_docs(args: Dict[str, str]):  # pylint: disable=unused-argument
         raise Exception("Docs are not built yet")
 
 
-def clean_docs(args: Dict[str, str]):  # pylint: disable=unused-argument
+def clean_docs(_: Dict[str, str]):
     """Clean the local python docs build folder"""
     shutil.rmtree(configs.DOCS_BUILD)
     print("Removed docs build directory")
@@ -149,17 +96,11 @@ COMMANDS = [
 ]
 
 
-def run_command(args: Dict[str, str]):
-    """Process cli args and run the appropriate commands"""
-    for cmd in COMMANDS:
-        if args[cmd.name]:
-            cmd.run(args)
-
-
 if __name__ == "__main__":
     try:
         # run the appropriate command
-        run_command(
+        utils.run_command(
+            COMMANDS,
             # process args
             docopt(
                 __doc__.format(
@@ -171,7 +112,7 @@ if __name__ == "__main__":
                     ),
                 ),
                 version="{} {}".format(__binname__, __version__),
-            )
+            ),
         )
     # gracefully handle exceptions and print results
     except Exception as run_ex:
--- dev/scripts/utils.py
+++ dev/scripts/utils.py
@@ -1,9 +1,30 @@
 """Dev scripts utilities"""
-from typing import List, Optional
+from typing import List, Optional, Dict
 import subprocess
+
+
+class Command:
+    """CLI command type"""
+
+    def __init__(self, name, target, args, run):
+        self.name = name
+        self.target = target
+        self.args = args
+        self.help = run.__doc__
+        self.template = " ".join(
+            [x for x in [self.name, self.target, " ".join(self.args)] if x]
+        )
+        self.run = run
 
 
 def system(args: List[str], cwd: Optional[str] = None):
     """Run a command and return the stdout"""
     res = subprocess.run(args, capture_output=True, check=True, cwd=cwd)
     return res.stdout.decode().strip()
+
+
+def run_command(commands: List[Command], args: Dict[str, str]):
+    """Process cli args and run the appropriate commands"""
+    for cmd in commands:
+        if args[cmd.name]:
+            cmd.run(args)
