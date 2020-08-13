16c34bffbbbd1a4395967415b49b0efcdbfae1c2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-13 13:37:07 -0700

    build script wont build docs on windows

--- dev/_apidocspy.py
+++ dev/_apidocspy.py
@@ -10,8 +10,23 @@ from scripts import utils
 from scripts import configs
 
 
+def _abort(message):
+    print("Building python docs has errors")
+    print(message)
+    sys.exit(1)
+
+
 def build_docs(_: Dict[str, str]):
     """Build the python docs"""
+    if sys.platform == 'win32':
+        print(
+            "Skipping building the docs on Windows\n"
+            " Some packages fail install due to end of life python 2.7"
+            )
+        return
+
+    print("Building python docs...")
+    report = utils.system(["pipenv", "install"], cwd=configs.DOCS_DIR)
     report = utils.system(
         [
             "pipenv",
@@ -24,7 +39,11 @@ def build_docs(_: Dict[str, str]):
         ],
         cwd=configs.DOCS_DIR,
     )
-    print(report)
+    if not report:
+        _abort("Sphinx is not configured to build the docs")
+    if any(x in report.lower() for x in ['warning', 'failed']):
+        _abort(report)
+    print("Building python docs completed successfully")
 
 
 def open_docs(_: Dict[str, str]):
