a32596e95f62fbce3c20ec2db33e4ef659bd66a9
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-13 08:16:42 -0700

    implemented #970 [tool]

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Rename Selected Views.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Rename Selected Views.pushbutton/script.py
@@ -1,10 +1,37 @@
 """Changes the selected view names."""
-
+#pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
+import re
 from pyrevit import revit, DB
 from pyrevit import forms
 
 
-def change_case(view_list, upper=True, verbose=False):
+def _lower(name):
+    return name.lower()
+
+def _upper(name):
+    return name.upper()
+
+def _camel(name):
+    string = re.sub(r"(_|-)+", " ", name).title().replace(" ", "")
+    return string[0].lower() + string[1:]
+
+def _title(name):
+    return name.title()
+
+def _sentence(name):
+    return name.capitalize()
+
+
+CASE_FUNCTIONS = {
+    'UPPER CASE': _upper,
+    'lower case': _lower,
+    'camel Case': _camel,
+    'Title Case': _title,
+    'Sentence case': _sentence,
+}
+
+
+def _change_case(view_list, casefunc, verbose=False):
     with revit.Transaction('Viewnames to Upper'):
         for view in view_list:
             if isinstance(view, DB.Viewport):
@@ -12,7 +39,7 @@ def change_case(view_list, upper=True, verbose=False):
             orig_name = revit.query.get_name(view)
             revit.update.set_name(
                 view,
-                orig_name.upper() if upper else orig_name.lower()
+                casefunc(orig_name)
                 )
             if verbose:
                 print("VIEW: {0}\n"
@@ -25,13 +52,12 @@ selected_views = forms.select_views(use_selection=True)
 if selected_views:
     selected_option, switches = \
         forms.CommandSwitchWindow.show(
-            ['to UPPERCASE',
-             'to lowercase'],
+            CASE_FUNCTIONS.keys(),
             switches=['Show Report'],
             message='Select rename option:'
             )
 
     if selected_option:
-        change_case(selected_views,
-                    upper=True if selected_option == 'to UPPERCASE' else False,
-                    verbose=switches['Show Report'])
+        _change_case(selected_views,
+                     casefunc=CASE_FUNCTIONS[selected_option],
+                     verbose=switches['Show Report'])
