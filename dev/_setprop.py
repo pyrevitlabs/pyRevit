6c9eefde12e65add7d97121d4052dd6618d3c142
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-13 21:25:54 -0700

    copyright and version files won't be updated if the value is unchanged

--- dev/_setprop.py
+++ dev/_setprop.py
@@ -6,32 +6,37 @@ import datetime
 from scripts import configs
 
 
+def _modify_contents(files, finder, new_value):
+    for text_file in files:
+        contents = []
+        file_changed = False
+        with open(text_file, "r") as sfile:
+            for cline in sfile.readlines():
+                newcline = finder.sub(new_value, cline)
+                if cline != newcline:
+                    file_changed = True
+                contents.append(newcline)
+        if file_changed:
+            with open(text_file, "w") as sfile:
+                sfile.writelines(contents)
+
+
 def set_year(_: Dict[str, str]):
     """Update copyright notice"""
     this_year = datetime.datetime.today().year
-    cp_finder = re.compile(r'© 2014-\d{4}')
-    new_copyright = f'© 2014-{this_year}'
+    cp_finder = re.compile(r"© 2014-\d{4}")
+    new_copyright = f"© 2014-{this_year}"
     print(f'Updating copyright notice to "{new_copyright}"...')
-    for copyright_file in configs.COPYRIGHT_FILES:
-        contents = []
-        with open(copyright_file, 'r') as sfile:
-            for cline in sfile.readlines():
-                cline = cp_finder.sub(new_copyright, cline)
-                contents.append(cline)
-        with open(copyright_file, 'w') as sfile:
-            sfile.writelines(contents)
+    _modify_contents(
+        files=configs.COPYRIGHT_FILES, finder=cp_finder, new_value=new_copyright
+    )
 
 
 def set_ver(args: Dict[str, str]):
     """Update version number"""
-    ver_finder = re.compile(r'4\.\d\.\d')
+    ver_finder = re.compile(r"4\.\d\.\d")
     new_version = args["<ver>"]
-    print(f'Updating version to v{new_version}...')
-    for version_file in configs.VERSION_FILES:
-        contents = []
-        with open(version_file, 'r') as vfile:
-            for cline in vfile.readlines():
-                cline = ver_finder.sub(new_version, cline)
-                contents.append(cline)
-        with open(version_file, 'w') as vfile:
-            vfile.writelines(contents)
+    print(f"Updating version to v{new_version}...")
+    _modify_contents(
+        files=configs.VERSION_FILES, finder=ver_finder, new_value=new_version
+    )
