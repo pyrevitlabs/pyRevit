957ca76c5afe6928cab26d74fe570515efaac487
melnikovalex <alex@hiteca.ru>
2020-08-14 21:14:39 +0200

    List Family Size combine two modes in one file

--- extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack/Inspect.pulldown/ListFamilySizeCreator.pushbutton/ListFamilySizeCreator_script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack/Inspect.pulldown/ListFamilySizeCreator.pushbutton/ListFamilySizeCreator_script.py
@@ -2,15 +2,14 @@
 """List all families and their sizes."""
 import os.path as op
 import math
-from collections import defaultdict
-
-from pyrevit import revit, DB
+from pyrevit import revit, forms, script, DB
 
 __title__ = 'Lists Family Sizes'
-__author__ = 'Frederic Beaupere'
+__authors__ = ['Frederic Beaupere', 'Alex Melnikov']
 __contact__ = 'https://github.com/frederic-beaupere'
 __credits__ = 'http://eirannejad.github.io/pyRevit/credits/'
 
+FIELDS = ["Size", "Name", "Category", "Creator"]
 
 def convert_size(size_bytes):
     if size_bytes == 0:
@@ -22,39 +21,94 @@ def convert_size(size_bytes):
     return "{}{}".format(size, size_unit[i])
 
 
+def print_totals(families):
+    total_size = sum([fam_item.get("Size") or 0 for fam_item in families])
+    print("%d families, found total size: %s\n\n" % (
+        len(families), convert_size(total_size)))
+
+
+def print_sorted(families, sort_by):
+    # sort list of fields
+    fields_rest = list(FIELDS)
+    fields_rest.pop(FIELDS.index(sort_by))
+    fields_sorted = [sort_by] + fields_rest
+
+    if sort_by not in ["Creator", "Category"]: # do not group by name and size
+        print("; ".join(fields_sorted))
+        families_grouped = {"": sorted(families, key=\
+            lambda fam_item: fam_item.get(sort_by))}
+
+    else:
+        print(";".join(fields_rest))
+        # convert to grouped dict
+        families_grouped = {}
+        for fam_item in sorted(families, key=\
+                lambda fam_item: fam_item.get(fields_rest[0])):
+            sort_by_value = fam_item[sort_by]
+            fam_item_reduced = dict(fam_item)
+            fam_item_reduced.pop(sort_by)
+            if sort_by_value not in families_grouped:
+                families_grouped[sort_by_value] = []
+            families_grouped[sort_by_value].append(fam_item_reduced)
+
+    for group_value in sorted(families_grouped.keys()):
+        print(50 * "-")
+        print("%s: %s" % (sort_by, group_value))
+        for fam_item in families_grouped[group_value]:
+            family_row = []
+
+            for field in fields_sorted:
+                value = fam_item.get(field)
+                if value is None:
+                    continue
+                if field == "Size":
+                    value = convert_size(value) if value > 0 else "N/A"
+                family_row.append(value)
+            print("; ".join(family_row))
+        print_totals(families_grouped[group_value])
+
+# main logic
+# ask use to choose sort option
+sort_by = forms.CommandSwitchWindow.show(FIELDS,
+     message='Sorting options:',
+)
+if not sort_by:
+    script.exit()
+
 all_fams = DB.FilteredElementCollector(revit.doc)\
              .OfClass(DB.Family)\
              .ToElements()
 
-fams_creator = defaultdict(list)
-
-for fam in all_fams:
-    if fam.IsEditable:
-        fam_doc = revit.doc.EditFamily(fam)
-        fam_path = fam_doc.PathName
-        fam_size = "0"
-        fam_creator = \
-            DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc,
-                                                          fam.Id).Creator
-        if fam_path:
-            if op.exists(fam_path):
-                fam_size = str(op.getsize(fam_path))
-        fams_creator[fam_creator].append(fam_size + "; " + fam.Name)
-
-print("Families overview:")
-for creator in fams_creator:
-    print(50*"-")
-    print("{} created:".format(creator))
-    sum_size_created = 0
-    for fam in fams_creator[creator]:
-        size_prefix = int(fam.split(";")[0])
-        fam_name = fam.split(";")[1]
-        sum_size_created += int(size_prefix)
-        if size_prefix == 0:
-            size_prefix = " N/A "
+all_family_items = []
+with forms.ProgressBar(title=__title__, cancellable=True) as pb:
+    i = 0
+    for fam in all_fams:
+        with revit.ErrorSwallower() as swallower:
+            if fam.IsEditable:
+                fam_doc = revit.doc.EditFamily(fam)
+                fam_path = fam_doc.PathName
+                fam_size = 0
+                fam_category = fam.FamilyCategory.Name if fam.FamilyCategory \
+                    else "N/A"
+                fam_creator = \
+                    DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc,
+                                                                  fam.Id).Creator
+                if fam_path:
+                    if op.exists(fam_path):
+                        fam_size = op.getsize(fam_path)
+                all_family_items.append({"Size": fam_size,
+                                         "Creator": fam_creator,
+                                         "Category": fam_category,
+                                         "Name": fam.Name})
+        if pb.cancelled:
+            break
         else:
-            size_prefix = convert_size(size_prefix)
-        print(size_prefix, fam_name)
-    print("{1} families, found total size: {0}"
-          .format(convert_size(sum_size_created),
-                  len(fams_creator[creator])))
+            pb.update_progress(i, len(all_fams))
+        i += 1
+
+
+# print results
+print("Families overview:")
+print_sorted(all_family_items, sort_by)
+print_totals(all_family_items)
+
