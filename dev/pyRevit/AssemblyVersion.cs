f2858b73bf2ed22d5aaadb4300f0c06d9411f29f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-16 16:14:29 -0700

    upped version to 4.8.1

--- dev/pyRevit/AssemblyVersion.cs
+++ dev/pyRevit/AssemblyVersion.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("4.8.0")]
-[assembly: AssemblyFileVersion("4.8.0")]
+[assembly: AssemblyVersion("4.8.1")]
+[assembly: AssemblyFileVersion("4.8.1")]
