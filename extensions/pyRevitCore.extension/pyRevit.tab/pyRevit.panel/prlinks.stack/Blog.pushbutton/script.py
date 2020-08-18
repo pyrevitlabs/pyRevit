b12e01ac1b2d1c0b64a19fad94b8f14b41fd1c85
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-08-18 16:40:38 -0700

    created bundle files

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/prlinks.stack/Blog.pushbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/prlinks.stack/Blog.pushbutton/script.py
@@ -5,11 +5,4 @@ from pyrevit import script
 from pyrevit.versionmgr import urls
 
 
-__context__ = 'zero-doc'
-__title__ = {
-    'en_us': "Blog",
-    'fa': "وبلاگ",
-    'ar': "بلاگ",
-}
-
 script.open_url(urls.PYREVIT_BLOG)
