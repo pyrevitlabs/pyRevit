1545fa874811f31c75a7642ec6d83200f7190795
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-09-27 11:53:26 -0700

    fixed issue #994 [tool]

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/Memory.pulldown/lib/iter_selection.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/Memory.pulldown/lib/iter_selection.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
-import os
+#pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
 import os.path as op
-import sys
 import pickle
 
-from pyrevit.framework import List
 from pyrevit import revit, DB
 from pyrevit import script
 
 
 logger = script.get_logger()
 
-datafile = script.get_document_data_file("SelList", "pym")
-index_datafile = script.get_document_data_file("SelListPrevNextIndex", "pym")
-
-
-selection = revit.get_selection()
-
 
 def iterate(mode, step_size=1):
+    """Iterate over elements in memorized selection"""
+    index_datafile = \
+        script.get_document_data_file("SelListPrevNextIndex", "pym")
+    datafile = \
+        script.get_document_data_file("SelList", "pym")
+
+    selection = revit.get_selection()
+
     if op.exists(index_datafile):
         with open(index_datafile, 'r') as f:
             idx = pickle.load(f)
@@ -30,19 +30,22 @@ def iterate(mode, step_size=1):
     else:
         idx = 0
 
-    try:
-        with open(datafile, 'r') as df:
-            cursel = pickle.load(df)
+    if op.exists(datafile):
+        try:
+            with open(datafile, 'r') as df:
+                cursel = pickle.load(df)
 
-        if idx < 0:
-            idx = abs(idx / len(cursel)) * len(cursel) + idx
-        elif idx >= len(cursel):
-            idx = idx - abs(idx / len(cursel)) * len(cursel)
+            if cursel:
+                if idx < 0:
+                    idx = abs(idx / len(cursel)) * len(cursel) + idx
+                elif idx >= len(cursel):
+                    idx = idx - abs(idx / len(cursel)) * len(cursel)
 
-        selection.set_to([DB.ElementId(int(list(cursel)[idx]))])
+                selection.set_to([DB.ElementId(int(list(cursel)[idx]))])
 
-        with open(index_datafile, 'w') as f:
-            pickle.dump(idx, f)
-
-    except Exception as io_err:
-        logger.error('Error read/write to: {} | {}'.format(datafile, io_err))
+                with open(index_datafile, 'w') as f:
+                    pickle.dump(idx, f)
+        except Exception as io_err:
+            logger.error(
+                'Error read/write to: %s | %s', datafile, io_err
+                )
