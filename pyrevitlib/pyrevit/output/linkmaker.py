7addd7390d571eed51f1e8e98d4a0a1a7f1a3e1f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-10-05 12:06:15 -0700

    enhanced the way element links show up on output window [api]

--- pyrevitlib/pyrevit/output/linkmaker.py
+++ pyrevitlib/pyrevit/output/linkmaker.py
@@ -10,9 +10,26 @@ mlogger = get_logger(__name__)
 
 
 PROTOCOL_NAME = 'revit://outputhelpers?'
-
-DEFAULT_LINK = '<a title="Click to select or show element" ' \
-                  'class="elementlink" {}>{}</a>'
+LINK_SHOW_ICON = \
+    'M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,' \
+    '14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,' \
+    '16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,' \
+    '9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z'
+DEFAULT_LINK = '<span>' \
+               '<a class="elementlink" ' \
+                  'style="margin-right:0px;padding-right:3px" ' \
+                  'title="Click to select or show element" ' \
+                  '{attrs_select} >' \
+                     '{ids}' \
+               '</a>' \
+               '<a class="elementlink" ' \
+                  'style="margin-left:0px;padding-left:3px" ' \
+                  '{attrs_show}>' \
+                     '<svg width="14" height="14" viewBox="2 2 20 20" >' \
+                        '<path fill="white" d="{show_icon}" />' \
+                     '</svg>' \
+               '</a>' \
+               '</span>'
 
 
 def make_link(element_ids, contents=None):
@@ -39,14 +56,22 @@ def make_link(element_ids, contents=None):
         elementquery.append('element[]={}'.format(strid))
 
     reviturl = '&'.join(elementquery)
-    linkname = ', '.join(strids)
+    link_title = ', '.join(strids)
 
     if len(reviturl) >= 2000:
         alertjs = 'alert(&quot;Url was too long and discarded!&quot;);'
-        linkattrs = 'href="#" onClick="{}"'.format(alertjs)
+        linkattrs_select = 'href="#" onClick="{}"'.format(alertjs)
+        linkattrs_show = linkattrs_select
     else:
-        linkattrs = 'href="{}{}{}"'.format(PROTOCOL_NAME,
-                                           '&command=select&',
-                                           reviturl)
+        linkattrs_select = \
+            'href="{}{}{}&show=false"'.format(PROTOCOL_NAME,
+                                              '&command=select&',
+                                              reviturl)
+        linkattrs_show = linkattrs_select.replace('&show=false', '&show=true')
 
-    return DEFAULT_LINK.format(linkattrs, contents or linkname)
+    return DEFAULT_LINK.format(
+        attrs_select=linkattrs_select,
+        attrs_show=linkattrs_show,
+        ids=contents or link_title,
+        show_icon=LINK_SHOW_ICON
+        )
