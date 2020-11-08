4af002b8c11b3ab057c9e6538cbaee9db46d5821
Hikmet Kibar <43708222+hikmet-kibar@users.noreply.github.com>
2020-11-08 20:35:55 +0100

    added Load Families.pushbutton (#1041)
    * added Load Families.pushbutton
    
    * moved 'Load Families' and 'Load More Types' into a common pulldown 'Load Family'. missing button icon.

---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools2.stack/Load Family.pulldown/Load Families.pushbutton/lib/file_utils.py
@@ -0,0 +1,73 @@
+""" Module to search for files in a directory """
+
+import re
+import pathlib
+import itertools
+
+from pyrevit import script, forms
+
+logger = script.get_logger()
+
+
+class FileFinder:
+    """
+    Handles the file search in a directory
+
+    Attributes
+    ----------
+    directory : str
+        Path of the target directory
+    paths : set()
+        Holds absolute paths of search result
+
+    Methods
+    -------
+    search(str)
+        Searches in the target directory for the given glob pattern.
+        Adds absolute paths to self.paths.
+
+    exclude_by_pattern(str)
+        Filters self.paths by the given regex pattern.
+    """
+    def __init__(self, directory):
+        """
+        Parameters
+        ----------
+        directory : str
+            Absolute path to target directory.
+        """
+        self.directory = directory
+        self.paths = set()
+
+    def search(self, pattern):
+        """
+        Searches in the target directory for the given glob pattern.
+        Adds absolute paths to self.paths.
+
+        Parameters
+        ----------
+        pattern : str
+            Glob pattern
+        """
+        result = pathlib.Path(self.directory).rglob(pattern)
+        for path in result:
+            logger.debug('Found file: {}'.format(path))
+            self.paths.add(str(path))
+        if len(self.paths) == 0:
+            logger.debug(
+                'No {} files in "{}" found.'.format(pattern, self.directory))
+            forms.alert(
+                'No {} files in "{}" found.'.format(pattern, self.directory))
+            script.exit()
+
+    def exclude_by_pattern(self, pattern):
+        """
+        Filters self.paths by the given regex pattern.
+
+        Parameters
+        ----------
+        pattern : str
+            Regular expression pattern
+        """
+        self.paths = itertools.ifilterfalse(
+            re.compile(pattern).match, self.paths)
---
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools2.stack/Load Family.pulldown/Load Families.pushbutton/script.py
@@ -0,0 +1,94 @@
+"""
+This tool gives you the option to load multiple Families from a folder and its subfolders.
+You can choose to load the Family with all its Types, or select Types for each Family.
+"""
+
+import os
+from pyrevit import forms, script
+
+# Custom modules in lib/
+from file_utils import FileFinder
+from family_utils import FamilyLoader
+
+logger = script.get_logger()
+output = script.get_output()
+
+
+# Get directory with families
+directory = forms.pick_folder("Select parent folder of families")
+logger.debug('Selected parent folder: {}'.format(directory))
+if directory is None:
+    logger.debug('No directory selected. Calling script.exit')
+    script.exit()
+
+# Find family files in directory
+finder = FileFinder(directory)
+finder.search('*.rfa')
+
+# Excluding backup files
+backup_pattern = r'^.*\.\d{4}\.rfa$'
+finder.exclude_by_pattern(backup_pattern)
+paths = finder.paths
+
+# Dictionary to look up absolute paths by relative paths
+path_dict = dict()
+for path in paths:
+    path_dict.update({os.path.relpath(path, directory): path})
+
+# User input -> Select families from directory
+family_select_options = sorted(
+    path_dict.keys(),
+    key=lambda x: (x.count(os.sep), x))  # Sort by nesting level
+selected_families = forms.SelectFromList.show(
+    family_select_options,
+    title="Select Families",
+    width=500,
+    button_name="Load Families",
+    multiselect=True)
+if selected_families is None:
+    logger.debug('No families selected. Calling script.exit()')
+    script.exit()
+logger.debug('Selected Families: {}'.format(selected_families))
+
+# Dictionary to look up FamilyLoader method by selected option
+family_loading_options = {
+    "Load all types": "load_all",
+    "Load types by selecting individually": "load_selective"}
+selected_loading_option = forms.CommandSwitchWindow.show(
+    family_loading_options.keys(),
+    message='Select loading option:',)
+if selected_loading_option is None:
+    logger.debug('No loading option selected. Calling script.exit()')
+    script.exit()
+
+# User input -> Select loading option (load all, load certain symbols)
+logger.debug('Selected loading option: {}'.format(selected_loading_option))
+laoding_option = family_loading_options[selected_loading_option]
+
+
+# Feedback on already loaded families
+already_loaded = set()
+
+# Loading selected families
+max_value = len(selected_families)
+with forms.ProgressBar(title='Loading Family {value} of {max_value}',
+                       cancellable=True) as pb:
+    for count, family_path in enumerate(selected_families, 1):
+        if pb.cancelled:
+            break
+        pb.update_progress(count, max_value)
+
+        family = FamilyLoader(path_dict[family_path])
+        logger.debug('Loading family: {}'.format(family.name))
+        loaded = family.is_loaded
+        if loaded:
+            logger.debug('Family is already loaded: {}'.format(family.path))
+            already_loaded.add(family)
+            continue
+        getattr(family, laoding_option)()
+
+# Feedback on already loaded families
+if len(already_loaded) != 0:
+    output.print_md('### Families that were already loaded:')
+    for family in sorted(already_loaded):
+        print(family.path)
