f6dcd35f8fa0ee0f4292de943bfac183cd66d3be
Ehsan Iran-Nejad <eirannejad@gmail.com>
2020-11-21 10:37:20 -0800

    cleanups
    - added selection for all titleblocks to output

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Sheets.pulldown/List TitleBlocks on Sheets.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Sheets.pulldown/List TitleBlocks on Sheets.pushbutton/script.py
@@ -1,7 +1,6 @@
 """Select title blocks on selected sheets for batch editing."""
-
-from pyrevit import coreutils
-from pyrevit import revit, DB
+# pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
+from pyrevit import revit
 from pyrevit import forms
 from pyrevit import script
 
@@ -10,21 +9,34 @@ logger = script.get_logger()
 
 
 def get_source_sheets():
-    sheet_elements = forms.select_sheets(button_name='Select TitleBlocks',
-                                         use_selection=True,
-                                         include_placeholder=False)
+    sheet_elements = forms.select_sheets(
+        button_name="List TitleBlocks",
+        use_selection=True,
+        include_placeholder=False,
+    )
     if not sheet_elements:
         script.exit()
     return sheet_elements
 
 
 def print_titleblocks(sheets):
+    all_tblocks = []
     for sheet in sheets:
         tblocks = revit.query.get_sheet_tblocks(sheet)
+        all_tblocks.extend([x.Id for x in tblocks])
         for tblock in tblocks:
-            print('SHEET: {0} - {1}\t\tTITLEBLOCK: {2} {3}'.format(sheet.SheetNumber, sheet.Name, tblock.Name, output.linkify(tblock.Id)))
+            print(
+                "SHEET: {0} - {1}\t\tTITLEBLOCK: {2} {3}".format(
+                    sheet.SheetNumber,
+                    sheet.Name,
+                    tblock.Name,
+                    output.linkify(tblock.Id),
+                )
+            )
+    print(
+        "{}".format(output.linkify(all_tblocks, title="Select All TitleBlocks"))
+    )
 
 
 # orchestrate
-sheets = get_source_sheets()
-tblocks = print_titleblocks(sheets)
+print_titleblocks(get_source_sheets())
