01f4c14dda993b4bb394fce6aa159dcb74088536
Ehsan Iran-Nejad <eirannejad@gmail.com>
2021-01-15 09:30:27 -0800

    Fixed bad script after change

--- extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/Preflight Checks.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/Preflight Checks.pushbutton/script.py
@@ -1,5 +1,5 @@
 """Run perflight checks on current model"""
-#pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
+# pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
 from pyrevit import preflight
 from pyrevit import forms
 from pyrevit import script
@@ -13,22 +13,23 @@ output = script.get_output()
 def ask_for_preflight_checks():
     """Ask user for preflight tests and run one by one"""
     # ask user for test case
-    selected_checks = forms.SelectFromList.show(
+    selected_check = forms.SelectFromList.show(
         preflight.get_all_preflight_checks(),
-        title='Select Preflight Check',
-        button_name='Run Check',
+        title="Select Preflight Check",
+        button_name="Run Check",
         multiselect=False,
         checked_only=True,
         height=400,
         item_template=forms.utils.load_ctrl_template(
-            script.get_bundle_file('PreflightCheckTemplate.xaml')
-            )
-        )
+            script.get_bundle_file("PreflightCheckTemplate.xaml")
+        ),
+    )
 
-    if selected_checks:
-        for check in selected_checks:
-            logger.debug('Running: {}'.format(check))
-            preflight.run_preflight_check(check, doc=revit.doc, output=output)
+    if selected_check:
+        logger.debug("Running: {}".format(selected_check))
+        preflight.run_preflight_check(
+            selected_check, doc=revit.doc, output=output
+        )
 
 
 if __name__ == "__main__":
