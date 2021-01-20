9295c86917ae08a03fb97c8c2a14fdbb3890fc61
jmcouffin <jmcouffin@users.noreply.github.com>
2021-01-20 18:22:32 +0100

    added myself as author, may I?

--- extensions/pyRevitTools.extension/checks/modelchecker_check.py
+++ extensions/pyRevitTools.extension/checks/modelchecker_check.py
@@ -1169,7 +1169,7 @@ class ModelChecker(PreflightTestCase):
 
 
     name = "Model Checker"
-    author = "David Vadkerti"
+    author = "David Vadkerti, Jean-Marc Couffin"
 
     def setUp(self, doc, output):
         pass
