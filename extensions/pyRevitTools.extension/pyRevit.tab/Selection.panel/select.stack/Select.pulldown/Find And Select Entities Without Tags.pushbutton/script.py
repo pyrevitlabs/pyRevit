4e38a1bc6f4bf7a8f186193ae854c61d592859d4
Jean-Marc Couffin <jmcouffin@gmail.com>
2022-10-03 17:43:09 +0200

    Related to https://discourse.pyrevitlabs.io/t/bug-suggestion-for-selecting-entities-without-tags/1158

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Find And Select Entities Without Tags.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Find And Select Entities Without Tags.pushbutton/script.py
@@ -2,8 +2,9 @@
 #pylint: disable=import-error,invalid-name
 from collections import namedtuple
 
-from pyrevit import revit, DB
+from pyrevit import revit, DB, HOST_APP
 from pyrevit import forms
+from System.Collections.Generic import List
 
 
 Taggable = namedtuple('Taggable', ['tag_type', 'element_type'])
@@ -167,9 +168,13 @@ if selected_switch:
         untagged_elements = []
         for eltid in target_tags:
             elt = revit.doc.GetElement(eltid)
-            if elt.TaggedLocalElementId != DB.ElementId.InvalidElementId:
-                tagged_elements.append(elt.TaggedLocalElementId.IntegerValue)
-
+            if HOST_APP.is_newer_than(2022, or_equal=True):
+                if elt.GetTaggedLocalElementIds() != DB.ElementId.InvalidElementId:
+                    tagged_elements.append(List[DB.ElementId](elt.GetTaggedLocalElementIds())[0].IntegerValue)
+            else:
+                if elt.TaggedLocalElementId != DB.ElementId.InvalidElementId:
+                    tagged_elements.append(elt.TaggedLocalElementId.IntegerValue)
+                    
         for elid in target_elements:
             el = revit.doc.GetElement(elid)
             if el.Id.IntegerValue not in tagged_elements:
