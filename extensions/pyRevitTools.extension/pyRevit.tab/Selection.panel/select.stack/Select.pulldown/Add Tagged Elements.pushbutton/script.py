b486bce700f78f21a72123f733ded9df8f3618bd
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2022-10-03 18:27:03 +0200

    Related to https://discourse.pyrevitlabs.io/t/bug-suggestion-for-sele… (#1633)
    * Related to https://discourse.pyrevitlabs.io/t/bug-suggestion-for-selecting-entities-without-tags/1158
    
    * added structural framing and foundations

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Add Tagged Elements.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Add Tagged Elements.pushbutton/script.py
@@ -1,5 +1,7 @@
-from pyrevit import revit, DB, UI
+from pyrevit import revit, DB, UI, HOST_APP
 from pyrevit import forms
+#  import List
+from System.Collections.Generic import List
 
 
 selection = revit.get_selection()
@@ -8,14 +10,26 @@ selection = revit.get_selection()
 tagged_elements = []
 
 for el in selection:
-    if isinstance(el, DB.IndependentTag):
-        tagged_elements.append(el.TaggedLocalElementId)
-    elif isinstance(el, DB.Architecture.RoomTag):
-        tagged_elements.append(el.TaggedLocalRoomId)
-    elif isinstance(el, DB.Mechanical.SpaceTag):
-        tagged_elements.append(el.Space.Id)
-    elif isinstance(el, DB.AreaTag):
-        tagged_elements.append(el.Area.Id)
+    if HOST_APP.is_newer_than(2022, or_equal=True):
+        if isinstance(el, DB.IndependentTag):
+            tagged_elements.append(List[DB.ElementId](el.GetTaggedLocalElementIds())[0])
+        elif isinstance(el, DB.Architecture.RoomTag):
+            tagged_elements.append(el.TaggedLocalRoomId)
+        elif isinstance(el, DB.Mechanical.SpaceTag):
+            tagged_elements.append(el.Space.Id)
+        elif isinstance(el, DB.AreaTag):
+            tagged_elements.append(el.Area.Id)
+    else:
+
+        if isinstance(el, DB.IndependentTag):
+            tagged_elements.append(el.TaggedLocalElementId)
+        elif isinstance(el, DB.Architecture.RoomTag):
+            tagged_elements.append(el.TaggedLocalRoomId)
+        elif isinstance(el, DB.Mechanical.SpaceTag):
+            tagged_elements.append(el.Space.Id)
+        elif isinstance(el, DB.AreaTag):
+            tagged_elements.append(el.Area.Id)
+
 
 if len(tagged_elements) > 0:
     selection.append(tagged_elements)
