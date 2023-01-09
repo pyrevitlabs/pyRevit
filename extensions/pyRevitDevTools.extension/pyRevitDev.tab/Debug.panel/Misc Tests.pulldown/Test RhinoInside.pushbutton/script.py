0c550be8c4fd714643d350cce875f6984fb95a40
Ehsan Iran Nejad <eirannejad@gmail.com>
2023-01-09 09:13:07 -0800

    updated rir test script

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test RhinoInside.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Misc Tests.pulldown/Test RhinoInside.pushbutton/script.py
@@ -1,6 +1,8 @@
 """Testing Rhino.Inside modules"""
 #pylint: disable=import-error,invalid-name,broad-except,wrong-import-position
+#pylint: disable=global-statement,unused-import,bare-except
 from pyrevit import clr, revit, DB
+from pyrevit import forms
 from pyrevit.framework import Enumerable
 from pyrevit import script
 
@@ -9,16 +11,31 @@ clr.AddReference('RhinoCommon')
 clr.AddReference('RhinoInside.Revit')
 
 
+import Rhino
 from Rhino import Geometry
 from RhinoInside.Revit import Revit, Convert
+from RhinoInside.Revit.Convert.Geometry import GeometryDecoder as GD
 
+RHINO_VERSION = -1
 
 logger = script.get_logger()
 output = script.get_output()
 
 
+def ensure_rir():
+    """Ensure Rhino.Inside.Revit is loaded"""
+    global RHINO_VERSION
+    try:
+        RHINO_VERSION = Rhino.RhinoApp.ExeVersion
+    except:
+        forms.alert("Rhino.Inside.Revit is not available", exitscript=True)
+
+
+# make sure Rhino.Inside.Revit is loaded
+ensure_rir()
+
 with revit.Transaction("Rhino.Inside Sample7"):
-    sphere = Geometry.Sphere(Geometry.Point3d.Origin, 12 * Revit.ModelUnits)
+    sphere = Geometry.Sphere(Geometry.Point3d.Origin, GD.ToModelLength(12))
     brep = sphere.ToBrep()
     meshes = \
         Geometry.Mesh.CreateFromBrep(brep, Geometry.MeshingParameters.Default)
@@ -30,6 +47,6 @@ with revit.Transaction("Rhino.Inside Sample7"):
     for mesh in meshes:
         solid = Convert.Geometry.GeometryEncoder.ToSolid(mesh)
         solids.append(solid)
-    
+
     for geometry in solids:
         ds.AppendShape([geometry])
