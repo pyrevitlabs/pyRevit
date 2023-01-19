7b77a9329eb2da2236708b51991a73b47496c895
jmcouffin <jmcouffin@users.noreply.github.com>
2023-01-19 22:12:39 +0100

    Revert "renamed extension"
    This reverts commit 998336c954cfdf77817998aceb1de34b49882787.

---
+++ extensions/pyRevitExtensionTemplate.extension/pyRevit Bundle Creator.tab/Button creation.panel/button_types/pushbutton_with_config/script.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+
+from pyrevit import script
+from pyrevit import revit, DB
+
+output = script.get_output()
+output.close_others(True)
+output.center()
+output.set_title('Models Checker')
+
+
+doc = revit.doc
+
+# Grab data from config
+my_config = script.get_config()
+try:
+    tests = getattr(my_config, "qc")
+except:
+    tests = ["Project Name", "Project Number", "Warnings"]
+
+
+# Series of queries
+
+def project_number(doc):
+    project_number = doc.ProjectInformation.Number
+    return project_number
+
+
+def project_name(doc):
+    project_name = doc.ProjectInformation.Name
+    return project_name
+
+
+def doc_warnings(doc):
+    warnings = doc.GetWarnings()
+    descriptions = []
+    for warning in warnings:
+        descriptions.append(DB.FailureMessage.GetDescriptionText(warning))
+    if len(descriptions):
+        return str(len(descriptions)) + ' Warnings in the project'
+
+
+# set minimal value to empty string
+pname, pnumber, warnings = "", "", ""
+
+# check if queries requested in config file
+if tests == [] or tests == None:
+    pname = project_name(doc)
+    pnumber = project_number(doc)
+    warnings = warnings(doc)
+if "Project Name" in tests:
+    pname = project_name(doc)
+if "Project Number" in tests:
+    pnumber = project_number(doc)
+if "Warnings" in tests:
+    warnings = doc_warnings(doc)
+
+# print the whole thing
+output.print_md(pname + "\n\n" + pnumber + "\n\n" + warnings)
