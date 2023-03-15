14ad338a0424d3147ac708697eb7b678de4bc4d9
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2023-03-15 09:41:53 +0100

    Revert "sort 'List Legends on Sheets' by Sheet Number (after Legend Name) #1759"

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Legends.pulldown/List Legends on Sheets.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Legends.pulldown/List Legends on Sheets.pushbutton/script.py
@@ -5,18 +5,12 @@ output.close_others()
 
 doc = revit.doc
 
-sort_by = \
-    forms.CommandSwitchWindow.show(
-        ['Legends', 'Sheets'],
-        message='Sort Results by:'
-    )
-
-legends = [
-    x for x in DB.FilteredElementCollector(doc)
+legends = [x for x in DB.FilteredElementCollector(doc)
     .OfCategory(DB.BuiltInCategory.OST_Views)
     .WhereElementIsNotElementType()
     .ToElements()
-    if x.ViewType == DB.ViewType.Legend]
+    if x.ViewType == DB.ViewType.Legend
+]
 
 legends_ids = [x.Id.IntegerValue for x in legends]
 
@@ -28,28 +22,17 @@ sheets = (
 )
 
 result = []
-
 for sheet in sheets:
     vps = sheet.GetAllPlacedViews()
     for vp in vps:
         if vp.IntegerValue in legends_ids:
             result.append(
-                (output.linkify(doc.GetElement(vp).Id), doc.GetElement(
-                    vp).Name, output.linkify(sheet.Id), sheet.SheetNumber, sheet.Name)
+                (doc.GetElement(vp).Name, sheet.SheetNumber, sheet.Name)
             )
-
-if len(result) != 0:
-    if sort_by == 'Sheets':
-        results = sorted(result, key=lambda x: x[1])
-        output.print_md("## Legends on Sheets")
-        headers = ["Legend selector", "Legend Name",
-                   "Sheet selector", "Sheet Number", "Sheet Name"]
-        output.print_table(results, headers)
-    else:
-        results = sorted(result, key=lambda x: x[3])
-        output.print_md("## Legends on Sheets")
-        headers = ["Legend selector", "Legend Name",
-                   "Sheet selector", "Sheet Number", "Sheet Name"]
-        output.print_table(results, headers)
+if len(result)!=0:
+    result = sorted(result, key=lambda x: x[0])
+    output.print_md("## Legends on Sheets")
+    headers = ["Legend Name", "Sheet Number", "Sheet Name"]
+    output.print_table(result, headers)
 else:
     forms.alert("No legends found on sheets.")
