ec766ee54dad2e13bf3f8ad4f4601314cf99298b
eirannejad <eirannejad@gmail.com>
2023-06-12 12:14:26 +0330

    Improved error handling of invoke buttons

--- pyrevitlib/pyrevit/runtime/CLREngine.cs
+++ pyrevitlib/pyrevit/runtime/CLREngine.cs
@@ -14,8 +14,10 @@ using pyRevitLabs.NLog.Config;
 using pyRevitLabs.NLog.Targets;
 using SHARED = pyRevitLabs.PyRevit.Runtime.Shared;
 
-namespace PyRevitLabs.PyRevit.Runtime {
-    public class ExecParams {
+namespace PyRevitLabs.PyRevit.Runtime
+{
+    public class ExecParams
+    {
         public string ExecId { get; set; }
         public string ExecTimeStamp { get; set; }
         public string ScriptPath { get; set; }
@@ -33,13 +35,17 @@ namespace PyRevitLabs.PyRevit.Runtime {
         public Autodesk.Windows.RibbonItem UIButton { get; set; }
     }
 
-    public class CLREngineOutputTarget : TargetWithLayout {
+    public class CLREngineOutputTarget : TargetWithLayout
+    {
         public SHARED.ExecParams CurrentExecParams { get; set; }
 
-        protected override void Write(LogEventInfo logEvent) {
-            try {
+        protected override void Write(LogEventInfo logEvent)
+        {
+            try
+            {
                 var message = Layout.Render(logEvent);
-                if (logEvent.Level == LogLevel.Debug) {
+                if (logEvent.Level == LogLevel.Debug)
+                {
                     if (CurrentExecParams.DebugMode)
                         Console.WriteLine(message);
                 }
@@ -50,31 +56,38 @@ namespace PyRevitLabs.PyRevit.Runtime {
         }
     }
 
-    public class CLREngine : ScriptEngine {
+    public class CLREngine : ScriptEngine
+    {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         private string scriptSig = string.Empty;
         private bool scriptDbg = false;
         private Assembly scriptAssm = null;
 
-        public override void Init(ref ScriptRuntime runtime) {
+        public override void Init(ref ScriptRuntime runtime)
+        {
             base.Init(ref runtime);
 
             // If the user is asking to refresh the cached engine for the command,
             UseNewEngine = runtime.ScriptRuntimeConfigs.RefreshEngine;
         }
 
-        public override int Execute(ref ScriptRuntime runtime) {
+        public override int Execute(ref ScriptRuntime runtime)
+        {
             // compile first
             // only if the signature doesn't match
             var errors = new List<string>();
-            if (scriptSig == null || runtime.ScriptSourceFileSignature != scriptSig || scriptDbg != runtime.ScriptRuntimeConfigs.DebugMode) {
-                try {
+            if (scriptSig == null || runtime.ScriptSourceFileSignature != scriptSig || scriptDbg != runtime.ScriptRuntimeConfigs.DebugMode)
+            {
+                try
+                {
                     scriptSig = runtime.ScriptSourceFileSignature;
                     scriptDbg = runtime.ScriptRuntimeConfigs.DebugMode;
                     scriptAssm = CompileCLRScript(ref runtime, out errors);
-                    if (scriptAssm == null) {
-                        if (runtime.RuntimeType == ScriptRuntimeType.ExternalCommand) {
+                    if (scriptAssm == null)
+                    {
+                        if (runtime.RuntimeType == ScriptRuntimeType.ExternalCommand)
+                        {
                             var errorReport = string.Join(Environment.NewLine, errors.ToArray());
                             runtime.OutputStream.WriteError(
                                 errorReport != string.Empty ? errorReport : "Failed to compile assembly for unknown reason",
@@ -86,14 +99,16 @@ namespace PyRevitLabs.PyRevit.Runtime {
                         return ScriptExecutorResultCodes.CompileException;
                     }
                 }
-                catch (Exception compileEx) {
+                catch (Exception compileEx)
+                {
                     // make sure a bad compile is not cached
                     scriptAssm = null;
                     string traceMessage = compileEx.ToString();
                     traceMessage = traceMessage.NormalizeNewLine();
                     runtime.TraceMessage = traceMessage;
 
-                    if (runtime.RuntimeType == ScriptRuntimeType.ExternalCommand) {
+                    if (runtime.RuntimeType == ScriptRuntimeType.ExternalCommand)
+                    {
                         var dialog = new TaskDialog(PyRevitLabsConsts.ProductName);
                         dialog.MainInstruction = "Error compiling .NET script.";
                         string errorReport = string.Empty;
@@ -109,10 +124,12 @@ namespace PyRevitLabs.PyRevit.Runtime {
             }
 
             // scriptAssm must have value
-            switch (runtime.RuntimeType) {
+            switch (runtime.RuntimeType)
+            {
                 // if is an external command
                 case ScriptRuntimeType.ExternalCommand:
-                    try {
+                    try
+                    {
                         // execute now
                         var resultCode = ExecuteExternalCommand(scriptAssm, null, ref runtime);
                         if (resultCode == ScriptExecutorResultCodes.ExternalInterfaceNotImplementedException)
@@ -123,7 +140,8 @@ namespace PyRevitLabs.PyRevit.Runtime {
                                     ));
                         return resultCode;
                     }
-                    catch (Exception execEx) {
+                    catch (Exception execEx)
+                    {
                         string traceMessage = execEx.ToString();
                         traceMessage = traceMessage.NormalizeNewLine();
                         runtime.TraceMessage = traceMessage;
@@ -138,10 +156,12 @@ namespace PyRevitLabs.PyRevit.Runtime {
 
                 // if is an event hook
                 case ScriptRuntimeType.EventHandler:
-                    try {
+                    try
+                    {
                         return ExecuteEventHandler(scriptAssm, ref runtime);
                     }
-                    catch (Exception execEx) {
+                    catch (Exception execEx)
+                    {
                         string traceMessage = execEx.ToString();
                         traceMessage = traceMessage.NormalizeNewLine();
                         runtime.TraceMessage = traceMessage;
@@ -156,27 +176,33 @@ namespace PyRevitLabs.PyRevit.Runtime {
             }
         }
 
-        public static IEnumerable<Type> GetTypesSafely(Assembly assembly) {
-            try {
+        public static IEnumerable<Type> GetTypesSafely(Assembly assembly)
+        {
+            try
+            {
                 return assembly.GetTypes();
             }
-            catch (ReflectionTypeLoadException ex) {
+            catch (ReflectionTypeLoadException ex)
+            {
                 return ex.Types.Where(x => x != null);
             }
         }
 
-        public static Assembly CompileCLRScript(ref ScriptRuntime runtime, out List<string> errors) {
+        public static Assembly CompileCLRScript(ref ScriptRuntime runtime, out List<string> errors)
+        {
             // https://stackoverflow.com/a/3188953
 
             // read the referenced dlls from env vars
             // pyrevit sets this when loading
             List<string> refFiles;
             var envDic = new EnvDictionary();
-            if (envDic.ReferencedAssemblies.Length == 0) {
+            if (envDic.ReferencedAssemblies.Length == 0)
+            {
                 var refs = AppDomain.CurrentDomain.GetAssemblies();
                 refFiles = refs.Select(a => a.Location).ToList();
             }
-            else {
+            else
+            {
                 refFiles = envDic.ReferencedAssemblies.ToList();
             }
 
@@ -199,7 +225,8 @@ namespace PyRevitLabs.PyRevit.Runtime {
             };
 
             // determine which compiler to use
-            switch (runtime.EngineType) {
+            switch (runtime.EngineType)
+            {
                 case ScriptEngineType.CSharp:
                     return CompileCSharp(runtime.ScriptSourceFile, outputAssembly, refFiles, defines, runtime.ScriptRuntimeConfigs.DebugMode, out errors);
                 case ScriptEngineType.VisualBasic:
@@ -208,8 +235,9 @@ namespace PyRevitLabs.PyRevit.Runtime {
                     throw new PyRevitException("Specified language does not have a compiler.");
             }
         }
-        
-        private static Assembly CompileCSharp(string sourceFile, string outputPath, List<string> refFiles, List<string> defines, bool debug, out List<string> errors) {
+
+        private static Assembly CompileCSharp(string sourceFile, string outputPath, List<string> refFiles, List<string> defines, bool debug, out List<string> errors)
+        {
             return pyRevitLabs.Common.CodeCompiler.CompileCSharpToAssembly(
                 sourceFiles: new string[] { sourceFile },
                 assemblyName: Path.GetFileName(outputPath),
@@ -220,7 +248,8 @@ namespace PyRevitLabs.PyRevit.Runtime {
                 );
         }
 
-        private static Assembly CompileVB(string sourceFile, string outputPath, List<string> refFiles, List<string> defines, bool debug, out List<string> errors) {
+        private static Assembly CompileVB(string sourceFile, string outputPath, List<string> refFiles, List<string> defines, bool debug, out List<string> errors)
+        {
             return pyRevitLabs.Common.CodeCompiler.CompileVisualBasicToAssembly(
                 sourceFiles: new string[] { sourceFile },
                 assemblyName: Path.GetFileName(outputPath),
@@ -231,11 +260,15 @@ namespace PyRevitLabs.PyRevit.Runtime {
                 );
         }
 
-        public static int ExecuteExternalCommand(Assembly assmObj, string className, ref ScriptRuntime runtime) {
-            foreach (Type assmType in GetTypesSafely(assmObj)) {
-                if (assmType.IsClass) {
+        public static int ExecuteExternalCommand(Assembly assmObj, string className, ref ScriptRuntime runtime)
+        {
+            foreach (Type assmType in GetTypesSafely(assmObj))
+            {
+                if (assmType.IsClass)
+                {
                     // find the appropriate type and execute
-                    if (className != null) {
+                    if (className != null)
+                    {
                         if (assmType.Name == className)
                             return ExecuteExternalCommandType(assmType, ref runtime);
                         else
@@ -249,7 +282,8 @@ namespace PyRevitLabs.PyRevit.Runtime {
             return ScriptExecutorResultCodes.ExternalInterfaceNotImplementedException;
         }
 
-        public static int ExecuteExternalCommandType(Type extCommandType, ref ScriptRuntime runtime) {
+        public static int ExecuteExternalCommandType(Type extCommandType, ref ScriptRuntime runtime)
+        {
             // create instance
             object extCommandInstance = Activator.CreateInstance(extCommandType);
 
@@ -274,8 +308,10 @@ namespace PyRevitLabs.PyRevit.Runtime {
             );
 
             FieldInfo execParamField = null;
-            foreach (var fieldInfo in extCommandType.GetFields()) {
-                if (fieldInfo.FieldType == typeof(SHARED.ExecParams)) {
+            foreach (var fieldInfo in extCommandType.GetFields())
+            {
+                if (fieldInfo.FieldType == typeof(SHARED.ExecParams))
+                {
                     execParamField = fieldInfo;
                     execParamField.SetValue(extCommandInstance, execParams);
                 }
@@ -302,16 +338,27 @@ namespace PyRevitLabs.PyRevit.Runtime {
 
             // execute
             string commandMessage = string.Empty;
-            extCommandType.InvokeMember(
-                "Execute",
-                BindingFlags.Default | BindingFlags.InvokeMethod,
-                null,
-                extCommandInstance,
-                new object[] {
+            try
+            {
+                extCommandType.InvokeMember(
+                    "Execute",
+                    BindingFlags.Default | BindingFlags.InvokeMethod,
+                    null,
+                    extCommandInstance,
+                    new object[] {
                     runtime.ScriptRuntimeConfigs.CommandData,
                     commandMessage,
                     runtime.ScriptRuntimeConfigs.SelectedElements}
-                );
+                    );
+            }
+            catch (Exception ex)
+            {
+                string message = "Error Invoking Command";
+                if (ex.InnerException is Exception inner)
+                    message += $"\n{inner.Message}";
+
+                TaskDialog.Show(PyRevitLabsConsts.ProductName, message);
+            }
 
             // revert logger back to previous
             LogManager.Configuration = prevLoggerCfg;
@@ -323,20 +370,22 @@ namespace PyRevitLabs.PyRevit.Runtime {
 
             // reroute console output back to original
             Console.SetOut(existingOutStream);
-            runtimeOutputStream = null;
 
             return ScriptExecutorResultCodes.Succeeded;
         }
 
-        public static int ExecuteEventHandler(Assembly assmObj, ref ScriptRuntime runtime) {
+        public static int ExecuteEventHandler(Assembly assmObj, ref ScriptRuntime runtime)
+        {
             var argsType = runtime.ScriptRuntimeConfigs.EventArgs.GetType();
             foreach (Type assmType in GetTypesSafely(assmObj))
-                foreach (MethodInfo methodInfo in assmType.GetMethods()) {
+                foreach (MethodInfo methodInfo in assmType.GetMethods())
+                {
                     var methodParams = methodInfo.GetParameters();
                     if (methodParams.Count() == 2
                             && methodParams[0].Name == "sender"
                             && (methodParams[1].Name == "e" || methodParams[1].Name == "args")
-                            && methodParams[1].ParameterType == argsType) {
+                            && methodParams[1].ParameterType == argsType)
+                    {
                         object extEventInstance = Activator.CreateInstance(assmType);
                         assmType.InvokeMember(
                             methodInfo.Name,
