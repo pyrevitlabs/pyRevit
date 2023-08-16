3bae96ce1b8a2e4e2b384562af3e8122034731fd
Andrea Ghensi <andrea.ghensi@gmail.com>
2023-08-17 00:43:41 +0200

    docs: add docstrings to revit.ui module

--- pyrevitlib/pyrevit/revit/ui.py
+++ pyrevitlib/pyrevit/revit/ui.py
@@ -15,10 +15,21 @@ from pyrevit.coreutils import envvars
 
 
 def get_mainwindow_hwnd():
+    """
+    Get the handle of the main window.
+
+    Returns:
+        (intptr): The handle of the main window.
+    """
     return HOST_APP.proc_window
 
 
 def get_mainwindow():
+    """Get the main window of the application.
+
+    Returns:
+        The root visual of the main window.
+    """
     try:
         hwnd_source = Interop.HwndSource.FromHwnd(HOST_APP.proc_window)
         return hwnd_source.RootVisual
@@ -27,6 +38,11 @@ def get_mainwindow():
 
 
 def get_statusbar_hwnd():
+    """Retrieves the handle of the status bar control belonging to the main window.
+
+    Returns:
+        (IntPtr): The handle of the status bar control.
+    """
     return Common.User32.FindWindowEx(
         get_mainwindow_hwnd(),
         IntPtr.Zero,
@@ -35,6 +51,14 @@ def get_statusbar_hwnd():
 
 
 def set_statusbar_text(text):
+    """Sets the text of the status bar.
+
+    Parameters:
+        text (str): The text to be displayed in the status bar.
+
+    Returns:
+        bool: True if the text was successfully set, False otherwise.
+    """
     status_bar_ptr = get_statusbar_hwnd()
 
     if status_bar_ptr != IntPtr.Zero:
@@ -45,15 +69,40 @@ def set_statusbar_text(text):
 
 
 def get_window_rectangle():
+    """Get the rectangle coordinates of the main window.
+
+    Returns:
+        (Tuple[int, int, int, int]): The left, top, right, and bottom 
+            coordinates of the window rectangle.
+    """
     return Common.User32.GetWindowRect(get_mainwindow_hwnd())
 
 
 def is_infocenter_visible():
+    """
+    Check if the InfoCenter toolbar is visible.
+
+    Returns:
+        (bool): True if the InfoCenter toolbar is visible, False otherwise.
+    """
     return ad.ComponentManager.InfoCenterToolBar.Visibility == \
         Windows.Visibility.Visible
 
 
 def toggle_infocenter():
+    """
+    Toggles the visibility of the InfoCenter toolbar.
+
+    This function retrieves the current visibility state of the InfoCenter 
+    toolbar and toggles it to the opposite state.
+    If the toolbar is currently collapsed, it will be set to visible,
+    and if it is currently visible, it will be set to collapsed.
+    The function then returns the visibility state of the InfoCenter toolbar
+    after the toggle operation.
+
+    Returns:
+        (bool): True if the InfoCenter toolbar is visible, False otherwise.
+    """
     current_state = ad.ComponentManager.InfoCenterToolBar.Visibility
     is_hidden = (current_state == Windows.Visibility.Collapsed)
     ad.ComponentManager.InfoCenterToolBar.Visibility = \
@@ -63,6 +112,11 @@ def toggle_infocenter():
 
 
 def get_ribbon_roottype():
+    """Get the type of the ribbon root.
+
+    Returns:
+        (type): type of the ribbon root
+    """
     ap_assm = clr.GetClrType(ap.Windows.RibbonTabList).Assembly
     for apt in ap_assm.GetTypes():
         if 'PanelSetListView' in apt.Name:
