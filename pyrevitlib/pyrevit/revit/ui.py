f5a50976072be5e1adc1ba07383465af3dc7080a
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2023-08-21 12:58:43 +0200

    Update ui.py

--- pyrevitlib/pyrevit/revit/ui.py
+++ pyrevitlib/pyrevit/revit/ui.py
@@ -98,6 +98,16 @@ def set_current_theme(theme='Dark'):
 
 
 def resolve_icon_file(directory, icon_name):
+    """
+    Resolves the icon file path based on the current UI theme.
+
+    Args:
+        directory (str): The directory where the icon file is located.
+        icon_name (str): The name of the icon file.
+
+    Returns:
+        str: The full file path of the icon file.
+    """
     full_file_path = op.join(directory, icon_name)
 
     if HOST_APP.is_newer_than(2024, True) and get_current_theme() == UITheme.Dark:
