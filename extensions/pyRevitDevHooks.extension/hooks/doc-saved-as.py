b2039840f8605d2c8cf383a3ff1f396f7001485d
Andrea Ghensi <agensi@systra.com>
2024-08-21 20:17:13 +0200

    fix(devhooks): make hooks behave

--- extensions/pyRevitDevHooks.extension/hooks/doc-saved-as.py
+++ extensions/pyRevitDevHooks.extension/hooks/doc-saved-as.py
@@ -5,11 +5,12 @@ import hooks_logger as hl
 
 args = EXEC_PARAMS.event_args
 
-hl.log_hook(__file__,
+hl.log_hook(
+    __file__,
     {
         "cancellable?": str(args.Cancellable),
         "doc": str(revit.doc),
-        "master_file": str(args.IsSavingAsMasterFile),
+        "master_file": str(args.IsSavingAsCentralFile if HOST_APP.is_newer_than(2021) else args.IsSavingAsMasterFile),
         "original_path": str(args.OriginalPath),
         "status": str(args.Status),
     },
