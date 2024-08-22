9ed50fe50bbf681f0921ba5aad433c3616c8ee55
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2024-08-22 13:14:15 +0200

    Update doc-updater.py

--- extensions/pyRevitDevHooks.extension/hooks/doc-updater.py
+++ extensions/pyRevitDevHooks.extension/hooks/doc-updater.py
@@ -4,5 +4,5 @@ from pyrevit import revit, DB
 doc = EXEC_PARAMS.event_doc
 
 for wall in revit.query.get_elements_by_class(DB.Wall, doc=doc):
-    p = wall.LookupParameter("Unconnected Height")
+    p = wall.get_Parameter(DB.BuiltInParameter.WALL_USER_HEIGHT_PARAM)
     print("wall: {} Unconnected Height: {}".format(str(wall.Id), p.AsDouble()))
