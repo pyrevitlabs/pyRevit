f25c6810bfdf9bf34282ffc5d7c9f81976f3654d
Jean-Marc Couffin <jmcouffin@gmail.com>
2024-08-28 19:28:21 +0200

    IntegerValue API Change handling

--- extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack/Links.pulldown/Create Workset For Linked Element.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Project.panel/ptools.stack/Links.pulldown/Create Workset For Linked Element.pushbutton/script.py
@@ -1,12 +1,13 @@
 from pyrevit import revit, DB, UI
 from pyrevit import script
 from pyrevit import forms
-
+from pyrevit.compat import get_value_func
 
 logger = script.get_logger()
 
 selection = revit.get_selection()
 
+value_func = get_value_func()
 
 linkedModelName = ''
 
@@ -26,7 +27,7 @@ if len(selection) > 0:
                     newWs = DB.Workset.Create(revit.doc, linkedModelName)
                     worksetParam = \
                         el.Parameter[DB.BuiltInParameter.ELEM_PARTITION_PARAM]
-                    worksetParam.Set(newWs.Id.IntegerValue)
+                    worksetParam.Set(value_func(newWs.Id))
                 except Exception as e:
                     print('Workset: {} already exists\nError: {}'.format(linkedModelName,e))
 else:
