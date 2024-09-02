9c0b5b5d188f2600d1c2104b4966176237869f00
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2024-09-02 15:56:24 +0200

    and with a little extra effort

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Move Viewport Label.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Move Viewport Label.pushbutton/script.py
@@ -1,11 +1,14 @@
 # encoding: utf-8
 # from https://discourse.pyrevitlabs.io/t/i-developed-a-tool-need-help-to-get-it-into-the-new-release-of-pyrevit/7639/3
 
-from pyrevit import DB, script, revit, HOST_APP
+from pyrevit import DB, HOST_APP
+from pyrevit.script import get_output
+from pyrevit.revit import pick_point, pick_element_by_category, Transaction
+from pyrevit.forms import alert
 
 doc = HOST_APP.doc
 uidoc = HOST_APP.uidoc
-script.get_output().close_others()
+get_output().close_others()
 
 
 def move_viewport_label(viewport, point):
@@ -15,10 +18,10 @@ def move_viewport_label(viewport, point):
 
 
 if __name__ == '__main__':
-    selected_point = revit.selection.pick_point("Select a point")
-    selected_viewport = revit.selection.pick_element_by_category(DB.BuiltInCategory.OST_Viewports, "Select a viewport")
+    selected_point = pick_point("Select a point")
+    selected_viewport = pick_element_by_category(DB.BuiltInCategory.OST_Viewports, "Select a viewport")
     if selected_point is not None and selected_viewport is not None:
-        with revit.Transaction("Move Label to Point", doc):
+        with Transaction("Move Label to Point", doc):
             move_viewport_label(selected_viewport, selected_point)
     else:
-        print("Invalid selection. Please try again.")
+        alert("Invalid selection. Please try again.")
