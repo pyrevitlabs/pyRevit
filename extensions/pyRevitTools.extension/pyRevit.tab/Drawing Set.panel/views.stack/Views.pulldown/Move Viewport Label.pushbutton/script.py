d8cd0624812883effef5cd801b5ffe42c7e9166e
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2024-09-02 16:05:24 +0200

    the cleaner the better

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Move Viewport Label.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/views.stack/Views.pulldown/Move Viewport Label.pushbutton/script.py
@@ -1,12 +1,9 @@
 # encoding: utf-8
 # from https://discourse.pyrevitlabs.io/t/i-developed-a-tool-need-help-to-get-it-into-the-new-release-of-pyrevit/7639/3
 
-from pyrevit import DB, HOST_APP
+from pyrevit import DB
 from pyrevit.revit import pick_point, pick_element_by_category, Transaction
 from pyrevit.forms import alert
-
-doc = HOST_APP.doc
-uidoc = HOST_APP.uidoc
 
 
 def move_viewport_label(viewport, point):
@@ -19,7 +16,7 @@ if __name__ == '__main__':
     selected_point = pick_point("Select a point")
     selected_viewport = pick_element_by_category(DB.BuiltInCategory.OST_Viewports, "Select a viewport")
     if selected_point is not None and selected_viewport is not None:
-        with Transaction("Move Label to Point", doc):
+        with Transaction("Move Label to Point"):
             move_viewport_label(selected_viewport, selected_point)
     else:
         alert("Invalid selection. Please try again.")
