29bc6ef83de4966a285779d3243966bcf71a02f4
dosymep <biseuv@yandex.ru>
2024-12-23 14:00:07 +0300

    add attribute settings

--- dev/pyRevitLabs/pyRevitLabs.Configurations/Constants.cs
+++ dev/pyRevitLabs/pyRevitLabs.Configurations/Constants.cs
@@ -2,47 +2,6 @@
 
 internal static class Constants
 {
-    // core 
-    public const string CoreSection = "core";
-    public const string BinaryCacheKey = "bincache";
-    public const string CheckUpdatesKey = "checkupdates";
-    public const string AutoUpdateKey = "autoupdate";
-    public const string RocketModeKey = "rocketmode";
-    public const string VerboseKey = "verbose";
-    public const string DebugKey = "debug";
-    public const string FileLoggingKey = "filelogging";
-    public const string StartupLogTimeoutKey = "startuplogtimeout";
-    public const string RequiredHostBuildKey = "requiredhostbuild";
-    public const string MinDriveSpaceKey = "minhostdrivefreespace";
-    public const string LoadBetaKey = "loadbeta";
-    public const string CPythonEngineKey = "cpyengine";
-    public const string LocaleKey = "user_locale";
-    public const string OutputStyleSheet = "outputstylesheet";
-    public const string UserExtensionsKey = "userextensions";
-    public const string UserCanUpdateKey = "usercanupdate";
-    public const string UserCanExtendKey = "usercanextend";
-    public const string UserCanConfigKey = "usercanconfig";
-    public const string ColorizeDocsKey = "colorize_docs";
-    public const string AppendTooltipExKey = "tooltip_debug_info";
-
-    // routes
-    public const string RoutesSection = "routes";
-    public const string RoutesServerKey = "enabled";
-    public const string RoutesHostKey = "host";
-    public const string RoutesPortKey = "port";
-    public const string LoadCoreAPIKey = "core_api";
-
-    // telemetry
-    public const string TelemetrySection = "telemetry";
-    public const string TelemetryUTCTimestampsKey = "utc_timestamps";
-    public const string TelemetryStatusKey = "active";
-    public const string TelemetryFileDirKey = "telemetry_file_dir";
-    public const string TelemetryServerUrlKey = "telemetry_server_url";
-    public const string TelemetryIncludeHooksKey = "include_hooks";
-    public const string AppTelemetryStatusKey = "active_app";
-    public const string AppTelemetryServerUrlKey = "apptelemetry_server_url";
-    public const string AppTelemetryEventFlagsKey = "apptelemetry_event_flags";
-
     // pyrevit.exe specific 
     public const string EnvSectionName = "environment";
     public const string EnvInstalledClonesKey = "clones";
--- dev/pyRevitLabs/pyRevitLabs.Configurations/CoreSection.cs
+++ dev/pyRevitLabs/pyRevitLabs.Configurations/CoreSection.cs
@@ -1,4 +1,6 @@
-﻿namespace pyRevitLabs.Configurations;
+﻿using pyRevitLabs.Configurations.Attributes;
+
+namespace pyRevitLabs.Configurations;
 
 public enum LogLevels
 {
@@ -7,30 +9,60 @@ public enum LogLevels
     Debug
 }
 
+[SectionName("core")]
 public sealed record CoreSection
 {
+    [KeyName("bincache")]
     public bool BinCache { get; set; }
 
+    [KeyName("loadbeta")]
     public bool LoadBeta { get; set; }
+    
+    [KeyName("autoupdate")]
     public bool AutoUpdate { get; set; }
+    
+    [KeyName("checkupdates")]
     public bool CheckUpdates { get; set; }
 
+    [KeyName("usercanupdate")]
     public bool UserCanUpdate { get; set; } = true;
+
+    [KeyName("usercanextend")]
     public bool UserCanExtend { get; set; } = true;
+    
+    [KeyName("usercanconfig")]
     public bool UserCanConfig { get; set; } = true;
 
+    [KeyName("rocketmode")]
     public bool RocketMode { get; set; } = true;
+    
+    [KeyName("user_locale")]
     public string UserLocale { get; set; } = "en_us";
 
+    [KeyName("log_level")]
     public LogLevels LogLevel { get; set; }
+    
+    [KeyName("filelogging")]
     public bool FileLogging { get; set; }
+    
+    [KeyName("startuplogtimeout")]
     public int StartupLogTimeout { get; set; } = 10;
 
+    [KeyName("cpyengine")]
     public int CpythonEngineVersion { get; set; }
+    
+    [KeyName("requiredhostbuild")]
     public bool RequiredHostBuild { get; set; }
+    
+    [KeyName("minhostdrivefreespace")]
     public long MinHostDriveFreeSpace { get; set; }
 
+    [KeyName("colorize_docs")]
     public bool ColorizeDocs { get; set; }
+    
+    [KeyName("tooltip_debug_info")]
     public bool TooltipDebugInfo { get; set; }
+    
+    [KeyName("outputstylesheet")]
     public string? OutputStylesheet { get; set; }
 }
