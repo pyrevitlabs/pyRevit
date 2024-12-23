da4575d3b40f2a41f35211524f2efbe9b170a344
dosymep <biseuv@yandex.ru>
2024-12-23 12:13:41 +0300

    add support requred and init features for net48

---
+++ dev/pyRevitLabs/pyRevitLabs.Configurations/System.Runtime.CompilerServices.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+#if !NET5_0_OR_GREATER
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+
+#endif // !NET5_0_OR_GREATER
+
+#if !NET7_0_OR_GREATER
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct
+                                           | AttributeTargets.Field | AttributeTargets.Property,
+        AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string featureName)
+        {
+            FeatureName = featureName;
+        }
+
+        public string FeatureName { get; }
+        public bool IsOptional { get; init; }
+
+        public const string RefStructs = nameof(RefStructs);
+        public const string RequiredMembers = nameof(RequiredMembers);
+    }
+
+#endif // !NET7_0_OR_GREATER
+}
