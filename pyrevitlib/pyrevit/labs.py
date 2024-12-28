c1fef094f5af96d4060c761cef1fa14c1a00bb65
dosymep <biseuv@yandex.ru>
2024-12-28 15:19:19 +0300

    add reference

--- pyrevitlib/pyrevit/labs.py
+++ pyrevitlib/pyrevit/labs.py
@@ -38,6 +38,10 @@ clr.AddReference('pyRevitLabs.DeffrelDB')
 clr.AddReference('pyRevitLabs.TargetApps.Revit')
 clr.AddReference('pyRevitLabs.PyRevit')
 clr.AddReference('PythonStubsBuilder')
+
+# configurations
+clr.AddReference('pyRevitLabs.Configurations')
+
 import Nett
 import MadMilkman.Ini
 import OpenMcdf
@@ -52,6 +56,7 @@ from pyRevitLabs import Language
 from pyRevitLabs import DeffrelDB
 from pyRevitLabs import TargetApps
 from pyRevitLabs import PyRevit
+from pyRevitLabs import Configurations
 from PythonStubs import PythonStubsBuilder
 
 from pyrevit import coreutils
