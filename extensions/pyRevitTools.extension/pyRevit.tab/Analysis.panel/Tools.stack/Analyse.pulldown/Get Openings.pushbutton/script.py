a2b4fa5618926a02a2abe95786ca90a397298a5d
EF <>
2025-02-11 21:21:20 +0100

    Corrected @devloai[bot] suggestions

--- extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack/Analyse.pulldown/Get Openings.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Analysis.panel/Tools.stack/Analyse.pulldown/Get Openings.pushbutton/script.py
@@ -13,30 +13,26 @@ update to select all openings with one click.
 Inspired by the initial work of Mohamed Bedair and Andreas Draxl.
 Special thanks to Erik Frits for his help.
 _____________________________________________________________________
-TO DO:
-- to do
-_____________________________________________________________________
 REQUIREMENTS:
 _____________________________________________________________________
 [24.03.2024] - Added selection Filter
 [23.03.2024] - 1.0 First Release"""
 
-# Imports
+# IMPORTS
 
+# .NET
+import clr
+clr.AddReference('System')
+from System.Collections.Generic import List
+import time
+
+# Revit
 from Autodesk.Revit.UI.Selection import *
-from Autodesk.Revit.DB import *
-import os, sys, math, datetime, time
 from Autodesk.Revit.DB import *
 
 # pyRevit
 from pyrevit import *
 from pyrevit import script
-
-# .NET Imports
-import clr
-clr.AddReference('System')
-from System.Collections.Generic import List
-import time
 
 doc         =__revit__.ActiveUIDocument.Document
 uidoc       =__revit__.ActiveUIDocument
@@ -46,17 +42,17 @@ timer_start = time.time()
 
 # GET ALL OPENINGS IN THE PROJECT
 
-# 1. List of categories
+# List of categories
 cats = [BuiltInCategory.OST_FloorOpening,
         BuiltInCategory.OST_SWallRectOpening,
         BuiltInCategory.OST_ShaftOpening,
         BuiltInCategory.OST_RoofOpening]
 list_cats = List[BuiltInCategory](cats)
 
-# 2. Create filter
+# Create filter
 multi_cat_filter = ElementMulticategoryFilter(list_cats)
 
-# 3. Apply filter to filteredElementCollector
+# Apply filter to filteredElementCollector
 all_elements = FilteredElementCollector(doc)\
                 .WherePasses(multi_cat_filter)\
                 .WhereElementIsNotElementType()\
@@ -66,10 +62,10 @@ all_elements = FilteredElementCollector(doc)\
 element_ids = FilteredElementCollector(doc).OfClass(Opening).ToElementIds()
 element_ids = List[ElementId](element_ids)
 
-# 4. Declaration of a list to contains list of wanted element properties
+# Declaration of a list to contains list of wanted element properties
 data = []
 
-# 5. Collect information about the object and put it into in the data list.
+# Collect information about the object and put it into in the data list.
 for e in all_elements:
     el = []
     el.append(e.Name)
@@ -79,47 +75,45 @@ for e in all_elements:
     el.append(e_link)
     data.append(el)
 
-# # Get names of current selection filters in doc an print them
-# namedFilters = FilteredElementCollector(doc).OfClass(FilterElement).ToElements()
-# for nF in namedFilters:
-#     print (nF.Name) # print names
-#     print (nF.Id) # print Id
-
 # Get All Selection Filters
 all_sel_filters  = FilteredElementCollector(doc).OfClass(SelectionFilterElement).ToElements()
 dict_sel_filters = {f.Name: f for f in all_sel_filters}
 
-t = Transaction(doc, 'Create Openings Filter')
-t.Start()
+try:
+    t = Transaction(doc, 'Create Openings Filter')
+    t.Start()
 
-# Selection Filter Name
-new_filter_name = '0_ShaftOpenings'
+    # Selection Filter Name
+    new_filter_name = '0_ShaftOpenings'
 
-# Create new if doesn't exist
-if new_filter_name not in dict_sel_filters:
-    new_fil = SelectionFilterElement.Create(doc, new_filter_name)
-    new_fil.AddSet(element_ids)
-    print ('Created a filter called : {}'.format(new_filter_name))
+    # Create new if doesn't exist
+    if new_filter_name not in dict_sel_filters:
+        new_fil = SelectionFilterElement.Create(doc, new_filter_name)
+        new_fil.AddSet(element_ids)
+        print ('Created a filter called : {}'.format(new_filter_name))
 
-# Update if already exists
-else:
-    existing_fil = dict_sel_filters[new_filter_name]
-    existing_fil.AddSet(element_ids)
-    print ('Updated a filter called : {}'.format(new_filter_name))
+    # Update if already exists
+    else:
+        existing_fil = dict_sel_filters[new_filter_name]
+        existing_fil.AddSet(element_ids)
+        print ('Updated a filter called : {}'.format(new_filter_name))
 
-t.Commit()
-
+    t.Commit()
+except Exception as ex:
+    if t.HasStarted():
+        t.RollBack()
+    script.exit(str(ex))
 # ╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ╠╦╝║╣ ╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╚═╚═╝╩  ╚═╝╩╚═ ╩ ╚═╝
 #==================================================
 
-output.print_md("#### There are {} shaftopenings in the project.".format(len(all_elements))) # TO DO Output link for all.
+output.print_md("#### There are {} openings (floor, wall, shaft, roof) in the project.".format(len(all_elements))) # TO DO Output link for all.
 if data:
     output.print_table(table_data=data, title="Shafts:", columns=["Family" ,"ElementId", "Select/Show Element"])
     #output.print_md("#####Total {} WDB/WA elements has been updated.".format(len(data)))
 else:
-    output.print_md("#####There are no shaft openings in the project")
+    output.print_md("#####There are no openings (floor, wall, shaft, roof) in the project")
 
 # End
 output.print_md('---')
