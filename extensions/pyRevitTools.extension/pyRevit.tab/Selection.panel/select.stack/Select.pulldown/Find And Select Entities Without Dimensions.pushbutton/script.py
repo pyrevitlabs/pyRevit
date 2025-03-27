44c7282d6870b30b7e9dd6a96795c5aa0966b34c
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2025-03-27 17:21:49 +0100

    fix #2604 - Select - Find and select entities without dimensions - Not working on Revit 2024
    fix #2604 - Select - Find and select entities without dimensions - Not working on Revit 2024

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Find And Select Entities Without Dimensions.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/select.stack/Select.pulldown/Find And Select Entities Without Dimensions.pushbutton/script.py
@@ -2,7 +2,6 @@
 #pylint: disable=import-error,invalid-name
 from pyrevit import revit, DB, HOST_APP
 from pyrevit import forms
-from pyrevit.compat import get_elementid_value_func
 
 categories = {
     'Rooms': DB.BuiltInCategory.OST_Rooms,
@@ -18,8 +17,6 @@ categories = {
     'Ceilings': DB.BuiltInCategory.OST_Ceilings,
     'Columns': DB.BuiltInCategory.OST_StructuralColumns,
     }
-
-get_elementid_value = get_elementid_value_func()
 
 # make sure active view is not a sheet
 if isinstance(revit.active_view, DB.ViewSheet):
@@ -37,7 +34,7 @@ if selected_switch:
     all_elements = DB.FilteredElementCollector(revit.doc, revit.active_view.Id)\
                      .OfCategory(target)\
                      .WhereElementIsNotElementType()
-    all_ids = set(get_elementid_value(x.Id) for x in all_elements)
+    all_ids = set(x.Id.IntegerValue for x in all_elements)
 
     all_dims = \
         DB.FilteredElementCollector(revit.doc, revit.active_view.Id)\
@@ -50,7 +47,7 @@ if selected_switch:
             if HOST_APP.is_newer_than(2023):
                 dimmed_ids.add(ref.ElementId.Value)
             else:
-                dimmed_ids.add(get_elementid_value(ref.ElementId))
+                dimmed_ids.add(ref.ElementId.IntegerValue)
 
     # find non dimmed
     not_dimmed_ids = all_ids.difference(dimmed_ids)
