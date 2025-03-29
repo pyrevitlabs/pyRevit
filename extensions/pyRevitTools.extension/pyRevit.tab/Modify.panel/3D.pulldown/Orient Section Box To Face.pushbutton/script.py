3fabfa874626bae08105e06858b9964d949cd2ac
Wurschdhaud <paul.ru@hotmail.de>
2025-03-29 21:14:51 +0100

    fix #2606, tested in Revit 2024, added context in bundle to open only in 3D views

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient Section Box To Face.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient Section Box To Face.pushbutton/script.py
@@ -4,25 +4,29 @@ from pyrevit.framework import Math
 from pyrevit import revit, DB, UI
 from pyrevit import forms
 
+# import Autodesk.Revit
+
+import Autodesk.Revit.UI.Selection.ObjectType
 
 curview = revit.active_view
 
 
 def orientsectionbox(view):
         try:
-            # Pick face to align to
-            with forms.WarningBar(title='Pick a face to align to:'):
-                face = revit.pick_face()
+            # Pick face to align to using uidoc.Selection instead of revit.pick_face to get the reference instead of the face
+            face = revit.uidoc.Selection.PickObject(UI.Selection.ObjectType.Face, 'Pick a face to align to:')
+            
+            revit.pick_face
 
             # Get the section box
             box = view.GetSectionBox()
 
             # Get the geometry object of the reference
-            geometry_object = revit.doc.GetElement(face.ElementId).GetGeometryObjectFromReference(face)
+            element = revit.doc.GetElement(face)
+            geometry_object = element.GetGeometryObjectFromReference(face)
             
-            # Check if the object might have a Transformation (by checking if it's Non-Instance and not a System Family)
-            element = revit.doc.GetElement(face.ElementId)
-            if isinstance(element, DB.FamilyInstance) and element.Symbol.Family.FamilyType != DB.FamilyType.System:
+            # Check if the object might have a Transformation (by checking if it's Non-Instance)
+            if isinstance(element, DB.FamilyInstance):
                 # Get the transform of the family instance (converts local to world coordinates)
                 transform = element.GetTransform()
                 # Get the face normal in local coordinates
@@ -31,7 +35,7 @@ def orientsectionbox(view):
                 world_normal = transform.OfVector(local_normal).Normalize()
                 norm = world_normal
             else:
-                norm = face.ComputeNormal(DB.UV(0, 0)).Normalize()
+                norm = geometry_object.ComputeNormal(DB.UV(0, 0)).Normalize()
             
             # Orient the box
             boxNormal = box.Transform.Basis[0].Normalize()
@@ -48,11 +52,11 @@ def orientsectionbox(view):
                                                             angle,
                                                             origin)
             box.Transform = box.Transform.Multiply(rotate)
-            with revit.Transaction('Orient Section Box to Face'):
+            with revit.Transaction("Orient Section Box to Face"):
                 view.SetSectionBox(box)
                 revit.uidoc.RefreshActiveView()
-        except Exception:
-            passforms.alert('Error: {0}'.format(str(ex)))
+        except Exception as ex:
+            forms.alert('Error: {0}'.format(str(ex)))
 
 
 if isinstance(curview, DB.View3D) and curview.IsSectionBoxActive:
@@ -60,4 +64,4 @@ if isinstance(curview, DB.View3D) and curview.IsSectionBoxActive:
 elif isinstance(curview, DB.View3D) and not curview.IsSectionBoxActive:
     forms.alert("The section box for View3D isn't active.")
 else:
-    forms.alert('You must be on a 3D view for this tool to work.')
+    forms.alert("You must be on a 3D view for this tool to work.")
