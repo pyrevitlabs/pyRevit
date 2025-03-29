9f37bb612c6a633e9c1d154296192379e7000757
Wurschdhaud <paul.ru@hotmail.de>
2025-03-29 21:28:27 +0100

    ran psf/black formatter

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient Section Box To Face.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/3D.pulldown/Orient Section Box To Face.pushbutton/script.py
@@ -4,59 +4,60 @@ from pyrevit.framework import Math
 from pyrevit import revit, DB, UI
 from pyrevit import forms
 
-# import Autodesk.Revit
-
 import Autodesk.Revit.UI.Selection.ObjectType
 
 curview = revit.active_view
 
 
 def orientsectionbox(view):
-        try:
-            # Pick face to align to using uidoc.Selection instead of revit.pick_face to get the reference instead of the face
-            face = revit.uidoc.Selection.PickObject(UI.Selection.ObjectType.Face, 'Pick a face to align to:')
-            
-            revit.pick_face
+    try:
+        # Pick face to align to using uidoc.Selection instead of revit.pick_face to get the reference instead of the face
+        face = revit.uidoc.Selection.PickObject(
+            UI.Selection.ObjectType.Face, "Pick a face to align to:"
+        )
 
-            # Get the section box
-            box = view.GetSectionBox()
+        revit.pick_face
 
-            # Get the geometry object of the reference
-            element = revit.doc.GetElement(face)
-            geometry_object = element.GetGeometryObjectFromReference(face)
-            
-            # Check if the object might have a Transformation (by checking if it's Non-Instance)
-            if isinstance(element, DB.FamilyInstance):
-                # Get the transform of the family instance (converts local to world coordinates)
-                transform = element.GetTransform()
-                # Get the face normal in local coordinates
-                local_normal = geometry_object.ComputeNormal(DB.UV(0, 0)).Normalize()
-                # Apply the transform to convert normal to world coordinates
-                world_normal = transform.OfVector(local_normal).Normalize()
-                norm = world_normal
-            else:
-                norm = geometry_object.ComputeNormal(DB.UV(0, 0)).Normalize()
-            
-            # Orient the box
-            boxNormal = box.Transform.Basis[0].Normalize()
-            angle = norm.AngleTo(boxNormal)
-            axis = DB.XYZ(0, 0, 1.0)
-            origin = DB.XYZ(box.Min.X + (box.Max.X - box.Min.X) / 2,
-                            box.Min.Y + (box.Max.Y - box.Min.Y) / 2, 0.0)
-            if norm.Y * boxNormal.X < 0:
-                rotate = \
-                    DB.Transform.CreateRotationAtPoint(axis, Math.PI / 2 - angle,
-                                                       origin)
-            else:
-                rotate = DB.Transform.CreateRotationAtPoint(axis,
-                                                            angle,
-                                                            origin)
-            box.Transform = box.Transform.Multiply(rotate)
-            with revit.Transaction("Orient Section Box to Face"):
-                view.SetSectionBox(box)
-                revit.uidoc.RefreshActiveView()
-        except Exception as ex:
-            forms.alert('Error: {0}'.format(str(ex)))
+        # Get the section box
+        box = view.GetSectionBox()
+
+        # Get the geometry object of the reference
+        element = revit.doc.GetElement(face)
+        geometry_object = element.GetGeometryObjectFromReference(face)
+
+        # Check if the object might have a Transformation (by checking if it's Non-Instance)
+        if isinstance(element, DB.FamilyInstance):
+            # Get the transform of the family instance (converts local to world coordinates)
+            transform = element.GetTransform()
+            # Get the face normal in local coordinates
+            local_normal = geometry_object.ComputeNormal(DB.UV(0, 0)).Normalize()
+            # Apply the transform to convert normal to world coordinates
+            world_normal = transform.OfVector(local_normal).Normalize()
+            norm = world_normal
+        else:
+            norm = geometry_object.ComputeNormal(DB.UV(0, 0)).Normalize()
+
+        # Orient the box
+        boxNormal = box.Transform.Basis[0].Normalize()
+        angle = norm.AngleTo(boxNormal)
+        axis = DB.XYZ(0, 0, 1.0)
+        origin = DB.XYZ(
+            box.Min.X + (box.Max.X - box.Min.X) / 2,
+            box.Min.Y + (box.Max.Y - box.Min.Y) / 2,
+            0.0,
+        )
+        if norm.Y * boxNormal.X < 0:
+            rotate = DB.Transform.CreateRotationAtPoint(
+                axis, Math.PI / 2 - angle, origin
+            )
+        else:
+            rotate = DB.Transform.CreateRotationAtPoint(axis, angle, origin)
+        box.Transform = box.Transform.Multiply(rotate)
+        with revit.Transaction("Orient Section Box to Face"):
+            view.SetSectionBox(box)
+            revit.uidoc.RefreshActiveView()
+    except Exception as ex:
+        forms.alert("Error: {0}".format(str(ex)))
 
 
 if isinstance(curview, DB.View3D) and curview.IsSectionBoxActive:
