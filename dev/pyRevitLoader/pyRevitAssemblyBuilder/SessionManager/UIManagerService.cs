847c24672f5ef14d2c9c455b3fd3c119b76f865c
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-04-14 23:24:39 +0200

    fix: UIManagerServite to handle stacked pulldowns correctly

--- dev/pyRevitLoader/pyRevitAssemblyBuilder/SessionManager/UIManagerService.cs
+++ dev/pyRevitLoader/pyRevitAssemblyBuilder/SessionManager/UIManagerService.cs
@@ -22,9 +22,7 @@ namespace pyRevitAssemblyBuilder.SessionManager
                 return;
 
             foreach (var obj in extension.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
-            {
                 RecursivelyBuildUI(obj, null, null, extension.Name, assemblyInfo);
-            }
         }
 
         private void RecursivelyBuildUI(object obj, object parentComponent, RibbonPanel parentPanel, string tabName, ExtensionAssemblyInfo assemblyInfo)
@@ -39,84 +37,133 @@ namespace pyRevitAssemblyBuilder.SessionManager
             {
                 case CommandComponentType.Tab:
                     try { _uiApp.CreateRibbonTab(component.Name); } catch { }
-                    foreach (var child in component.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
+                    foreach (var child in component.Children ?? Enumerable.Empty<object>())
                         RecursivelyBuildUI(child, component, null, component.Name, assemblyInfo);
                     break;
 
                 case CommandComponentType.Panel:
                     var panel = _uiApp.GetRibbonPanels(tabName).FirstOrDefault(p => p.Name == component.Name)
                              ?? _uiApp.CreateRibbonPanel(tabName, component.Name);
-                    foreach (var child in component.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
+                    foreach (var child in component.Children ?? Enumerable.Empty<object>())
                         RecursivelyBuildUI(child, component, panel, tabName, assemblyInfo);
                     break;
 
                 case CommandComponentType.Stack:
-                    var buttonDatas = new List<RibbonItemData>();
+                    var itemDataList = new List<RibbonItemData>();
+                    var originalItems = new List<ICommandComponent>();
 
                     foreach (var child in component.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
                     {
                         var subCmd = child as ICommandComponent;
-                        if (subCmd == null) continue;
+                        if (subCmd == null)
+                            continue;
 
                         var subType = CommandComponentTypeExtensions.FromExtension(subCmd.Type);
-                        switch (subType)
+
+                        if (subType == CommandComponentType.PushButton)
                         {
-                            case CommandComponentType.PushButton:
-                                buttonDatas.Add(new PushButtonData(subCmd.Name, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId));
-                                break;
-                            case CommandComponentType.PullDown:
-                                buttonDatas.Add(new PulldownButtonData(subCmd.Name, subCmd.Name));
-                                break;
+                            itemDataList.Add(new PushButtonData(subCmd.UniqueId, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId));
+                            originalItems.Add(subCmd);
+                        }
+                        else if (subType == CommandComponentType.PullDown)
+                        {
+                            var pdData = new PulldownButtonData(subCmd.UniqueId, subCmd.Name);
+                            itemDataList.Add(pdData);
+                            originalItems.Add(subCmd); // to match later
                         }
                     }
 
-                    if (buttonDatas.Count == 2)
-                        parentPanel?.AddStackedItems(buttonDatas[0], buttonDatas[1]);
-                    else if (buttonDatas.Count >= 3)
-                        parentPanel?.AddStackedItems(buttonDatas[0], buttonDatas[1], buttonDatas[2]);
-                    break;
+                    if (itemDataList.Count >= 2)
+                    {
+                        IList<RibbonItem> stackedItems = null;
+                        if (itemDataList.Count == 2)
+                            stackedItems = parentPanel?.AddStackedItems(itemDataList[0], itemDataList[1]);
+                        else if (itemDataList.Count >= 3)
+                            stackedItems = parentPanel?.AddStackedItems(itemDataList[0], itemDataList[1], itemDataList[2]);
 
+                        // Now post-process pulldowns to add nested pushbuttons
+                        if (stackedItems != null)
+                        {
+                            for (int i = 0; i < stackedItems.Count; i++)
+                            {
+                                var ribbonItem = stackedItems[i];
+                                var origComponent = originalItems[i];
+
+                                if (ribbonItem is PulldownButton pdBtn)
+                                {
+                                    foreach (var child in origComponent.Children ?? Enumerable.Empty<object>())
+                                    {
+                                        if (child is ICommandComponent subCmd &&
+                                            CommandComponentTypeExtensions.FromExtension(subCmd.Type) == CommandComponentType.PushButton)
+                                        {
+                                            var subData = new PushButtonData(subCmd.UniqueId, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId);
+                                            pdBtn.AddPushButton(subData);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
                 case CommandComponentType.PushButton:
                 case CommandComponentType.SmartButton:
-                    var pbData = new PushButtonData(component.Name, component.Name, assemblyInfo.Location, component.UniqueId);
+                    var pbData = new PushButtonData(component.UniqueId, component.Name, assemblyInfo.Location, component.UniqueId);
                     var btn = parentPanel?.AddItem(pbData) as PushButton;
                     if (!string.IsNullOrEmpty(component.Tooltip))
                         btn.ToolTip = component.Tooltip;
                     break;
 
                 case CommandComponentType.PullDown:
-                    var pdBtnData = new PulldownButtonData(component.Name, component.Name);
-                    var pdBtn = parentPanel?.AddItem(pdBtnData) as PulldownButton;
-                    if (pdBtn == null) return;
-
-                    foreach (var child in component.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
-                    {
-                        if (child is ICommandComponent subCmd &&
-                            CommandComponentTypeExtensions.FromExtension(subCmd.Type) == CommandComponentType.PushButton)
-                        {
-                            var subData = new PushButtonData(subCmd.Name, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId);
-                            pdBtn.AddPushButton(subData);
-                        }
-                    }
+                    CreatePulldown(component, parentPanel, tabName, assemblyInfo, true);
                     break;
 
                 case CommandComponentType.SplitButton:
                 case CommandComponentType.SplitPushButton:
-                    var splitData = new SplitButtonData(component.Name, component.Name);
+                    var splitData = new SplitButtonData(component.UniqueId, component.Name);
                     var splitBtn = parentPanel?.AddItem(splitData) as SplitButton;
                     if (splitBtn == null) return;
 
-                    foreach (var child in component.Children as IEnumerable<object> ?? Enumerable.Empty<object>())
+                    foreach (var child in component.Children ?? Enumerable.Empty<object>())
                     {
                         if (child is ICommandComponent subCmd &&
                             CommandComponentTypeExtensions.FromExtension(subCmd.Type) == CommandComponentType.PushButton)
                         {
-                            var subData = new PushButtonData(subCmd.Name, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId);
+                            var subData = new PushButtonData(subCmd.UniqueId, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId);
                             splitBtn.AddPushButton(subData);
                         }
                     }
                     break;
             }
+        }
+
+        private PulldownButtonData CreatePulldown(
+            ICommandComponent component,
+            RibbonPanel parentPanel,
+            string tabName,
+            ExtensionAssemblyInfo assemblyInfo,
+            bool addToPanel)
+        {
+            var pdData = new PulldownButtonData(component.UniqueId, component.Name);
+
+            if (!addToPanel)
+                return pdData;
+
+            PulldownButton pdBtn = parentPanel.AddItem(pdData) as PulldownButton;
+            if (pdBtn == null)
+                return null;
+
+
+            foreach (var child in component.Children ?? Enumerable.Empty<object>())
+            {
+                if (child is ICommandComponent subCmd &&
+                    CommandComponentTypeExtensions.FromExtension(subCmd.Type) == CommandComponentType.PushButton)
+                {
+                    var subData = new PushButtonData(subCmd.UniqueId, subCmd.Name, assemblyInfo.Location, subCmd.UniqueId);
+                    pdBtn.AddPushButton(subData);
+                }
+            }
+
+            return pdData;
         }
     }
 }
