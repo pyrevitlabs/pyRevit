4411154869941a27c41d40bfeadcc01f5cebff5b
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-04-16 22:47:41 +0200

    feat: replace DI architecture with plain intantiating of services. remove dependencies

--- dev/pyRevitLoader/pyRevitAssemblyBuilder/SessionManager/ExtensionManagerService.cs
+++ dev/pyRevitLoader/pyRevitAssemblyBuilder/SessionManager/ExtensionManagerService.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using pyRevitExtensionParser;
 
-namespace pyRevitAssemblyBuilder.SessionManager
+namespace pyRevitAssemblyBuilder.FolderParser
 {
     public class ExtensionManagerService
     {
--- dev/pyRevitLoader/pyRevitAssemblyBuilder/SessionManager/ISessionManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace pyRevitAssemblyBuilder.SessionManager
-{
-    public interface ISessionManager
-    {
-        Task<string> LoadSessionAsync();
-    }
-}
