832cb8a161224543ceed7aad7d409983ec1c137f
Jared Holloway <41764450+jaredholloway94@users.noreply.github.com>
2025-06-09 13:35:12 -0500

    fix bitmap_from_file method in utils.py
    See issue #2699

--- pyrevitlib/pyrevit/forms/utils.py
+++ pyrevitlib/pyrevit/forms/utils.py
@@ -1,21 +1,21 @@
 """Utility functions to support forms module."""
 
 from pyrevit import framework
-from pyrevit.framework import wpf, Controls, Imaging
+from pyrevit.framework import wpf, Controls, Imaging, Uri, UriKind
 
 
 def bitmap_from_file(bitmap_file):
     """Create BitmapImage from a bitmap file.
 
     Args:
-        bitmap_file (str): path to bitmap file
+        bitmap_file (str): absolute path to bitmap file
 
     Returns:
         (BitmapImage): bitmap image object
     """
     bitmap = Imaging.BitmapImage()
     bitmap.BeginInit()
-    bitmap.UriSource = framework.Uri(bitmap_file)
+    _, bitmap.UriSource = Uri.TryCreate(bitmap_file, UriKind.RelativeOrAbsolute)
     bitmap.CacheOption = Imaging.BitmapCacheOption.OnLoad
     bitmap.CreateOptions = Imaging.BitmapCreateOptions.IgnoreImageCache
     bitmap.EndInit()
