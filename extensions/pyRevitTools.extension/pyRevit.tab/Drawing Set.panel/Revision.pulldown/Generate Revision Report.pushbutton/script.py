23344345b6c9a29c4ca36036695adb23c0c1aa5e
MohamedASLI <aslimhmd@gmail.com>
2025-08-22 19:35:41 +0200

    fix #2004 bug

--- extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Revision.pulldown/Generate Revision Report.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Drawing Set.panel/Revision.pulldown/Generate Revision Report.pushbutton/script.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Generates a report from all revisions in current project."""
-"""tooltip."""
 #pylint: disable=import-error,invalid-name,broad-except,superfluous-parens
 from pyrevit import coreutils
 from pyrevit import revit, DB
@@ -19,7 +17,8 @@ all_sheets = sorted(sheetsnotsorted, key=lambda x: x.SheetNumber)
 # collect all clouds
 all_clouds = DB.FilteredElementCollector(revit.doc)\
                .OfCategory(DB.BuiltInCategory.OST_RevisionClouds)\
-               .WhereElementIsNotElementType()
+               .WhereElementIsNotElementType()\
+               .ToElements()
 
 # collect all revisions
 all_revisions = forms.select_revisions(
@@ -82,11 +81,12 @@ class RevisedSheet:
     def __init__(self, rvt_sheet):
         self._rvt_sheet = rvt_sheet
 
-        self._clouds = []
-        self._find_all_clouds()
-        self._find_all_revisions()
+        self._sheet_revisions = self._find_all_revisions_in_sheet()
+        self._sheet_clouds = self._find_all_clouds_in_sheet()
+        self._rev_numbers = self._find_revision_numbers()
 
-    def _find_all_clouds(self):
+    def _find_all_clouds_in_sheet(self):
+        sheet_clouds = []
         ownerview_ids = [self._rvt_sheet.Id]
         # add all the sheeted views to list
         ownerview_ids.extend(
@@ -94,20 +94,48 @@ class RevisedSheet:
                 revit.doc.GetElement(x).ViewId
                 for x in self._rvt_sheet.GetAllViewports()
             ])
-        for rev_cloud in all_clouds:
-            if rev_cloud.OwnerViewId in ownerview_ids:
-                self._clouds.append(rev_cloud)
 
-    def _find_all_revisions(self):
-        self._revisions = set([cloud.RevisionId for cloud in self._clouds])
-        self._revisions.update(
+        primary_owenerview_ids = [
+            revit.doc.GetElement(view_id).GetPrimaryViewId()
+            for view_id in ownerview_ids
+            if revit.doc.GetElement(view_id).GetPrimaryViewId() != DB.ElementId.InvalidElementId
+        ]
+
+        for rev_cloud in all_clouds:
+            is_rev_cloud_visible_in_sheet = self._is_revision_cloud_visible_in_sheet(rev_cloud)
+            is_rev_cloud_in_sheet_revisions = rev_cloud.RevisionId in self._sheet_revisions
+
+            if (is_rev_cloud_in_sheet_revisions and is_rev_cloud_visible_in_sheet and (
+                rev_cloud.OwnerViewId in ownerview_ids or rev_cloud.OwnerViewId in primary_owenerview_ids
+            )):
+                    sheet_clouds.append(rev_cloud)
+        return sheet_clouds
+
+    def _is_revision_cloud_visible_in_sheet(self, rev_cloud):
+        """Check if a revision cloud is visible in the sheet."""
+        is_visible = False
+        for view_id in self._rvt_sheet.GetAllViewports():
+            viewport = revit.doc.GetElement(view_id)
+            view = revit.doc.GetElement(viewport.ViewId)
+            if not rev_cloud.IsHidden(view):
+                is_visible = True
+                break
+        return is_visible
+
+    def _find_all_revisions_in_sheet(self):
+        revisions = set(self._rvt_sheet.GetAllRevisionIds())
+        revisions.update(
             set(self._rvt_sheet.GetAdditionalRevisionIds())
         )
-        self._rev_numbers = set(
+        return revisions
+
+    def _find_revision_numbers(self):
+        rev_numbers = set(
             [
                 revit.query.get_rev_number(revit.doc.GetElement(rev_id))
-                for rev_id in self._revisions
+                for rev_id in self._sheet_revisions
             ])
+        return rev_numbers
 
     @property
     def sheet_number(self):
@@ -119,28 +147,32 @@ class RevisedSheet:
 
     @property
     def cloud_count(self):
-        return len(self._clouds)
+        return len(self._sheet_clouds)
 
     @property
     def rev_count(self):
-        return len(self._revisions)
+        return len(self._sheet_revisions)
+
+    @property
+    def additional_revisions_count(self):
+        return len(set(self._rvt_sheet.GetAdditionalRevisionIds()))
 
     def get_clouds(self):
-        return self._clouds
+        return self._sheet_clouds
 
     def get_comments(self):
         all_comments = set()
-        for cloud in self._clouds:
+        for cloud in self._sheet_clouds:
             cparam = \
                 cloud.Parameter[DB.BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS]
             comment = cparam.AsString()
             if not coreutils.is_blank(comment):
                 all_comments.add(comment)
         return all_comments
-    
+
     def get_all_revision_marks(self):
         all_marks = set()
-        for cloud in self._clouds:
+        for cloud in self._sheet_clouds:
             cparam = \
                 cloud.Parameter[DB.BuiltInParameter.ALL_MODEL_MARK]
             mark = cparam.AsString()
@@ -206,6 +238,13 @@ for rev_sheet in revised_sheets:
                     rev_sheet.cloud_count,
                     coreutils.join_strings(rev_sheet.get_revision_numbers(),
                                            separator=',')))
+
+        if rev_sheet.additional_revisions_count > 0:
+            console.print_md(
+                'Additional Revisions Count: {} (revisions not associated with a cloud on this sheet)'
+                .format(rev_sheet.additional_revisions_count)
+            )
+
         comments = rev_sheet.get_comments()
         if comments:
             print('\t\tRevision comments:\n{}'
