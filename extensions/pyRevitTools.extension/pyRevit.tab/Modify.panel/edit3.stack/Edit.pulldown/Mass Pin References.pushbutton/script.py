719a3c2e15a86d87ab3b5d92261a3ba30dc5ccb6
Wurschdhaud <paul.ru@hotmail.de>
2025-08-23 18:49:33 +0200

    add project base and survey, make bundle more readable

--- extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit3.stack/Edit.pulldown/Mass Pin References.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Modify.panel/edit3.stack/Edit.pulldown/Mass Pin References.pushbutton/script.py
@@ -1,28 +1,26 @@
 from pyrevit import revit, forms, DB, EXEC_PARAMS
 
+category_map = {}
+
+category_map["Imported DWG Files"] = "DWG_IMPORT"
+category_map["Project Base Point"] = "PROJECT_BASE_POINT"
+category_map["Survey Point"] = "SURVEY_POINT"
 options = [
     DB.BuiltInCategory.OST_Grids,
     DB.BuiltInCategory.OST_Levels,
     DB.BuiltInCategory.OST_RvtLinks,
     DB.BuiltInCategory.OST_SectionBox,
 ]
-
-category_map = {}
 for cat in options:
     name = DB.LabelUtils.GetLabelFor(cat)
     category_map[name] = cat
 
-# Add pseudo-category for DWG Imports
-dwg_label = "Imported DWG Files"
-category_map[dwg_label] = "DWG_IMPORT"
-
 selection = forms.SelectFromList.show(
     sorted(category_map.keys()),
     multiselect=True,
-    title="Select categories to %s"
-    % ("UNPIN" if EXEC_PARAMS.config_mode else "PIN"),
+    title="Select categories to %s" % ("UNPIN" if EXEC_PARAMS.config_mode else "PIN"),
     width=320,
-    height=300,
+    height=340,
 )
 
 if selection:
@@ -37,6 +35,19 @@ if selection:
                 .WhereElementIsNotElementType()
                 .ToElements()
             )
+        elif cat in ["PROJECT_BASE_POINT", "SURVEY_POINT"]:
+            elements = []
+            basepoints = (
+                DB.FilteredElementCollector(revit.doc)
+                .OfClass(DB.BasePoint)
+                .WhereElementIsNotElementType()
+                .ToElements()
+            )
+            for bp in basepoints:
+                if (cat == "PROJECT_BASE_POINT" and not bp.IsShared) or (
+                    cat == "SURVEY_POINT" and bp.IsShared
+                ):
+                    elements.append(bp)
         else:
             elements = (
                 DB.FilteredElementCollector(revit.doc)
