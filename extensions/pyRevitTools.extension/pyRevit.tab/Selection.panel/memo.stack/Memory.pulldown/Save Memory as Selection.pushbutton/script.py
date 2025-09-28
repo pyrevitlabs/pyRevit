74e1138786aaa0b2b2f48ca2531823713b109219
Wurschdhaud <paul.ru@hotmail.de>
2025-09-28 19:27:14 +0200

    cleanup code by using pyrevit.compat module for ElementId creation and value extraction

--- extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/Memory.pulldown/Save Memory as Selection.pushbutton/script.py
+++ extensions/pyRevitTools.extension/pyRevit.tab/Selection.panel/memo.stack/Memory.pulldown/Save Memory as Selection.pushbutton/script.py
@@ -3,13 +3,15 @@
 # pylint: disable=import-error,invalid-name,broad-except
 import os.path as op
 import pickle as pl
-from System import Int64
 
 from pyrevit import coreutils
-from pyrevit import revit, DB, HOST_APP
+from pyrevit import revit, DB
 from pyrevit import script
+from pyrevit.compat import get_elementid_from_value_func
 
 logger = script.get_logger()
+
+get_elementid_from_value = get_elementid_from_value_func()
 
 proj_info = revit.query.get_project_info()
 data_file = script.get_document_data_file("SelList", "pym")
@@ -28,9 +30,5 @@ if op.exists(data_file):
         selection_filter = DB.SelectionFilterElement.Create(
             revit.doc, coreutils.cleanup_filename(filter_name)
         )
-        is_version_newer_than2025 = HOST_APP.is_newer_than(2025)
         for elid in cursel:
-            if is_version_newer_than2025:
-                selection_filter.AddSingle(DB.ElementId(Int64(elid)))
-            else:
-                selection_filter.AddSingle(DB.ElementId(int(elid)))
+            selection_filter.AddSingle(get_elementid_from_value(elid))
