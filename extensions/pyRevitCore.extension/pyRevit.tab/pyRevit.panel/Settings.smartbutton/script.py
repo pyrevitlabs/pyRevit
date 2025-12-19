d4a7ce806a1db1928818790b53c73da8b97bc545
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-12-19 01:36:55 +0100

    feat: make a settings options more explicit and intuitive

--- extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/Settings.smartbutton/script.py
+++ extensions/pyRevitCore.extension/pyRevit.tab/pyRevit.panel/Settings.smartbutton/script.py
@@ -159,6 +159,7 @@ class SettingsWindow(forms.WPFWindow):
         
         self.new_loader.IsChecked = user_config.new_loader
         self.use_roslyn_loader.IsChecked = user_config.use_roslyn_loader
+        self._update_roslyn_loader_state()
 
         self.minimize_consoles_cb.IsChecked = user_config.output_close_others
 
@@ -518,6 +519,20 @@ class SettingsWindow(forms.WPFWindow):
     def resetcache(self, sender, args):
         """Callback method for resetting cache config to defaults"""
         self.bincache_rb.IsChecked = True
+
+    def new_loader_changed(self, sender, args):
+        """Callback method for when new_loader toggle changes"""
+        self._update_roslyn_loader_state()
+
+    def _update_roslyn_loader_state(self):
+        """Enable/disable roslyn loader option based on new_loader state.
+        
+        Roslyn loader option only applies when the new C# loader is enabled,
+        so it should be greyed out when new_loader is disabled.
+        """
+        is_new_loader_enabled = self.new_loader.IsChecked
+        self.use_roslyn_loader.IsEnabled = is_new_loader_enabled
+        self.roslyn_loader_text.IsEnabled = is_new_loader_enabled
 
     def copy_envvar_value(self, sender, args):
         """Callback method for copying selected env var value to clipboard"""
