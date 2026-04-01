a6983f3a3fad1bf228fbb8145a97493d17af340e
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2026-04-01 21:38:43 +0200

    Refactor storage file handling in CommonUtils.cs
    Updated the method for reading structured storage files to use the RootStorage class and improved memory management with using statements. This change enhances the reliability and performance of stream data retrieval.

--- dev/pyRevitLabs/pyRevitLabs.Common/CommonUtils.cs
+++ dev/pyRevitLabs/pyRevitLabs.Common/CommonUtils.cs
@@ -1,4 +1,4 @@
-﻿using OpenMcdf;
+using OpenMcdf;
 using System;
 using System.Text;
 using System.Diagnostics;
@@ -216,14 +216,18 @@ namespace pyRevitLabs.Common {
             int res = StgIsStorageFile(filePath);
 
             if (res == 0) {
-                CompoundFile cf = new CompoundFile(filePath);
-                logger.Debug($"Found CF Root: {cf.RootStorage}");
-                if (cf.RootStorage.TryGetStream(streamName, out var foundStream)) {
-                    byte[] streamData = foundStream.GetData();
-                    cf.Close();
-                    return streamData;
+                using (var root = RootStorage.OpenRead(filePath)) {
+                    logger.Debug(string.Format("Opened structured storage root at \"{0}\"", filePath));
+                    if (root.TryOpenStream(streamName, out CfbStream foundStream)) {
+                        using (foundStream) {
+                            using (var ms = new MemoryStream()) {
+                                foundStream.CopyTo(ms);
+                                return ms.ToArray();
+                            }
+                        }
+                    }
+                    return null;
                 }
-                return null;
             }
             else {
                 throw new NotSupportedException("File is not a structured storage file");
