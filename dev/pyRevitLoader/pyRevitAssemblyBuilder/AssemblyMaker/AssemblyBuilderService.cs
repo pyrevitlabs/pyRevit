2024133fc55c196b7ee59e3d28baad4b228ab8cc
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2026-04-02 23:20:13 +0200

    Update Microsoft.CodeAnalysis packages to version 4.11.0 and add deployment targets for Roslyn satellites in .NET Core.

--- dev/pyRevitLoader/pyRevitAssemblyBuilder/AssemblyMaker/AssemblyBuilderService.cs
+++ dev/pyRevitLoader/pyRevitAssemblyBuilder/AssemblyMaker/AssemblyBuilderService.cs
@@ -3,9 +3,9 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using System.Text;
 using pyRevitExtensionParser;
 using pyRevitAssemblyBuilder.SessionManager;
 
