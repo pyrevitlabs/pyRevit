866d2c3e30b7c475567e354628c89793b94ca30a
Roman Golev <67314258+romangolev@users.noreply.github.com>
2026-09-09 23:42:11 +0200

    build: run configuration tests in pipeline

--- build/Program.cs
+++ build/Program.cs
@@ -53,6 +53,7 @@ if (runCi)
     builder.Services.AddModule<BuildLoadersModule>();
     builder.Services.AddModule<BuildRuntimeModule>();
     builder.Services.AddModule<BuildRunnersModule>();
+    builder.Services.AddModule<TestConfigurationsModule>();
     builder.Services.AddModule<StageBinAssetsModule>();
     builder.Services.AddModule<BuildAutocompModule>();
     builder.Services.AddModule<VerifyLibGit2Module>();
